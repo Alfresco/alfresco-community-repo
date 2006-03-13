5659614fffc59a1b7a8fb71b980389865e44f2ec
Kevin Roast <kevin.roast@alfresco.com>
2006-03-13 18:06:58 +0000

    Checkpoint of code/config for the action config definition and externalisation task
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@2543 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

---
+++ source/java/org/alfresco/web/action/ActionEvaluator.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2005 Alfresco, Inc.
+ *
+ * Licensed under the Mozilla Public License version 1.1 
+ * with a permitted attribution clause. You may obtain a
+ * copy of the License at
+ *
+ *   http://www.alfresco.org/legal/license.txt
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the
+ * License.
+ */
+package org.alfresco.web.action;
+
+import org.alfresco.web.bean.repository.Node;
+
+/**
+ * Contract supported by all classes that provide dynamic evaluation for a UI action.
+ * <p>
+ * Evaluators are supplied with a Node instance context object.
+ * <p>
+ * The evaluator should decide if the action precondition is valid based on the appropriate
+ * logic and the properties etc. of the Node context and return the result.
+ * 
+ * @author Kevin Roast
+ */
+public interface ActionEvaluator
+{
+   public boolean evaluate(Node node);
+}
---
+++ source/java/org/alfresco/web/action/evaluator/EditDocHttpEvaluator.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2005 Alfresco, Inc.
+ *
+ * Licensed under the Mozilla Public License version 1.1 
+ * with a permitted attribution clause. You may obtain a
+ * copy of the License at
+ *
+ *   http://www.alfresco.org/legal/license.txt
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the
+ * License.
+ */
+package org.alfresco.web.action.evaluator;
+
+import javax.faces.context.FacesContext;
+
+import org.alfresco.model.ContentModel;
+import org.alfresco.service.cmr.lock.LockService;
+import org.alfresco.web.action.ActionEvaluator;
+import org.alfresco.web.app.Application;
+import org.alfresco.web.bean.repository.Node;
+import org.alfresco.web.bean.repository.Repository;
+
+/**
+ * @author Kevin Roast
+ */
+public class EditDocHttpEvaluator implements ActionEvaluator
+{
+   /**
+    * @see org.alfresco.web.action.ActionEvaluator#evaluate(org.alfresco.web.bean.repository.Node)
+    */
+   public boolean evaluate(Node node)
+   {
+      FacesContext fc = FacesContext.getCurrentInstance();
+      
+      // if the node is inline editable, the default http behaviour should 
+      // always be used otherwise the configured approach is used
+      if (node.hasAspect(ContentModel.ASPECT_INLINEEDITABLE) == true ||
+          "http".equals(Application.getClientConfig(fc).getEditLinkType()))
+      {
+         LockService lockService =
+            Repository.getServiceRegistry(FacesContext.getCurrentInstance()).getLockService();
+         
+         if (Repository.isNodeOwner(node, lockService) == true ||
+             (node.isLocked() == false && node.hasAspect(ContentModel.ASPECT_WORKING_COPY)))
+         {
+            return true;
+         }
+      }
+      
+      return false;
+   }
+}
+/*
+<a:booleanEvaluator value="#{(r.locked == false && r.workingCopy == false) || r.owner == true}">
+   <a:booleanEvaluator value="#{r.editLinkType == 'http'}">
+*/

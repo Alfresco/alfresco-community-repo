4aacad6f52f4b492448b3302e5896280f9f3ad59
Gavin Cornwell <gavin.cornwell@alfresco.com>
2006-04-26 08:35:30 +0000

    - Changed discussion icon from forum_large to forum - Disabled regex validation
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@2706 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/java/org/alfresco/repo/forum/DiscussableAspect.java
+++ source/java/org/alfresco/repo/forum/DiscussableAspect.java
@@ -247,7 +247,7 @@ public class DiscussableAspect
 
            // apply the uifacets aspect
            Map<QName, Serializable> uiFacetsProps = new HashMap<QName, Serializable>(5);
-           uiFacetsProps.put(ContentModel.PROP_ICON, "forum_large");
+           uiFacetsProps.put(ContentModel.PROP_ICON, "forum");
            this.nodeService.addAspect(destinationForum, ContentModel.ASPECT_UIFACETS, uiFacetsProps);
         }
         

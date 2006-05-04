3c5457c60212a9c2448713d119235e032d821450
Paul Holmes-Higgin <paulhh@alfresco.com>
2006-05-04 12:37:20 +0000

    changed enterprise headers to open
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@2757 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/cpp/CAlfrescoApp/includes/util/Exception.h
+++ source/cpp/CAlfrescoApp/includes/util/Exception.h
@@ -1,26 +1,19 @@
 /*
-* Copyright (C) 2005 Alfresco, Inc.
-*
-* Licensed under the Alfresco Network License. You may obtain a
-* copy of the License at
-*
-*   http://www.alfrescosoftware.com/legal/
-*
-* Please view the license relevant to your network subscription.
-*
-* BY CLICKING THE "I UNDERSTAND AND ACCEPT" BOX, OR INSTALLING,  
-* READING OR USING ALFRESCO'S Network SOFTWARE (THE "SOFTWARE"),  
-* YOU ARE AGREEING ON BEHALF OF THE ENTITY LICENSING THE SOFTWARE    
-* ("COMPANY") THAT COMPANY WILL BE BOUND BY AND IS BECOMING A PARTY TO 
-* THIS ALFRESCO NETWORK AGREEMENT ("AGREEMENT") AND THAT YOU HAVE THE   
-* AUTHORITY TO BIND COMPANY. IF COMPANY DOES NOT AGREE TO ALL OF THE   
-* TERMS OF THIS AGREEMENT, DO NOT SELECT THE "I UNDERSTAND AND AGREE"   
-* BOX AND DO NOT INSTALL THE SOFTWARE OR VIEW THE SOURCE CODE. COMPANY   
-* HAS NOT BECOME A LICENSEE OF, AND IS NOT AUTHORIZED TO USE THE    
-* SOFTWARE UNLESS AND UNTIL IT HAS AGREED TO BE BOUND BY THESE LICENSE  
-* TERMS. THE "EFFECTIVE DATE" FOR THIS AGREEMENT SHALL BE THE DAY YOU  
-* CHECK THE "I UNDERSTAND AND ACCEPT" BOX.
-*/
+ * Copyright (C) 2005-2006 Alfresco, Inc.
+ *
+ * Licensed under the Mozilla Public License version 1.1 
+ * with a permitted attribution clause. You may obtain a
+ * copy of the License at
+ *
+ *   http://www.alfresco.org/legal/license.txt
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the
+ * License.
+ */
 
 #ifndef _JavaException_H
 #define _JavaException_H

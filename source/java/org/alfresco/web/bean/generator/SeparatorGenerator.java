02408541cd77181191f9543b15280a55aa2dc463
Gavin Cornwell <gavin.cornwell@alfresco.com>
2006-09-01 20:47:27 +0000

    - Improved flexibility of separator generators
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@3656 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/java/org/alfresco/web/bean/generator/SeparatorGenerator.java
+++ source/java/org/alfresco/web/bean/generator/SeparatorGenerator.java
@@ -1,49 +1,16 @@
 package org.alfresco.web.bean.generator;
 
-import javax.faces.component.UIComponent;
-import javax.faces.context.FacesContext;
-
-import org.alfresco.web.ui.repo.component.property.PropertySheetItem;
-import org.alfresco.web.ui.repo.component.property.UIPropertySheet;
-
 /**
- * Generates a component to represent a separator.
+ * Generates a component to represent a separator using the HTML <code>&lt;hr/&gt;</code> element.
  * 
  * @author gavinc
  */
-public class SeparatorGenerator extends BaseComponentGenerator
+public class SeparatorGenerator extends HtmlSeparatorGenerator
 {
-   @SuppressWarnings("unchecked")
-   public UIComponent generate(FacesContext context, String id)
+   public SeparatorGenerator()
    {
-      UIComponent component = this.createOutputTextComponent(context, id);      
-      component.getAttributes().put("escape", Boolean.FALSE);
+      // For the standard separator just show a <hr/> element
       
-      return component;
-   }
-
-   @Override
-   @SuppressWarnings("unchecked")
-   protected UIComponent createComponent(FacesContext context, UIPropertySheet propertySheet, 
-         PropertySheetItem item)
-   {
-      UIComponent component = this.generate(context, item.getName());
-      
-      // set the HTML to use 
-      component.getAttributes().put("value", getHtml(component, item));
-      
-      return component;
-   }
-   
-   /**
-    * Returns the HTML to display for the separator
-    * 
-    * @param component The JSF component representing the separator
-    * @param item The separator item
-    * @return The HTML
-    */
-   protected String getHtml(UIComponent component, PropertySheetItem item)
-   {
-      return "<div style='margin-top: 6px; margin-bottom: 6px;'><hr/></div>";
+      this.html = "<div style='margin-top: 6px; margin-bottom: 6px;'><hr/></div>";
    }
 }

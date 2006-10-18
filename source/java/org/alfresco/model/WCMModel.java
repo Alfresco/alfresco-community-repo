dd5c46339e74916e1a3117c49bb1c543da46bdd8
Ariel Backenroth <bounce@alfresco.com>
2006-10-18 19:01:45 +0000

    - terminology changes to wcmModel (needs to be propogated to java classes as well). template is now form. template output method is now form transformer.
    - added an aspect to differentiate form derived from form transformer derived (allowed removing some workarounds from XSLTOutputMethod)
    
    - namespacing variables passed to the xsl processor with alfresco:
    
    
    
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/BRANCHES/WCM-DEV2/root@4154 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/java/org/alfresco/model/WCMModel.java
+++ source/java/org/alfresco/model/WCMModel.java
@@ -1,5 +1,18 @@
-/**
- * 
+/*
+ * Copyright (C) 2005 Alfresco, Inc.
+ *
+ * Licensed under the Mozilla Public License version 1.1 
+ * with a permitted attribution clause. You may obtain a
+ * copy of the License at
+ *
+ *   http://www.alfresco.org/legal/license.txt
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the
+ * License.
  */
 package org.alfresco.model;
 
@@ -24,18 +37,22 @@ public interface WCMModel
     public static final QName TYPE_AVM_LAYERED_FOLDER = QName.createQName(NamespaceService.WCM_MODEL_1_0_URI, "avmlayeredfolder");
     public static final QName PROP_AVM_DIR_INDIRECTION = QName.createQName(NamespaceService.WCM_MODEL_1_0_URI, "avmdirindirection");
     
-    // The XForms data capture template aspect.
-    public static final QName ASPECT_TEMPLATE = QName.createQName(NamespaceService.WCM_MODEL_1_0_URI, "template");
+    // The XForms data capture form aspect.
+    public static final QName ASPECT_FORM = QName.createQName(NamespaceService.WCM_MODEL_1_0_URI, "form");
     public static final QName PROP_SCHEMA_ROOT_TAG_NAME = QName.createQName(NamespaceService.WCM_MODEL_1_0_URI, "schemaroottagname");
-    public static final QName ASSOC_TEMPLATE_OUTPUT_METHODS = QName.createQName(NamespaceService.WCM_MODEL_1_0_URI, "templateoutputmethods");
+    public static final QName ASSOC_FORM_TRANSFORMERS = QName.createQName(NamespaceService.WCM_MODEL_1_0_URI, "formtransformers");
     
     // An XML to something else tranformer aspect.
-    public static final QName ASPECT_TEMPLATE_OUTPUT_METHOD = QName.createQName(NamespaceService.WCM_MODEL_1_0_URI, "templateoutputmethod");
-    public static final QName PROP_TEMPLATE_OUTPUT_METHOD_TYPE = QName.createQName(NamespaceService.WCM_MODEL_1_0_URI, "templateoutputmethodtype");
-    public static final QName PROP_TEMPLATE_OUTPUT_METHOD_DERIVED_FILE_EXTENSION = QName.createQName(NamespaceService.WCM_MODEL_1_0_URI, "templateoutputmethodderivedfileextension");
-    public static final QName PROP_TEMPLATE_SOURCE = QName.createQName(NamespaceService.WCM_MODEL_1_0_URI, "templatesource");
+    public static final QName ASPECT_FORM_TRANSFORMER = QName.createQName(NamespaceService.WCM_MODEL_1_0_URI, "formtransformer");
+    public static final QName PROP_FORM_TRANSFORMER_TYPE = QName.createQName(NamespaceService.WCM_MODEL_1_0_URI, "formtransformertype");
+    public static final QName PROP_FORM_TRANSFORMER_DERIVED_FILE_EXTENSION = QName.createQName(NamespaceService.WCM_MODEL_1_0_URI, "formtransformerderivedfileextension");
+    public static final QName PROP_FORM_SOURCE = QName.createQName(NamespaceService.WCM_MODEL_1_0_URI, "formsource");
 
-    public static final QName ASPECT_TEMPLATE_DERIVED = QName.createQName(NamespaceService.WCM_MODEL_1_0_URI, "templatederived");
-    public static final QName PROP_TEMPLATE_DERIVED_FROM = QName.createQName(NamespaceService.WCM_MODEL_1_0_URI, "templatederivedfrom");
-    public static final QName PROP_TEMPLATE_DERIVED_FROM_NAME = QName.createQName(NamespaceService.WCM_MODEL_1_0_URI, "templatederivedfromname");
+    public static final QName ASPECT_FORM_DERIVED = QName.createQName(NamespaceService.WCM_MODEL_1_0_URI, "formderived");
+    public static final QName PROP_FORM_DERIVED_FROM = QName.createQName(NamespaceService.WCM_MODEL_1_0_URI, "formderivedfrom");
+    public static final QName PROP_FORM_DERIVED_FROM_NAME = QName.createQName(NamespaceService.WCM_MODEL_1_0_URI, "formderivedfromname");
+
+    public static final QName ASPECT_FORM_TRANSFORMER_DERIVED = QName.createQName(NamespaceService.WCM_MODEL_1_0_URI, "formtransformerderived");
+    public static final QName PROP_FORM_TRANSFORMER_DERIVED_FROM = QName.createQName(NamespaceService.WCM_MODEL_1_0_URI, "formtransformerderivedfrom");
+    public static final QName PROP_PRIMARY_FORM_DERIVED = QName.createQName(NamespaceService.WCM_MODEL_1_0_URI, "primaryformderived");
 }

d9ac93d9098352719b543c562f38ac744d0bd248
Derek Hulley <derek.hulley@alfresco.com>
2006-12-19 12:18:46 +0000

    Merged 1.4 to HEAD    svn merge svn://svn.alfresco.com:3691/alfresco/BRANCHES/V1.4@4316 svn://svn.alfresco.com:3691/alfresco/BRANCHES/V1.4@4317 .    svn merge svn://svn.alfresco.com:3691/alfresco/BRANCHES/V1.4@4319 svn://svn.alfresco.com:3691/alfresco/BRANCHES/V1.4@4328 .
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@4652 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/java/org/alfresco/repo/webdav/WebDAVMethod.java
+++ source/java/org/alfresco/repo/webdav/WebDAVMethod.java
@@ -29,8 +29,6 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import org.alfresco.model.ContentModel;
 import org.alfresco.repo.security.permissions.AccessDeniedException;
-import org.alfresco.repo.transaction.TransactionUtil;
-import org.alfresco.repo.transaction.TransactionUtil.TransactionWork;
 import org.alfresco.service.ServiceRegistry;
 import org.alfresco.service.cmr.lock.LockService;
 import org.alfresco.service.cmr.model.FileFolderService;
@@ -139,10 +137,13 @@ public abstract class WebDAVMethod
         }
         catch (Throwable e)
         {
-            Throwable cause = e.getCause();
-            if (cause instanceof WebDAVServerException)
+            if (e instanceof WebDAVServerException)
             {
-                throw (WebDAVServerException) cause;
+               throw (WebDAVServerException) e;
+            }
+            else if (e.getCause() instanceof WebDAVServerException)
+            {
+                throw (WebDAVServerException) e.getCause();
             }
             else
             {

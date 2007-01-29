e8e74fe621ca262f6179b2453c8981e1c858d364
Gary Spencer <gary.spencer@alfresco.com>
2007-01-29 17:56:41 +0000

    Fix for unit test/build issue, too many brackets in XPath query string.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@4959 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/java/org/alfresco/repo/model/filefolder/FileFolderServiceImpl.java
+++ source/java/org/alfresco/repo/model/filefolder/FileFolderServiceImpl.java
@@ -99,7 +99,7 @@ public class FileFolderServiceImpl implements FileFolderService
         "[like(@cm:name, $cm:name, false)" +
         " and not (subtypeOf('" + ContentModel.TYPE_SYSTEM_FOLDER + "'))" +
         " and (subtypeOf('" + ContentModel.TYPE_FOLDER + "') or subtypeOf('" + ContentModel.TYPE_CONTENT + "')" +
-        " and (subtypeOf('" + ContentModel.TYPE_LINK + "'))]";
+        " and subtypeOf('" + ContentModel.TYPE_LINK + "'))]";
     
     /** empty parameters */
     private static final QueryParameterDefinition[] PARAMS_ANY_NAME = new QueryParameterDefinition[1];

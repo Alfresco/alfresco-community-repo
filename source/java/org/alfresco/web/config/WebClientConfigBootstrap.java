f365359147749a1147bf2178f2fbdf61cddc8471
Derek Hulley <derek.hulley@alfresco.com>
2007-01-29 14:02:14 +0000

    Merged DEV\EXTENSIONS to HEAD    svn merge svn://svn.alfresco.com:3691/alfresco/BRANCHES/DEV/EXTENSIONS@4832 svn://svn.alfresco.com:3691/alfresco/BRANCHES/DEV/EXTENSIONS@4833 .    svn merge svn://svn.alfresco.com:3691/alfresco/BRANCHES/DEV/EXTENSIONS@4862 svn://svn.alfresco.com:3691/alfresco/BRANCHES/DEV/EXTENSIONS@4863 .    svn merge svn://svn.alfresco.com:3691/alfresco/BRANCHES/DEV/EXTENSIONS@4866 svn://svn.alfresco.com:3691/alfresco/BRANCHES/DEV/EXTENSIONS@4867 .    svn rm --force root\projects\repository\source\java\org\alfresco\repo\extension    svn rm --force root\projects\repository\source\java\org\alfresco\service\cmr\extension
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@4954 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

---
+++ source/java/org/alfresco/web/config/WebClientConfigBootstrap.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2005 Alfresco, Inc.
+ *
+ * Licensed under the Mozilla Public License version 1.1 
+ * with a permitted attribution clause. You may obtain a
+ * copy of the License at
+ *
+ *   http://www.alfresco.org/legal/license.txt
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the
+ * License.
+ */
+package org.alfresco.web.config;
+
+import java.util.List;
+
+import org.alfresco.config.ConfigService;
+import org.alfresco.config.ConfigSource;
+import org.alfresco.config.source.UrlConfigSource;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+
+/**
+ * Web client config bootstrap
+ * 
+ * @author Roy Wetherall
+ */
+public class WebClientConfigBootstrap implements ApplicationContextAware
+{
+    private ApplicationContext applicationContext;
+    
+    private List<String> configs;
+    
+    public void setConfigs(List<String> configs)
+    {
+        this.configs = configs;
+    }
+    
+    public void init()
+    {
+        if (this.applicationContext.containsBean("webClientConfigService") == true)
+        {    
+            ConfigService configService = (ConfigService)this.applicationContext.getBean("webClientConfigService"); 
+            if (configService != null && this.configs != null && this.configs.size() != 0)
+            {
+                System.out.println("****************************************");
+                UrlConfigSource configSource = new UrlConfigSource(this.configs);
+                configService.appendConfig(configSource);
+            }
+        }
+        else
+        {
+
+            System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        }
+    }
+
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException
+    {
+        this.applicationContext = applicationContext;        
+    }
+}

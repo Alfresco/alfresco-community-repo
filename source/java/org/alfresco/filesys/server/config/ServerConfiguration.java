3e478f1950f2948c48ffb417ededc9f001fb4b25
Gary Spencer <gary.spencer@alfresco.com>
2007-02-02 11:49:07 +0000

    Set a default domain/workgroup name of 'WORKGROUP', if the domain name cannot be found it can cause a null pointer exception when building the CIFS session setup response. Fix for AR-1069.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@5013 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/java/org/alfresco/filesys/server/config/ServerConfiguration.java
+++ source/java/org/alfresco/filesys/server/config/ServerConfiguration.java
@@ -201,7 +201,7 @@ public class ServerConfiguration extends AbstractLifecycleBean
 
     // Server domain
 
-    private String m_domain;
+    private String m_domain = "WORKGROUP";
 
     // Network broadcast mask string
 

e7c83d8731d3e06810eed9bf75c5d5aaeb71b36e
Derek Hulley <derek.hulley@alfresco.com>
2007-02-20 11:23:11 +0000

    Merged V2.0 to HEAD    svn merge svn://svn.alfresco.com:3691/alfresco/BRANCHES/V2.0@5093 svn://svn.alfresco.com:3691/alfresco/BRANCHES/V2.0@5094 .       - Fix AR-1024: AVM workflow submit NPE    svn merge svn://svn.alfresco.com:3691/alfresco/BRANCHES/V2.0@5188 svn://svn.alfresco.com:3691/alfresco/BRANCHES/V2.0@5189 .       - PostgreSQL sample    svn merge svn://svn.alfresco.com:3691/alfresco/BRANCHES/V2.0@5191 svn://svn.alfresco.com:3691/alfresco/BRANCHES/V2.0@5193 .       - PostgreSQL lurking JDBC driver
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@5195 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/java/org/alfresco/repo/avm/wf/AVMRemoveWFStoreHandler.java
+++ source/java/org/alfresco/repo/avm/wf/AVMRemoveWFStoreHandler.java
@@ -20,49 +20,37 @@
  * and Open Source Software ("FLOSS") applications as described in Alfresco's 
  * FLOSS exception.  You should have recieved a copy of the text describing 
  * the FLOSS exception, and it is also available here: 
- * http://www.alfresco.com/legal/licensing"
+ * http://www.alfresco.com/legal/licensing
  */
 package org.alfresco.repo.avm.wf;
 
-import java.util.List;
+import java.util.Map;
 
 import org.alfresco.repo.avm.AVMNodeConverter;
+import org.alfresco.repo.domain.PropertyValue;
 import org.alfresco.repo.workflow.jbpm.JBPMNode;
 import org.alfresco.repo.workflow.jbpm.JBPMSpringActionHandler;
-import org.alfresco.service.cmr.avm.AVMNodeDescriptor;
 import org.alfresco.service.cmr.avm.AVMService;
-import org.alfresco.service.cmr.avmsync.AVMDifference;
-import org.alfresco.service.cmr.avmsync.AVMSyncService;
 import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.namespace.QName;
 import org.alfresco.util.Pair;
 import org.jbpm.graph.exe.ExecutionContext;
 import org.springframework.beans.factory.BeanFactory;
 
 
 /**
- * Clear "submitted" mark from (source of) items within the WCM Workflow Package
+ * Remove WF sandbox
  * 
- * @author davidc
+ * @author brittp
  */
-public class AVMClearSubmittedHandler extends JBPMSpringActionHandler 
+public class AVMRemoveWFStoreHandler extends JBPMSpringActionHandler 
 {
     private static final long serialVersionUID = 4113360751217684995L;
 
     /**
      * The AVMService instance.
      */
-    private AVMService fAVMService;
-    
-    /**
-     * The AVMSyncService instance.
-     */
-    private AVMSyncService fAVMSyncService;
-    
-    /**
-     * The AVMSubmittedAspect instance.
-     */
-    private AVMSubmittedAspect fAVMSubmittedAspect;
-    
+    private AVMService fAVMService;    
 
     
     /**
@@ -73,8 +61,6 @@ public class AVMClearSubmittedHandler extends JBPMSpringActionHandler
     protected void initialiseHandler(BeanFactory factory) 
     {
         fAVMService = (AVMService)factory.getBean("AVMService");
-        fAVMSyncService = (AVMSyncService)factory.getBean("AVMSyncService");
-        fAVMSubmittedAspect = (AVMSubmittedAspect)factory.getBean("AVMSubmittedAspect");
     }
 
     /**
@@ -86,23 +72,20 @@ public class AVMClearSubmittedHandler extends JBPMSpringActionHandler
         // TODO: Allow submit parameters to be passed into this action handler
         //       rather than pulling directly from execution context
         
-        // NOTE: Submitted items can only be marked as "submitted" if we know where they came from
-        String from = (String)executionContext.getContextInstance().getVariable("wcmwf_fromPath");
-        if (from != null && from.length() > 0)
-        {
-            // retrieve list of changes in submitted package
-            NodeRef pkg = ((JBPMNode)executionContext.getContextInstance().getVariable("bpm_package")).getNodeRef();
-            Pair<Integer, String> pkgPath = AVMNodeConverter.ToAVMVersionPath(pkg);
-            AVMNodeDescriptor pkgDesc = fAVMService.lookup(pkgPath.getFirst(), pkgPath.getSecond());
-            String targetPath = pkgDesc.getIndirection();
-            List<AVMDifference> diffs = fAVMSyncService.compare(pkgPath.getFirst(), pkgPath.getSecond(), -1, targetPath, null);
+        // retrieve submitted package
+        NodeRef pkg = ((JBPMNode)executionContext.getContextInstance().getVariable("bpm_package")).getNodeRef();
+        Pair<Integer, String> pkgPath = AVMNodeConverter.ToAVMVersionPath(pkg);
 
-            // for each change, mark original as submitted
-            for (AVMDifference diff : diffs)
-            {
-                String submittedPath = from + diff.getSourcePath().substring(pkgPath.getSecond().length());
-                fAVMSubmittedAspect.clearSubmitted(-1, submittedPath);
-            }
+        // Now delete the stores in the workflow sandbox.
+        String [] storePath = pkgPath.getSecond().split(":");
+        // Get the sandbox id for the package.
+        Map<QName, PropertyValue> matches = fAVMService.queryStorePropertyKey(storePath[0], QName.createQName(null, ".sandbox-id%"));
+        QName sandboxID = matches.keySet().iterator().next();
+        // Get all the stores in the sandbox.
+        Map<String, Map<QName, PropertyValue>> stores = fAVMService.queryStoresPropertyKeys(sandboxID);
+        for (String storeName : stores.keySet())
+        {
+            fAVMService.purgeStore(storeName);
         }
     }
     

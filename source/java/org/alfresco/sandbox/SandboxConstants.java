6c332588a956aae606ac72fa44eaee97e84e4e96
Jon Cox <jon.cox@alfresco.com>
2007-05-08 02:33:18 +0000

    Point checkin. Reorg to allow sandbox-related constants to be accessed more easily from other compilation units.   AVMConstants had become a dumping ground for things that had nothing to do with const values.   Therefore, this is now AVMUtil, and sandbox-related constants have been moved to org.alfresco.sandbox.SandboxConstants, which builds in repository.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@5637 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

---
+++ source/java/org/alfresco/sandbox/SandboxConstants.java
@@ -0,0 +1,48 @@
+/*-----------------------------------------------------------------------------
+*  Copyright 2007 Alfresco Inc.
+*  
+*  This program is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*  
+*  This program is distributed in the hope that it will be useful, but
+*  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+*  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+*  for more details.
+*  
+*  You should have received a copy of the GNU General Public License along
+*  with this program; if not, write to the Free Software Foundation, Inc.,
+*  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  As a special
+*  exception to the terms and conditions of version 2.0 of the GPL, you may
+*  redistribute this Program in connection with Free/Libre and Open Source
+*  Software ("FLOSS") applications as described in Alfresco's FLOSS exception.
+*  You should have received a copy of the text describing the FLOSS exception,
+*  and it is also available here:   http://www.alfresco.com/legal/licensing
+*  
+*  
+*  Author  Jon Cox  <jcox@alfresco.com>
+*  File    SandboxConstants.java
+*----------------------------------------------------------------------------*/
+
+package org.alfresco.sandbox;
+import org.alfresco.service.namespace.QName;
+
+/**
+*  AVM sandbox constants
+*/
+public class  SandboxConstants
+{
+    // system property keys for sandbox identification and DNS virtualisation mapping
+    public final static String PROP_BACKGROUND_LAYER        = ".background-layer.";
+    public final static String PROP_SANDBOXID               = ".sandbox-id.";
+    public final static String PROP_DNS                     = ".dns.";
+    public final static String PROP_SANDBOX_STORE_PREFIX    = ".sandbox.store.";
+    public final static QName PROP_SANDBOX_STAGING_MAIN     = QName.createQName(null, ".sandbox.staging.main");
+    public final static QName PROP_SANDBOX_STAGING_PREVIEW  = QName.createQName(null, ".sandbox.staging.preview");
+    public final static QName PROP_SANDBOX_AUTHOR_MAIN      = QName.createQName(null, ".sandbox.author.main");
+    public final static QName PROP_SANDBOX_AUTHOR_PREVIEW   = QName.createQName(null, ".sandbox.author.preview");
+    public final static QName PROP_SANDBOX_WORKFLOW_MAIN    = QName.createQName(null, ".sandbox.workflow.main");
+    public final static QName PROP_SANDBOX_WORKFLOW_PREVIEW = QName.createQName(null, ".sandbox.workflow.preview");
+    public final static QName PROP_WEBSITE_NAME             = QName.createQName(null, ".website.name");
+}

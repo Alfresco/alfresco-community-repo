ee39d66a0e751f8fc86eee3dbf0a2659c2f41983
David Caruana <david.caruana@alfresco.com>
2007-06-19 10:14:49 +0000

    Fix error in sample.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@6014 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- config/alfresco/bootstrap/webscripts/categorysearch.get.js
+++ config/alfresco/bootstrap/webscripts/categorysearch.get.js
@@ -1,11 +1,10 @@
 // check category exists?
-var category = search.luceneSearch("PATH:\"/cm:generalclassifiable//cm:" + url.extension);
-if (category is null)
+var category = search.luceneSearch("PATH:\"/cm:generalclassifiable//cm:" + url.extension + "\"");
+if (category == undefined)
 {
    status.code = 404;
    status.message = "Category " + url.extension + " not found.";
    status.redirect = true;
-   return;
 }
 else
 {

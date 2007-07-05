7d537bf42d4cad2898b488453a940457ac0afbf9
Jon Cox <jon.cox@alfresco.com>
2007-07-05 22:43:31 +0000

    Added transaction hook for store purges, version creations and version purges; this allows an internal cache to be maintained that permits most calls to the heavyweight getLatestSnapshotID(String store) AVM API to be skipped. This addresses a performance problem noticed when the number of stores is large.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@6182 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/java/org/alfresco/repo/avm/CreateVersionCallback.java
+++ source/java/org/alfresco/repo/avm/CreateVersionCallback.java
@@ -10,7 +10,7 @@ package org.alfresco.repo.avm;
 public interface CreateVersionCallback 
 {
     /**
-     * A version of a store has been purged.
+     * A version of a store has been created.
      * @param storeName The name of the store in which a new version has been created.
      * @param versionID The version id of the new version.
      */

3a43e6dfec6660576cd82cd2bc085fcf386b77bf
Kevin Roast <kevin.roast@alfresco.com>
2007-12-07 22:07:53 +0000

    Create script for app:project type now sets email alias value to project name for Email Archive space
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@7597 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- config/alfresco/module/projects/script/onCreateNode_app_projectfolder.js
+++ config/alfresco/module/projects/script/onCreateNode_app_projectfolder.js
@@ -1,1 +1,19 @@
-// add onCreateNode policy code for app:projectfolder here
+// onCreateNode policy code for app:projectfolder
+var project = behaviour.args[0];
+var results = search.luceneSearch("+PATH:\"" + project.qnamePath + "/*\" +ASPECT:\"{http://www.alfresco.org/model/emailserver/1.0}aliasable\"");
+if (results.length == 1)
+{
+   results[0].properties["emailserver:alias"] = normalise(project.name);
+   results[0].save();
+   logger.log("Applied email alias of: " + normalise(project.name));
+}
+else
+{
+   logger.log("No email folder found!");
+}
+
+function normalise(s)
+{
+   // email alias has strict constraint
+   return new String(s).toLowerCase().replace(/[^a-z^0-9^.]/g, "-");
+}

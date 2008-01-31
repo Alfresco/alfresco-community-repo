5bdd6a9c8b5c0d0af65dd5a643e8733901a28605
David Caruana <david.caruana@alfresco.com>
2008-01-31 13:01:00 +0000

    Web Scripts: minor fixes: - removed obselete WrapFactory - fixed missing closing " in web script index html - addition of system.out to script logger
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@8139 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/java/org/alfresco/repo/jscript/ScriptLogger.java
+++ source/java/org/alfresco/repo/jscript/ScriptLogger.java
@@ -33,7 +33,8 @@ import org.apache.commons.logging.LogFactory;
  */
 public final class ScriptLogger extends BaseProcessorExtension
 {
-    private static final Log    logger = LogFactory.getLog(ScriptLogger.class);
+    private static final Log logger = LogFactory.getLog(ScriptLogger.class);
+    private static final SystemOut systemOut = new SystemOut();
     
     public boolean isLoggingEnabled()
     {
@@ -44,4 +45,18 @@ public final class ScriptLogger extends BaseProcessorExtension
     {
         logger.debug(str);
     }
+
+    public SystemOut getSystem()
+    {
+        return systemOut;
+    }
+    
+    public static class SystemOut
+    {
+        public void out(String str)
+        {
+            System.out.println(str);
+        }
+    }
+
 }

ae8522df09c8807bf5c803ef61c989a6961dcb32
Mike Hatfield <mike.hatfield@alfresco.com>
2008-04-25 08:25:12 +0000

    New template format and Freemarker macros. Improved YUILoader wrapper. Placeholder doclist using YUI DataTable for testing.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@8883 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- config/alfresco/templates/webscripts/org/alfresco/slingshot/documentlibrary/doclist.get.js
+++ config/alfresco/templates/webscripts/org/alfresco/slingshot/documentlibrary/doclist.get.js
@@ -7,10 +7,10 @@
  * Outputs:
  *  doclist - object containing list of child folders and documents in the parent space
  */
-model.doclist = getDoclist(args["nodeRef"]);
+model.doclist = getDoclist(args["nodeRef"], args["path"], args["type"]);
 
 /* Create collection of documents and folders in the given space */
-function getDoclist(nodeRef)
+function getDoclist(nodeRef, path, type)
 {
    var items = new Array();
    
@@ -20,15 +20,34 @@ function getDoclist(nodeRef)
    {
       parentSpace = search.findNode(nodeRef);
    }
+   else if ((path != null) && path != "")
+   {
+      parentSpace = companyhome.childByNamePath(path);
+   }
    if (parentSpace == null)
    {
-      return jsonError("Parent space nodeRef not supplied");
+      // return jsonError("Parent space nodeRef not supplied");
+      parentSpace = companyhome;
+   }
+
+   var showDocs = true,
+      showFolders = true;
+      
+   if ((type != null) && (type != ""))
+   {
+      showDocs = (type == "documents");
+      showFolders = (type == "folders");
    }
    
    for each(item in parentSpace.children)
    {
-      items.push(item);
+      if ((item.isContainer && showFolders) || (!item.isContainer && showDocs))
+      {      
+         items.push(item);
+      }
    }
+   
+   items.sort(sortByType);
    
    return ({
       "items": items
@@ -45,4 +64,13 @@ function jsonError(errorString)
    };
    
    return obj;
+}
+
+function sortByType(a, b)
+{
+   if (a.isContainer == b.isContainer)
+   {
+      return (b.name.toLowerCase() > a.name.toLowerCase() ? -1 : 1);
+   }
+   return (a.isContainer ? -1 : 1);
 }

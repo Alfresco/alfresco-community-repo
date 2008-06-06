80729976572a95178d675934d3c8940d704559bf
Kevin Roast <kevin.roast@alfresco.com>
2008-06-06 14:10:39 +0000

    The infamous JavaScript Debugger is now available in the Web-tier (Web-Framework):  - set this log level: log4j.logger.org.alfresco.web.scripts.AlfrescoScriptDebugger=debug  - or use this webscript: [your-web-server]/slingshot/service/api/javascript/debugger The usual repo JavaScript Debugger is available here:  - set this log level: log4j.logger.org.alfresco.repo.web.scripts.AlfrescoRhinoScriptDebugger=debug  - or use this webscript: [your-repo-server]/alfresco/service/api/javascript/debugger You can use both debuggers at the same time if required. Rewrite of the bizare IProxy code in the javascript debugger class - which in turn fixes the bug ALFCOM-1743 where closing the JavaScript debugger window would have caused all future JavaScripts to block until the server was restarted.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@9410 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

---
+++ source/java/org/alfresco/repo/web/scripts/AlfrescoRhinoScriptDebugger.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (C) 2005-2007 Alfresco Software Limited.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+ * As a special exception to the terms and conditions of version 2.0 of 
+ * the GPL, you may redistribute this Program in connection with Free/Libre 
+ * and Open Source Software ("FLOSS") applications as described in Alfresco's 
+ * FLOSS exception.  You should have recieved a copy of the text describing 
+ * the FLOSS exception, and it is also available here: 
+ * http://www.alfresco.com/legal/licensing"
+ */
+package org.alfresco.repo.web.scripts;
+
+import org.alfresco.repo.security.authentication.AuthenticationUtil;
+import org.alfresco.repo.security.authentication.AuthenticationUtil.RunAsWork;
+import org.alfresco.web.scripts.AlfrescoScriptDebugger;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.mozilla.javascript.ContextFactory;
+import org.mozilla.javascript.tools.debugger.Dim;
+import org.mozilla.javascript.tools.debugger.SwingGui;
+
+
+/**
+ * Alfresco implementation of Rhino JavaScript debugger
+ * 
+ * Provides support for authenticated access to object inspection.
+ * 
+ * @author davidc
+ */
+public class AlfrescoRhinoScriptDebugger extends AlfrescoScriptDebugger
+{
+    private static final Log logger = LogFactory.getLog(AlfrescoRhinoScriptDebugger.class);
+    
+    // Logger
+    private ContextFactory factory = null;
+    private SwingGui gui = null;
+    
+    
+    @Override
+    protected void initDebugger()
+    {
+        dim = new AlfrescoDim();
+    }
+    
+    @Override
+    public void start()
+    {
+        if (logger.isDebugEnabled())
+        {
+            activate();
+            show();
+        }
+    }
+
+
+    public static class AlfrescoDim extends Dim
+    {
+        /* (non-Javadoc)
+         * @see org.mozilla.javascript.tools.debugger.Dim#objectToString(java.lang.Object)
+         */
+        @Override
+        public String objectToString(final Object arg0)
+        {
+            // execute command in context of currently selected user
+            return AuthenticationUtil.runAs(new RunAsWork<String>()
+            {
+                @SuppressWarnings("synthetic-access")
+                public String doWork() throws Exception
+                {
+                    return AlfrescoDim.super.objectToString(arg0);
+                }
+            }, AuthenticationUtil.getSystemUserName());
+        }
+    }
+}

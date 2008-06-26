88ea0e482e06111a0957d78264a807312671be0e
Simon Buckle <bounce@alfresco.com>
2008-06-26 16:35:31 +0000

    First cut of user dashboard dashlet that shows an aggregated view of all the events of all the sites the user belongs to.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@9579 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

---
+++ config/alfresco/templates/webscripts/org/alfresco/slingshot/calendar/userevents.get.js
@@ -0,0 +1,60 @@
+/** 
+ * Limits the number of events that get returned.
+ * Would be nice to have this supported in the Lucene query syntax
+ */
+model.limit = args.limit;
+
+// Get the username of the currently logged in person
+var username = person.properties["cm:userName"];
+
+var range = {};
+var dateFilter = args.from;
+if (dateFilter)
+{
+	range["fromdate"] = dateFilter;
+}
+
+model.events = getUserEvents(username, range);
+
+function getUserEvents(user, range)
+{
+	if (!user)
+	{
+		return [];
+	}
+	
+	var paths = [];
+	/**
+	 * This part is inefficient as it looks through all of the sites
+	 * and tries to determine if the user is a member or not; however, until something like
+	 * /people/{userid}/sites is exposed through the JavaScript API, it will have to do.
+	 * 
+	 */
+	var availableSites = siteService.listSites(null, null);
+	for (var j=0; j < availableSites.length; j++)
+	{
+		var site = availableSites[j];
+		if (site.isMember(user))
+		{
+			paths.push("PATH:\"/cm:sites/cm:" + site.shortName + "/cm:calendar/*\"");
+		}
+	}
+	
+	var results = [];
+	
+	if (paths.length > 0)
+	{
+		var luceneQuery = "+(" + paths.join(" OR ") + ") +TYPE:\"{com.infoaxon.alfresco.calendar}calendarEvent\"";
+		if (range.fromdate)
+		{
+			// Expects the date in the format yyyy/mm/dd
+			var from = range.fromdate.split("/").join("\\-"); 
+			var dateClause = " +@ia\\:fromDate:[" + from + "T00:00:00 TO 2032\\-1\\-1T00:00:00]";
+			luceneQuery += dateClause;
+		}
+		results = search.luceneSearch("workspace://SiteStore", luceneQuery, "ia:fromDate", true);
+	}
+
+	return results;
+}
+
---
+++ config/alfresco/templates/webscripts/org/alfresco/slingshot/calendar/userevents.get.json.ftl
@@ -0,0 +1,18 @@
+<#if !limit?exists><#assign limit = -1></#if>
+{
+<#if events?exists>
+"events": [
+<#list events as event>
+	<#if event_index?string == limit?string><#break></#if>
+	{
+		"name" : "${event.name}",
+		"title" : "${event.properties["ia:whatEvent"]}",
+<#--	"description" : "${event.properties["ia:descriptionEvent"]}", -->
+		"where" : "${event.properties["ia:whereEvent"]}",
+		"start" : "${event.properties["ia:fromDate"]?string("dd MMMM yyyy")}",
+		"end" : "${event.properties["ia:toDate"]?string("dd MMMM yyyy")}"
+	}<#if event_has_next>,</#if>
+</#list>
+]
+</#if>
+}

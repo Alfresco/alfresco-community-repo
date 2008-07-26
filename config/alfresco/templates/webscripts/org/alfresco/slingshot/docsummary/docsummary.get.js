1061838a4c0492e9e81eb18bc21e908de532b4e7
Mike Hatfield <mike.hatfield@alfresco.com>
2008-07-26 04:40:33 +0000

    Doc Summary dashlet now doesn't show working copies
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@10044 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- config/alfresco/templates/webscripts/org/alfresco/slingshot/docsummary/docsummary.get.js
+++ config/alfresco/templates/webscripts/org/alfresco/slingshot/docsummary/docsummary.get.js
@@ -43,7 +43,8 @@ function getDocs(siteId, filter)
       var query = "+TYPE:\"{http://www.alfresco.org/model/content/1.0}content\" +PATH:\"" + 
                   path + "\" +@cm\\:modified:[" + fromQuery + "T00\\:00\\:00 TO " + 
                   toQuery + "T23\\:59\\:59]" +
-                  "-TYPE:\"{http://www.alfresco.org/model/content/1.0}thumbnail\"";
+                  "-TYPE:\"{http://www.alfresco.org/model/content/1.0}thumbnail\"" +
+                  "-ASPECT:\"{http://www.alfresco.org/model/content/1.0}workingcopy\"";
       
       if (logger.isLoggingEnabled())
          logger.log("docsummary query = " + query);

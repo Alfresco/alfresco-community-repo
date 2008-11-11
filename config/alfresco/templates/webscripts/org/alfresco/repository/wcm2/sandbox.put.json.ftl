b82201a6bba0cf427a6e1a65e65edcf207ce9260
Michael Uzqu <uzquiano@gmail.com>
2008-11-11 00:22:42 +0000

    Post-merge cleanup for Web Studio - Added "chrome" element to Slingshot so that it renders properly in Web Framework - Web Studio now calls out to WCM Services and works really nicely - Removed remote api placeholder services
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@11840 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- config/alfresco/templates/webscripts/org/alfresco/repository/wcm2/sandbox.put.json.ftl
+++
@@ -1,5 +0,0 @@
-[
-	<#list tags as tag>
-		${jsonUtils.encodeJSONString(tag)}<#if tag_has_next>,</#if>
-	</#list>
-]
--- config/alfresco/templates/webscripts/org/alfresco/webframework/jsf-client-redirect.get.js
+++ config/alfresco/templates/webscripts/org/alfresco/webframework/jsf-client-redirect.get.js
@@ -14,7 +14,14 @@ if("browse" == model.command)
 	
 	if("webproject" == model.objectType)
 	{
-		model.webProjectId = url.templateArgs["webProjectId"];
+		model.webprojectref = url.templateArgs["id"];
+		
+		// load the web project metadata
+		var service = webprojects;
+		var data = service.getWebProject(model.webprojectref);
+		
+		// look up the node ref for the web project
+		// TODO: could this be made to be part of the web projects service?
 		
 		// look up the web project
 		var dmType = "{http://www.alfresco.org/model/wcmappmodel/1.0}webfolder";	
@@ -25,8 +32,8 @@ if("browse" == model.command)
 		// walk through the projects, get the avm store id (for staging)
 		for(var i = 0; i < webprojects.length; i++)
 		{
-			var projId = webprojects[i].name;
-			if(projId == model.webProjectId)
+			var dns = webprojects[i].properties["{http://www.alfresco.org/model/wcmappmodel/1.0}avmstore"];
+			if(dns == model.webprojectref)
 			{
 				var storeType = webprojects[i].properties["{http://www.alfresco.org/model/system/1.0}store-protocol"];
 				var storeId = webprojects[i].properties["{http://www.alfresco.org/model/system/1.0}store-identifier"];

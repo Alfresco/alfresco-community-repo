091c01adef524f7509eca5ad9c82e661d94ca9db
Kevin Roast <kevin.roast@alfresco.com>
2008-12-17 11:06:12 +0000

    Merged V3.0 to HEAD    11417: Removed 'Settings' link until feature is implemented    11418: Fav icons for JSF and Share client apps    11419: SSO authentication touch webscript updated to use Java backed bean impl.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@12435 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

---
+++ source/java/org/alfresco/repo/web/scripts/bean/Touch.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2005-2007 Alfresco Software Limited.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+ * As a special exception to the terms and conditions of version 2.0 of 
+ * the GPL, you may redistribute this Program in connection with Free/Libre 
+ * and Open Source Software ("FLOSS") applications as described in Alfresco's 
+ * FLOSS exception.  You should have recieved a copy of the text describing 
+ * the FLOSS exception, and it is also available here: 
+ * http://www.alfresco.com/legal/licensing
+ */
+package org.alfresco.repo.web.scripts.bean;
+
+import java.io.IOException;
+
+import org.alfresco.web.scripts.AbstractWebScript;
+import org.alfresco.web.scripts.Status;
+import org.alfresco.web.scripts.WebScriptRequest;
+import org.alfresco.web.scripts.WebScriptResponse;
+
+/**
+ * WebScript java backed bean implementation - to simple return a STATUS_OK message
+ * as a touch point for SSO authentication mechanisms on the web-tier. Such as NTLM.
+ * 
+ * @author Kevin Roast
+ */
+public class Touch extends AbstractWebScript
+{
+    /* (non-Javadoc)
+     * @see org.alfresco.web.scripts.WebScript#execute(org.alfresco.web.scripts.WebScriptRequest, org.alfresco.web.scripts.WebScriptResponse)
+     */
+    public void execute(WebScriptRequest req, WebScriptResponse res) throws IOException
+    {
+        res.setStatus(Status.STATUS_OK);
+        res.getWriter().close();
+    }
+}

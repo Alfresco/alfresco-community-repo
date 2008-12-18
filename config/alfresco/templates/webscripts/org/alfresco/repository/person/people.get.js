966b7b0b76f67d2f887b244a3b1af980d9be510c
Kevin Roast <kevin.roast@alfresco.com>
2008-12-18 13:58:20 +0000

    Merged V3.0 to HEAD    11943: Fix for ETHREEOH-879 & ETHREEOH-783: Multi-valued properties not allowed in Alfresco 3.0 (due to java.lang.UnsupportedOperationException)    11944: Fix for ETHREEOH-865    11947: Build fix for site service unit test failures.  This will be reviewed since it works round the issue rather than tackle why runAs is now failing.    11952: ETHREEOH-845, ETHREEOH-871, ETHREEOH-853, ETHREEOH-839    11953: ETHREEOH-483 Unable to upload files [with Flash 10 installed] Fixed to fit into yui 2.6.0    11954: Added missing 'protocolOrder' configuration value.    11956: Fix for ETHREEOH-895    11957: Fix for ETHREEOH-891.    11958: Readded generated source line for RemoteAPI project.    11959: ETHREEOH-483 Unable to upload files [with Flash 10 installed] Missed to add this image    11960: Removed JDK6 specific method.    11962: Fixed missing setup of the share mapper class name when the <class> config tag is used. ALFCOM-2060.    11964: fix for ETHREEOH-266 - restrict length of webapp to 150 chars.    11965: Merged 2.2 to 3.0       11926: Fox for ETHREEOH-725 User doesn't receive email to his box when rule 'Send an Email to specified users' is created    11966: ETHREEOH-872: Editing Email-notify-rules fails w/ ClassCastException    11967: MT - test fixes (post runAs merge)    11968: Changed Windows x64 NetBIOS warning message to be a debug message. ETHREEOH-897.    11971: ETHREEOH-829 Case issue when inserting Document Share links into a discussion using richtext editor    11973: Fix for ETHREEOH-890 - users with apostrophe in their login name can now login to Alfresco Explorer (and Share).
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@12490 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- config/alfresco/templates/webscripts/org/alfresco/repository/person/people.get.js
+++ config/alfresco/templates/webscripts/org/alfresco/repository/person/people.get.js
@@ -1,8 +1,9 @@
-// Get the filter query string
+// Get the args
 var filter = args["filter"];
+var maxResults = args["maxResults"];
 
 // Get the collection of people
-var peopleCollection = people.getPeople(filter);
+var peopleCollection = people.getPeople(filter, maxResults != null ? parseInt(maxResults) : 0);
 
 // Pass the queried sites to the template
 model.people = peopleCollection;

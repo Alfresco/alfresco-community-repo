6548c1c52217eb8c1329f6a7794ee9090937910a
Kevin Roast <kevin.roast@alfresco.com>
2009-03-11 10:37:53 +0000

    Merged V3.1 to HEAD    13065: SiteService - listSites() "nameFilter" and "sizeFilter" now implemented. Optimization to retrieve the Site Root noderef and cache it within the SiteService.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@13556 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- config/alfresco/templates/webscripts/org/alfresco/repository/site/sites.get.js
+++ config/alfresco/templates/webscripts/org/alfresco/repository/site/sites.get.js
@@ -6,25 +6,7 @@ function main()
     var sizeString = args["size"];
     
     // Get the list of sites
-    var sites = siteService.listSites(nameFilter, sitePreset);
-    
-    if (sizeString != null)
-    {
-        var size = parseInt(sizeString);
-        
-        if (size < sites.length)
-        {
-            // Only return the first n sites based on the passed page size
-            var pagedSites = new Array(size);
-            for (var index = 0; index < size; index++)
-            {
-                pagedSites[index] = sites[index];   
-            }
-            
-            sites = pagedSites;
-        }
-    }
-    
+    var sites = siteService.listSites(nameFilter, sitePreset, sizeString != null ? parseInt(sizeString) : 0);
     model.sites = sites;
 }
 

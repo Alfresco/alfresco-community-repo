965d72670f443b53b67c7851f832633162c2ef7f
Kevin Roast <kevin.roast@alfresco.com>
2009-05-06 13:38:15 +0000

    RM Share Site preset:  - Accessable via a webscript to create a RM site: <yourserver>/share/service/utils/create-rmsite?shortname={sitename}  - Preset adds RM pages; RM Document Library, RM Search and RM Reports  - Only RM Document Library page is functional at present (and currently points to the existing doclib components)
    NOTE: to access the webscript and for the RM site to display correctly, the files from modules\dod-5015-share must be deployed, use "ant deploy-rm-share" for now - once the Share AMP scripts are setup these files will be AMPed into the share.war and the temporary ANT target removed.
    
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@14207 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- config/alfresco/templates/webscripts/org/alfresco/repository/site/sites.post.json.js
+++ config/alfresco/templates/webscripts/org/alfresco/repository/site/sites.post.json.js
@@ -1,55 +1,64 @@
 function main()
 {
-	// Get the details of the site
-	var shortName = json.get("shortName");
-	if (shortName == null || shortName.length == 0)
-	{
-		status.setCode(status.STATUS_BAD_REQUEST, "Short name missing when creating site");
-		return;
-	}
-
-    // See if the shortName is available
-    var site = siteService.getSite(shortName);
-    if (site != null)
-    {
-        status.setCode(status.STATUS_INTERNAL_SERVER_ERROR, "error.duplicateShortName");
-        return;
-    }
-
-    var sitePreset = json.get("sitePreset");
-	if (shortName == null || shortName.length == 0)
-	{
-		status.setCode(status.STATUS_BAD_REQUEST, "Site preset missing when creating site");
-		return;
-	}
-	
-	var title = json.get("title");
-	var description = json.get("description");
-	
-	// Use the visibility flag before the isPublic flag
-	var visibility = siteService.PUBLIC_SITE;
-    if (json.has("visibility") == true)
-    {
-        visibility = json.get("visibility");
-    }
-    else if (json.has("isPublic") == true)
-    {
-       var isPublic = json.getBoolean("isPublic");
-       if (isPublic == true)
-       {
-          visibility = siteService.PUBLIC_SITE;
-       }
-       else
-       {
-          visibility = siteService.PRIVATE_SITE;
-       }
-    }
-	
-	// Create the site 
-	var site = siteService.createSite(sitePreset, shortName, title, description, visibility);
-	
-	// Put the created site into the model
-	model.site = site;
+   // Get the details of the site
+   if (json.has("shortName") == false || json.get("shortName").length == 0)
+   {
+      status.setCode(status.STATUS_BAD_REQUEST, "Short name missing when creating site");
+      return;
+   }
+   var shortName = json.get("shortName");
+   
+   // See if the shortName is available
+   var site = siteService.getSite(shortName);
+   if (site != null)
+   {
+      status.setCode(status.STATUS_INTERNAL_SERVER_ERROR, "error.duplicateShortName");
+      return;
+   }
+   
+   if (json.has("sitePreset") == false || json.get("sitePreset").length == 0)
+   {
+      status.setCode(status.STATUS_BAD_REQUEST, "Site preset missing when creating site");
+      return;
+   }
+   var sitePreset = json.get("sitePreset");
+   
+   var title = null;
+   if (json.has("title"))
+   {
+      title = json.get("title");
+   }
+      
+   var description = null;
+   if (json.has("description"))
+   {
+      description = json.get("description");
+   }
+   
+   // Use the visibility flag before the isPublic flag
+   var visibility = siteService.PUBLIC_SITE;
+   if (json.has("visibility"))
+   {
+      visibility = json.get("visibility");
+   }
+   else if (json.has("isPublic"))
+   {
+      var isPublic = json.getBoolean("isPublic");
+      if (isPublic == true)
+      {
+         visibility = siteService.PUBLIC_SITE;
+      }
+      else
+      {
+         visibility = siteService.PRIVATE_SITE;
+      }
+   }
+   
+   // Create the site 
+   var site = siteService.createSite(sitePreset, shortName, title, description, visibility);
+   
+   // Put the created site into the model
+   model.site = site;
 }
 
-main();	
+main();

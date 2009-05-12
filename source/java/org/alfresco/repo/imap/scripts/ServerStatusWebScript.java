b19cad46de6aef788f1a14d4bf3a2f0e05e2e710
Gavin Cornwell <gavin.cornwell@alfresco.com>
2009-05-12 13:41:08 +0000

    Initial cut of IMAP support (disabled by default, to enable move imap sample files into extension folder)
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@14279 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

---
+++ source/java/org/alfresco/repo/imap/scripts/ServerStatusWebScript.java
@@ -0,0 +1,30 @@
+package org.alfresco.repo.imap.scripts;
+
+import java.io.IOException;
+
+import org.alfresco.web.scripts.AbstractWebScript;
+import org.alfresco.web.scripts.WebScriptRequest;
+import org.alfresco.web.scripts.WebScriptResponse;
+
+public class ServerStatusWebScript extends AbstractWebScript
+{
+    private boolean imapServerEnabled;
+
+    public void execute(WebScriptRequest req, WebScriptResponse res) throws IOException
+    {
+        if (imapServerEnabled)
+        {
+            res.getWriter().write("enabled");
+        }
+        else
+        {
+            res.getWriter().write("disabled");
+        }
+    }
+
+    public void setImapServerEnabled(boolean imapServerEnabled)
+    {
+        this.imapServerEnabled = imapServerEnabled;
+    }
+
+}

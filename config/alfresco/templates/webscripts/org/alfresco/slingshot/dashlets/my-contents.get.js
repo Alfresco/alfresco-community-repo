80f83ebecdc9f36a07a731e8ad0f98298c36d286
Kevin Roast <kevin.roast@alfresco.com>
2009-06-11 09:22:31 +0000

    Fix up double-encoding issue Content I'm Editing dashlet.  - also clean up of template and optimization of data webscript queries  - fixes issue where any edited content would be shown as a Wiki item in the dashlet  - error handled fixed in dashlet web-tier javascript
     - missing file
    
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@14650 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- config/alfresco/templates/webscripts/org/alfresco/slingshot/dashlets/my-contents.get.js
+++ config/alfresco/templates/webscripts/org/alfresco/slingshot/dashlets/my-contents.get.js
@@ -6,49 +6,38 @@
 function getDraftBlogPostList()
 {
    var q = " +TYPE:\"{http://www.alfresco.org/model/content/1.0}content\"" +
-           " +PATH:\"/app:company_home/st:sites/*//*\" " +
-           " -ISNOTNULL:\"{http://www.alfresco.org/model/content/1.0}published\" " +
-           " +@cm\\:creator:" + person.properties.userName + 
-           " -TYPE:\"{http://www.alfresco.org/model/content/1.0}thumbnail\" " + 
-           " -TYPE:\"{http://www.alfresco.org/model/forum/1.0}post\" "+
-           " -TYPE:\"{http://www.alfresco.org/model/linksmodel/1.0}link\" "+
-           " -TYPE:\"{http://www.alfresco.org/model/calendar}calendarEvent\" " +
-           " -ASPECT:\"cm:versionable\" " +
-           " -ASPECT:\"cm:lockable\"";
-   q += " +(@\\{http\\://www.alfresco.org/model/content/1.0\\}content.mimetype:application/octet-stream OR";
-   q += "  @\\{http\\://www.alfresco.org/model/content/1.0\\}content.mimetype:text/html)"
-
-   // // get the data
-   nodes = search.luceneSearch(q,"cm:modified",false);
+           " +PATH:\"/app:company_home/st:sites/*/cm:blog/*\"" +
+           " -ISNOTNULL:\"{http://www.alfresco.org/model/content/1.0}published\"" +
+           " +@cm\\:creator:" + person.properties.userName;
+   
+   // get the data
+   nodes = search.luceneSearch(q,'cm:modified',false);
    // return nodes.length;
    return processResults(nodes, 3);
 }
 
 function getWikiPages()
 {
-   var q = " +TYPE:\"{http://www.alfresco.org/model/content/1.0}content\" " +
-           " +PATH:\"/app:company_home/st:sites/*//*\" " +
-           " +@cm\\:modifier:admin " + 
-           " +ASPECT:\"cm:titled\" " +
-           " -ASPECT:\"cm:lockable\" " +
-           " +ASPECT:\"cm:versionable\"";
+   var q = " +TYPE:\"{http://www.alfresco.org/model/content/1.0}content\"" +
+           " +PATH:\"/app:company_home/st:sites/*/cm:wiki/*\"" +
+           " +@cm\\:modifier:" + person.properties.userName;
 
    nodes = search.luceneSearch(q,'cm:modified',false);
 
-   return processResults(nodes,3);
+   return processResults(nodes, 3);
 }
 
 function getDiscussions()
 {
-   var q = " +TYPE:\"{http://www.alfresco.org/model/forum/1.0}post\" " +
-           " +PATH:\"/app:company_home/st:sites/*//*\" " +
-           " +@cm\\:creator:" + person.properties.userName +
-           " +ASPECT:\"cm:syndication\"";
+   var q = " +TYPE:\"{http://www.alfresco.org/model/forum/1.0}post\"" +
+           " +PATH:\"/app:company_home/st:sites/*/cm:discussions//*\"" +
+           " +@cm\\:creator:" + person.properties.userName;
 
    nodes = search.luceneSearch(q,'cm:modified',false);
 
-   return processResults(nodes,3);         
+   return processResults(nodes, 3);         
 }
+
 model.data = {};
 
 model.data.blogPosts = getDraftBlogPostList();

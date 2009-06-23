038bdbc1f612af677ece331d98df62279e878d28
Derek Hulley <derek.hulley@alfresco.com>
2009-06-23 15:21:35 +0000

    Removed duplicated SQL script and applied ${TRUE} to generic script for boolean values
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@14871 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- config/alfresco/dbscripts/upgrade/3.2/org.hibernate.dialect.Dialect/modify-AVM-mimetype.sql
+++ config/alfresco/dbscripts/upgrade/3.2/org.hibernate.dialect.Dialect/modify-AVM-mimetype.sql
@@ -9,16 +9,16 @@
 -- Please contact support@alfresco.com if you need assistance with the upgrade.
 --
 
-alter table avm_nodes modify mime_type varchar(100);
+ALTER TABLE avm_nodes MODIFY mime_type VARCHAR(100);
 
 --
 -- Record script finish
 --
-DELETE FROM alf_applied_patch WHERE id = 'patch.db-V3.2-Modify-AVM-mimetype';
+DELETE FROM alf_applied_patch WHERE id = 'patch.db-V3.2-Modify-AVM-MimeType';
 INSERT INTO alf_applied_patch
   (id, description, fixes_from_schema, fixes_to_schema, applied_to_schema, target_schema, applied_on_date, applied_to_server, was_executed, succeeded, report)
   VALUES
   (
-    'patch.db-V3.2-Modify-AVM-mimetype', 'Manually executed script upgrade V3.2 to modify AVM mimetype',
-     0, 2016, -1, 2017, null, 'UNKOWN', 1, 1, 'Script completed'
+    'patch.db-V3.2-Modify-AVM-MimeType', 'Manually executed script upgrade V3.2 to modify AVM mimetype',
+     0, 2016, -1, 2017, null, 'UNKOWN', ${TRUE}, ${TRUE}, 'Script completed'
    );

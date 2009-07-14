5185c894b790bb3d71106e82c7e5eb7448e7a49a
Roy Wetherall <roy.wetherall@alfresco.com>
2009-07-14 13:51:45 +0000

    RM Search Aspect Added   - Key properties are collasped onto a common aspect which is automatically maintained and applied to the record or record folder   - Added immediately period type
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@15177 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

---
+++ source/java/org/alfresco/repo/dictionary/types/period/Immediately.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2005-2007 Alfresco Software Limited.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+ * As a special exception to the terms and conditions of version 2.0 of 
+ * the GPL, you may redistribute this Program in connection with Free/Libre 
+ * and Open Source Software ("FLOSS") applications as described in Alfresco's 
+ * FLOSS exception.  You should have recieved a copy of the text describing 
+ * the FLOSS exception, and it is also available here: 
+ * http://www.alfresco.com/legal/licensing"
+ */
+package org.alfresco.repo.dictionary.types.period;
+
+import java.util.Date;
+
+import org.alfresco.service.namespace.QName;
+
+/**
+ * Immediate Period type "immediate"
+ * 
+ * @author Roy Wetherall
+ */
+public class Immediately extends AbstractPeriodProvider
+{
+    /**
+     * 
+     */
+    public static final String PERIOD_TYPE = "immediately";
+
+    /**
+     * Default constructor
+     */
+    public Immediately()
+    {
+
+    }
+
+    public String getDefaultExpression()
+    {
+        return null;
+    }
+
+    public ExpressionMutiplicity getExpressionMutiplicity()
+    {
+        return ExpressionMutiplicity.NONE;
+    }
+
+    public Date getNextDate(Date date, String expression)
+    {
+        Date result = null;
+        if (date == null)
+        {
+            result = new Date();
+        }
+        else
+        {
+            result = date;
+        }
+        return result;
+    }
+
+    public String getPeriodType()
+    {
+        return PERIOD_TYPE;
+    }
+
+    public QName getExpressionDataType()
+    {
+        return null;
+    }
+}

71fb7f707ba015efe5b2771015a24b40591047ae
Mike Hatfield <mike.hatfield@alfresco.com>
2009-08-13 08:50:09 +0000

    Fix to saved search DocLib filter regression
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@15720 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- config/alfresco/templates/webscripts/org/alfresco/slingshot/documentlibrary/filters.lib.js
+++ config/alfresco/templates/webscripts/org/alfresco/slingshot/documentlibrary/filters.lib.js
@@ -153,7 +153,7 @@ var Filters =
       }
 
       // Specialise by passed-in type
-      filterParams.query += " " + Filters.TYPE_MAP[parsedArgs.type] || "";
+      filterParams.query += " " + (Filters.TYPE_MAP[parsedArgs.type] || "");
 
       return filterParams;
    }

86d18590ac7261388eac8ce27bbf97938831bc86
Mike Hatfield <mike.hatfield@alfresco.com>
2009-08-19 10:37:13 +0000

    MOB-1260 - Electronic filing - Record Type
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@15811 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- config/alfresco/templates/webscripts/org/alfresco/repository/upload/upload.post.js
+++ config/alfresco/templates/webscripts/org/alfresco/repository/upload/upload.post.js
@@ -1,97 +1,104 @@
 function main()
 {
-	var filename = null;
-	var content = null;
-	var mimetype = null;
-	var siteId = null;
-	var containerId = null;
-	var thumbnailNames = null;
-	
-	// Upload specific
-	var uploadDirectory = null;
-	var title = "";
-	var overwrite = true; // If a filename clashes for a versionable file 
-	
-	// Update specific
-	var updateNodeRef = null;
-	var majorVersion = false;
-	var description = "";
-	
-	// Parse file attributes
-	for each (field in formdata.fields)
-	{
-	   switch (String(field.name).toLowerCase())
-	   {
-	      case "filedata":
-	         if (field.isFile)
-	         {
-	            filename = field.filename;
-	            content = field.content;
-	            mimetype = field.mimetype;
-	         }
-	         break;
-	      
-	      case "siteid":
-	         siteId = field.value;
-	         break;
-	         
-	      case "containerid":
-	         containerId = field.value;
-	         break;
-	      
-	      case "uploaddirectory":
-	         uploadDirectory = field.value;
-	         // Remove any leading "/" from the uploadDirectory
-	         if (uploadDirectory.substr(0, 1) == "/")
-	         {
-	            uploadDirectory = uploadDirectory.substr(1);
-	         }
-	         // Ensure uploadDirectory ends with "/" if not the root folder
-	         if ((uploadDirectory.length > 0) && (uploadDirectory.substring(uploadDirectory.length - 1) != "/"))
-	         {
-	            uploadDirectory = uploadDirectory + "/";
-	         }
-	         break;
-	
-	      case "updatenoderef":
-	         updateNodeRef = field.value;
-	         break;
-	
-	      case "filename":
-	         title = field.value;
-	         break;
-	
-	      case "description":
-	         description = field.value;
-	         break;
-	
-	      case "contenttype":
-	         contentType = field.value;
-	         break;
-	
-	      case "majorversion":
-	         majorVersion = field.value == "true";
-	         break;
-	
-	      case "overwrite":
-	         overwrite = field.value == "true";
-	         break;
-	      
-	      case "thumbnails":
-	         thumbnailNames = field.value;
-	         break;
-	   }
-	}
-	
-	// Ensure mandatory file attributes have been located
-	if (siteId === null || containerId === null || filename === null || content === null)
-	{
-	   status.code = 400;
-	   status.message = "Required parameters are missing";
-	   status.redirect = true;
-	   return;
-	}
-	
+   var filename = null,
+      content = null,
+      mimetype = null,
+      siteId = null,
+      containerId = null,
+      thumbnailNames = null,
+      i;
+
+   // Upload specific
+   var uploadDirectory = null,
+      title = "",
+      contentType = null,
+      aspects = [],
+      overwrite = true; // If a filename clashes for a versionable file 
+
+   // Update specific
+   var updateNodeRef = null,
+      majorVersion = false,
+      description = "";
+
+   // Parse file attributes
+   for each (field in formdata.fields)
+   {
+      switch (String(field.name).toLowerCase())
+      {
+         case "filedata":
+            if (field.isFile)
+            {
+               filename = field.filename;
+               content = field.content;
+               mimetype = field.mimetype;
+            }
+            break;
+
+         case "siteid":
+            siteId = field.value;
+            break;
+
+         case "containerid":
+            containerId = field.value;
+            break;
+
+         case "uploaddirectory":
+            uploadDirectory = field.value;
+            // Remove any leading "/" from the uploadDirectory
+            if (uploadDirectory.substr(0, 1) == "/")
+            {
+               uploadDirectory = uploadDirectory.substr(1);
+            }
+            // Ensure uploadDirectory ends with "/" if not the root folder
+            if ((uploadDirectory.length > 0) && (uploadDirectory.substring(uploadDirectory.length - 1) != "/"))
+            {
+               uploadDirectory = uploadDirectory + "/";
+            }
+            break;
+
+         case "updatenoderef":
+            updateNodeRef = field.value;
+            break;
+
+         case "filename":
+            title = field.value;
+            break;
+
+         case "description":
+            description = field.value;
+            break;
+
+         case "contenttype":
+            contentType = field.value;
+            break;
+
+         case "aspects":
+            aspects = field.value !== "-" ? field.value.split(",") : [];
+            break;
+
+         case "majorversion":
+            majorVersion = field.value == "true";
+            break;
+
+         case "overwrite":
+            overwrite = field.value == "true";
+            break;
+
+         case "thumbnails":
+            thumbnailNames = field.value;
+            break;
+      }
+   }
+
+   // Ensure mandatory file attributes have been located
+   if (siteId === null || containerId === null || filename === null || content === null)
+   {
+      status.code = 400;
+      status.message = "Required parameters are missing";
+      status.redirect = true;
+      return;
+   }
+
    var site = siteService.getSite(siteId);
    if (site === null)
    {
@@ -100,21 +107,21 @@ function main()
       status.redirect = true;
       return;
    }
-   
+
    // Upload mode, since uploadDirectory was used
    var container = site.getContainer(containerId);
    if (container === null)
    {
       container = site.createContainer(containerId);
    }
-   
+
    if (container === null)
    {
       status.code = 404;
       status.message = "Component container (" + containerId + ") not found.";
       status.redirect = true;
    }
-   
+
    if (updateNodeRef !== null && updateNodeRef != "" && (uploadDirectory === null || uploadDirectory == ""))
    {
       // Update existing file mode
@@ -127,29 +134,29 @@ function main()
          status.redirect = true;
          return;
       }
-      
+
       if (!workingCopy.hasAspect("cm:workingcopy"))
       {
-	      // Ensure the original file is versionable - may have been uploaded via different route
-	      if (!workingCopy.hasAspect("cm:versionable"))
-	      {
-		      // Ensure the file is versionable - but do not autoversion or create initial version yet
-		      var props = new Array(2);
-		      props["cm:autoVersion"] = false;
-		      props["cm:initialVersion"] = false;
-		      workingCopy.addAspect("cm:versionable", props);
-	      }
-	      
-	      if (workingCopy.versionHistory == null)
-	      {
-		      // Create the first version manually so we have 1.0 before checkout
-		      workingCopy.createVersion("", true);
-	      }
-      	
+         // Ensure the original file is versionable - may have been uploaded via different route
+         if (!workingCopy.hasAspect("cm:versionable"))
+         {
+            // Ensure the file is versionable - but do not autoversion or create initial version yet
+            var props = new Array(2);
+            props["cm:autoVersion"] = false;
+            props["cm:initialVersion"] = false;
+            workingCopy.addAspect("cm:versionable", props);
+         }
+
+         if (workingCopy.versionHistory == null)
+         {
+            // Create the first version manually so we have 1.0 before checkout
+            workingCopy.createVersion("", true);
+         }
+
          // It's not a working copy, do a check out to get the actual working copy
          workingCopy = workingCopy.checkout();
       }
-      
+
       // Update the working copy content
       workingCopy.properties.content.write(content);
       // Reset working copy mimetype and encoding
@@ -157,12 +164,12 @@ function main()
       workingCopy.properties.content.encoding = "UTF-8";
       // check it in again, with supplied version history note
       workingCopy = workingCopy.checkin(description, majorVersion);
-      
+
       model.document = workingCopy;
    }
    else if (uploadDirectory !== null && (updateNodeRef === null || updateNodeRef == ""))
    {
-   	// Upload file mode
+      // Upload file mode
       var destNode = container;
       if (uploadDirectory != "")
       {
@@ -175,7 +182,7 @@ function main()
          status.redirect = true;
          return;
       }
-		
+
       var existingFile = container.childByNamePath(uploadDirectory + filename);
       if (existingFile !== null)
       {
@@ -201,28 +208,40 @@ function main()
             filename = tmpFilename;
          }
       }
-		
+
       // save the new file (original or renamed file) as long as an overwrite hasn't been performed
       var newFile = destNode.createFile(filename);
-      newFile.properties.contentType = contentType;
+      if (contentType !== null)
+      {
+         newFile.specializeType(contentType);
+      }
       newFile.properties.content.write(content);
-      
+
       // Reapply mimetype as upload may have been via Flash - which always sends binary mimetype
       newFile.properties.content.guessMimetype(filename);
       newFile.properties.content.encoding = "UTF-8";      
       newFile.save();
-      
+
       // Ensure the file is versionable - but do not autoversion or create initial version
       var props = new Array(2);
       props["cm:autoVersion"] = false;
       props["cm:initialVersion"] = false;
       newFile.addAspect("cm:versionable", props);
-      
+
+      // Additional aspects?
+      if (aspects.length > 0)
+      {
+         for (i = 0; i < aspects.length; i++)
+         {
+            newFile.addAspect(aspects[i]);
+         }
+      }
+
       // Create thumbnail?
       if (thumbnailNames != null)
       {
          var thumbnails = thumbnailNames.split(",");
-         for (var i = 0; i < thumbnails.length; i++)
+         for (i = 0; i < thumbnails.length; i++)
          {
             var thumbnailName = thumbnails[i];
             if (thumbnailName != "" && thumbnailService.isThumbnailNameRegistered(thumbnailName))
@@ -231,7 +250,7 @@ function main()
             }
          }
       }
-      
+
       // Extract metadata - via repo action for now
       // This should use the MetadataExtracter API to fetch properties, allowing
       // for possible failures.
@@ -241,7 +260,7 @@ function main()
          // Call using readOnly = false, newTransaction = false
          emAction.execute(newFile, false, false);
       }
-      
+
       // Set the title if none set during meta-data extract
       newFile.reset();
       if (newFile.properties.title == null)
@@ -249,7 +268,7 @@ function main()
         newFile.properties.title = title;
         newFile.save();
       }
-      
+
       model.document = newFile;
    }
    else

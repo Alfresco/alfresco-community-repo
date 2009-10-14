2ed2c7eaccf3c1b45ed3e3342c79cf8afa8cb077
Erik Winlof <erik.winlof@alfresco.com>
2009-10-14 15:16:21 +0000

    Merged V3.2 to HEAD    16716: Exception when Quota is exceeded is now clearly displayed, fixed ETHREEOH-1676 & ETHREEOH-1802    16864: Merged V3.1 to V3.2       15025: ETHREEOH-2274 Replacing content in DocLib in Alfresco Share using Flash upload changes the content-type to Octet Stream
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@16929 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- config/alfresco/templates/webscripts/org/alfresco/repository/upload/upload.post.js
+++ config/alfresco/templates/webscripts/org/alfresco/repository/upload/upload.post.js
@@ -1,280 +1,306 @@
 function main()
 {
-   var filename = null,
-      content = null,
-      mimetype = null,
-      siteId = null,
-      containerId = null,
-      thumbnailNames = null,
-      i;
-
-   // Upload specific
-   var uploadDirectory = null,
-      title = "",
-      contentType = null,
-      aspects = [],
-      overwrite = true; // If a filename clashes for a versionable file 
-
-   // Update specific
-   var updateNodeRef = null,
-      majorVersion = false,
-      description = "";
-
-   // Parse file attributes
-   for each (field in formdata.fields)
+   try
    {
-      switch (String(field.name).toLowerCase())
+      var filename = null,
+         content = null,
+         mimetype = null,
+         siteId = null,
+         containerId = null,
+         thumbnailNames = null,
+         i;
+
+      // Upload specific
+      var uploadDirectory = null,
+         title = "",
+         contentType = null,
+         aspects = [],
+         overwrite = true; // If a filename clashes for a versionable file
+
+      // Update specific
+      var updateNodeRef = null,
+         majorVersion = false,
+         description = "";
+
+      // Parse file attributes
+      for each (field in formdata.fields)
       {
-         case "filedata":
-            if (field.isFile)
-            {
-               filename = field.filename;
-               content = field.content;
-               mimetype = field.mimetype;
-            }
-            break;
+         switch (String(field.name).toLowerCase())
+         {
+            case "filedata":
+               if (field.isFile)
+               {
+                  filename = field.filename;
+                  content = field.content;
+                  mimetype = field.mimetype;
+               }
+               break;
 
-         case "siteid":
-            siteId = field.value;
-            break;
+            case "siteid":
+               siteId = field.value;
+               break;
 
-         case "containerid":
-            containerId = field.value;
-            break;
+            case "containerid":
+               containerId = field.value;
+               break;
 
-         case "uploaddirectory":
-            uploadDirectory = field.value;
-            // Remove any leading "/" from the uploadDirectory
-            if (uploadDirectory.substr(0, 1) == "/")
-            {
-               uploadDirectory = uploadDirectory.substr(1);
-            }
-            // Ensure uploadDirectory ends with "/" if not the root folder
-            if ((uploadDirectory.length > 0) && (uploadDirectory.substring(uploadDirectory.length - 1) != "/"))
-            {
-               uploadDirectory = uploadDirectory + "/";
-            }
-            break;
+            case "uploaddirectory":
+               uploadDirectory = field.value;
+               // Remove any leading "/" from the uploadDirectory
+               if (uploadDirectory.substr(0, 1) == "/")
+               {
+                  uploadDirectory = uploadDirectory.substr(1);
+               }
+               // Ensure uploadDirectory ends with "/" if not the root folder
+               if ((uploadDirectory.length > 0) && (uploadDirectory.substring(uploadDirectory.length - 1) != "/"))
+               {
+                  uploadDirectory = uploadDirectory + "/";
+               }
+               break;
 
-         case "updatenoderef":
-            updateNodeRef = field.value;
-            break;
+            case "updatenoderef":
+               updateNodeRef = field.value;
+               break;
 
-         case "filename":
-            title = field.value;
-            break;
+            case "filename":
+               title = field.value;
+               break;
 
-         case "description":
-            description = field.value;
-            break;
+            case "description":
+               description = field.value;
+               break;
 
-         case "contenttype":
-            contentType = field.value;
-            break;
+            case "contenttype":
+               contentType = field.value;
+               break;
 
-         case "aspects":
-            aspects = field.value != "-" ? field.value.split(",") : [];
-            break;
+            case "aspects":
+               aspects = field.value != "-" ? field.value.split(",") : [];
+               break;
 
-         case "majorversion":
-            majorVersion = field.value == "true";
-            break;
+            case "majorversion":
+               majorVersion = field.value == "true";
+               break;
 
-         case "overwrite":
-            overwrite = field.value == "true";
-            break;
+            case "overwrite":
+               overwrite = field.value == "true";
+               break;
 
-         case "thumbnails":
-            thumbnailNames = field.value;
-            break;
+            case "thumbnails":
+               thumbnailNames = field.value;
+               break;
+         }
       }
-   }
 
-   // Ensure mandatory file attributes have been located
-   if (siteId === null || containerId === null || filename === null || content === null)
-   {
-      status.code = 400;
-      status.message = "Required parameters are missing";
-      status.redirect = true;
-      return;
-   }
-
-   var site = siteService.getSite(siteId);
-   if (site === null)
-   {
-      status.code = 404;
-      status.message = "Site (" + siteId + ") not found.";
-      status.redirect = true;
-      return;
-   }
-
-   // Upload mode, since uploadDirectory was used
-   var container = site.getContainer(containerId);
-   if (container === null)
-   {
-      container = site.createContainer(containerId);
-   }
-
-   if (container === null)
-   {
-      status.code = 404;
-      status.message = "Component container (" + containerId + ") not found.";
-      status.redirect = true;
-   }
-
-   if (updateNodeRef !== null && updateNodeRef != "" && (uploadDirectory === null || uploadDirectory == ""))
-   {
-      // Update existing file mode
-      var workingCopy = search.findNode(updateNodeRef);
-      if (workingCopy.isLocked)
+      // Ensure mandatory file attributes have been located
+      if (siteId === null || containerId === null || filename === null || content === null)
       {
-         // We cannot update a locked document
-         status.code = 404;
-         status.message = "Cannot update locked document '" + updateNodeRef + "', supply a reference to its working copy instead.";
+         status.code = 400;
+         status.message = "Required parameters are missing";
          status.redirect = true;
          return;
       }
 
-      if (!workingCopy.hasAspect("cm:workingcopy"))
-      {
-         // Ensure the original file is versionable - may have been uploaded via different route
-         if (!workingCopy.hasAspect("cm:versionable"))
-         {
-            // Ensure the file is versionable - but do not autoversion or create initial version yet
-            var props = new Array(2);
-            props["cm:autoVersion"] = false;
-            props["cm:initialVersion"] = false;
-            workingCopy.addAspect("cm:versionable", props);
-         }
-
-         if (workingCopy.versionHistory == null)
-         {
-            // Create the first version manually so we have 1.0 before checkout
-            workingCopy.createVersion("", true);
-         }
-
-         // It's not a working copy, do a check out to get the actual working copy
-         workingCopy = workingCopy.checkout();
-      }
-
-      // Update the working copy content
-      workingCopy.properties.content.write(content);
-      // Reset working copy mimetype and encoding
-      workingCopy.properties.content.guessMimetype(filename);
-      workingCopy.properties.content.encoding = "UTF-8";
-      // check it in again, with supplied version history note
-      workingCopy = workingCopy.checkin(description, majorVersion);
-
-      model.document = workingCopy;
-   }
-   else if (uploadDirectory !== null && (updateNodeRef === null || updateNodeRef == ""))
-   {
-      // Upload file mode
-      var destNode = container;
-      if (uploadDirectory != "")
-      {
-         destNode = container.childByNamePath(uploadDirectory);
-      }
-      if (destNode === null)
+      var site = siteService.getSite(siteId);
+      if (site === null)
       {
          status.code = 404;
-         status.message = "Cannot upload file since uploadDirectory '" + uploadDirectory + "' does not exist.";
+         status.message = "Site (" + siteId + ") not found.";
          status.redirect = true;
          return;
       }
 
-      var existingFile = container.childByNamePath(uploadDirectory + filename);
-      if (existingFile !== null)
+      // Upload mode, since uploadDirectory was used
+      var container = site.getContainer(containerId);
+      if (container === null)
       {
-         // File already exists, decide what to do
-         if (existingFile.hasAspect("cm:versionable") && overwrite)
+         container = site.createContainer(containerId);
+      }
+
+      if (container === null)
+      {
+         status.code = 404;
+         status.message = "Component container (" + containerId + ") not found.";
+         status.redirect = true;
+      }
+
+      if (updateNodeRef !== null && updateNodeRef != "" && (uploadDirectory === null || uploadDirectory == ""))
+      {
+         // Update existing file mode
+         var workingCopy = search.findNode(updateNodeRef);
+         if (workingCopy.isLocked)
          {
-            // Upload component was configured to overwrite files if name clashes
-            existingFile.properties.content.write(content);
-            model.document = existingFile;
+            // We cannot update a locked document
+            status.code = 404;
+            status.message = "Cannot update locked document '" + updateNodeRef + "', supply a reference to its working copy instead.";
+            status.redirect = true;
             return;
          }
-         else
+
+         if (!workingCopy.hasAspect("cm:workingcopy"))
          {
-            // Upload component was configured to find a new unique name for clashing filenames
-            var suffix = 1;
-            var tmpFilename;
-            while (existingFile !== null)
-            {                               
-               tmpFilename = filename.substring(0, filename.lastIndexOf(".")) + "-" + suffix + filename.substring(filename.lastIndexOf("."));
-               existingFile = container.childByNamePath(uploadDirectory + tmpFilename);
-               suffix++;
-            }
-            filename = tmpFilename;
-         }
-      }
-
-      // save the new file (original or renamed file) as long as an overwrite hasn't been performed
-      var newFile = destNode.createFile(filename);
-      if (contentType !== null)
-      {
-         newFile.specializeType(contentType);
-      }
-      newFile.properties.content.write(content);
-
-      // Reapply mimetype as upload may have been via Flash - which always sends binary mimetype
-      newFile.properties.content.guessMimetype(filename);
-      newFile.properties.content.encoding = "UTF-8";      
-      newFile.save();
-
-      // Ensure the file is versionable - but do not autoversion or create initial version
-      var props = new Array(2);
-      props["cm:autoVersion"] = false;
-      props["cm:initialVersion"] = false;
-      newFile.addAspect("cm:versionable", props);
-
-      // Additional aspects?
-      if (aspects.length > 0)
-      {
-         for (i = 0; i < aspects.length; i++)
-         {
-            newFile.addAspect(aspects[i]);
-         }
-      }
-
-      // Create thumbnail?
-      if (thumbnailNames != null)
-      {
-         var thumbnails = thumbnailNames.split(",");
-         for (i = 0; i < thumbnails.length; i++)
-         {
-            var thumbnailName = thumbnails[i];
-            if (thumbnailName != "" && thumbnailService.isThumbnailNameRegistered(thumbnailName))
+            // Ensure the original file is versionable - may have been uploaded via different route
+            if (!workingCopy.hasAspect("cm:versionable"))
             {
-               newFile.createThumbnail(thumbnailName, true);
+               // Ensure the file is versionable - but do not autoversion or create initial version yet
+               var props = new Array(2);
+               props["cm:autoVersion"] = false;
+               props["cm:initialVersion"] = false;
+               workingCopy.addAspect("cm:versionable", props);
+            }
+
+            if (workingCopy.versionHistory == null)
+            {
+               // Create the first version manually so we have 1.0 before checkout
+               workingCopy.createVersion("", true);
+            }
+
+            // It's not a working copy, do a check out to get the actual working copy
+            workingCopy = workingCopy.checkout();
+         }
+
+         // Update the working copy content
+         workingCopy.properties.content.write(content);
+         // Reset working copy mimetype and encoding
+         workingCopy.properties.content.guessMimetype(filename);
+         workingCopy.properties.content.encoding = "UTF-8";
+         // check it in again, with supplied version history note
+         workingCopy = workingCopy.checkin(description, majorVersion);
+
+         model.document = workingCopy;
+      }
+      else if (uploadDirectory !== null && (updateNodeRef === null || updateNodeRef == ""))
+      {
+         // Upload file mode
+         var destNode = container;
+         if (uploadDirectory != "")
+         {
+            destNode = container.childByNamePath(uploadDirectory);
+         }
+         if (destNode === null)
+         {
+            status.code = 404;
+            status.message = "Cannot upload file since uploadDirectory '" + uploadDirectory + "' does not exist.";
+            status.redirect = true;
+            return;
+         }
+
+         var existingFile = container.childByNamePath(uploadDirectory + filename);
+         if (existingFile !== null)
+         {
+            // File already exists, decide what to do
+            if (existingFile.hasAspect("cm:versionable") && overwrite)
+            {
+               // Upload component was configured to overwrite files if name clashes
+               existingFile.properties.content.write(content);
+               model.document = existingFile;
+
+               // Upload component was configured to overwrite files if name clashes
+               existingFile.properties.content.write(content);
+
+               // Reapply mimetype as upload may have been via Flash - which always sends binary mimetype
+               existingFile.properties.content.guessMimetype(filename);
+               existingFile.properties.content.encoding = "UTF-8";
+               existingFile.save();
+
+               model.document = existingFile;
+               return;
+            }
+            else
+            {
+               // Upload component was configured to find a new unique name for clashing filenames
+               var suffix = 1;
+               var tmpFilename;
+               while (existingFile !== null)
+               {
+                  tmpFilename = filename.substring(0, filename.lastIndexOf(".")) + "-" + suffix + filename.substring(filename.lastIndexOf("."));
+                  existingFile = container.childByNamePath(uploadDirectory + tmpFilename);
+                  suffix++;
+               }
+               filename = tmpFilename;
             }
          }
-      }
 
-      // Extract metadata - via repo action for now
-      // This should use the MetadataExtracter API to fetch properties, allowing
-      // for possible failures.
-      var emAction = actions.create("extract-metadata");
-      if (emAction != null)
+         // save the new file (original or renamed file) as long as an overwrite hasn't been performed
+         var newFile = destNode.createFile(filename);
+         if (contentType !== null)
+         {
+            newFile.specializeType(contentType);
+         }
+         newFile.properties.content.write(content);
+
+         // Reapply mimetype as upload may have been via Flash - which always sends binary mimetype
+         newFile.properties.content.guessMimetype(filename);
+         newFile.properties.content.encoding = "UTF-8";
+         newFile.save();
+
+         // Ensure the file is versionable - but do not autoversion or create initial version
+         var props = new Array(2);
+         props["cm:autoVersion"] = false;
+         props["cm:initialVersion"] = false;
+         newFile.addAspect("cm:versionable", props);
+
+         // Additional aspects?
+         if (aspects.length > 0)
+         {
+            for (i = 0; i < aspects.length; i++)
+            {
+               newFile.addAspect(aspects[i]);
+            }
+         }
+
+         // Create thumbnail?
+         if (thumbnailNames != null)
+         {
+            var thumbnails = thumbnailNames.split(",");
+            for (i = 0; i < thumbnails.length; i++)
+            {
+               var thumbnailName = thumbnails[i];
+               if (thumbnailName != "" && thumbnailService.isThumbnailNameRegistered(thumbnailName))
+               {
+                  newFile.createThumbnail(thumbnailName, true);
+               }
+            }
+         }
+
+         // Extract metadata - via repo action for now
+         // This should use the MetadataExtracter API to fetch properties, allowing
+         // for possible failures.
+         var emAction = actions.create("extract-metadata");
+         if (emAction != null)
+         {
+            // Call using readOnly = false, newTransaction = false
+            emAction.execute(newFile, false, false);
+         }
+
+         // Set the title if none set during meta-data extract
+         newFile.reset();
+         if (newFile.properties.title == null)
+         {
+            newFile.properties.title = title;
+            newFile.save();
+         }
+
+         model.document = newFile;
+      }
+      else
       {
-         // Call using readOnly = false, newTransaction = false
-         emAction.execute(newFile, false, false);
+         status.code = 404;
+         status.message = "Illegal arguments: updateNodeRef OR uploadDirectory must be provided (not both)";
+         status.redirect = true;
+         return;
       }
-
-      // Set the title if none set during meta-data extract
-      newFile.reset();
-      if (newFile.properties.title == null)
-      {
-        newFile.properties.title = title;
-        newFile.save();
-      }
-
-      model.document = newFile;
    }
-   else
+   catch (e)
    {
-      status.code = 404;
-      status.message = "Illegal arguments: updateNodeRef OR uploadDirectory must be provided (not both)";
+      var x = e;
+      status.code = 500;
+      status.message = "Unexpected error occured during upload of new content.";
+      if(x.message && x.message.indexOf("org.alfresco.service.cmr.usage.ContentQuotaException") == 0)
+      {
+         status.code = 413;
+         status.message = x.message;
+      }
       status.redirect = true;
       return;
    }
--- config/alfresco/templates/webscripts/org/alfresco/slingshot/profile/uploadavatar.post.js
+++ config/alfresco/templates/webscripts/org/alfresco/slingshot/profile/uploadavatar.post.js
@@ -8,82 +8,99 @@
 
 function main()
 {
-   var filename = null;
-   var content = null;
-   var username = null;
-   
-   // locate file attributes
-   for each (field in formdata.fields)
+   try
    {
-      if (field.name == "filedata" && field.isFile)
+
+      var filename = null;
+      var content = null;
+      var username = null;
+
+      // locate file attributes
+      for each (field in formdata.fields)
       {
-         filename = field.filename;
-         content = field.content;
+         if (field.name == "filedata" && field.isFile)
+         {
+            filename = field.filename;
+            content = field.content;
+         }
+         else if (field.name == "username")
+         {
+            username = field.value;
+         }
       }
-      else if (field.name == "username")
+
+      // ensure all mandatory attributes have been located
+      if (filename == undefined || content == undefined)
       {
-         username = field.value;
+         status.code = 400;
+         status.message = "Uploaded file cannot be located in request";
+         status.redirect = true;
+         return;
       }
+      if (username == null || username.length == 0)
+      {
+         status.code = 500;
+         status.message = "Username parameter not supplied.";
+         status.redirect = true;
+         return;
+      }
+
+      var user = people.getPerson(username);
+      // ensure we found a valid user and that it is the current user or we are an admin
+      if (user == null ||
+          (people.isAdmin(person) == false && new String(username).toLowerCase != new String(user.properties.userName).toLowerCase))
+      {
+         status.code = 500;
+         status.message = "Failed to locate valid user to modify.";
+         status.redirect = true;
+         return;
+      }
+
+      // ensure cm:person has 'cm:preferences' aspect applied - as we want to add the avatar as
+      // the child node of the 'cm:preferenceImage' association
+      if (!user.hasAspect("cm:preferences"))
+      {
+         user.addAspect("cm:preferences");
+      }
+
+      // remove old image child node if we already have one
+      var assocs = user.childAssocs["cm:preferenceImage"];
+      if (assocs != null && assocs.length == 1)
+      {
+         assocs[0].remove();
+      }
+
+      // create the new image node
+      var image = user.createNode(filename, "cm:content", "cm:preferenceImage");
+      image.properties.content.write(content);
+      image.properties.content.guessMimetype(filename);
+      image.properties.content.encoding = "UTF-8";
+      image.save();
+
+      // wire up 'cm:avatar' target association - backward compatible with JSF web-client avatar
+      assocs = user.associations["cm:avatar"];
+      if (assocs != null && assocs.length == 1)
+      {
+         user.removeAssociation(assocs[0], "cm:avatar");
+      }
+      user.createAssociation(image, "cm:avatar");
+
+      // save ref to be returned
+      model.image = image;
    }
-   
-   // ensure all mandatory attributes have been located
-   if (filename == undefined || content == undefined)
+   catch (e)
    {
-      status.code = 400;
-      status.message = "Uploaded file cannot be located in request";
-      status.redirect = true;
-      return;
-   }
-   if (username == null || username.length == 0)
-   {
+      var x = e;
       status.code = 500;
-      status.message = "Username parameter not supplied.";
+      status.message = "Unexpected error occured during upload of new content.";
+      if(x.message && x.message.indexOf("org.alfresco.service.cmr.usage.ContentQuotaException") == 0)
+      {
+         status.code = 413;
+         status.message = x.message;
+      }
       status.redirect = true;
       return;
    }
-   
-   var user = people.getPerson(username);
-   // ensure we found a valid user and that it is the current user or we are an admin
-   if (user == null ||
-       (people.isAdmin(person) == false && new String(username).toLowerCase != new String(user.properties.userName).toLowerCase))
-   {
-      status.code = 500;
-      status.message = "Failed to locate valid user to modify.";
-      status.redirect = true;
-      return;
-   }
-   
-   // ensure cm:person has 'cm:preferences' aspect applied - as we want to add the avatar as
-   // the child node of the 'cm:preferenceImage' association
-   if (!user.hasAspect("cm:preferences"))
-   {
-      user.addAspect("cm:preferences");
-   }
-   
-   // remove old image child node if we already have one
-   var assocs = user.childAssocs["cm:preferenceImage"];
-   if (assocs != null && assocs.length == 1)
-   {
-      assocs[0].remove();
-   }
-   
-   // create the new image node
-   var image = user.createNode(filename, "cm:content", "cm:preferenceImage");
-   image.properties.content.write(content);
-   image.properties.content.guessMimetype(filename);
-   image.properties.content.encoding = "UTF-8";
-   image.save();
-   
-   // wire up 'cm:avatar' target association - backward compatible with JSF web-client avatar
-   assocs = user.associations["cm:avatar"];
-   if (assocs != null && assocs.length == 1)
-   {
-      user.removeAssociation(assocs[0], "cm:avatar");
-   }
-   user.createAssociation(image, "cm:avatar");
-   
-   // save ref to be returned
-   model.image = image;
 }
 
 main();

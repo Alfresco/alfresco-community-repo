31e459ff5e9564293995ec34f3aba28898883152
Mark Rogers <mark.rogers@alfresco.com>
2010-02-26 12:21:24 +0000

    added a package-info for the transfer service.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@18882 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

---
+++ source/java/org/alfresco/service/cmr/transfer/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Provides the public interface for the transfer service which can be used to 
+ * transfer nodes from one repository to another.
+ * @since 3.3
+ */
+package org.alfresco.service.cmr.transfer;

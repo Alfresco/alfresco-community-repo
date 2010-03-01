43e93f3c1419e84be4061b4c346ccd38f09d3b9e
Paul Holmes-Higgin <paulhh@alfresco.com>
2010-03-01 22:09:17 +0000

    Updated header to LGPL
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@18926 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/java/org/alfresco/filesys/avm/StoreType.java
+++ source/java/org/alfresco/filesys/avm/StoreType.java
@@ -1,18 +1,20 @@
 /*
- * Copyright (C) 2005-2006 Alfresco, Inc.
+ * Copyright (C) 2005-2010 Alfresco Software Limited.
  *
- * Licensed under the Mozilla Public License version 1.1 
- * with a permitted attribution clause. You may obtain a
- * copy of the License at
+ * This file is part of Alfresco
  *
- *   http://www.alfresco.org/legal/license.txt
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific
- * language governing permissions and limitations under the
- * License.
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.alfresco.filesys.avm;
 
--- source/java/org/alfresco/filesys/debug/FileServerDebugInterface.java
+++ source/java/org/alfresco/filesys/debug/FileServerDebugInterface.java
@@ -1,9 +1,22 @@
 package org.alfresco.filesys.debug;
 
 /*
- * FileServerDebugInterface.java
+ * Copyright (C) 2007-2010 Alfresco Software Limited.
  *
- * Copyright (c) 2007 Starlasoft. All rights reserved.
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
  */
 
 import org.springframework.extensions.config.ConfigElement;

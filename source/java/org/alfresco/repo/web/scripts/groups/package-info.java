5245d38b610ea00d22358df7cfba1b776ffd621b
Mark Rogers <mark.rogers@alfresco.com>
2010-03-09 16:26:47 +0000

    TransferService:    No-Op implementation of script transfer service.    TransferService interface rework for createTransferTarget
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@19163 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

---
+++ source/java/org/alfresco/repo/web/scripts/groups/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Provides a unit tests for the web scripts of the REST bindings of the groups service.
+ * @since 3.1
+ */
+package org.alfresco.repo.web.scripts.groups;
---
+++ source/java/org/alfresco/repo/web/scripts/transfer/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Provides the implementation of the transfer service web scripts.   Which are used to receive transfers from other 
+ * instances of Alfresco.
+ * <p>
+ * The code in this package should be considered private to Alfresco and is liable to 
+ * change with each version of Alfresco. 
+ * <p>
+ * @since 3.3
+ */
+package org.alfresco.repo.web.scripts.transfer;

4843d27b7b99fd909100280e1650a4165afe0582
Andrew Hind <andy.hind@alfresco.com>
2010-07-05 20:18:08 +0000

    Tidy up DataModel project split - move related tests that needed their own context - should improve build speed a few seconds
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@20951 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/java/org/alfresco/util/ApplicationContextHelper.java
+++ source/java/org/alfresco/util/ApplicationContextHelper.java
@@ -18,10 +18,6 @@
  */
 package org.alfresco.util;
 
-import java.util.Arrays;
-
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -31,13 +27,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * 
  * @author Derek Hulley
  */
-public class ApplicationContextHelper
-{
-    private static ClassPathXmlApplicationContext instance;
-    private static String[] usedConfiguration;
-    private static boolean useLazyLoading = false;
-    private static boolean noAutoStart = false;
-    
+public class ApplicationContextHelper extends BaseApplicationContextHelper
+{   
     /** location of required configuration files */
     public static final String[] CONFIG_LOCATIONS = new String[] { "classpath:alfresco/application-context.xml" };
     
@@ -52,99 +43,10 @@ public class ApplicationContextHelper
      */
     public synchronized static ApplicationContext getApplicationContext()
     {
-        return getApplicationContext(CONFIG_LOCATIONS);
+        return BaseApplicationContextHelper.getApplicationContext(CONFIG_LOCATIONS);
     }
     
-    /**
-     * Provides a static, single instance of the application context.  This method can be
-     * called repeatedly.
-     * <p/>
-     * If the configuration requested differs from one used previously, then the previously-created
-     * context is shut down.
-     * 
-     * @return Returns an application context for the given configuration
-     */
-    public synchronized static ApplicationContext getApplicationContext(String[] configLocations)
-    {
-        if (configLocations == null)
-        {
-            throw new IllegalArgumentException("configLocations argument is mandatory.");
-        }
-        if (usedConfiguration != null && Arrays.deepEquals(configLocations, usedConfiguration))
-        {
-            // The configuration was used to create the current context
-            return instance;
-        }
-        // The config has changed so close the current context (if any)
-        closeApplicationContext();
-       
-        if(useLazyLoading || noAutoStart) {
-           instance = new VariableFeatureClassPathXmlApplicationContext(configLocations); 
-        } else {
-           instance = new ClassPathXmlApplicationContext(configLocations);
-        }
-                    
-        usedConfiguration = configLocations;
-        
-        return instance;
-    }
-    
-    /**
-     * Closes and releases the application context.  On the next call to
-     * {@link #getApplicationContext()}, a new context will be given.
-     */
-    public static synchronized void closeApplicationContext()
-    {
-        if (instance == null)
-        {
-            // Nothing to do
-            return;
-        }
-        instance.close();
-        instance = null;
-        usedConfiguration = null;
-    }
-    
-    /**
-     * Should the Spring beans be initilised in a lazy manner, or
-     *  all in one go?
-     * Normally lazy loading/intialising shouldn't be used when
-     *  running with the full context, but it may be appropriate
-     *  to reduce startup times when using a small, cut down context.
-     */
-    public static void setUseLazyLoading(boolean lazyLoading) {
-       useLazyLoading = lazyLoading;
-    }
-    /**
-     * Will the Spring beans be initilised in a lazy manner, or
-     *  all in one go? The default it to load everything in one
-     *  go, as spring normally does.
-     */
-    public static boolean isUsingLazyLoading() {
-       return useLazyLoading;
-    }
-    
-    /**
-     * Should the autoStart=true property on subsystems
-     *  be honoured, or should this property be ignored
-     *  and the auto start prevented?
-     * Normally we will use the spring configuration to
-     *  decide what to start, but when running tests,
-     *  you can use this to prevent the auto start.
-     */
-    public static void setNoAutoStart(boolean noAutoStart) {
-       ApplicationContextHelper.noAutoStart = noAutoStart;
-    }
-    /**
-     * Will Subsystems with the autoStart=true property set
-     *  on them be allowed to auto start? The default is to
-     *  honour the spring configuration and allow them to,
-     *  but they can be prevented if required.
-     */
-    public static boolean isNoAutoStart() {
-       return noAutoStart;
-    }
-    
+   
     public static void main(String ... args)
     {
         ClassPathXmlApplicationContext ctx = (ClassPathXmlApplicationContext) getApplicationContext();
@@ -153,28 +55,5 @@ public class ApplicationContextHelper
             try { ctx.wait(10000L); } catch (Throwable e) {}
         }
         ctx.close();
-    }
-    
-   
-    /**
-     * A wrapper around {@link ClassPathXmlApplicationContext} which
-     *  allows us to enable lazy loading or prevent Subsystem 
-     *  autostart as requested.
-     */
-    protected static class VariableFeatureClassPathXmlApplicationContext extends ClassPathXmlApplicationContext {
-       protected VariableFeatureClassPathXmlApplicationContext(String[] configLocations) throws BeansException {
-          super(configLocations);
-       }
-       
-       protected void initBeanDefinitionReader(XmlBeanDefinitionReader reader) {
-          super.initBeanDefinitionReader(reader);
-          
-          if(useLazyLoading) {
-             LazyClassPathXmlApplicationContext.postInitBeanDefinitionReader(reader);
-          }
-          if(noAutoStart) {
-             NoAutoStartClassPathXmlApplicationContext.postInitBeanDefinitionReader(reader);
-          }
-       }
     }
 }

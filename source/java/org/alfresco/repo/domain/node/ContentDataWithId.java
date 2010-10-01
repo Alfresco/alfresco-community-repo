d82de33326ea309bb942195d058bf925b0661faa
Derek Hulley <derek.hulley@alfresco.com>
2010-10-01 13:01:29 +0000

    Better fix for ALF-4919: Content Property format has changed  - Append ID to previously-informative 'info' URL in ContentDataWithId  - Add proper conversion in DefaultTypeConverter
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@22810 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/java/org/alfresco/repo/domain/node/ContentDataWithId.java
+++ source/java/org/alfresco/repo/domain/node/ContentDataWithId.java
@@ -34,12 +34,20 @@ import org.alfresco.service.cmr.repository.ContentData;
  */
 public class ContentDataWithId extends ContentData
 {
+    private static final long serialVersionUID = -5305648398812370806L;
+
     private final Long id;
 
     public ContentDataWithId(ContentData contentData, Long id)
     {
         super(contentData);
         this.id = id;
+    }
+
+    @Override
+    public String toString()
+    {
+        return getInfoUrl() + "|id=" + (id == null ? "" : id.toString());
     }
 
     public Long getId()

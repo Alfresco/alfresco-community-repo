2be73ece54b5f8eecceeca532cb819616756b230
Derek Hulley <derek.hulley@alfresco.com>
2011-05-30 20:21:29 +0000

    Merged DEV/SWIFT to HEAD    25927: (RECORD ONLY) Initial work on a Data List spreadsheet exporter. Needs more work in another iDay, but the basics are in place    25938: (RECORD ONLY) Move the Data List Export Webscript to a dev branch for now    25944: Added copyright notices           Enhancements to SOLR remote api client library           - get node text content transform status and exception    25945: Removed superfluous file    25952: ALF-7339: SOLR 020 Index track and build from SOLR           - WIP - tracking transactions (accounting for holes), Adding and removing leaf docs with ID only, tracker status reporting via admin update handler    25953: ALF-7339: SOLR 020 Index track and build from SOLR           - WIP - compile fix    25976: Query related build fixes (mixture of collateral damage from SOLR and OpenCMIS)
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@28117 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/java/org/alfresco/repo/domain/solr/ibatis/SOLRDAOImpl.java
+++ source/java/org/alfresco/repo/domain/solr/ibatis/SOLRDAOImpl.java
@@ -1,8 +1,25 @@
+/*
+ * Copyright (C) 2005-2011 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.alfresco.repo.domain.solr.ibatis;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -22,7 +39,6 @@ import org.alfresco.repo.domain.solr.SOLRDAO;
 import org.alfresco.repo.domain.solr.SOLRTransactionParameters;
 import org.alfresco.repo.domain.solr.Transaction;
 import org.alfresco.service.cmr.dictionary.DictionaryService;
-import org.alfresco.service.cmr.repository.AssociationRef;
 import org.alfresco.service.cmr.repository.ChildAssociationRef;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.Path;

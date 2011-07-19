aea1bf678d462f5a64e2885ed2a8a0d0268e8cd1
Steven Glover <steven.glover@alfresco.com>
2011-07-19 11:34:36 +0000

    More missing files for ALF-8791: "RSOLR 036: Update IndexChecker to support SOLR"
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@29181 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

---
+++ source/java/org/alfresco/encryption/EncryptorTest.java
@@ -0,0 +1,72 @@
+package org.alfresco.encryption;
+
+import java.io.Serializable;
+import java.security.AlgorithmParameters;
+
+import junit.framework.TestCase;
+
+import org.alfresco.encryption.DefaultEncryptor;
+import org.alfresco.util.Pair;
+import org.bouncycastle.util.Arrays;
+
+/**
+ * @since 4.0
+ */
+public class EncryptorTest extends TestCase
+{
+    private DefaultEncryptor encryptor;
+    
+    public void setUp() throws Exception
+    {
+        encryptor = new DefaultEncryptor(
+                KeyStoreKeyProviderTest.getTestKeyStoreProvider(),
+                "DESede/CBC/PKCS5Padding",
+                null);
+        encryptor.init();        // Not currently necessary
+    }
+    
+    public void testBasicBytes_NoKey()
+    {
+        byte[] bytes = new byte[] {11, 12, 13};
+
+        Pair<byte[], AlgorithmParameters> encryptedPair = encryptor.encrypt("fluff", null, bytes);
+        byte[] decrypted = encryptor.decrypt(
+                "fluff",
+                encryptedPair.getSecond(),
+                encryptedPair.getFirst());
+        assertTrue("Encryption round trip failed. ", Arrays.areEqual(bytes, decrypted));
+    }
+    
+    public void testBasicBytes_WithKey()
+    {
+        byte[] bytes = new byte[] {11, 12, 13};
+
+        Pair<byte[], AlgorithmParameters> encryptedPair = encryptor.encrypt("mykey1", null, bytes);
+        byte[] decrypted = encryptor.decrypt(
+                "mykey1",
+                encryptedPair.getSecond(),
+                encryptedPair.getFirst());
+        assertTrue("Encryption round trip failed. ", Arrays.areEqual(bytes, decrypted));
+    }
+    
+    public void testBasicObject()
+    {
+        Object testObject = "   This is a string, but will be serialized    ";
+
+        Pair<byte[], AlgorithmParameters> encryptedPair = encryptor.encryptObject("mykey2", null, testObject);
+        Object output = encryptor.decryptObject(
+                "mykey2",
+                encryptedPair.getSecond(),
+                encryptedPair.getFirst());
+        assertEquals("Encryption round trip failed. ", testObject, output);
+    }
+    
+    public void testSealedObject()
+    {
+        Serializable testObject = "   This is a string, but will be serialized    ";
+
+        Serializable sealedObject = encryptor.sealObject("mykey2", null, testObject);
+        Object output = encryptor.unsealObject("mykey2", sealedObject);
+        assertEquals("Encryption round trip failed. ", testObject, output);
+    }
+}

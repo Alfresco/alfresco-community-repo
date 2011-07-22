92c45359d87310440380074e573c06fa74eaa894
Kevin Roast <kevin.roast@alfresco.com>
2011-07-22 09:20:26 +0000

    Category Manager contribution code cleanup and fix to incorrectly defined I18N messages that were not appearing.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@29268 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- config/alfresco/templates/webscripts/org/alfresco/repository/category/category.delete.js
+++ config/alfresco/templates/webscripts/org/alfresco/repository/category/category.delete.js
@@ -2,45 +2,45 @@
  * Deletes a given category.
  */
 
-main();
-
 function main()
 {
-    // Extract template args and build the node reference
-    var nodeRef = url.templateArgs.store_protocol + "://" + url.templateArgs.store_id + "/" + url.templateArgs.node_id;
- 
-    // Log information
-    if (logger.isLoggingEnabled() == true)
-    {
-        logger.log("Attempting to remove category (nodeRef=" + nodeRef + ")");
-    }
+   // Extract template args and build the node reference
+   var nodeRef = url.templateArgs.store_protocol + "://" + url.templateArgs.store_id + "/" + url.templateArgs.node_id;
    
-	// Try and find the category node
-	var category = classification.getCategory(nodeRef);
-	
-	// Return 404 if category is not found
-	if (category == null)
-	{
-		// Log information
-	    if (logger.isLoggingEnabled() == true)
-	    {
-	        logger.log("Category being removed could not be found. (nodeRef=" + nodeRef + ")");
-	    }
-	    
-	    // Set status code
-		status.setCode(status.STATUS_NOT_FOUND, "Category could not be found. (nodeRef=" + nodeRef + ")");
-		return;
-	}
+   // Log information
+   if (logger.isLoggingEnabled() == true)
+   {
+      logger.log("Attempting to remove category (nodeRef=" + nodeRef + ")");
+   }
    
-	// Remove the category
-	category.removeCategory();
-
-	// Log information
-    if (logger.isLoggingEnabled() == true)
-    {
-        logger.log("Category successfully removed. (nodeRef=" + nodeRef + ")");
-    }
-	
-	// Set model properties
-    model.message = "Category successfully removed. (nodeRef=" + nodeRef + ")";
+   // Try and find the category node
+   var category = classification.getCategory(nodeRef);
+   
+   // Return 404 if category is not found
+   if (category == null)
+   {
+      // Log information
+      if (logger.isLoggingEnabled() == true)
+      {
+         logger.log("Category being removed could not be found. (nodeRef=" + nodeRef + ")");
+      }
+      
+      // Set status code
+      status.setCode(status.STATUS_NOT_FOUND, "Category could not be found. (nodeRef=" + nodeRef + ")");
+      return;
+   }
+   
+   // Remove the category
+   category.removeCategory();
+   
+   // Log information
+   if (logger.isLoggingEnabled() == true)
+   {
+      logger.log("Category successfully removed. (nodeRef=" + nodeRef + ")");
+   }
+   
+   // Set model properties
+   model.message = "Category successfully removed. (nodeRef=" + nodeRef + ")";
 }
+
+main();
--- config/alfresco/templates/webscripts/org/alfresco/repository/category/category.post.json.js
+++ config/alfresco/templates/webscripts/org/alfresco/repository/category/category.post.json.js
@@ -17,71 +17,71 @@ var PROP_ASPECT = "aspect";
 // Default property values
 var DEFAULT_ASPECT = "{http://www.alfresco.org/model/content/1.0}generalclassifiable";
 
-main();
-
 function main()
 {
-	// Check we have a json request
-    if (typeof json === "undefined")
-    {
-        if (logger.isWarnLoggingEnabled() == true)
-        {
-            logger.warn("Could not create category, because json object was undefined.");
-        }
-        
-        status.setCode(501, "Could not create category, because json object was undefined.");
-        return;
-    }
+   // Check we have a json request
+   if (typeof json === "undefined")
+   {
+      if (logger.isWarnLoggingEnabled() == true)
+      {
+         logger.warn("Could not create category, because json object was undefined.");
+      }
+      
+      status.setCode(501, "Could not create category, because json object was undefined.");
+      return;
+   }
    
-    // Try and retrieve the name of the new category
-    var name = json.get(PROP_NAME);       
-    if (name == null || name.length === 0)
-    {
-       status.setCode(status.STATUS_BAD_REQUEST, "Could not create category, because 'prop_cm_name' parameter is missing from json request.");
-       return;
-    }
+   // Try and retrieve the name of the new category
+   var name = json.get(PROP_NAME);
+   if (name == null || name.length === 0)
+   {
+      status.setCode(status.STATUS_BAD_REQUEST, "Could not create category, because 'prop_cm_name' parameter is missing from json request.");
+      return;
+   }
     
-    // Build the node reference string
-    var nodeRef = null;
-    var aspect = null;
-    if (url.templateArgs.length != 0)
-    {    	
-    	nodeRef = url.templateArgs.store_protocol + "://" + url.templateArgs.store_id + "/" + url.templateArgs.node_id;
-    	
-    	// Log message
-        if (logger.isLoggingEnabled() == true)
-        {
-            logger.log("Attempting to create new category. (nodeRef=" + nodeRef + ", name=" + name + ")");     
-        }
-    }
-    else
-    {
-    	// See if an aspect has been specified
-    	var aspect = DEFAULT_ASPECT;
-    	if (json.has(PROP_ASPECT) == true)
-    	{
-    		aspect = json.get(PROP_ASPECT);
-    	}
-    }
+   // Build the node reference string
+   var nodeRef = null;
+   var aspect = null;
+   if (url.templateArgs.length != 0)
+   {       
+      nodeRef = url.templateArgs.store_protocol + "://" + url.templateArgs.store_id + "/" + url.templateArgs.node_id;
+      
+      // Log message
+      if (logger.isLoggingEnabled() == true)
+      {
+         logger.log("Attempting to create new category. (nodeRef=" + nodeRef + ", name=" + name + ")");
+      }
+   }
+   else
+   {
+      // See if an aspect has been specified
+      var aspect = DEFAULT_ASPECT;
+      if (json.has(PROP_ASPECT) == true)
+      {
+         aspect = json.get(PROP_ASPECT);
+      }
+   }
     
-    if (nodeRef != null)
-    {
-       var category = classification.getCategory(nodeRef);
-       if (category !== null)
-       {
-    	  model.persistedObject = category.createSubCategory(name);
-       }
-       else
-       {
-          status.setCode(status.STATUS_BAD_REQUEST, "Unable to create category, because category node was found or is not a category.  (nodeRef=" + nodeRef + ", name=" + name + ")");
-          return;
-       }
-    }
-    else
-    {
-       model.persistedObject = classification.createRootCategory(aspect, name);
-    }       
-    
-    model.message = "Successfully added category";
-    model.name = name;
+   if (nodeRef != null)
+   {
+      var category = classification.getCategory(nodeRef);
+      if (category !== null)
+      {
+         model.persistedObject = category.createSubCategory(name);
+      }
+      else
+      {
+         status.setCode(status.STATUS_BAD_REQUEST, "Unable to create category, because category node was found or is not a category.  (nodeRef=" + nodeRef + ", name=" + name + ")");
+         return;
+      }
+   }
+   else
+   {
+      model.persistedObject = classification.createRootCategory(aspect, name);
+   }
+   
+   model.message = "Successfully added category";
+   model.name = name;
 }
+
+main();

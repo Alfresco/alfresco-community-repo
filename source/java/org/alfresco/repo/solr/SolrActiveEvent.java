0ec0b0939e6a5c70aeec02d729724a5ff8710017
Steven Glover <steven.glover@alfresco.com>
2011-07-25 08:52:52 +0000

    Missed a file
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@29299 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/java/org/alfresco/repo/solr/SolrActiveEvent.java
+++ source/java/org/alfresco/repo/solr/SolrActiveEvent.java
@@ -1,13 +1,34 @@
+/*
+ * Copyright (C) 2005-2011 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.alfresco.repo.solr;
 
-import org.springframework.context.ApplicationEvent;
 
-public class SolrActiveEvent extends ApplicationEvent
+/**
+ * 
+ * @since 4.0
+ *
+ */
+public class SolrActiveEvent extends SolrEvent
 {
 	private static final long serialVersionUID = -7361024456694701653L;
 
 	public SolrActiveEvent(Object source) {
         super(source);
     }
-
 }

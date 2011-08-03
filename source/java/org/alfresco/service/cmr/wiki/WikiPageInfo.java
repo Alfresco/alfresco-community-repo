7f54672204ecbe561649765925bf90651be962ee
Nick Burch <nick.burch@quanticate.com>
2011-08-03 14:44:48 +0000

    ALF-9157 stub out a new wiki service
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@29526 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

---
+++ source/java/org/alfresco/service/cmr/wiki/WikiPageInfo.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2005-2010 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.alfresco.service.cmr.wiki;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import org.alfresco.repo.security.permissions.PermissionCheckValue;
+import org.alfresco.service.cmr.repository.NodeRef;
+
+/**
+ * This class represents a Wiki Paeg in a site 
+ * 
+ * @author Nick Burch
+ * @since 4.0
+ */
+public interface WikiPageInfo extends Serializable, PermissionCheckValue {
+   /**
+    * @return the NodeRef of the underlying wiki page
+    */
+   NodeRef getNodeRef();
+   
+   /**
+    * @return the NodeRef of the site container this belongs to
+    */
+   NodeRef getContainerNodeRef();
+   
+   /**
+    * @return the name of the wiki page
+    */
+   String getSystemName();
+   
+   /**
+    * @return the Title of the wiki page
+    */
+   String getTitle();
+   
+   /**
+    * Sets the Title of the wiki page
+    */
+   void setTitle(String title);
+   
+   /**
+    * @return the HTML Content of the wiki page
+    */
+   String getContents();
+   
+   /**
+    * Sets the (HTML) Content of the wiki page
+    */
+   void setContents(String contentHTML);
+   
+   /**
+    * @return the creator of the wiki page
+    */
+   String getCreator();
+   
+   /**
+    * @return the modifier of the wiki page
+    */
+   String getModifier();
+   
+   /**
+    * @return the creation date and time
+    */
+   Date getCreatedAt();
+   
+   /**
+    * @return the modification date and time
+    */
+   Date getModifiedAt();
+   
+   /**
+    * @return the Tags associated with the wiki page 
+    */
+   List<String> getTags();
+}
---
+++ source/java/org/alfresco/service/cmr/wiki/WikiPageService.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2005-2011 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.alfresco.service.cmr.wiki;
+
+import java.util.Date;
+
+import org.alfresco.query.PagingRequest;
+import org.alfresco.query.PagingResults;
+import org.alfresco.service.NotAuditable;
+
+/**
+ * The Wiki Page service.
+ * 
+ * @author Nick Burch
+ * @since 4.0
+ */
+public interface WikiPageService {
+   /**
+    * Creates a new {@link WikiPageInfo} in the given site, with the
+    *  specified contents
+    *  
+    * @return The newly created {@link WikiPageInfo}
+    */
+   @NotAuditable
+   WikiPageInfo createWikiPage(String siteShortName, String title,
+         String contents);
+   
+   /**
+    * Updates an existing {@link WikiPageInfo} in the repository.
+    *  
+    * @return The updated {@link WikiPageInfo}
+    */
+   @NotAuditable
+   WikiPageInfo updateWikiPage(WikiPageInfo wikiPage);
+   
+   /**
+    * Deletes an existing {@link WikiPageInfo} from the repository
+    */
+   @NotAuditable
+   void deleteWikiPage(WikiPageInfo wikiPage);
+   
+   /**
+    * Retrieves an existing {@link WikiPageInfo} from the repository
+    */
+   @NotAuditable
+   WikiPageInfo getWikiPage(String siteShortName, String pageTitle);
+
+   /**
+    * Retrieves all {@link WikiPageInfo} instances in the repository
+    *  for the given site.
+    */
+   @NotAuditable
+   PagingResults<WikiPageInfo> listWikiPages(String siteShortName, PagingRequest paging);
+
+   /**
+    * Retrieves all {@link WikiPageInfo} instances in the repository
+    *  for the given site and the specified user.
+    */
+   @NotAuditable
+   PagingResults<WikiPageInfo> listWikiPages(String siteShortName, String user, PagingRequest paging);
+
+   /**
+    * Retrieves all {@link WikiPageInfo} instances in the repository
+    *  for the given site, created in the specified date range
+    */
+   @NotAuditable
+   PagingResults<WikiPageInfo> listWikiPagesByCreated(String siteShortName, Date from, Date to, PagingRequest paging);
+
+   /**
+    * Retrieves all {@link WikiPageInfo} instances in the repository
+    *  for the given site, modified in the specified date range
+    */
+   @NotAuditable
+   PagingResults<WikiPageInfo> listWikiPagesByModified(String siteShortName, Date from, Date to, PagingRequest paging);
+}

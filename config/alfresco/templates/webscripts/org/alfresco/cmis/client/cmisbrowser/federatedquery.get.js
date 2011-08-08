cddb8ca50832d7a5125b55210563b13d160bbccf
Florian Mü <fmui@apache.org>
2011-08-08 17:41:16 +0000

    Fixed CMIS object parents collection
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@29621 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- config/alfresco/templates/webscripts/org/alfresco/cmis/client/cmisbrowser/federatedquery.get.js
+++ config/alfresco/templates/webscripts/org/alfresco/cmis/client/cmisbrowser/federatedquery.get.js
@@ -19,9 +19,12 @@ script:
         		continue;
         	}
         	
+        	var oc = connections.get(i).session.createOperationContext();
+        	oc.setIncludeAllowableActions(false);
+        	
             var skipCount = (args[connections.get(i).id + "_skipCount"] == null) ? 0 : parseInt(args[connections.get(i).id + "_skipCount"]);
             skipCount = (skipCount < 0) ? 0 : skipCount;
-            var iterable = connections.get(i).session.query(statement, false).skipTo(skipCount).getPage(model.maxItems);
+            var iterable = connections.get(i).session.query(statement, false, oc).skipTo(skipCount).getPage(model.maxItems);
             
             var result = new Object();
             result.conn = connections.get(i);

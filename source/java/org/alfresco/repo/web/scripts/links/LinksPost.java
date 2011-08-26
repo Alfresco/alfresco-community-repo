c252a3e4b5f37525a2e615160dbf1e08198698fc
Nick Burch <nick.burch@quanticate.com>
2011-08-26 15:37:27 +0000

    Switch the Links creating webscript to be Java backed using the new service, and enhance unit tests
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@30108 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/java/org/alfresco/repo/web/scripts/links/LinksPost.java
+++ source/java/org/alfresco/repo/web/scripts/links/LinksPost.java
@@ -19,10 +19,13 @@
 package org.alfresco.repo.web.scripts.links;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.alfresco.repo.security.permissions.AccessDeniedException;
 import org.alfresco.service.cmr.links.LinkInfo;
 import org.alfresco.service.cmr.site.SiteInfo;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.extensions.webscripts.Cache;
 import org.springframework.extensions.webscripts.Status;
@@ -30,27 +33,74 @@ import org.springframework.extensions.webscripts.WebScriptException;
 import org.springframework.extensions.webscripts.WebScriptRequest;
 
 /**
- * This class is the controller for the link fetching link.get webscript.
+ * This class is the controller for the link fetching links.post webscript.
  * 
  * @author Nick Burch
  * @since 4.0
  */
-public class LinkGet extends AbstractLinksWebScript
+public class LinksPost extends AbstractLinksWebScript
 {
+   private static final String PARAM_MESSAGE = "message";
+   
    @Override
    protected Map<String, Object> executeImpl(SiteInfo site, String linkName,
          WebScriptRequest req, JSONObject json, Status status, Cache cache) {
       Map<String, Object> model = new HashMap<String, Object>();
       
-      // Try to find the link
-      LinkInfo link = linksService.getLink(site.getShortName(), linkName);
-      if(link == null)
+      // Get the new link details from the JSON
+      String title;
+      String description;
+      String url;
+      boolean internal;
+      List<String> tags;
+      try
       {
-         String message = "No link found with that name";
-         throw new WebScriptException(Status.STATUS_NOT_FOUND, message);
+         // Fetch the main properties
+         title = getOrNull(json, "title");
+         description = getOrNull(json, "description");
+         url = getOrNull(json, "url");
+         
+         // Handle internal / not internal
+         internal = json.has("internal");
+         
+         // Do the tags
+         tags = getTags(json);
+      }
+      catch(JSONException je)
+      {
+         throw new WebScriptException(Status.STATUS_BAD_REQUEST, "Invalid JSON: " + je.getMessage());
       }
       
+      
+      // Create the link
+      LinkInfo link;
+      try
+      {
+         link = linksService.createLink(site.getShortName(), title, description, url, internal);
+      }
+      catch(AccessDeniedException e)
+      {
+         String message = "You don't have permission to create a link";
+         
+         status.setCode(Status.STATUS_FORBIDDEN);
+         status.setMessage(message);
+         model.put(PARAM_MESSAGE, message);
+         return model;
+      }
+      
+      // Set the tags if required
+      if(tags != null && tags.size() > 0)
+      {
+         link.getTags().addAll(tags);
+         linksService.updateLink(link);
+      }
+      
+      // Generate an activity for the change
+      addActivityEntry("created", link, site, req, json);
+
+      
       // Build the model
+      model.put(PARAM_MESSAGE, link.getSystemName()); // Really!
       model.put("item", renderLink(link));
       model.put("node", link.getNodeRef());
       model.put("link", link);

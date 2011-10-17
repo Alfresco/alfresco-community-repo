38d03ca5ae9717fa5a3a6dd8455503cd52f293cc
Kevin Roast <kevin.roast@alfresco.com>
2011-10-17 14:48:17 +0000

    Fix for ALF-10797 - "Recently added" and "Recently modified" sections in "My content" page are empty  - addition of index control aspect to surf-config objects - config xml files no longer indexed at all  - refactoring of My Profile user contents fts-alfresco queries to search specific known containers only rather than all
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@31288 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/java/org/alfresco/repo/admin/patch/impl/AVMToADMRemoteStorePatch.java
+++ source/java/org/alfresco/repo/admin/patch/impl/AVMToADMRemoteStorePatch.java
@@ -553,6 +553,9 @@ public class AVMToADMRemoteStorePatch extends AbstractPatch
                         // create new node and perform writer content copy of the content from the AVM to the DM store
                         FileInfo fileInfo = fileFolderService.create(
                                 parentFolder, avmNode.getName(), ContentModel.TYPE_CONTENT);
+                        Map<QName, Serializable> aspectProperties = new HashMap<QName, Serializable>(1, 1.0f);
+                        aspectProperties.put(ContentModel.PROP_IS_INDEXED, false);
+                        nodeService.addAspect(fileInfo.getNodeRef(), ContentModel.ASPECT_INDEX_CONTROL, aspectProperties);
                         ContentWriter writer = contentService.getWriter(
                                 fileInfo.getNodeRef(), ContentModel.PROP_CONTENT, true);
                         writer.putContent(avmService.getContentReader(-1, avmNode.getPath()));
@@ -567,6 +570,9 @@ public class AVMToADMRemoteStorePatch extends AbstractPatch
                     // create new node and perform writer content copy of the content from the AVM to the DM store
                     FileInfo fileInfo = fileFolderService.create(
                             parentFolder, avmNode.getName(), ContentModel.TYPE_CONTENT);
+                    Map<QName, Serializable> aspectProperties = new HashMap<QName, Serializable>(1, 1.0f);
+                    aspectProperties.put(ContentModel.PROP_IS_INDEXED, false);
+                    nodeService.addAspect(fileInfo.getNodeRef(), ContentModel.ASPECT_INDEX_CONTROL, aspectProperties);
                     ContentWriter writer = contentService.getWriter(
                             fileInfo.getNodeRef(), ContentModel.PROP_CONTENT, true);
                     writer.putContent(avmService.getContentReader(-1, avmNode.getPath()));

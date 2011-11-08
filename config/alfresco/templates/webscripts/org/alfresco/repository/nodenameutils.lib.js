6189036b56436c6974f66fe08d44a028ce70816e
Matt Ward <matt.ward@alfresco.com>
2011-11-08 15:41:02 +0000

    ALF-5986: nodenameutils.js possible (but improbable) infinite loop
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@31852 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- config/alfresco/templates/webscripts/org/alfresco/repository/nodenameutils.lib.js
+++ config/alfresco/templates/webscripts/org/alfresco/repository/nodenameutils.lib.js
@@ -17,15 +17,29 @@ function getTwoDigitNum(num)
  * The name will be of form prefix_yyyy-mm-dd_hh-mm["","_x"],
  * where x represents a number > 1 and is only added if a node
  * with the base name already exists
+ * 
+ * The date parameter is for testing and needn't be used.
  */
-function getUniqueChildName(parentNode, prefix)
+function getUniqueChildName(parentNode, prefix, date)
 {
-   // we create a name looking like 
-   var date = new Date(), name = prefix + "-" + date.getTime();
+   // we create a name looking like prefix-datetimestamp 
+   if (typeof date === 'undefined')
+   {
+     date = new Date();
+   }
+   name = prefix + "-" + date.getTime();
 
    // check that no child for the given name exists
-   var finalName = name + "_" + Math.floor(Math.random() * 1000), count = 0;
-   while (parentNode.childByNamePath(finalName) !== null || count > 100)
+   if (parentNode.childByNamePath(name) === null)
+   {
+      return name;
+   }
+   
+   // if there is already a prefix-datetimestamp node then start looking for a unique
+   // name by appending random numbers - try a maximum of 100 times.
+   var finalName = name + "_" + Math.floor(Math.random() * 1000);
+   var count = 0;
+   while (parentNode.childByNamePath(finalName) !== null && count < 100)
    {
       finalName = name + "_" + Math.floor(Math.random() * 1000);
       ++count;

5f640917d667b1cb94dd41c037132d23f7baaa38
Andrew Hind <andy.hind@alfresco.com>
2011-12-02 15:10:37 +0000

    Fix merge forward of
       32411: Fix for ALF-11344 SORT clause in CMIS query (ORDER BY) drastically affects performance of search.
       - use in memory sort rather than relying on the lucene field cache for result sets up to 1000 by default 
       - this is configurable across all query languages (and per query via SearchParameters and QueryOptions)
               lucene.indexer.useInMemorySort=true
               lucene.indexer.maxRawResultSetSizeForInMemorySort=1000
       32425: Fix for ALF-11344 SORT clause in CMIS query (ORDER BY) drastically affects performance of search.
       - fix for score sorting
    
    - requires fixes for 3.4 -> 4.0 refactoring
    
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@32479 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/java/org/alfresco/repo/search/impl/lucene/LuceneAlfrescoFtsQueryLanguage.java
+++ source/java/org/alfresco/repo/search/impl/lucene/LuceneAlfrescoFtsQueryLanguage.java
@@ -80,8 +80,6 @@ public class LuceneAlfrescoFtsQueryLanguage extends AbstractLuceneQueryLanguage
                 searchParameters.getNamespace());
 
         QueryOptions options = QueryOptions.create(searchParameters);
-        options.setUseInMemorySort(searchParameters.getUseInMemorySort());
-        options.setMaxRawResultSetSizeForInMemorySort(searchParameters.getMaxRawResultSetSizeForInMemorySort());
 
         FTSParser.Mode mode;
 

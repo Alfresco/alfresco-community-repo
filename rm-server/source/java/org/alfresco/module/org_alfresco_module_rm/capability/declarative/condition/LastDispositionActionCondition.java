42c1070f09c782c7e8c7386adda99529ae8afef6
Roy Wetherall <roy.wetherall@alfresco.com>
2012-07-06 03:45:53 +0000

    Fix RM build:
      * still after all these years I miss new files when I check-in!
    
    
    
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/BRANCHES/V2.0@38852 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

---
+++ rm-server/source/java/org/alfresco/module/org_alfresco_module_rm/capability/declarative/condition/LastDispositionActionCondition.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2005-2011 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.alfresco.module.org_alfresco_module_rm.capability.declarative.condition;
+
+import org.alfresco.module.org_alfresco_module_rm.capability.declarative.AbstractCapabilityCondition;
+import org.alfresco.module.org_alfresco_module_rm.disposition.DispositionAction;
+import org.alfresco.module.org_alfresco_module_rm.disposition.DispositionService;
+import org.alfresco.service.cmr.repository.NodeRef;
+
+/**
+ * Last disposition action condition.
+ * 
+ * @author Roy Wetherall
+ */
+public class LastDispositionActionCondition extends AbstractCapabilityCondition
+{
+    private DispositionService dispositionService;
+    
+    private String dispositionActionName;
+    
+    public void setDispositionService(DispositionService dispositionService)
+    {
+        this.dispositionService = dispositionService;
+    }
+    
+    public void setDispositionActionName(String dispositionActionName)
+    {
+        this.dispositionActionName = dispositionActionName;
+    }
+    
+    /**
+     * @see org.alfresco.module.org_alfresco_module_rm.capability.declarative.CapabilityCondition#evaluate(org.alfresco.service.cmr.repository.NodeRef)
+     */
+    @Override
+    public boolean evaluate(NodeRef nodeRef)
+    {
+        boolean result = false;
+        DispositionAction dispositionAction = dispositionService.getLastCompletedDispostionAction(nodeRef);
+        if (dispositionAction != null && 
+            dispositionActionName.equals(dispositionAction.getName()) == true)
+        {
+            result = true;
+        }
+        return result;        
+    }
+
+}

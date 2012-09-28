f4a8eb8549b84de0178da7d68e6c56437ec118c7
Frederik Heremans <frederik.heremans@alfresco.com>
2012-09-28 10:22:25 +0000

    ALF-16098: Using wrong patch-name in SQL-upgrade script when recording script-finish in DB (thanks for the pointer Kev)
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@42135 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- config/alfresco/dbscripts/upgrade/4.2/org.hibernate.dialect.PostgreSQLDialect/activiti-upgrade-5-10.sql
+++ config/alfresco/dbscripts/upgrade/4.2/org.hibernate.dialect.PostgreSQLDialect/activiti-upgrade-5-10.sql
@@ -98,6 +98,6 @@ INSERT INTO alf_applied_patch
   (id, description, fixes_from_schema, fixes_to_schema, applied_to_schema, target_schema, applied_on_date, applied_to_server, was_executed, succeeded, report)
   VALUES
   (
-    'patch.db-V4.1-upgrade-to-activiti-5.10', 'Manually executed script upgrade V4.1: Upgraded Activiti tables to 5.10 version',
+    'patch.db-V4.2-upgrade-to-activiti-5.10', 'Manually executed script upgrade V4.2: Upgraded Activiti tables to 5.10 version',
     0, 5111, -1, 5112, null, 'UNKNOWN', ${TRUE}, ${TRUE}, 'Script completed'
   );

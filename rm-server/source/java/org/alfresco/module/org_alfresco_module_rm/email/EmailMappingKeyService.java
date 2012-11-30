0f7197eb256f979b2cd8f58228c5e4823e10a123
Tuna Aksoy <tuna.aksoy@alfresco.com>
2012-11-30 11:54:54 +0000

    Made Email mapping keys customisable
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/HEAD@44190 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

---
+++ rm-server/source/java/org/alfresco/module/org_alfresco_module_rm/email/EmailMappingKeyService.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2005-2012 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.alfresco.module.org_alfresco_module_rm.email;
+
+import java.util.List;
+
+/**
+ * EMail Mapping Key Service
+ *
+ * @author Tuna Aksoy
+ * @since @2.1
+ */
+public interface EmailMappingKeyService
+{
+    /**
+     * Gets the list of email mapping keys
+     *
+     * @return Email mapping keys
+     */
+    public List<String> getEmailMappingKeys();
+
+    /**
+     * Makes a email mapping key customisable.
+     *
+     * @param emailMappingKey  emailMappingKey to make customisable
+     */
+    public void makeCustomisable(String emailMappingKey);
+}

1c16e38f33059b8f9c661234b74e1acd6f5f72ff
Roy Wetherall <roy.wetherall@alfresco.com>
2012-12-18 23:58:08 +0000

    RM: Attempt at build fix
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/HEAD@44824 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- rm-server/test/java/org/alfresco/module/org_alfresco_module_rm/test/util/BaseRMTestCase.java
+++ rm-server/test/java/org/alfresco/module/org_alfresco_module_rm/test/util/BaseRMTestCase.java
@@ -429,7 +429,7 @@ public abstract class BaseRMTestCase extends RetryingTransactionHelperTestCase
                 }
             }
         }, 
-        AuthenticationUtil.getSystemUserName());    	
+        AuthenticationUtil.getAdminUserName());    	
     }
     
     /**

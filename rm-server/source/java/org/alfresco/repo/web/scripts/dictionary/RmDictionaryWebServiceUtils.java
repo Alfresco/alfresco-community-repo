5c5259d1deeffa345116b2a0fc8865cdec4366b6
Tuna Aksoy <tuna.aksoy@alfresco.com>
2013-02-27 17:25:01 +0000

    RM-595
    A records management administrator can select aspects and types relevant to RM when defining rules with aspect or type related conditions 
    
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/HEAD@47244 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

---
+++ rm-server/source/java/org/alfresco/repo/web/scripts/dictionary/RmDictionaryWebServiceUtils.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2005-2013 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.alfresco.repo.web.scripts.dictionary;
+
+import org.alfresco.service.cmr.site.SiteInfo;
+import org.alfresco.service.cmr.site.SiteService;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.extensions.webscripts.WebScriptRequest;
+
+/**
+ * Util class for dictionary web services
+ *
+ * @author Tuna Aksoy
+ * @since 2.1
+ */
+public class RmDictionaryWebServiceUtils
+{
+    private static final String SITE_ID = "siteId";
+    private static final String SITE_PRESET = "rm-site-dashboard";
+
+    public static boolean isRmSite(WebScriptRequest req, SiteService siteService)
+    {
+        boolean isRmSite = false;
+        String siteId = req.getParameter(SITE_ID);
+        if (StringUtils.isNotBlank(siteId) == true)
+        {
+            SiteInfo site = siteService.getSite(siteId);
+            if (site != null)
+            {
+                if (site.getSitePreset().equals(SITE_PRESET) == true)
+                {
+                    isRmSite = true;
+                }
+            }
+        }
+        return isRmSite;
+    }
+}

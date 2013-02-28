0c853cc6c00de7d0f4e8a7053435247c6e84949b
Tuna Aksoy <tuna.aksoy@alfresco.com>
2013-02-28 17:46:11 +0000

    RM-597 (A records manager can create a rule that automatically rejects an unfiled record)
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/HEAD@47343 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- rm-server/source/java/org/alfresco/module/org_alfresco_module_rm/action/impl/RejectAction.java
+++ rm-server/source/java/org/alfresco/module/org_alfresco_module_rm/action/impl/RejectAction.java
@@ -18,8 +18,13 @@
  */
 package org.alfresco.module.org_alfresco_module_rm.action.impl;
 
+import java.util.List;
+
 import org.alfresco.module.org_alfresco_module_rm.action.RMActionExecuterAbstractBase;
+import org.alfresco.repo.action.ParameterDefinitionImpl;
 import org.alfresco.service.cmr.action.Action;
+import org.alfresco.service.cmr.action.ParameterDefinition;
+import org.alfresco.service.cmr.dictionary.DataTypeDefinition;
 import org.alfresco.service.cmr.repository.NodeRef;
 
 /**
@@ -43,5 +48,14 @@ public class RejectAction extends RMActionExecuterAbstractBase
     protected void executeImpl(Action action, NodeRef actionedUponNodeRef)
     {
         recordService.rejectRecord(actionedUponNodeRef, (String) action.getParameterValue(PARAM_REASON));
+    }
+
+    /**
+     * @see org.alfresco.module.org_alfresco_module_rm.action.RMActionExecuterAbstractBase#addParameterDefinitions(java.util.List)
+     */
+    @Override
+    protected void addParameterDefinitions(List<ParameterDefinition> paramList)
+    {
+        paramList.add(new ParameterDefinitionImpl(PARAM_REASON, DataTypeDefinition.TEXT, false, getParamDisplayLabel(PARAM_REASON)));
     }
 }

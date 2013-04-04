942b0c5110f5bb51ad7adf004854171ccdaabddd
Tuna Aksoy <tuna.aksoy@alfresco.com>
2013-04-04 21:26:12 +0000

    * RM-630 (Definition for 'request for info' workflow) * RM-631 (The records management team can create a rule to request information about an undeclared record) * RM-632 (The records management team can use an UI action and custom UI to start a "request for information" workflow about an undeclared record)
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/HEAD@48854 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

---
+++ rm-server/source/java/org/alfresco/workflow/requestInfo/RequestInfoVariableHandler.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2005-2013 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.alfresco.workflow.requestInfo;
+
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.DelegateTask;
+import org.activiti.engine.delegate.TaskListener;
+import org.alfresco.util.ParameterCheck;
+
+/**
+ * A variable handler for saving the task variables to the execution context.
+ * Some of the information will be needed in other tasks (e.g. "rmwf_message").
+ * This variable handler saves the local task variable to the execution context.
+ *
+ * @author Tuna Aksoy
+ * @since 2.1
+ */
+public class RequestInfoVariableHandler implements TaskListener
+{
+    /**
+     * @see org.activiti.engine.delegate.TaskListener#notify(org.activiti.engine.delegate.DelegateTask)
+     */
+    @Override
+    public void notify(DelegateTask delegateTask)
+    {
+        ParameterCheck.mandatory("delegateTask", delegateTask);
+
+        // Save the variable from the task
+        DelegateExecution execution = delegateTask.getExecution();
+        execution.setVariable("rmwf_message", delegateTask.getVariable("rmwf_message"));
+    }
+}

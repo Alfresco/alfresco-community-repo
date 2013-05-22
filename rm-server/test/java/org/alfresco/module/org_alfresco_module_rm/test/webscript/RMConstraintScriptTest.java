615afda2ffacd86c1f52af5f40c12b9448fa3054
Tuna Aksoy <tuna.aksoy@alfresco.com>
2013-05-22 15:36:44 +0000

    RM-722 (REST API - Add and remove authorities from roles)
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/HEAD@50274 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- rm-server/test/java/org/alfresco/module/org_alfresco_module_rm/test/webscript/RMConstraintScriptTest.java
+++ rm-server/test/java/org/alfresco/module/org_alfresco_module_rm/test/webscript/RMConstraintScriptTest.java
@@ -21,13 +21,9 @@ package org.alfresco.module.org_alfresco_module_rm.test.webscript;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.alfresco.model.ContentModel;
 import org.alfresco.module.org_alfresco_module_rm.caveat.RMCaveatConfigService;
 import org.alfresco.module.org_alfresco_module_rm.test.util.BaseRMWebScriptTestCase;
 import org.alfresco.repo.security.authentication.AuthenticationUtil;
-import org.alfresco.service.cmr.security.MutableAuthenticationService;
-import org.alfresco.service.cmr.security.PersonService;
-import org.alfresco.util.PropertyMap;
 import org.json.JSONObject;
 import org.springframework.extensions.webscripts.Status;
 import org.springframework.extensions.webscripts.TestWebScriptServer.GetRequest;
@@ -40,27 +36,23 @@ import org.springframework.extensions.webscripts.TestWebScriptServer.Response;
  */
 public class RMConstraintScriptTest extends BaseRMWebScriptTestCase
 {
-    private MutableAuthenticationService authenticationService;
     private RMCaveatConfigService caveatConfigService;
-    private PersonService personService;
-    
+
     protected final static String RM_LIST          = "rmc:smListTest";
     protected final static String RM_LIST_URI_ELEM = "rmc_smListTest";
-    
+
     private static final String URL_RM_CONSTRAINTS = "/api/rma/rmconstraints";
-  
+
     @Override
-    protected void initServices() 
+    protected void initServices()
     {
     	super.initServices();
-    	
+
         this.caveatConfigService = (RMCaveatConfigService)getServer().getApplicationContext().getBean("CaveatConfigService");
-        this.authenticationService = (MutableAuthenticationService)getServer().getApplicationContext().getBean("AuthenticationService");
-        this.personService = (PersonService)getServer().getApplicationContext().getBean("PersonService");
     }
-    
+
     /**
-     * 
+     *
      * @throws Exception
      */
     public void testGetRMConstraint() throws Exception
@@ -76,62 +68,44 @@ public class RMConstraintScriptTest extends BaseRMWebScriptTestCase
             caveatConfigService.deleteRMConstraint(RM_LIST);
         }
         caveatConfigService.addRMConstraint(RM_LIST, "my title", new String[0]);
-        
-        
+
+
         createUser("fbloggs");
         createUser("jrogers");
         createUser("jdoe");
-      
-        
+
+
         List<String> values = new ArrayList<String>();
         values.add("NOFORN");
         values.add("FGI");
         caveatConfigService.updateRMConstraintListAuthority(RM_LIST, "fbloggs", values);
         caveatConfigService.updateRMConstraintListAuthority(RM_LIST, "jrogers", values);
         caveatConfigService.updateRMConstraintListAuthority(RM_LIST, "jdoe", values);
-        
+
         AuthenticationUtil.setFullyAuthenticatedUser("jdoe");
         /**
-         * Positive test Get the constraint 
+         * Positive test Get the constraint
          */
         {
             String url = URL_RM_CONSTRAINTS + "/" + RM_LIST_URI_ELEM;
             Response response = sendRequest(new GetRequest(url), Status.STATUS_OK);
             JSONObject top = new JSONObject(response.getContentAsString());
-            
+
             JSONObject data = top.getJSONObject("data");
             System.out.println(response.getContentAsString());
-            
+
             data.getJSONArray("allowedValuesForCurrentUser");
 
         }
-        
+
 
         AuthenticationUtil.setFullyAuthenticatedUser(AuthenticationUtil.getAdminUserName());
-        personService.deletePerson("fbloggs");
-        personService.deletePerson("jrogers");
-        personService.deletePerson("jdoe");
-                
+        deleteUser("fbloggs");
+        deleteUser("jrogers");
+        deleteUser("jdoe");
+
     }
-    
-    private void createUser(String userName)
-    {
-        if (this.authenticationService.authenticationExists(userName) == false)
-        {
-            this.authenticationService.createAuthentication(userName, "PWD".toCharArray());
-            
-            PropertyMap ppOne = new PropertyMap(4);
-            ppOne.put(ContentModel.PROP_USERNAME, userName);
-            ppOne.put(ContentModel.PROP_AUTHORITY_DISPLAY_NAME, "title" + userName);
-            ppOne.put(ContentModel.PROP_FIRSTNAME, "firstName");
-            ppOne.put(ContentModel.PROP_LASTNAME, "lastName");
-            ppOne.put(ContentModel.PROP_EMAIL, "email@email.com");
-            ppOne.put(ContentModel.PROP_JOBTITLE, "jobTitle");
-            
-            this.personService.createPerson(ppOne);
-        }        
-    }
+
 }
-    
-     
- 
+
+
---
+++ rm-server/test/java/org/alfresco/module/org_alfresco_module_rm/test/webscript/RmChildrenRestApiTest.java
@@ -0,0 +1,215 @@
+/*
+ * Copyright (C) 2005-2013 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.alfresco.module.org_alfresco_module_rm.test.webscript;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Set;
+
+import org.alfresco.module.org_alfresco_module_rm.role.FilePlanRoleService;
+import org.alfresco.module.org_alfresco_module_rm.test.util.BaseRMWebScriptTestCase;
+import org.alfresco.service.cmr.repository.StoreRef;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.extensions.webscripts.Status;
+import org.springframework.extensions.webscripts.TestWebScriptServer.DeleteRequest;
+import org.springframework.extensions.webscripts.TestWebScriptServer.PostRequest;
+import org.springframework.extensions.webscripts.TestWebScriptServer.Response;
+
+/**
+ * REST API Tests for adding/removing users/groups to/from a role
+ *
+ * @author Tuna Aksoy
+ * @since 2.1
+ */
+public class RmChildrenRestApiTest extends BaseRMWebScriptTestCase
+{
+    /** URL for the REST APIs */
+    private static final String RM_CHILDREN_URL = "/api/rm/%s/role/%s/children/%s";
+
+    /** Constant for the content type */
+    private static final String APPLICATION_JSON = "application/json";
+
+    /**
+     * Test the REST API to add/remove a user to/from a role
+     *
+     * @throws IOException
+     * @throws JSONException
+     */
+    public void testRmAddRemoveUser() throws IOException, JSONException
+    {
+        // Create a test user
+        String userName = "geshjsjuasftg";
+        createUser(userName);
+
+        // Check if the user is already assigned to the role
+        assertFalse(getUsersAssignedToRole().contains(userName));
+
+        // Format url and send request
+        String url = getFormattedUrlString(userName);
+        Response response = postRequest(url);
+
+        // Check the content from the response
+        checkContent(response);
+
+        // The user should be added to the role
+        assertTrue(getUsersAssignedToRole().contains(userName));
+
+        // Remove the user from the role
+        response = deleteRequest(url);
+
+        // Check the content from the response
+        checkContent(response);
+
+        // The user should be removed from the role
+        assertFalse(getUsersAssignedToRole().contains(userName));
+
+        // Delete the user
+        deleteUser(userName);
+    }
+
+    /**
+     * Test the REST API to add/remove a group to/from a role
+     *
+     * @throws IOException
+     * @throws JSONException
+     */
+    public void testRmAddRemoveGroup() throws IOException, JSONException
+    {
+        // Create a group
+        String groupName = "arhweurawy";
+        createGroup(groupName);
+
+        // Check if the group is already assigned to the role
+        assertFalse(getGroupsAssignedToRole().contains(groupName));
+
+        // Format url and send request
+        String url = getFormattedUrlString(groupName);
+        Response response = postRequest(url);
+
+        // Check the content from the response
+        checkContent(response);
+
+        // The group should be added to the role
+        assertTrue(getGroupsAssignedToRole().contains(groupName));
+
+        // Remove the group from the role
+        response = deleteRequest(url);
+
+        // Check the content from the response
+        checkContent(response);
+
+        // The user should be removed from the role
+        assertFalse(getGroupsAssignedToRole().contains(groupName));
+
+        // Delete the group
+        deleteGroup(groupName);
+    }
+
+    /**
+     * Util method to get a set of groups assigned to a role
+     *
+     * @return Returns a set of groups assigned to a role
+     */
+    private Set<String> getGroupsAssignedToRole()
+    {
+        return filePlanRoleService.getGroupsAssignedToRole(filePlan, FilePlanRoleService.ROLE_SECURITY_OFFICER);
+    }
+
+    /**
+     * Util method to get a set of users assigned to a role
+     *
+     * @return Returns a set of users assigned to a role
+     */
+    private Set<String> getUsersAssignedToRole()
+    {
+        return filePlanRoleService.getUsersAssignedToRole(filePlan, FilePlanRoleService.ROLE_SECURITY_OFFICER);
+    }
+
+    /**
+     * Util method to get a formatted nodeRef string
+     *
+     * @return Returns a formatted nodeRef string
+     */
+    private String getFormattedFilePlanString()
+    {
+        StoreRef storeRef = filePlan.getStoreRef();
+        String storeType = storeRef.getProtocol();
+        String storeId = storeRef.getIdentifier();
+        String id = filePlan.getId();
+
+        StringBuffer sb = new StringBuffer();
+        sb.append(storeType);
+        sb.append("/");
+        sb.append(storeId);
+        sb.append("/");
+        sb.append(id);
+
+        return sb.toString();
+    }
+
+    /**
+     * Util method to get a formatted url string
+     *
+     * @param authorityName The name of the authority which should be added/removed to/from a role
+     * @return Returns a formatted url string
+     */
+    private String getFormattedUrlString(String authorityName)
+    {
+        return String.format(RM_CHILDREN_URL, getFormattedFilePlanString(), FilePlanRoleService.ROLE_SECURITY_OFFICER, authorityName);
+    }
+
+    /**
+     * Util method to send a post request
+     *
+     * @param url The url which should be used to make the post request
+     * @return Returns the response from the server
+     * @throws UnsupportedEncodingException
+     * @throws IOException
+     */
+    private Response postRequest(String url) throws UnsupportedEncodingException, IOException
+    {
+        return sendRequest(new PostRequest(url, new JSONObject().toString(), APPLICATION_JSON), Status.STATUS_OK);
+    }
+
+    /**
+     * Util method to send a delete request
+     *
+     * @param url The url which should be used to make the delete request
+     * @return Returns the response from the server
+     * @throws IOException
+     */
+    private Response deleteRequest(String url) throws IOException
+    {
+        return sendRequest(new DeleteRequest(url), Status.STATUS_OK);
+    }
+
+    /**
+     * Util method to check the server response
+     *
+     * @param response The server response
+     * @throws UnsupportedEncodingException
+     */
+    private void checkContent(Response response) throws UnsupportedEncodingException
+    {
+        String contentAsString = response.getContentAsString();
+        assertNotNull(contentAsString);
+        assertTrue(contentAsString.equals("{}"));
+    }
+}

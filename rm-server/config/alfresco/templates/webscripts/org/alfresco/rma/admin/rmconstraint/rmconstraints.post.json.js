696886df796473af29bdc5ad716f186fd2c5f4a0
Tuna Aksoy <tuna.aksoy@alfresco.com>
2013-08-13 10:51:11 +0000

    RM-473 ("Rename" allows you to duplicate list name)
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/HEAD@53995 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- rm-server/config/alfresco/templates/webscripts/org/alfresco/rma/admin/rmconstraint/rmconstraints.post.json.js
+++ rm-server/config/alfresco/templates/webscripts/org/alfresco/rma/admin/rmconstraint/rmconstraints.post.json.js
@@ -1,70 +1,54 @@
+<import resource="classpath:/alfresco/templates/webscripts/org/alfresco/rma/admin/rmconstraint/rmconstraint-utils.js">
+
 /**
  * Create a new RM Constraint List
- */ 
+ */
 function main()
 {
    // Parse the passed in details
-   var title = null;
-   var name = null;
-   var allowedValues = {};
-   
-   if (json.has("allowedValues"))
-   {
-      values = json.getJSONArray("allowedValues");
-      
-      var i = 0;
-      allowedValues = new Array();
-      
-      if (values != null)
-      {
-         for (var x = 0; x < values.length(); x++)
-         {  
-            allowedValues[i++] = values.get(x);
-         }
-      }
-   }
-   
+   var title = null,
+      name = null,
+      allowedValues = {};
+
    if (json.has("constraintName"))
    {
-      name = json.get("constraintName"); 
+      name = json.get("constraintName");
    }
-   
+
    if (json.has("constraintTitle"))
    {
-      title = json.get("constraintTitle"); 
+      title = json.get("constraintTitle");
    }
    else
    {
       title = name;
    }
-   
 
-   var constraints = caveatConfig.allConstraints;
-   
-   // Check for existing constraint...
-   var alreadyExists = false;
-   for (var i=0; i<constraints.length; i++)
-   {
-      var currTitle = constraints[i].title;
-      if (currTitle + "" == title)
-      {
-         alreadyExists = true;
-         break;
-      }
-   }
-   
-   var existingConstraint = caveatConfig.getConstraint(title);
-   if (!alreadyExists)
-   {
-      var constraint = caveatConfig.createConstraint(name, title, allowedValues);
-      model.constraint = constraint;
-   }
-   else
+   if (existsTitle(caveatConfig, title))
    {
       status.code = 400;
       model.errorMessage = "rm.admin.list-already-exists";
       model.title = title;
+      return;
    }
+
+   if (json.has("allowedValues"))
+   {
+      values = json.getJSONArray("allowedValues");
+
+      var i = 0;
+      allowedValues = new Array();
+
+      if (values != null)
+      {
+         for (var x = 0; x < values.length(); x++)
+         {
+            allowedValues[i++] = values.get(x);
+         }
+      }
+   }
+
+   model.constraint = caveatConfig.createConstraint(name, title, allowedValues);
 }
 
 main();

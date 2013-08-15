53e3f9d36b4e8ccfc3d1e7d69d55b3233f36e4cf
Roy Wetherall <roy.wetherall@alfresco.com>
2013-08-15 06:25:18 +0000

    RM-784: Possible to create categories/folders with the same identifier
     * started out adding a RM query DOA so canned queries could be added .. turned out this wasn't required, but it's a handy
       bit of framework to have in place for the future so I've left it in with an example query and unit test.
    
    
    
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/HEAD@54102 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

---
+++ rm-server/source/java/org/alfresco/module/org_alfresco_module_rm/query/RecordsManagementQueryDAOImpl.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2005-2013 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.alfresco.module.org_alfresco_module_rm.query;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.alfresco.module.org_alfresco_module_rm.model.RecordsManagementModel;
+import org.alfresco.repo.domain.qname.QNameDAO;
+import org.alfresco.service.namespace.QName;
+import org.alfresco.util.Pair;
+import org.mybatis.spring.SqlSessionTemplate;
+
+/**
+ * Records management query DAO implementation
+ * 
+ * @author Roy Wetherall
+ * @since 2.1
+ */
+public class RecordsManagementQueryDAOImpl implements RecordsManagementQueryDAO, RecordsManagementModel
+{
+    private static final String COUNT_IDENTIFIER = "alfresco.query.rm.select_CountRMIndentifier";
+    
+    /** SQL session template */
+    protected SqlSessionTemplate template;
+    
+    /** QName DAO */
+    protected QNameDAO qnameDAO;
+    
+    /**
+     * @param sqlSessionTemplate    SQL session template
+     */
+    public final void setSqlSessionTemplate(SqlSessionTemplate sqlSessionTemplate) 
+    {
+        this.template = sqlSessionTemplate;
+    }
+    
+    /**
+     * @param qnameDAO  qname DAO
+     */
+    public final void setQnameDAO(QNameDAO qnameDAO)
+    {
+        this.qnameDAO = qnameDAO;
+    }
+    
+    /**
+     * @see org.alfresco.module.org_alfresco_module_rm.query.RecordsManagementQueryDAO#getCountRmaIdentifier(java.lang.String)
+     */
+    @Override
+    public int getCountRmaIdentifier(String identifierValue)
+    {
+        int result = 0;
+        
+        // lookup the id of the identifier property qname
+        Pair<Long, QName> pair = qnameDAO.getQName(PROP_IDENTIFIER);
+        if (pair != null)
+        {        
+            // create query params
+            Map<String, Object> params = new HashMap<String, Object>(2);
+            params.put("qnameId", pair.getFirst());
+            params.put("idValue", identifierValue);
+            
+            // return the number of rma identifiers found that match the passed value
+            Integer count = (Integer)template.selectOne(COUNT_IDENTIFIER, params);
+            
+            if (count != null)
+            {
+                result = count;
+            }
+        }
+        
+        return result;
+    }
+
+}
---
+++ rm-server/test/java/org/alfresco/module/org_alfresco_module_rm/test/service/RecordsManagementQueryDAOImplTest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2005-2012 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.alfresco.module.org_alfresco_module_rm.test.service;
+
+import org.alfresco.module.org_alfresco_module_rm.model.RecordsManagementModel;
+import org.alfresco.module.org_alfresco_module_rm.query.RecordsManagementQueryDAO;
+import org.alfresco.module.org_alfresco_module_rm.record.RecordService;
+import org.alfresco.module.org_alfresco_module_rm.test.util.BaseRMTestCase;
+
+/**
+ * Records Management Query DAO
+ * 
+ * @author Roy Wetherall
+ * @since 2.1
+ */
+public class RecordsManagementQueryDAOImplTest extends BaseRMTestCase implements RecordsManagementModel
+{
+    protected RecordsManagementQueryDAO queryDAO;
+    
+    /**
+     * @see org.alfresco.module.org_alfresco_module_rm.test.util.BaseRMTestCase#initServices()
+     */
+    @Override
+    protected void initServices()
+    {
+        super.initServices();
+
+        queryDAO = (RecordsManagementQueryDAO)applicationContext.getBean("recordsManagementQueryDAO");
+    }
+
+    /**
+     * This is a record test
+     * 
+     * @see org.alfresco.module.org_alfresco_module_rm.test.util.BaseRMTestCase#isRecordTest()
+     */
+    @Override
+    protected boolean isRecordTest()
+    {
+        return true;
+    }
+
+    /**
+     * @see RecordService#getRecordMetaDataAspects()
+     */
+    public void testGetRecordMetaDataAspects() throws Exception
+    {
+        doTestInTransaction(new Test<Void>()
+        {
+            @Override
+            public Void run()
+            {
+                int count = queryDAO.getCountRmaIdentifier("abc-123");
+                assertEquals(0, count);
+                
+                String existingID = (String)nodeService.getProperty(recordOne, PROP_IDENTIFIER);
+                count = queryDAO.getCountRmaIdentifier(existingID);
+                assertEquals(1, count);    
+                
+                return null;
+            }
+        });
+    }
+
+  
+}

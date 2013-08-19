0e67b7fc5885b74d6b2e74bc510751f6f5f71f36
Tuna Aksoy <tuna.aksoy@alfresco.com>
2013-08-19 08:37:48 +0000

    Fixes the following bugs:
    - RM-249 (Disposition steps sequence can be violated)
    - RM-790 (Cannot delete disposition schedule step)
    
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/HEAD@54223 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- rm-server/source/java/org/alfresco/module/org_alfresco_module_rm/script/DispositionActionDefinitionDelete.java
+++ rm-server/source/java/org/alfresco/module/org_alfresco_module_rm/script/DispositionActionDefinitionDelete.java
@@ -18,7 +18,7 @@
  */
 package org.alfresco.module.org_alfresco_module_rm.script;
 
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.alfresco.module.org_alfresco_module_rm.disposition.DispositionActionDefinition;
@@ -29,7 +29,7 @@ import org.springframework.extensions.webscripts.WebScriptRequest;
 
 /**
  * Implementation for Java backed webscript to delete a dispostion action definition.
- * 
+ *
  * @author Gavin Cornwell
  */
 public class DispositionActionDefinitionDelete extends DispositionAbstractBase
@@ -42,14 +42,33 @@ public class DispositionActionDefinitionDelete extends DispositionAbstractBase
     {
         // parse the request to retrieve the schedule object
         DispositionSchedule schedule = parseRequestForSchedule(req);
-        
+
         // parse the request to retrieve the action definition object
         DispositionActionDefinition actionDef = parseRequestForActionDefinition(req, schedule);
-        
+
         // remove the action definition from the schedule
-        this.dispositionService.removeDispositionActionDefinition(schedule, actionDef);
-        
-        // return an empty model
-        return new HashMap<String, Object>();
+        removeDispositionActionDefinitions(schedule, actionDef);
+
+        // return the disposition schedule model
+        return getDispositionScheduleModel(req);
+    }
+
+    /**
+     * Helper method to remove a disposition action definition and the following definition(s)
+     *
+     * @param schedule The disposition schedule
+     * @param actionDef The disposition action definition
+     */
+    private void removeDispositionActionDefinitions(DispositionSchedule schedule, DispositionActionDefinition actionDef)
+    {
+        int index = actionDef.getIndex();
+        List<DispositionActionDefinition> dispositionActionDefinitions = schedule.getDispositionActionDefinitions();
+        for (DispositionActionDefinition dispositionActionDefinition : dispositionActionDefinitions)
+        {
+            if (dispositionActionDefinition.getIndex() >= index)
+            {
+                dispositionService.removeDispositionActionDefinition(schedule, dispositionActionDefinition);
+            }
+        }
     }
 }
--- rm-server/source/java/org/alfresco/module/org_alfresco_module_rm/script/DispositionScheduleGet.java
+++ rm-server/source/java/org/alfresco/module/org_alfresco_module_rm/script/DispositionScheduleGet.java
@@ -18,15 +18,8 @@
  */
 package org.alfresco.module.org_alfresco_module_rm.script;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import org.alfresco.module.org_alfresco_module_rm.disposition.DispositionActionDefinition;
-import org.alfresco.module.org_alfresco_module_rm.disposition.DispositionSchedule;
-import org.alfresco.module.org_alfresco_module_rm.model.RecordsManagementModel;
-import org.alfresco.service.cmr.repository.NodeRef;
 import org.springframework.extensions.webscripts.Cache;
 import org.springframework.extensions.webscripts.Status;
 import org.springframework.extensions.webscripts.WebScriptRequest;
@@ -34,7 +27,7 @@ import org.springframework.extensions.webscripts.WebScriptRequest;
 /**
  * Implementation for Java backed webscript to return full details
  * about a disposition schedule.
- * 
+ *
  * @author Gavin Cornwell
  */
 public class DispositionScheduleGet extends DispositionAbstractBase
@@ -45,54 +38,7 @@ public class DispositionScheduleGet extends DispositionAbstractBase
     @Override
     protected Map<String, Object> executeImpl(WebScriptRequest req, Status status, Cache cache)
     {
-        // parse the request to retrieve the schedule object
-        DispositionSchedule schedule = parseRequestForSchedule(req);
-
-        // add all the schedule data to Map
-        Map<String, Object> scheduleModel = new HashMap<String, Object>(8);
-        
-        // build url
-        String serviceUrl = req.getServiceContextPath() + req.getPathInfo();
-        scheduleModel.put("url", serviceUrl);
-        String actionsUrl = serviceUrl + "/dispositionactiondefinitions";
-        scheduleModel.put("actionsUrl", actionsUrl);
-        scheduleModel.put("nodeRef", schedule.getNodeRef().toString());
-        scheduleModel.put("recordLevelDisposition", schedule.isRecordLevelDisposition());
-        scheduleModel.put("canStepsBeRemoved", 
-                    !this.dispositionService.hasDisposableItems(schedule));
-        
-        if (schedule.getDispositionAuthority() != null)
-        {
-            scheduleModel.put("authority", schedule.getDispositionAuthority());
-        }
-        
-        if (schedule.getDispositionInstructions() != null)
-        {
-            scheduleModel.put("instructions", schedule.getDispositionInstructions());
-        }
-        
-        boolean unpublishedUpdates = false;
-        boolean publishInProgress = false;                
-        
-        List<Map<String, Object>> actions = new ArrayList<Map<String, Object>>();
-        for (DispositionActionDefinition actionDef : schedule.getDispositionActionDefinitions())
-        {
-            NodeRef actionDefNodeRef = actionDef.getNodeRef();
-            if (nodeService.hasAspect(actionDefNodeRef, RecordsManagementModel.ASPECT_UNPUBLISHED_UPDATE) == true)
-            {
-                unpublishedUpdates = true;
-                publishInProgress = ((Boolean)nodeService.getProperty(actionDefNodeRef, RecordsManagementModel.PROP_PUBLISH_IN_PROGRESS)).booleanValue();
-            }
-            
-            actions.add(createActionDefModel(actionDef, actionsUrl + "/" + actionDef.getId()));
-        }
-        scheduleModel.put("actions", actions);                
-        scheduleModel.put("unpublishedUpdates", unpublishedUpdates);
-        scheduleModel.put("publishInProgress", publishInProgress);
-        
-        // create model object with just the schedule data
-        Map<String, Object> model = new HashMap<String, Object>(1);
-        model.put("schedule", scheduleModel);
-        return model;
+        // return the disposition schedule model
+        return getDispositionScheduleModel(req);
     }
 }

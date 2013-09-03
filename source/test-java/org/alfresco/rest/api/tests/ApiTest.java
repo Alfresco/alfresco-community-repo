21c2e26c2ffb523890246972e5ebeec84c980eaa
Derek Hulley <derek.hulley@alfresco.com>
2013-09-03 20:41:40 +0000

    Fomatting in passing
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@54868 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/test-java/org/alfresco/rest/api/tests/ApiTest.java
+++ source/test-java/org/alfresco/rest/api/tests/ApiTest.java
@@ -12,31 +12,31 @@ import org.junit.runners.Suite;
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-	TestSites.class,
-	TestNodeComments.class,
-	TestCMIS.class,
-	TestFavouriteSites.class,
-	TestSiteContainers.class,
-	TestNodeRatings.class,
-	TestUserPreferences.class,
-	TestTags.class,
-	TestNetworks.class,
-	TestActivities.class,
-	TestPeople.class,
-	TestSiteMembers.class,
-	TestPersonSites.class,
-	TestSiteMembershipRequests.class,
-	TestFavourites.class,
-	TestPublicApi128.class,
-//	TestEnterpriseAtomPubTCK.class,
-	TestPublicApiAtomPubTCK.class,
-	TestPublicApiBrowserTCK.class
+    TestSites.class,
+    TestNodeComments.class,
+    TestCMIS.class,
+    TestFavouriteSites.class,
+    TestSiteContainers.class,
+    TestNodeRatings.class,
+    TestUserPreferences.class,
+    TestTags.class,
+    TestNetworks.class,
+    TestActivities.class,
+    TestPeople.class,
+    TestSiteMembers.class,
+    TestPersonSites.class,
+    TestSiteMembershipRequests.class,
+    TestFavourites.class,
+    TestPublicApi128.class,
+//    TestEnterpriseAtomPubTCK.class,
+    TestPublicApiAtomPubTCK.class,
+    TestPublicApiBrowserTCK.class
 })
 public class ApiTest
 {
-	@AfterClass
-	public static void after() throws Exception
-	{
-//		EnterprisePublicApiTestFixture.cleanup();
-	}
+    @AfterClass
+    public static void after() throws Exception
+    {
+//        EnterprisePublicApiTestFixture.cleanup();
+    }
 }

ca94ab4aeb80a0dadd45bd2be1f357cbae286525
Roy Wetherall <roy.wetherall@alfresco.com>
2013-09-06 05:14:15 +0000

    RM-926: File plan filters (Hold, Transfer, Unfiled) may not show up to date results when Rm configured for SOLR
      * unfiled filter is now backed by a canned query so shouldn't be effected by SOLR indexes being out of date
      * todo holds and transfers
    
    
    
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/HEAD@55004 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- rm-server/config/alfresco/templates/webscripts/org/alfresco/slingshot/documentlibrary-v2/rm-doclist.get.js
+++ rm-server/config/alfresco/templates/webscripts/org/alfresco/slingshot/documentlibrary-v2/rm-doclist.get.js
@@ -3,7 +3,6 @@
 <import resource="classpath:/alfresco/templates/webscripts/org/alfresco/slingshot/documentlibrary-v2/rm-filters.lib.js">
 <import resource="classpath:/alfresco/templates/webscripts/org/alfresco/slingshot/documentlibrary-v2/parse-args.lib.js">
 <import resource="classpath:/alfresco/templates/webscripts/org/alfresco/slingshot/documentlibrary-v2/rm-parse-args.lib.js">
-<import resource="classpath:/alfresco/templates/webscripts/org/alfresco/slingshot/documentlibrary-v2/doclist.lib.js">
 <import resource="classpath:/alfresco/templates/webscripts/org/alfresco/slingshot/documentlibrary-v2/rm-doclist.lib.js">
 
 /**

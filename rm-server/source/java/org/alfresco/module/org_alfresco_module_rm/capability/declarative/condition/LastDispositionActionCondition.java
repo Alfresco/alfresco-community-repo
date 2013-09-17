8ca1a310aff514afcb063b73fe2d213065e5bdb7
Roy Wetherall <roy.wetherall@alfresco.com>
2013-09-17 03:37:09 +0000

    RM-967: Disposition actions appearing for records when disposition set to record folder
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/HEAD@55374 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- rm-server/source/java/org/alfresco/module/org_alfresco_module_rm/capability/declarative/condition/LastDispositionActionCondition.java
+++ rm-server/source/java/org/alfresco/module/org_alfresco_module_rm/capability/declarative/condition/LastDispositionActionCondition.java
@@ -50,18 +50,6 @@ public class LastDispositionActionCondition extends AbstractCapabilityCondition
     @Override
     public boolean evaluate(NodeRef nodeRef)
     {
-        boolean result = evaluateImpl(nodeRef);
-        
-        if (result == false && recordService.isRecord(nodeRef) == true)
-        {
-            result = evaluateImpl(nodeService.getPrimaryParent(nodeRef).getParentRef());
-        }
-        
-        return result;
-    }
-    
-    private boolean evaluateImpl(NodeRef nodeRef)
-    {
         boolean result = false;
         DispositionAction dispositionAction = dispositionService.getLastCompletedDispostionAction(nodeRef);
         if (dispositionAction != null && 
@@ -69,7 +57,6 @@ public class LastDispositionActionCondition extends AbstractCapabilityCondition
         {
             result = true;
         }
-        return result;        
+        return result; 
     }
-
 }

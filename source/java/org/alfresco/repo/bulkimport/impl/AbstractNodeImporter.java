acaee2e905bbc23c96614d1cfbc2305cd91e6af0
Alan Davis <alan.davis@alfresco.com>
2013-11-19 10:52:23 +0000

    Merged V4.1-BUG-FIX (4.1.8) to V4.2-BUG-FIX (4.2.1)    57979: Merged V4.1.7 (4.1.7) to V4.1-BUG-FIX (4.1.8)       57968: Merged DEV to V4.1.7 (4.1.7)          57967: MNT-9989 : CLONE - Meta-data from bulk-import not correct          Changed log level to WARN for the file/folder that cannot be created          while import.          Changed the logic to fully skip the inexistent files.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/BRANCHES/DEV/V4.2-BUG-FIX/root@57982 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/java/org/alfresco/repo/bulkimport/impl/AbstractNodeImporter.java
+++ source/java/org/alfresco/repo/bulkimport/impl/AbstractNodeImporter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005-2011 Alfresco Software Limited.
+ * Copyright (C) 2005-2013 Alfresco Software Limited.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License

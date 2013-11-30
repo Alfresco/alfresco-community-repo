dcb4acb449dadbb740aac03faa01a6d77aa22137
Tuna Aksoy <tuna.aksoy@alfresco.com>
2013-11-30 21:26:05 +0000

    RM-1099 (Refactor Transfer and Accession reports)  * Created a base report action and splitted the file report action to transfer report action and destruction report action, so that capability checks can be run for those actions separately and each action can pass additional properties to the report template model
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/HEAD@58444 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

---
+++ rm-server/source/java/org/alfresco/module/org_alfresco_module_rm/report/action/DestructionReportAction.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2005-2013 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.alfresco.module.org_alfresco_module_rm.report.action;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.alfresco.service.cmr.repository.NodeRef;
+
+/**
+ * Destruction report action
+ *
+ * @author Tuna Aksoy
+ * @since 2.2
+ */
+public class DestructionReportAction extends BaseReportAction
+{
+    @Override
+    protected Map<String, Serializable> addProperties(NodeRef nodeRef)
+    {
+        return new HashMap<String, Serializable>(1);
+    }
+}

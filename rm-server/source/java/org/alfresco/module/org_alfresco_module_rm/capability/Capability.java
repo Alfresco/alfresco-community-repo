ebe454f13e14fbd65cd12843a277c242a3a84a8a
Roy Wetherall <roy.wetherall@alfresco.com>
2014-01-08 06:38:59 +0000

    Revert to original code structure pre-mavenization
      * merges from previous branches are now possible without tree conflicts
      * added back missing commits when structure was changed (r59445, r59446) .. see RM-765
      * updated Maven POM's to use existing code structure
      * NOTE: r59454 and r59473 may have been missed in this update .. will go back and re-add
    
    
    
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/HEAD@59491 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

---
+++ rm-server/source/java/org/alfresco/module/org_alfresco_module_rm/capability/Capability.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (C) 2005-2013 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.alfresco.module.org_alfresco_module_rm.capability;
+
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.security.AccessStatus;
+
+/**
+ * Capability Interface.
+ *
+ * @author andyh
+ * @author Roy Wetherall
+ */
+public interface Capability
+{
+    /**
+     * Does this capability apply to this nodeRef?
+     * @param nodeRef
+     * @return
+     */
+    AccessStatus hasPermission(NodeRef nodeRef);
+
+    /**
+     *
+     * @param nodeRef
+     * @return
+     */
+    int hasPermissionRaw(NodeRef nodeRef);
+
+    /**
+     * Evaluates the capability.
+     *
+     * @param nodeRef
+     * @return
+     */
+    int evaluate(NodeRef nodeRef);
+
+    /**
+     * Evaluates the capability, taking into account a target.
+     *
+     * @param source    source node reference
+     * @param target    target node reference
+     * @return int      permission value
+     */
+    int evaluate(NodeRef source, NodeRef target);
+
+    /**
+     * Indicates whether this is a private capability or not.  Private capabilities are used internally, otherwise
+     * they are made available to the user to assign to roles.
+     *
+     * @return  boolean true if private, false otherwise
+     */
+    boolean isPrivate();
+
+    /**
+     * Get the name of the capability
+     *
+     * @return  String  capability name
+     */
+    String getName();
+
+    /**
+     * Get the title of the capability
+     *
+     * @return  String  capability title
+     */
+    String getTitle();
+
+    /**
+     * Get the description of the capability
+     *
+     * @return  String  capability description
+     */
+    String getDescription();
+
+    /**
+     * Gets the group of a capability
+     *
+     * @return Group capability group
+     */
+    Group getGroup();
+
+    /**
+     * Gets the index of a capability
+     *
+     * @return int capability index
+     */
+    int getIndex();
+}

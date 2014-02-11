f6104e80a9a24989c0064cf680fd99fd3e8f5152
Alan Davis <alan.davis@alfresco.com>
2014-02-11 22:19:28 +0000

    Merged HEAD-BUG-FIX (4.3/Cloud) to HEAD (4.3/Cloud)    57852: BDE-195 Try to fix remote-api tests in Maven build: do not run rest API twice, and use JUnit category to run some tests in their own JVM
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@61897 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

---
+++ source/test-java/org/alfresco/test_category/OwnJVMTestsCategory.java
@@ -0,0 +1,11 @@
+package org.alfresco.test_category;
+
+/**
+ * Marker interface to group tests which require their own JVM.
+ *
+ * @author Jamal Kaabi-Mofrad
+ */
+public interface OwnJVMTestsCategory
+{
+
+}

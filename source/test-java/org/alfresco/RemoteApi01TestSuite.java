6f87d32e0b05e856f41d580a64a47de41587427f
Alan Davis <alan.davis@alfresco.com>
2014-02-12 10:27:41 +0000

    Merged HEAD-BUG-FIX (4.3/Cloud) to HEAD (4.3/Cloud)    60131: Merged V4.2-BIG-FIX (4.2.2) to HEAD-BUG-FIX (Cloud/4.3)       60130: Merged V4.2.1 (4.2.1) to V4.2-BIG-FIX (4.2.2)          60129: BDE-239 (4.2) Identify tests starting the repository which are not in suites             - Merged DEV to Merged DEV to V4.1.2 (4.2.1)                59645,59650-59651,59653-59654,59663,59750-59751,59754-59755,59758,59762-59763,          	  59768,59770,59834,59841,59946,60111,60115
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@62270 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

---
+++ source/test-java/org/alfresco/RemoteApi01TestSuite.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2005-2014 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.alfresco;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import junit.framework.JUnit4TestAdapter;
+
+/**
+ * All Remote API project test classes and test suites as a sequence of RemoteApi&lt;NN>TestSuite
+ * classes. The original order is the same as run by ant to avoid any data issues.
+ * The new test suite boundaries exist to allow tests to have different suite setups.
+ * It is better to have &lt;NN> startups than one for each test. 
+ */
+public class RemoteApi01TestSuite extends TestSuite
+{
+    /**
+     * Creates the test suite
+     *
+     * @return  the test suite
+     */
+    public static Test suite()
+    {
+        TestSuite suite = new TestSuite();
+
+        tests1(suite);
+
+        return suite;
+    }
+    
+    static void tests1(TestSuite suite)
+    {
+        suite.addTestSuite(org.alfresco.cmis.changelog.CMISChangeLogServiceTest.class);
+        suite.addTestSuite(org.alfresco.repo.cmis.rest.test.AspectTest.class);
+        suite.addTestSuite(org.alfresco.repo.cmis.rest.test.CMISAtomTemplatesTest.class);
+    }
+    
+    static void tests2(TestSuite suite) // 
+    {
+        suite.addTestSuite(org.alfresco.repo.cmis.rest.test.PolicyTest.class);
+    }
+    
+    static void tests3(TestSuite suite) // 
+    {
+        suite.addTestSuite(org.alfresco.repo.management.subsystems.test.SubsystemsTest.class);
+        suite.addTestSuite(org.alfresco.repo.remoteticket.RemoteAlfrescoTicketServiceTest.class);
+    }
+    
+    static void tests4(TestSuite suite) // 
+    {
+        suite.addTest(org.alfresco.repo.web.scripts.WebScriptTestSuite.suite());
+    }
+    
+    static void tests5(TestSuite suite) // 
+    {
+        suite.addTestSuite(org.alfresco.repo.web.util.PagingCursorTest.class);
+        suite.addTestSuite(org.alfresco.repo.web.util.paging.PagingTest.class);
+        suite.addTest(new JUnit4TestAdapter(org.alfresco.repo.webdav.GetMethodTest.class));
+        suite.addTest(new JUnit4TestAdapter(org.alfresco.repo.webdav.LockInfoImplTest.class));
+        suite.addTest(new JUnit4TestAdapter(org.alfresco.repo.webdav.MoveMethodTest.class));
+        suite.addTest(new JUnit4TestAdapter(org.alfresco.repo.webdav.RenameShuffleDetectionTest.class));
+        suite.addTest(new JUnit4TestAdapter(org.alfresco.repo.webdav.UnlockMethodTest.class));
+        suite.addTest(new JUnit4TestAdapter(org.alfresco.repo.webdav.WebDAVHelperIntegrationTest.class));
+        suite.addTest(new JUnit4TestAdapter(org.alfresco.repo.webdav.WebDAVHelperTest.class));
+        suite.addTest(new JUnit4TestAdapter(org.alfresco.repo.webdav.WebDAVLockServiceImplTest.class));
+        suite.addTest(new JUnit4TestAdapter(org.alfresco.repo.webdav.WebDAVMethodTest.class));
+        suite.addTestSuite(org.alfresco.repo.webservice.CMLUtilTest.class);
+    }
+    
+    static void tests6(TestSuite suite) // 
+    {
+        // TestNodeComments.testNodeComments() fails 50% of the time we with previous tests
+        // TestCMIS.testCMIS()                 fails 30% of the time with previous tests
+        suite.addTest(new JUnit4TestAdapter(org.alfresco.rest.api.tests.ApiTest.class));
+    }
+    
+    static void tests7(TestSuite suite) // 
+    {
+        suite.addTest(new JUnit4TestAdapter(org.alfresco.rest.api.tests.TestCMISAPI.class)); // Fails with previous or following tests
+    }
+    
+    static void tests8(TestSuite suite) // 
+    {
+        suite.addTest(new JUnit4TestAdapter(org.alfresco.rest.framework.tests.core.AllRestFrameworkTest.class));
+        suite.addTest(new JUnit4TestAdapter(org.alfresco.rest.workflow.api.tests.DeploymentWorkflowApiTest.class));
+        suite.addTest(new JUnit4TestAdapter(org.alfresco.rest.workflow.api.tests.ProcessDefinitionWorkflowApiTest.class));
+    }
+    
+    static void tests9(TestSuite suite) // 
+    {
+        suite.addTest(new JUnit4TestAdapter(org.alfresco.rest.workflow.api.tests.ProcessWorkflowApiTest.class));
+        suite.addTest(new JUnit4TestAdapter(org.alfresco.rest.workflow.api.tests.TaskWorkflowApiTest.class));
+    }
+}

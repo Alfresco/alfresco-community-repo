1cc54d1d2443318d976c061a4cc1d7ddb3f8906a
Alan Davis <alan.davis@alfresco.com>
2014-02-12 12:48:57 +0000

    Merged HEAD-BUG-FIX (4.3/Cloud) to HEAD (4.3/Cloud)    60178: Merged V4.2-BUG-FIX (4.2.2) to HEAD-BUG-FIX (Cloud/4.3)       60175: Merged V4.2.1 (4.2.1) to V4.2-BUG-FIX (4.2.2)          60174: Merged DEV (V4.2.1-2014_01_09) to V4.2.1 (4.2.1)             60167: ACE-183 Intermittent test failure: LockStoreImplTxTest testReadsWhenNoTransaction                - Stop running LockStoreImplTxTest - testReadsWhenNoTransaction (Matt says there is little value)
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@62297 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/test-java/org/alfresco/repo/lock/mem/AbstractLockStoreTxTest.java
+++ source/test-java/org/alfresco/repo/lock/mem/AbstractLockStoreTxTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005-2013 Alfresco Software Limited.
+ * Copyright (C) 2005-2014 Alfresco Software Limited.
  *
  * This file is part of Alfresco
  *
@@ -202,7 +202,7 @@ public abstract class AbstractLockStoreTxTest<T extends LockStore>
         }
     }
     
-    @Test
+    // @Test - only run by HazelcastLockStoreTxTest - fails a lot in HazelcastLockStoreTxTest
     public void testReadsWhenNoTransaction() throws NotSupportedException, SystemException
     {
         final NodeRef nodeRef = new NodeRef("workspace://SpacesStore/UUID-1");

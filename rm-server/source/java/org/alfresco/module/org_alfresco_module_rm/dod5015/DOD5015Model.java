c352137a94ec793b67b58466c5fc8d96ef221b64
Tuna Aksoy <tuna.aksoy@alfresco.com>
2014-03-02 16:49:44 +0000

    Fixed minor issues reported by sonar (Redundant Modifier)
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/HEAD@63496 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- rm-server/source/java/org/alfresco/module/org_alfresco_module_rm/dod5015/DOD5015Model.java
+++ rm-server/source/java/org/alfresco/module/org_alfresco_module_rm/dod5015/DOD5015Model.java
@@ -23,67 +23,67 @@ import org.alfresco.service.namespace.QName;
 
 /**
  * Helper class containing DOD 5015 model qualified names
- * 
+ *
  * @author Roy Wetherall
  */
 public interface DOD5015Model
-{	
-	// Namespace details
-	public static final String DOD_URI = "http://www.alfresco.org/model/dod5015/1.0";
-	public static final String DOD_PREFIX = "dod";
-	
-	// DOD Site
-	public static final QName TYPE_DOD_5015_SITE = QName.createQName(DOD_URI, "site");
-	
-	// DOD File Plan
-	public static final QName TYPE_DOD_5015_FILE_PLAN = QName.createQName(DOD_URI, "filePlan");
-	
-    // DOD Record	
-	public static final QName ASPECT_DOD_5015_RECORD = QName.createQName(DOD_URI, "dod5015record");
-	public static final QName PROP_ORIGINATOR = QName.createQName(DOD_URI, "originator");
-	public static final QName PROP_ORIGINATING_ORGANIZATION = QName.createQName(DOD_URI, "originatingOrganization");
-	public static final QName PROP_PUBLICATION_DATE = QName.createQName(DOD_URI, "publicationDate");
-	public static final QName PROP_MEDIA_TYPE = QName.createQName(DOD_URI, "mediaType");
-	public static final QName PROP_FORMAT = QName.createQName(DOD_URI, "format");
-	public static final QName PROP_DATE_RECEIVED = QName.createQName(DOD_URI, "dateReceived");
-		
+{
+    // Namespace details
+    static final String DOD_URI = "http://www.alfresco.org/model/dod5015/1.0";
+    static final String DOD_PREFIX = "dod";
+
+    // DOD Site
+    static final QName TYPE_DOD_5015_SITE = QName.createQName(DOD_URI, "site");
+
+    // DOD File Plan
+    static final QName TYPE_DOD_5015_FILE_PLAN = QName.createQName(DOD_URI, "filePlan");
+
+    // DOD Record
+    static final QName ASPECT_DOD_5015_RECORD = QName.createQName(DOD_URI, "dod5015record");
+    static final QName PROP_ORIGINATOR = QName.createQName(DOD_URI, "originator");
+    static final QName PROP_ORIGINATING_ORGANIZATION = QName.createQName(DOD_URI, "originatingOrganization");
+    static final QName PROP_PUBLICATION_DATE = QName.createQName(DOD_URI, "publicationDate");
+    static final QName PROP_MEDIA_TYPE = QName.createQName(DOD_URI, "mediaType");
+    static final QName PROP_FORMAT = QName.createQName(DOD_URI, "format");
+    static final QName PROP_DATE_RECEIVED = QName.createQName(DOD_URI, "dateReceived");
+
     // Scanned Record
-    public static final QName ASPECT_SCANNED_RECORD = QName.createQName(DOD_URI, "scannedRecord");
-    public static final QName PROP_SCANNED_FORMAT = QName.createQName(DOD_URI, "scannedFormat");
-    public static final QName PROP_SCANNED_FORMAT_VERSION = QName.createQName(DOD_URI, "scannedFormatVersion");
-    public static final QName PROP_RESOLUTION_X = QName.createQName(DOD_URI, "resolutionX");
-    public static final QName PROP_RESOLUTION_Y = QName.createQName(DOD_URI, "resolutionY");
-    public static final QName PROP_SCANNED_BIT_DEPTH = QName.createQName(DOD_URI, "scannedBitDepth");
+    static final QName ASPECT_SCANNED_RECORD = QName.createQName(DOD_URI, "scannedRecord");
+    static final QName PROP_SCANNED_FORMAT = QName.createQName(DOD_URI, "scannedFormat");
+    static final QName PROP_SCANNED_FORMAT_VERSION = QName.createQName(DOD_URI, "scannedFormatVersion");
+    static final QName PROP_RESOLUTION_X = QName.createQName(DOD_URI, "resolutionX");
+    static final QName PROP_RESOLUTION_Y = QName.createQName(DOD_URI, "resolutionY");
+    static final QName PROP_SCANNED_BIT_DEPTH = QName.createQName(DOD_URI, "scannedBitDepth");
 
     // PDF Record
-    public static final QName ASPECT_PDF_RECORD = QName.createQName(DOD_URI, "pdfRecord");
-    public static final QName PROP_PRODUCING_APPLICATION = QName.createQName(DOD_URI, "producingApplication");
-    public static final QName PROP_PRODUCING_APPLICATION_VERSION = QName.createQName(DOD_URI, "producingApplicationVersion");
-    public static final QName PROP_PDF_VERSION = QName.createQName(DOD_URI, "pdfVersion");
-    public static final QName PROP_CREATING_APPLICATION = QName.createQName(DOD_URI, "creatingApplication");
-    public static final QName PROP_DOCUMENT_SECURITY_SETTINGS = QName.createQName(DOD_URI, "documentSecuritySettings");
+    static final QName ASPECT_PDF_RECORD = QName.createQName(DOD_URI, "pdfRecord");
+    static final QName PROP_PRODUCING_APPLICATION = QName.createQName(DOD_URI, "producingApplication");
+    static final QName PROP_PRODUCING_APPLICATION_VERSION = QName.createQName(DOD_URI, "producingApplicationVersion");
+    static final QName PROP_PDF_VERSION = QName.createQName(DOD_URI, "pdfVersion");
+    static final QName PROP_CREATING_APPLICATION = QName.createQName(DOD_URI, "creatingApplication");
+    static final QName PROP_DOCUMENT_SECURITY_SETTINGS = QName.createQName(DOD_URI, "documentSecuritySettings");
 
     // Digital Photograph Record
-    public static final QName ASPECT_DIGITAL_PHOTOGRAPH_RECORD = QName.createQName(DOD_URI, "digitalPhotographRecord");
-    public static final QName PROP_CAPTION = QName.createQName(DOD_URI, "caption");
-    public static final QName PROP_PHOTOGRAPHER = QName.createQName(DOD_URI, "photographer");
-    public static final QName PROP_COPYRIGHT = QName.createQName(DOD_URI, "copyright");
-    public static final QName PROP_BIT_DEPTH = QName.createQName(DOD_URI, "bitDepth");
-    public static final QName PROP_IMAGE_SIZE_X = QName.createQName(DOD_URI, "imageSizeX");
-    public static final QName PROP_IMAGE_SIZE_Y = QName.createQName(DOD_URI, "imageSizeY");
-    public static final QName PROP_IMAGE_SOURCE = QName.createQName(DOD_URI, "imageSource");
-    public static final QName PROP_COMPRESSION = QName.createQName(DOD_URI, "compression");
-    public static final QName PROP_ICC_ICM_PROFILE = QName.createQName(DOD_URI, "iccIcmProfile");
-    public static final QName PROP_EXIF_INFORMATION = QName.createQName(DOD_URI, "exifInformation");
+    static final QName ASPECT_DIGITAL_PHOTOGRAPH_RECORD = QName.createQName(DOD_URI, "digitalPhotographRecord");
+    static final QName PROP_CAPTION = QName.createQName(DOD_URI, "caption");
+    static final QName PROP_PHOTOGRAPHER = QName.createQName(DOD_URI, "photographer");
+    static final QName PROP_COPYRIGHT = QName.createQName(DOD_URI, "copyright");
+    static final QName PROP_BIT_DEPTH = QName.createQName(DOD_URI, "bitDepth");
+    static final QName PROP_IMAGE_SIZE_X = QName.createQName(DOD_URI, "imageSizeX");
+    static final QName PROP_IMAGE_SIZE_Y = QName.createQName(DOD_URI, "imageSizeY");
+    static final QName PROP_IMAGE_SOURCE = QName.createQName(DOD_URI, "imageSource");
+    static final QName PROP_COMPRESSION = QName.createQName(DOD_URI, "compression");
+    static final QName PROP_ICC_ICM_PROFILE = QName.createQName(DOD_URI, "iccIcmProfile");
+    static final QName PROP_EXIF_INFORMATION = QName.createQName(DOD_URI, "exifInformation");
 
     // Web Record
-    public static final QName ASPECT_WEB_RECORD = QName.createQName(DOD_URI, "webRecord");
-    public static final QName PROP_WEB_FILE_NAME = QName.createQName(DOD_URI, "webFileName");
-    public static final QName PROP_WEB_PLATFORM = QName.createQName(DOD_URI, "webPlatform");
-    public static final QName PROP_WEBSITE_NAME = QName.createQName(DOD_URI, "webSiteName");
-    public static final QName PROP_WEB_SITE_URL = QName.createQName(DOD_URI, "webSiteURL");
-    public static final QName PROP_CAPTURE_METHOD = QName.createQName(DOD_URI, "captureMethod");
-    public static final QName PROP_CAPTURE_DATE = QName.createQName(DOD_URI, "captureDate");
-    public static final QName PROP_CONTACT = QName.createQName(DOD_URI, "contact");
-    public static final QName PROP_CONTENT_MANAGEMENT_SYSTEM = QName.createQName(DOD_URI, "contentManagementSystem");    
+    static final QName ASPECT_WEB_RECORD = QName.createQName(DOD_URI, "webRecord");
+    static final QName PROP_WEB_FILE_NAME = QName.createQName(DOD_URI, "webFileName");
+    static final QName PROP_WEB_PLATFORM = QName.createQName(DOD_URI, "webPlatform");
+    static final QName PROP_WEBSITE_NAME = QName.createQName(DOD_URI, "webSiteName");
+    static final QName PROP_WEB_SITE_URL = QName.createQName(DOD_URI, "webSiteURL");
+    static final QName PROP_CAPTURE_METHOD = QName.createQName(DOD_URI, "captureMethod");
+    static final QName PROP_CAPTURE_DATE = QName.createQName(DOD_URI, "captureDate");
+    static final QName PROP_CONTACT = QName.createQName(DOD_URI, "contact");
+    static final QName PROP_CONTENT_MANAGEMENT_SYSTEM = QName.createQName(DOD_URI, "contentManagementSystem");
 }

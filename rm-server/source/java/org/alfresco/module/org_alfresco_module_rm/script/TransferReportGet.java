90575729d4b5289369086872d89b8960f2b6b4ee
Tuna Aksoy <tuna.aksoy@alfresco.com>
2014-03-22 16:21:52 +0000

    Fixed major issues reported by sonar (Reliance on default encoding)
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/HEAD@65399 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- rm-server/source/java/org/alfresco/module/org_alfresco_module_rm/script/TransferReportGet.java
+++ rm-server/source/java/org/alfresco/module/org_alfresco_module_rm/script/TransferReportGet.java
@@ -19,9 +19,11 @@
 package org.alfresco.module.org_alfresco_module_rm.script;
 
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.nio.charset.Charset;
 import java.util.Date;
 import java.util.List;
 
@@ -128,7 +130,7 @@ public class TransferReportGet extends BaseTransferWebScript
             }
 
             // create the writer
-            writer = new FileWriter(report);
+            writer = new OutputStreamWriter(new FileOutputStream(report), Charset.forName("UTF-8"));
 
             // use RMService to get disposition authority
             String dispositionAuthority = null;
--- rm-server/source/java/org/alfresco/module/org_alfresco_module_rm/script/TransferReportPost.java
+++ rm-server/source/java/org/alfresco/module/org_alfresco_module_rm/script/TransferReportPost.java
@@ -19,10 +19,12 @@
 package org.alfresco.module.org_alfresco_module_rm.script;
 
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.io.Serializable;
 import java.io.Writer;
+import java.nio.charset.Charset;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -236,7 +238,7 @@ public class TransferReportPost extends BaseTransferWebScript
             }
 
             // create the writer
-            writer = new FileWriter(report);
+            writer = new OutputStreamWriter(new FileOutputStream(report), Charset.forName("UTF-8"));
 
             // use RMService to get disposition authority
             String dispositionAuthority = null;

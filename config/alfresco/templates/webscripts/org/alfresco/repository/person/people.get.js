0ee790b33b38b3d6e47cde5a3a1e1afac2fa162f
Pavel Yurke <Pavel.Yurkevich@synesis.ru>
2014-03-24 13:03:59 +0000

    Merged DEV to HEAD (4.3.0)    65440: ACE-1150: Sorting the list of user does not work       - people.get.js was corrected to take in account the sortBy and sortAsc paramenters.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@65463 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- config/alfresco/templates/webscripts/org/alfresco/repository/person/people.get.js
+++ config/alfresco/templates/webscripts/org/alfresco/repository/person/people.get.js
@@ -5,7 +5,8 @@ var sortBy = args["sortBy"];
 var sortAsc = args["dir"] != "desc";
 
 // Get the collection of people
-var peopleCollection = people.getPeople(filter, maxResults != null ? parseInt(maxResults) : 0, sortBy, sortAsc);
+var paging = utils.createPaging(maxResults != null ? parseInt(maxResults) : 0, -1);
+var peopleCollection = people.getPeoplePaging(filter, paging, sortBy, sortAsc);
 
 // Pass the queried sites to the template
 model.peoplelist = peopleCollection;

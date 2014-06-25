89c79796cbe6f126e4bfd15945f1d8b6079a4fe3
Will Abson <will.abson@alfresco.com>
2014-06-25 16:34:55 +0000

    Merged HEAD-BUG-FIX (5.0/Cloud) to HEAD (4.3/Cloud)    74317: Merged V4.2-BUG-FIX (4.2.3) to HEAD-BUG-FIX (5.0/Cloud)       74274: Merged DEV to V4.2-BUG-FIX (4.2.3)          73971 : MNT-11227 : getObjectResponse does not provide a valid xml response against Alfresco-Core.xsd              - Changing order of elements in xml response for getObject method
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@74897 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/test-java/org/alfresco/repo/cmis/rest/test/CMISAtomTemplatesTest.java
+++ source/test-java/org/alfresco/repo/cmis/rest/test/CMISAtomTemplatesTest.java
@@ -19,8 +19,17 @@
 package org.alfresco.repo.cmis.rest.test;
 
 import java.io.Serializable;
+import java.io.StringReader;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.Source;
+import javax.xml.transform.sax.SAXSource;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
 
 import org.alfresco.model.ApplicationModel;
 import org.alfresco.model.ContentModel;
@@ -28,6 +37,7 @@ import org.alfresco.repo.security.authentication.AuthenticationUtil;
 import org.alfresco.service.cmr.repository.ChildAssociationRef;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
+import org.alfresco.service.namespace.NamespaceService;
 import org.alfresco.service.namespace.QName;
 import org.apache.abdera.model.Entry;
 import org.apache.abdera.model.Link;
@@ -37,6 +47,7 @@ import org.junit.Test;
 import org.springframework.extensions.webscripts.TestWebScriptServer.GetRequest;
 import org.springframework.extensions.webscripts.TestWebScriptServer.Request;
 import org.springframework.extensions.webscripts.TestWebScriptServer.Response;
+import org.xml.sax.InputSource;
 
 public class CMISAtomTemplatesTest extends BaseCMISTest
 {
@@ -129,5 +140,79 @@ public class CMISAtomTemplatesTest extends BaseCMISTest
         
 
         
+    }
+    
+    @Test
+    public void testCheckXmlResponse() throws Exception
+    {
+        String testFolderRefStr = testCaseFolder.getId().toString().replace("urn:uuid:", "workspace://SpacesStore/");
+        NodeRef testFolderRef = new NodeRef(testFolderRefStr);
+        
+        Map<QName, Serializable> properties = new HashMap<QName, Serializable>();
+        properties.put(ContentModel.PROP_NAME, "testcontent");
+        properties.put(ContentModel.PROP_DESCRIPTION, "content - test doc for test");
+
+        AuthenticationUtil.pushAuthentication();
+        AuthenticationUtil.setAdminUserAsFullyAuthenticatedUser();
+        
+        NodeRef testDocNodeRef = null;
+        try
+        {
+            ChildAssociationRef testDoc = nodeService.createNode(testFolderRef, ContentModel.ASSOC_CONTAINS,
+                    QName.createQName(NamespaceService.CONTENT_MODEL_1_0_URI, "testcontent"), ContentModel.TYPE_CONTENT, properties);
+            testDocNodeRef = testDoc.getChildRef();
+            properties.clear();
+            properties.put(ApplicationModel.PROP_EDITINLINE, true);
+            nodeService.addAspect(testDocNodeRef, ApplicationModel.ASPECT_INLINEEDITABLE, properties);
+            
+            String id = testDocNodeRef.toString().replace("workspace://SpacesStore/", "workspace:SpacesStore/i/");
+            Request get = new GetRequest("/cmis/s/" + id);
+            Response res = sendRequest(get, 200);
+            String xml = res.getContentAsString();
+            
+            try
+            {
+                validateXmlResponse(xml);
+            }
+            catch (Exception e)
+            {
+                fail(e.getMessage());
+            }
+
+        }
+        finally
+        {
+            if (testDocNodeRef != null)
+            {
+                nodeService.deleteNode(testDocNodeRef);
+            }
+            AuthenticationUtil.popAuthentication();
+        }
+    }
+    
+    private void validateXmlResponse(String xml) throws Exception
+    {
+        Source[] shemas = new Source[] { 
+                new StreamSource(new StringReader(localTemplates.load("xml.xsd"))),
+                new StreamSource(new StringReader(localTemplates.load("atom.xsd"))),
+                new StreamSource(new StringReader(localTemplates.load("app.xsd"))),
+                new StreamSource(new StringReader(localTemplates.load("CMIS-Core.xsd"))), 
+                new StreamSource(new StringReader(localTemplates.load("envelope.xsd"))), 
+                new StreamSource(new StringReader(localTemplates.load("Alfresco-Core.xsd")))
+                };
+        
+        Schema schema = null;
+        String language = XMLConstants.W3C_XML_SCHEMA_NS_URI;
+        SchemaFactory factory = SchemaFactory.newInstance(language);
+        schema = factory.newSchema(shemas);
+
+        // creating a Validator instance
+        Validator validator = schema.newValidator();
+
+        // preparing the XML file as a SAX source
+        SAXSource source = new SAXSource(new InputSource(new StringReader(xml)));
+
+        // validating the SAX source against the schema
+        validator.validate(source);
     }
 }

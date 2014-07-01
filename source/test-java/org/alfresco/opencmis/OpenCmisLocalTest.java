4892876aff2688c91c2daefd89f037cac748757f
Will Abson <will.abson@alfresco.com>
2014-07-01 15:12:19 +0000

    Merged HEAD-BUG-FIX (5.0/Cloud) to HEAD (5.0/Cloud)    75047: Merged WAT2 (5.0/Cloud) to HEAD-BUG-FIX (5.0/Cloud)       71978: Re-enabled cmis download test
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@75375 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/test-java/org/alfresco/opencmis/OpenCmisLocalTest.java
+++ source/test-java/org/alfresco/opencmis/OpenCmisLocalTest.java
@@ -32,7 +32,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import junit.framework.TestCase;
 
-import org.alfresco.events.types.ContentEvent;
 import org.alfresco.events.types.ContentReadEvent;
 import org.alfresco.events.types.ContentReadRangeEvent;
 import org.alfresco.repo.content.MimetypeMap;
@@ -66,7 +65,6 @@ import org.apache.chemistry.opencmis.commons.server.CallContext;
 import org.apache.chemistry.opencmis.commons.server.CmisService;
 import org.apache.chemistry.opencmis.server.shared.ThresholdOutputStream;
 import org.apache.chemistry.opencmis.server.shared.ThresholdOutputStreamFactory;
-import org.junit.Ignore;
 import org.junit.experimental.categories.Category;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.context.ApplicationContext;
@@ -182,7 +180,6 @@ public class OpenCmisLocalTest extends TestCase
         folder.createDocument(fileProps, fileContent, VersioningState.MAJOR);
     }
     
-    @Ignore
     public void testDownloadEvent() throws InterruptedException
     {
         Repository repository = getRepository("admin", "admin");

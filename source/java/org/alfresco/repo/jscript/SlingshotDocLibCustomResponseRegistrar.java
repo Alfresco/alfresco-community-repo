5d80ebd215ea390c2bbba06e56e17ffd36193879
Will Abson <will.abson@alfresco.com>
2014-08-06 08:14:03 +0000

    Merged HEAD-BUG-FIX (5.0/Cloud) to HEAD (5.0/Cloud)    78695: ACE-2309: Edit in Google Docs™ option missing      - Define a registrar bean which can be used by extensions to add custom responses to the original list defined in slingshot-context.xml      - Abstract bean is named registerSlingshotCustomResponse, developers just need to add the key and value properties to a new instance to add them
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@79004 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

---
+++ source/java/org/alfresco/repo/jscript/SlingshotDocLibCustomResponseRegistrar.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2005-2014 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.alfresco.repo.jscript;
+
+import java.util.Map;
+
+/**
+ * Register an additional custom metadata output for the DocLib webscript response
+ *
+ * @author: Will Abson
+ */
+public final class SlingshotDocLibCustomResponseRegistrar
+{
+    private Map<String, Object> responsesMap;
+    private String key;
+    private Object value;
+
+    public String getKey()
+    {
+        return key;
+    }
+
+    public void setKey(String key)
+    {
+        this.key = key;
+    }
+
+    public Object getValue()
+    {
+        return value;
+    }
+
+    public void setValue(Object value)
+    {
+        this.value = value;
+    }
+
+    public Map<String, Object> getResponsesMap()
+    {
+        return responsesMap;
+    }
+
+    public void setResponsesMap(Map<String, Object> responsesMap)
+    {
+        this.responsesMap = responsesMap;
+    }
+
+    public void addCustomResponse()
+    {
+        responsesMap.put(key, value);
+    }
+}

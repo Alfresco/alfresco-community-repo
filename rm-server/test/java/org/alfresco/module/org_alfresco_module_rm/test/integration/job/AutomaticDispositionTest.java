514af5404db1a48f9de2067c61aeb7b239e9430d
Tuna Aksoy <tuna.aksoy@alfresco.com>
2014-11-10 21:51:51 +0000

    RM-1771 (SOLR dependent integration tests failing)
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/HEAD@90204 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- rm-server/test/java/org/alfresco/module/org_alfresco_module_rm/test/integration/job/AutomaticDispositionTest.java
+++ rm-server/test/java/org/alfresco/module/org_alfresco_module_rm/test/integration/job/AutomaticDispositionTest.java
@@ -18,8 +18,19 @@
  */
 package org.alfresco.module.org_alfresco_module_rm.test.integration.job;
 
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.alfresco.module.org_alfresco_module_rm.action.impl.CutOffAction;
 import org.alfresco.module.org_alfresco_module_rm.audit.RecordsManagementAuditService;
+import org.alfresco.module.org_alfresco_module_rm.disposition.DispositionAction;
+import org.alfresco.module.org_alfresco_module_rm.disposition.DispositionSchedule;
 import org.alfresco.module.org_alfresco_module_rm.test.util.BaseRMTestCase;
+import org.alfresco.module.org_alfresco_module_rm.test.util.CommonRMTestUtils;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.namespace.QName;
+import org.alfresco.util.GUID;
 
 /**
  * Test automatic disposition via scheduled job.
@@ -57,63 +68,62 @@ public class AutomaticDispositionTest extends BaseRMTestCase
      * Given there is a complete record eligible for cut off, when the correct frequency of time passes, then
      * the record will be automatically cut off
      */
-//    FIXME!!!: Commented out. Will be fixed.
-//    public void testAutomaticCutOff()
-//    {
-//        doBehaviourDrivenTest(new BehaviourDrivenTest()
-//        {
-//            NodeRef sourceCategory;
-//            NodeRef sourceRecordFolder;
-//            NodeRef record;
-//
-//            public void given()
-//            {
-//                // create test data
-//                sourceCategory = filePlanService.createRecordCategory(filePlan, GUID.generate());
-//                DispositionSchedule dis = utils.createBasicDispositionSchedule(sourceCategory, GUID.generate(), GUID.generate(), true, false);
-//                Map<QName, Serializable> adParams = new HashMap<QName, Serializable>(3);
-//                adParams.put(PROP_DISPOSITION_ACTION_NAME, CutOffAction.NAME);
-//                adParams.put(PROP_DISPOSITION_DESCRIPTION, GUID.generate());
-//                adParams.put(PROP_DISPOSITION_PERIOD, CommonRMTestUtils.PERIOD_IMMEDIATELY);
-//                dispositionService.addDispositionActionDefinition(dis, adParams);
-//                sourceRecordFolder = recordFolderService.createRecordFolder(sourceCategory, GUID.generate());
-//
-//                // create and complete record
-//                record = utils.createRecord(sourceRecordFolder, GUID.generate());
-//                utils.completeRecord(record);
-//
-//                // check the disposition action details
-//                DispositionAction dispositionAction = dispositionService.getNextDispositionAction(record);
-//                assertNotNull(dispositionAction);
-//                assertNotNull(CutOffAction.NAME, dispositionAction.getName());
-//                assertTrue(dispositionService.isNextDispositionActionEligible(record));
-//            }
-//
-//            public void when() throws Exception
-//            {
-//                // sleep .. allowing the job time to execute
-//                Thread.sleep(30000);
-//            }
-//
-//            public void then()
-//            {
-//                // record should now be cut off
-//                assertTrue(dispositionService.isDisposableItemCutoff(record));
-//
-//                // TODO uncomment and ensure is working
-//
-//                //RecordsManagementAuditQueryParameters params = new RecordsManagementAuditQueryParameters();
-//                //params.setEvent(CutOffAction.NAME);
-//                //params.setMaxEntries(1);
-//                //List<RecordsManagementAuditEntry> entries = auditService.getAuditTrail(params);
-//                //assertNotNull(entries);
-//                //assertEquals(1, entries.size());
-//
-//                //RecordsManagementAuditEntry entry = entries.get(0);
-//                //assertEquals(record, entry.getNodeRef());
-//            }
-//        });
-//    }
+    public void testAutomaticCutOff()
+    {
+        doBehaviourDrivenTest(new BehaviourDrivenTest()
+        {
+            NodeRef sourceCategory;
+            NodeRef sourceRecordFolder;
+            NodeRef record;
+
+            public void given()
+            {
+                // create test data
+                sourceCategory = filePlanService.createRecordCategory(filePlan, GUID.generate());
+                DispositionSchedule dis = utils.createBasicDispositionSchedule(sourceCategory, GUID.generate(), GUID.generate(), true, false);
+                Map<QName, Serializable> adParams = new HashMap<QName, Serializable>(3);
+                adParams.put(PROP_DISPOSITION_ACTION_NAME, CutOffAction.NAME);
+                adParams.put(PROP_DISPOSITION_DESCRIPTION, GUID.generate());
+                adParams.put(PROP_DISPOSITION_PERIOD, CommonRMTestUtils.PERIOD_IMMEDIATELY);
+                dispositionService.addDispositionActionDefinition(dis, adParams);
+                sourceRecordFolder = recordFolderService.createRecordFolder(sourceCategory, GUID.generate());
+
+                // create and complete record
+                record = utils.createRecord(sourceRecordFolder, GUID.generate());
+                utils.completeRecord(record);
+
+                // check the disposition action details
+                DispositionAction dispositionAction = dispositionService.getNextDispositionAction(record);
+                assertNotNull(dispositionAction);
+                assertNotNull(CutOffAction.NAME, dispositionAction.getName());
+                assertTrue(dispositionService.isNextDispositionActionEligible(record));
+            }
+
+            public void when() throws Exception
+            {
+                // sleep .. allowing the job time to execute
+                Thread.sleep(30000);
+            }
+
+            public void then()
+            {
+                // record should now be cut off
+                assertTrue(dispositionService.isDisposableItemCutoff(record));
+
+                // TODO uncomment and ensure is working
+
+                //RecordsManagementAuditQueryParameters params = new RecordsManagementAuditQueryParameters();
+                //params.setEvent(CutOffAction.NAME);
+                //params.setMaxEntries(1);
+                //List<RecordsManagementAuditEntry> entries = auditService.getAuditTrail(params);
+                //assertNotNull(entries);
+                //assertEquals(1, entries.size());
+
+                //RecordsManagementAuditEntry entry = entries.get(0);
+                //assertEquals(record, entry.getNodeRef());
+            }
+        });
+    }
 
     // TODO automatic retain
 

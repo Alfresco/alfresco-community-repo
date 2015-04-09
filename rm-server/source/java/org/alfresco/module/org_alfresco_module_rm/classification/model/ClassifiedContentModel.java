2cc8ad2843bbcf9949c11deaf69ced7c6901007a
Roy Wetherall <roy.wetherall@alfresco.com>
2015-04-09 02:24:39 +0000

    Add stub classified content model
     * model I18n added
     * added Java contanstants via interface for convenience
     * includes a couple of renames for consistency
     * relates to RM-1947
    
    +review RM-18
    
    
    
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/HEAD@101539 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

---
+++ rm-server/source/java/org/alfresco/module/org_alfresco_module_rm/classification/model/ClassifiedContentModel.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2005-2015 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.alfresco.module.org_alfresco_module_rm.classification.model;
+
+/**
+ * Classified content model interface.
+ * <p>
+ * Helper containing reusable information about the classified content model.
+ * 
+ * @author Roy Wetherall
+ * @since 3.0
+ */
+public interface ClassifiedContentModel
+{
+    /** Namespace details */
+    String RM_URI = "http://www.alfresco.org/model/classifiedcontent/1.0";
+    String RM_PREFIX = "clf";
+}

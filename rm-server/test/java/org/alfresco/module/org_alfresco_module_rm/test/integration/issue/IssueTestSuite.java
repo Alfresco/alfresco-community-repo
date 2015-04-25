67157be80e0ab9735a716bce7af99f81dc891a51
Tuna Aksoy <tuna.aksoy@alfresco.com>
2015-04-25 13:51:08 +0000

    RM-2192 (User has no access to the recorded document after it was filed)
    +review RM @rwetherall
    
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/BRANCHES/V2.2.1.x@102687 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- rm-server/test/java/org/alfresco/module/org_alfresco_module_rm/test/integration/issue/IssueTestSuite.java
+++ rm-server/test/java/org/alfresco/module/org_alfresco_module_rm/test/integration/issue/IssueTestSuite.java
@@ -43,7 +43,8 @@ import org.junit.runners.Suite.SuiteClasses;
     RM994Test.class,
     RM1039Test.class,
     RM1799Test.class,
-    RM2190Test.class
+    RM2190Test.class,
+    RM2192Test.class
 })
 public class IssueTestSuite
 {

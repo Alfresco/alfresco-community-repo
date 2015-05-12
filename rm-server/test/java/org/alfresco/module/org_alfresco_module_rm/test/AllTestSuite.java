21893ae3f2faa7467990e6225cfe462a939475cd
Tuna Aksoy <tuna.aksoy@alfresco.com>
2015-05-12 19:58:34 +0000

    RM-2206 (Dynamic test suite configuration)
    +review RM-49
    
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/HEAD@104045 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- rm-server/test/java/org/alfresco/module/org_alfresco_module_rm/test/AllTestSuite.java
+++ rm-server/test/java/org/alfresco/module/org_alfresco_module_rm/test/AllTestSuite.java
@@ -33,7 +33,7 @@ import org.junit.runner.RunWith;
 @RunWith(ClasspathSuite.class)
 @SuiteTypes({SuiteType.TEST_CLASSES, SuiteType.RUN_WITH_CLASSES, SuiteType.JUNIT38_TEST_CLASSES})
 @ClassnameFilters({
-    // Execute all tests classes ending with "Test"
+    // Execute all test classes ending with "Test"
     ".*Test",
     // Exclude the ones ending with "UnitTest"
     "!.*UnitTest",
@@ -47,7 +47,9 @@ import org.junit.runner.RunWith;
     "!.*NotificationServiceHelperSystemTest",
     "!.*RetryingTransactionHelperBaseTest",
     "!.*RMCaveatConfigServiceImplTest",
-    // This test is running successfully locally but not on bamboo
+    // This test is running successfully locally but not on bamboo (if executed as a single test).
+    // The problem can be reproduced if the whole test suite is run locally as well.
+    // Tests should not be dependant on other test classes and should run in any order without any problems.
     "!.*EmailMapScriptTest"
 })
 public class AllTestSuite

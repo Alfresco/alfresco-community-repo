2ce49b1a2cb7be5b7c1765bc5ac3d588478003c4
Tatyana Valkevych <tatyana.valkevych@alfresco.com>
2015-06-05 11:02:59 +0000

    Merged HEAD-BUG-FIX (5.1/Cloud) to HEAD (5.1/Cloud)    105528: Merged 5.0.N (5.0.3) to HEAD-BUG-FIX (5.1/Cloud)       105505: Merged V4.2-BUG-FIX (4.2.5) to 5.0.N (5.0.3)          104963: Merged DEV to V4.2-BUG-FIX (4.2.5)             100339: MNT-13742: SortType: NullPointerException when trying to sort the result set (SOLR)                - Fixed the sorting depending on the type.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@105591 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/java/org/alfresco/repo/search/impl/solr/SolrQueryHTTPClient.java
+++ source/java/org/alfresco/repo/search/impl/solr/SolrQueryHTTPClient.java
@@ -673,8 +673,21 @@ public class SolrQueryHTTPClient implements BeanFactoryAware
                 sortBuffer.append(encoder.encode(", ", "UTF-8"));
             }
             // MNT-8557 fix, manually replace ' ' with '%20'
-            sortBuffer.append(encoder.encode(sortDefinition.getField().replaceAll(" ", "%20"), "UTF-8")).append(encoder.encode(" ", "UTF-8"));
-            if (sortDefinition.isAscending())
+            // The sort can be different, see MNT-13742
+            switch (sortDefinition.getSortType())
+            {
+                case DOCUMENT:
+                    sortBuffer.append(encoder.encode("_docid_", "UTF-8")).append(encoder.encode(" ", "UTF-8"));
+                    break;
+                case SCORE:
+                    sortBuffer.append(encoder.encode("score", "UTF-8")).append(encoder.encode(" ", "UTF-8"));
+                    break;
+                case FIELD:
+                default:
+                    sortBuffer.append(encoder.encode(sortDefinition.getField().replaceAll(" ", "%20"), "UTF-8")).append(encoder.encode(" ", "UTF-8"));
+                    break;
+            }
+	        if (sortDefinition.isAscending())
             {
                 sortBuffer.append(encoder.encode("asc", "UTF-8"));
             }

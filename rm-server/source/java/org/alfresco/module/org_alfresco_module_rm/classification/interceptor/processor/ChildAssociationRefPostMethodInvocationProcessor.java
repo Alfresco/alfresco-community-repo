cc8fdb71b1672ebc4d9aba46462356d95b3bbc9a
Tuna Aksoy <tuna.aksoy@alfresco.com>
2015-06-11 09:37:24 +0000

    RM-2130 (Check classification after method execution, filtering results where appropriate)
     * Initial (early) commit to get feedback from the review
     * The "check" method will be changed later
    
    +review RM @rwetherall
    
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/DEV/ENFORCE@105923 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

---
+++ rm-server/source/java/org/alfresco/module/org_alfresco_module_rm/classification/interceptor/processor/ChildAssociationRefPostMethodInvocationProcessor.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2005-2015 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.alfresco.module.org_alfresco_module_rm.classification.interceptor.processor;
+
+import static org.alfresco.util.ParameterCheck.mandatory;
+
+import org.alfresco.service.cmr.repository.ChildAssociationRef;
+import org.alfresco.service.cmr.repository.NodeRef;
+
+/**
+ * ChildAssociationRef Post Method Invocation Processor
+ *
+ * @author Tuna Aksoy
+ * @since 3.0
+ */
+public class ChildAssociationRefPostMethodInvocationProcessor extends BasePostMethodInvocationProcessor
+{
+    /**
+     * @see org.alfresco.module.org_alfresco_module_rm.classification.interceptor.processor.BasePostMethodInvocationProcessor#getClassName()
+     */
+    @Override
+    public Class<ChildAssociationRef> getClassName()
+    {
+        return ChildAssociationRef.class;
+    }
+
+    /**
+     * @see org.alfresco.module.org_alfresco_module_rm.classification.interceptor.processor.BasePostMethodInvocationProcessor#process(java.lang.Object)
+     */
+    @Override
+    public <T> T process(T object)
+    {
+        mandatory("object", object);
+
+        ChildAssociationRef childAssociationRef = ((ChildAssociationRef) object);
+
+        NodeRef childRef = childAssociationRef.getChildRef();
+        check(childRef);
+
+        NodeRef parentRef = childAssociationRef.getParentRef();
+        check(parentRef);
+
+        return object;
+    }
+}

3fceea8655b6fba128f9d565aa287ab0dd447a2c
Neil McErlean <neil.mcerlean@alfresco.com>
2015-08-13 16:29:38 +0000

    Implementation and unit tests for RM-2481, RM-2482, RM-2483 and RM-2489.
    This checkin adds behaviours for ensuring that classification aspect/properties are correclty copied between nodes and their renditions on the creation of new renditions, on the classification or reclassification of nodes with existing renditions.
    
    To do this I needed what should be, in my opinion, a common Alfresco utility method - the ability to copy an aspect from one node to another (aspect meaning property group i.e. no assocs).
    I've created that method and put it in CoreServicesExtras. It should really be moved to core.
    
    + review RM
    
    
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/HEAD@110069 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

---
+++ rm-server/source/java/org/alfresco/module/org_alfresco_module_rm/model/clf/ClassifiedRenditions.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2005-2015 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.alfresco.module.org_alfresco_module_rm.model.clf;
+
+import org.alfresco.module.org_alfresco_module_rm.classification.ContentClassificationService;
+import org.alfresco.module.org_alfresco_module_rm.classification.model.ClassifiedContentModel;
+import org.alfresco.module.org_alfresco_module_rm.model.BaseBehaviourBean;
+import org.alfresco.module.org_alfresco_module_rm.util.CoreServicesExtras;
+import org.alfresco.repo.node.NodeServicePolicies;
+import org.alfresco.repo.policy.Behaviour.NotificationFrequency;
+import org.alfresco.repo.policy.annotation.Behaviour;
+import org.alfresco.repo.policy.annotation.BehaviourBean;
+import org.alfresco.repo.policy.annotation.BehaviourKind;
+import org.alfresco.service.cmr.rendition.RenditionService;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.namespace.QName;
+
+/**
+ * Behaviour bean for classified rendition nodes.
+ *
+ * @since 3.0.a
+ */
+@BehaviourBean
+(
+   defaultType = "rn:rendition"
+)
+public class ClassifiedRenditions extends BaseBehaviourBean
+                                  implements NodeServicePolicies.OnAddAspectPolicy,
+                                             ClassifiedContentModel
+{
+    private ContentClassificationService contentClassificationService;
+    private CoreServicesExtras           servicesExtras;
+    private RenditionService             renditionService;
+
+    public void setContentClassificationService(ContentClassificationService service)
+    {
+        this.contentClassificationService = service;
+    }
+
+    public void setCoreServicesExtras(CoreServicesExtras extras)
+    {
+        this.servicesExtras = extras;
+    }
+
+    public void setRenditionService(RenditionService service)
+    {
+        this.renditionService = service;
+    }
+
+    /**
+     * Behaviour associated with creating a rendition of an already classified node.
+     */
+    @Override
+    @Behaviour
+    (
+       kind = BehaviourKind.CLASS,
+       notificationFrequency = NotificationFrequency.EVERY_EVENT
+    )
+    public void onAddAspect(final NodeRef renditionNodeRef, final QName aspectTypeQName)
+    {
+        authenticationUtil.runAs(new org.alfresco.repo.security.authentication.AuthenticationUtil.RunAsWork<Void>()
+        {
+            public Void doWork()
+            {
+                final NodeRef sourceNode = renditionService.getSourceNode(renditionNodeRef).getParentRef();
+                if (contentClassificationService.isClassified(sourceNode))
+                {
+                    // All renditions should be given the same classification as their source node
+                    servicesExtras.copyAspect(sourceNode, renditionNodeRef, ASPECT_CLASSIFIED);
+                }
+                return null;
+            }
+        }, authenticationUtil.getSystemUserName());
+    }
+}
---
+++ rm-server/unit-test/java/org/alfresco/module/org_alfresco_module_rm/model/clf/ClassifiedRenditionsUnitTest.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2005-2015 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.alfresco.module.org_alfresco_module_rm.model.clf;
+
+import static java.util.Arrays.asList;
+
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import org.alfresco.model.RenditionModel;
+import org.alfresco.module.org_alfresco_module_rm.classification.ContentClassificationService;
+import org.alfresco.module.org_alfresco_module_rm.classification.model.ClassifiedContentModel;
+import org.alfresco.module.org_alfresco_module_rm.model.clf.aspect.ClassifiedAspect;
+import org.alfresco.module.org_alfresco_module_rm.test.util.MockAuthenticationUtilHelper;
+import org.alfresco.module.org_alfresco_module_rm.util.AuthenticationUtil;
+import org.alfresco.module.org_alfresco_module_rm.util.CoreServicesExtras;
+import org.alfresco.service.cmr.rendition.RenditionService;
+import org.alfresco.service.cmr.repository.ChildAssociationRef;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.NodeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+/**
+ * Unit tests for {@link ClassifiedRenditions}.
+ *
+ * @since 3.0.a
+ */
+public class ClassifiedRenditionsUnitTest implements ClassifiedContentModel
+{
+    private static final NodeRef SOURCE_NODE = new NodeRef("node://ref/");
+    private static final NodeRef RENDITION_1 = new NodeRef("node://rendition1/");
+    private static final NodeRef RENDITION_2 = new NodeRef("node://rendition2/");
+
+    @InjectMocks ClassifiedAspect classifiedAspect;
+
+    @Mock AuthenticationUtil           mockAuthenticationUtil;
+    @Mock ContentClassificationService mockContentClassificationService;
+    @Mock CoreServicesExtras           mockCoreServicesExtras;
+    @Mock NodeService                  mockNodeService;
+    @Mock RenditionService             mockRenditionService;
+
+    @Before
+    public void setUp()
+    {
+        initMocks(this);
+
+        MockAuthenticationUtilHelper.setup(mockAuthenticationUtil);
+    }
+
+    @Test public void newRenditionOfClassifiedNodeShouldItselfBeClassified()
+    {
+        when(mockRenditionService.getRenditions(eq(SOURCE_NODE)))
+                .thenReturn(asList(rendition(SOURCE_NODE, RENDITION_1), rendition(SOURCE_NODE, RENDITION_2)));
+        when(mockRenditionService.getSourceNode(eq(RENDITION_1))).thenReturn(rendition(SOURCE_NODE, RENDITION_1));
+        when(mockRenditionService.getSourceNode(eq(RENDITION_2))).thenReturn(rendition(SOURCE_NODE, RENDITION_2));
+        when(mockContentClassificationService.isClassified(eq(SOURCE_NODE))).thenReturn(true);
+
+        final ClassifiedRenditions behaviour = new ClassifiedRenditions();
+        behaviour.setAuthenticationUtil(mockAuthenticationUtil);
+        behaviour.setContentClassificationService(mockContentClassificationService);
+        behaviour.setCoreServicesExtras(mockCoreServicesExtras);
+        behaviour.setNodeService(mockNodeService);
+        behaviour.setRenditionService(mockRenditionService);
+
+        behaviour.onAddAspect(RENDITION_2, RenditionModel.ASPECT_RENDITION);
+
+        verify(mockCoreServicesExtras).copyAspect(eq(SOURCE_NODE), eq(RENDITION_2), eq(ClassifiedContentModel.ASPECT_CLASSIFIED));
+    }
+
+    /** Creates a mock Rendition ChildAssociationRef. */
+    private ChildAssociationRef rendition(NodeRef source, NodeRef rendition)
+    {
+        return new ChildAssociationRef(RenditionModel.ASSOC_RENDITION, source, RenditionModel.ASSOC_RENDITION, rendition);
+    }
+}

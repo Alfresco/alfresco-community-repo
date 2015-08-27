b60b158946d7bee8b99faa22138c1056d47ec528
David Webster <david.webster@alfresco.com>
2015-08-27 08:18:03 +0000

    RM-2565: Add node data to repo items too so that documents not in a site also have classified labels.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/HEAD@110987 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- rm-server/config/alfresco/templates/webscripts/org/alfresco/slingshot/rmsearch/faceted/rmsearch.lib.js
+++ rm-server/config/alfresco/templates/webscripts/org/alfresco/slingshot/rmsearch/faceted/rmsearch.lib.js
@@ -2,12 +2,24 @@
 
 // Wrap the original document item method with our own one that appends RM specific properties.
 // Additional properties will also need rendering in rmsearch.get.json.ftl.
-var getOriginalDocumentItem = getDocumentItem;
+var getOriginalDocumentItem = getDocumentItem,
+   getOriginalRepositoryItem = getRepositoryItem;
 getDocumentItem = function(siteId, containerId, pathParts, node, populate){
    // Get original Document item.
    var item = getOriginalDocumentItem(siteId, containerId, pathParts, node, populate);
 
    item.nodeJSON = appUtils.toJSON(node, true);
+
+   return item;
+};
+
+getRepositoryItem = function(folderPath, node, populate){
+   // Get Original Repo item
+   var item = getOriginalRepositoryItem(folderPath, node, populate);
+
+   if (item.type === "document") {
+      item.nodeJSON = appUtils.toJSON(node, true);
+   }
 
    return item;
 };

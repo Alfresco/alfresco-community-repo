11e59543bdca29aea6a550cc48d9e2ff15b6d9dc
Roy Wetherall <roy.wetherall@alfresco.com>
2015-11-23 03:15:46 +0000

    RM-2194: unit tests used to demonstrate current behaviour
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/BRANCHES/V2.3@118277 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

---
+++ rm-server/test/java/org/alfresco/module/org_alfresco_module_rm/test/integration/version/AutoVersionTest.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (C) 2005-2014 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.alfresco.module.org_alfresco_module_rm.test.integration.version;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.alfresco.model.ContentModel;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.version.VersionHistory;
+import org.alfresco.service.namespace.QName;
+import org.alfresco.util.GUID;
+
+
+/**
+ * Test recorded version histories when interacting with cm:versionable aspect 
+ * and the auto-version behvaiour.
+ *  
+ * @author Roy Wetherall
+ * @since 2.3.1
+ */
+public class AutoVersionTest extends RecordableVersionsBaseTest 
+{
+    /**
+     * Given a versionable document
+     * When I specialise the type of the document
+     * Then the version history has only one initial version
+     * And it does not represent the current type of the document
+     */
+    public void testSpecialisedNodeInitialVersionCreated()
+    {
+        doBehaviourDrivenTest(new BehaviourDrivenTest(dmCollaborator)
+        {
+            private NodeRef myDocument;
+            
+            public void given() throws Exception
+            {
+                // create a document
+                myDocument = fileFolderService.create(dmFolder, GUID.generate(), ContentModel.TYPE_CONTENT).getNodeRef();
+                
+                // make versionable
+                nodeService.addAspect(myDocument, ContentModel.ASPECT_VERSIONABLE, null);                                      
+            }
+            
+            public void when()
+            {   
+                // specialise document
+                nodeService.setType(myDocument, TYPE_CUSTOM_TYPE);
+            }            
+            
+            public void then()
+            {
+                VersionHistory versionHistory = versionService.getVersionHistory(myDocument);
+                
+                assertNotNull(versionHistory);
+                assertEquals(1, versionHistory.getAllVersions().size());
+                
+                NodeRef frozenState = versionHistory.getHeadVersion().getFrozenStateNodeRef();
+                assertEquals(ContentModel.TYPE_CONTENT, nodeService.getType(frozenState));
+                assertEquals(TYPE_CUSTOM_TYPE, nodeService.getType(myDocument));
+            }
+        });        
+    }  
+    
+    /**
+     * Given a versionable document with initial version turned off
+     * When I specialise the type of the document
+     * Then the version history remains empty
+     */
+    public void testSpecialisedNodeInitialVersionNotCreated()
+    {
+        doBehaviourDrivenTest(new BehaviourDrivenTest(dmCollaborator)
+        {
+            private NodeRef myDocument;
+            
+            public void given() throws Exception
+            {
+                // create a document
+                myDocument = fileFolderService.create(dmFolder, GUID.generate(), ContentModel.TYPE_CONTENT).getNodeRef();
+                
+                // make versionable
+                Map<QName, Serializable> props = new HashMap<QName, Serializable>(1);
+                props.put(ContentModel.PROP_INITIAL_VERSION, false);
+                nodeService.addAspect(myDocument, ContentModel.ASPECT_VERSIONABLE, props);                                      
+            }
+            
+            public void when()
+            {   
+                // specialise document
+                nodeService.setType(myDocument, TYPE_CUSTOM_TYPE);
+            }            
+            
+            public void then()
+            {
+                VersionHistory versionHistory = versionService.getVersionHistory(myDocument);                
+                assertNull(versionHistory);
+            }
+        });        
+    } 
+    
+    
+}

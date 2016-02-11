3d9f2b0d7b6387aa27145ab6886e60b133de6bd1
Roy Wetherall <roy.wetherall@alfresco.com>
2016-02-11 14:42:23 +1100

    Spring configuration for module compatibility component.

--- rm-community/rm-community-repo/source/java/org/alfresco/module/org_alfresco_module_rm/bootstrap/ModuleCompatibilityComponent.java
+++ rm-community/rm-community-repo/source/java/org/alfresco/module/org_alfresco_module_rm/bootstrap/ModuleCompatibilityComponent.java
@@ -21,38 +21,93 @@ package org.alfresco.module.org_alfresco_module_rm.bootstrap;
 import org.alfresco.service.cmr.admin.RepoUsage.LicenseMode;
 import org.alfresco.service.cmr.module.ModuleService;
 import org.alfresco.service.descriptor.DescriptorService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.event.ContextStartedEvent;
 
 /**
+ * Module compatibility component.
+ * <p>
+ * Checks that the currently installed RM AMP licence mode matches that of the
+ * underlying repository.
+ * 
  * @author Roy Wetherall
+ * @since 2.4
  */
 public class ModuleCompatibilityComponent implements ApplicationListener<ContextStartedEvent>
 {
+	/** Logger */
+    private static Log logger = LogFactory.getLog(ModuleCompatibilityComponent.class);
+	
+	// TODO get this from somewhere
 	private static final String RM_ENT_MODULE_ID = "alfresco-rm-enterprise-repo";
 	
+	/** descriptor service */
 	private DescriptorService descriptorService;
 	
+	/** module service */
 	private ModuleService moduleService;
 	
+	/**
+	 * @param descriptorService	descriptor service
+	 */
+	public void setDescriptorService(DescriptorService descriptorService) 
+	{
+		this.descriptorService = descriptorService;
+	}
+	
+	/**
+	 * @param moduleService	module service
+	 */
+	public void setModuleService(ModuleService moduleService) 
+	{
+		this.moduleService = moduleService;
+	}
+	
+	/**
+	 * @see org.springframework.context.ApplicationListener#onApplicationEvent(org.springframework.context.ApplicationEvent)
+	 */
 	@Override
 	public void onApplicationEvent(ContextStartedEvent contextStartedEvent) 
 	{
 		// get the license mode
 		LicenseMode licenseMode = descriptorService.getLicenseDescriptor().getLicenseMode();
-		if (LicenseMode.ENTERPRISE.equals(licenseMode))
+		
+		if (LicenseMode.ENTERPRISE.equals(licenseMode) &&
+			moduleService.getModule(RM_ENT_MODULE_ID) == null)
 		{
-			// ensure RM enterprise module is installed
-			if (moduleService.getModule(RM_ENT_MODULE_ID) == null)
-			{
-				// log something
-				
-				// report an error				
-				
-				// close the application context!
-				((ConfigurableApplicationContext)contextStartedEvent.getApplicationContext()).close();
-			}
+			// running enterprise rm on community core so close application context
+			closeApplicationContext(contextStartedEvent.getApplicationContext(), 
+					"Running Community Records Management Module on Enterprise Alfresco One is not a supported configuration.");
+			
 		}
+		else if (!LicenseMode.ENTERPRISE.equals(licenseMode) &&
+				 moduleService.getModule(RM_ENT_MODULE_ID) != null)
+		{
+			// running community rm on enterprise core so close application context
+			closeApplicationContext(contextStartedEvent.getApplicationContext(), 
+					"Running Enterprise Records Management module on Community Alfresco One is not a supported configuration.");
+		}
+	}
+	
+	/**
+	 * Close application context, logging message.
+	 * 
+	 * @param applicationContext	application context
+	 * @param message				closure message
+	 */
+	private void closeApplicationContext(ApplicationContext applicationContext, String message)
+	{
+		// log closure message
+		if (logger.isErrorEnabled())
+		{
+			logger.error(message);
+		}
+		
+		// close the application context!
+		((ConfigurableApplicationContext)applicationContext).close();
 	}
 }

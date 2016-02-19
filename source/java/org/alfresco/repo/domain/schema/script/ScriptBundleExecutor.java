509a828b4386dd518571550db47409f37e26609a
Derek Hulley <derek.hulley@alfresco.com>
2016-02-19 01:33:19 +0000

    Add option to propagate exceptions out during SQL execution, in order to detect failures (ACE-5143)
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/BRANCHES/DEV/5.1.N/root@122469 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/java/org/alfresco/repo/domain/schema/script/ScriptBundleExecutor.java
+++ source/java/org/alfresco/repo/domain/schema/script/ScriptBundleExecutor.java
@@ -18,6 +18,8 @@
  */
 package org.alfresco.repo.domain.schema.script;
 
+import org.alfresco.error.AlfrescoRuntimeException;
+
 /**
  * Executes a set of zero or more SQL scripts.
  * 
@@ -25,6 +27,16 @@ package org.alfresco.repo.domain.schema.script;
  */
 public interface ScriptBundleExecutor
 {
+    /**
+     * Runs a bundle of scripts. If any script within the bundle fails, then the rest of the files are not run.
+     * 
+     * @param logOnly   <tt>true</tt> to catch and log any exceptions or <tt>false</tt> to rethrow 
+     * @param dir        Directory where the script bundle may be found.
+     * @param scripts    Names of the SQL scripts to run, relative to the specified directory.
+     * @throws AlfrescoRuntimeException if a script fails and the <tt>logOnly</tt> flag is <tt>false</tt>
+     */
+    void exec(boolean logOnly, String dir, String... scripts);
+    
     /**
      * Runs a bundle of scripts. If any script within the bundle fails, then the rest of the files are not run.
      *  

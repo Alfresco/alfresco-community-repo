412353c04418d4a4b7db3b84ca1d4bbe7a0bdf70
Roy Wetherall <rwetherall@gmail.com>
2016-03-01 13:06:00 +1100

    Add managed license headers to community projects for java, js and ftl source files.

--- rm-community/rm-community-repo/source/java/org/alfresco/module/org_alfresco_module_rm/action/RecordsManagementActionResult.java
+++ rm-community/rm-community-repo/source/java/org/alfresco/module/org_alfresco_module_rm/action/RecordsManagementActionResult.java
@@ -2,6 +2,28 @@
 
 package org.alfresco.module.org_alfresco_module_rm.action;
 
+/*
+ * #%L
+ * This file is part of Alfresco.
+ * %%
+ * Copyright (C) 2005 - 2016 Alfresco Software Limited
+ * %%
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *  
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+
 /**
  * Records management action result.
  * 

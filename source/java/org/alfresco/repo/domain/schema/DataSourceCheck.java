ffda23e39348d7001c8e6e0c891e81bf061febda
Alan Davis <alan.davis@alfresco.com>
2016-03-11 21:40:52 +0000

    Merged 5.1.N (5.1.1) to HEAD (5.1)    120968 amorarasu: Merged 5.0.N (5.0.4) to 5.1.N (5.1.1)       120952 aleahu: Merged V4.2-BUG-FIX (4.2.6) to 5.0.N (5.0.4)          120889 amukha: Merged AMUKHA/MNT-15398 (4.2.6) to V4.2-BUG-FIX (4.2.6)             120887 amukha: MNT-15398: Supportability - warn about MS-SQL server mis-configuration                - Added a check for the db.txn.isolation to be set to 4096 for the MS SQL Server dialect.                - Used the message bundle for all logger messages.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@123636 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/java/org/alfresco/repo/domain/schema/DataSourceCheck.java
+++ source/java/org/alfresco/repo/domain/schema/DataSourceCheck.java
@@ -23,8 +23,15 @@ import java.sql.DatabaseMetaData;
 
 import javax.sql.DataSource;
 
+import org.alfresco.error.AlfrescoRuntimeException;
+import org.alfresco.hibernate.DialectFactory;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.dialect.Dialect;
+import org.hibernate.dialect.SQLServerDialect;
+import org.springframework.extensions.surf.util.I18NUtil;
+import org.springframework.orm.hibernate3.LocalSessionFactoryBean;
 
 /**
  * Bean to log connection details and attempt to ensure that the connection is OK.
@@ -36,27 +43,83 @@ public class DataSourceCheck
 {
     private static Log logger = LogFactory.getLog("org.alfresco.repo.admin");
     
-    private static final String MSG_DB_CONNECTION = "Using database URL '%s' with user '%s'.";
-    private static final String MSG_DB_VERSION = "Connected to database %s version %s";
-    private static final String ERR_DB_CONNECTION = "Database connection failed: %s";
-    
-    /**
-     * Constructor-based check of DB connection
-     */
-    public DataSourceCheck(String dbUrl, String dbUsername, DataSource dataSource)
+    private static final String MSG_DB_CONNECTION = "system.config_check.info.db_connection";
+    private static final String MSG_DB_VERSION = "system.config_check.info.db_version";
+    private static final String ERR_DB_CONNECTION = "system.config_check.err.db_connection";
+    private static final String ERR_WRONG_TRANSACTION_ISOLATION_SQL_SERVER =
+            "system.config_check.err.wrong_transaction_isolation_sql_server";
+    /** The required transaction isolation */
+    private static final int SQL_SERVER_TRANSACTION_ISOLATION = 4096;
+
+    private Configuration cfg;
+    private String dbUrl;
+    private String dbUsername;
+    private int transactionIsolation;
+    private DataSource dataSource;
+
+    public void setLocalSessionFactory(LocalSessionFactoryBean localSessionFactory)
     {
-        logger.info(String.format(MSG_DB_CONNECTION, dbUrl, dbUsername));
+        this.cfg = localSessionFactory.getConfiguration();
+    }
+
+    public void setDbUrl(String dbUrl)
+    {
+        this.dbUrl = dbUrl;
+    }
+
+    public void setDbUsername(String dbUsername)
+    {
+        this.dbUsername = dbUsername;
+    }
+
+    public void setDataSource(DataSource dataSource)
+    {
+        this.dataSource = dataSource;
+    }
+
+    public void setTransactionIsolation(int transactionIsolation)
+    {
+        this.transactionIsolation = transactionIsolation;
+    }
+
+    public void init()
+    {
+        logger.info(I18NUtil.getMessage(MSG_DB_CONNECTION, dbUrl, dbUsername));
         
         Connection con = null;
         try
         {
             con = dataSource.getConnection();
+            con.setAutoCommit(true);
             DatabaseMetaData meta = con.getMetaData();
-            logger.info(String.format(MSG_DB_VERSION, meta.getDatabaseProductName(), meta.getDatabaseProductVersion()));
+            logger.info(I18NUtil.getMessage(
+                    MSG_DB_VERSION,
+                    meta.getDatabaseProductName(), meta.getDatabaseProductVersion()));
+
+            Dialect dialect = DialectFactory.buildDialect(
+                    cfg.getProperties(),
+                    meta.getDatabaseProductName(),
+                    meta.getDatabaseMajorVersion());
+
+            // Check MS SQL Server specific settings
+            if (dialect instanceof SQLServerDialect)
+            {
+                if (transactionIsolation != SQL_SERVER_TRANSACTION_ISOLATION)
+                {
+                    throw new AlfrescoRuntimeException(
+                            ERR_WRONG_TRANSACTION_ISOLATION_SQL_SERVER,
+                            new Object[] {transactionIsolation, SQL_SERVER_TRANSACTION_ISOLATION});
+                }
+            }
+        }
+        catch (RuntimeException re)
+        {
+            // just rethrow
+            throw re;
         }
         catch (Exception e)
         {
-            throw new RuntimeException(String.format(ERR_DB_CONNECTION, e.getMessage()), e);
+            throw new AlfrescoRuntimeException(ERR_DB_CONNECTION, new Object[] {e.getMessage()}, e);
         }
         finally
         {

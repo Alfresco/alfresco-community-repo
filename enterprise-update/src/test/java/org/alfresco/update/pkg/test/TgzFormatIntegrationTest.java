6f9893799c832b0bbd59867a980bc6a69f8cdca3
Mark Rogers <mark.rogers@alfresco.com>
2016-03-15 10:55:45 +0000

    Merged WOLF-6 (WOLF.0.0) to 5.1.1 (5.1.1)    111925 adavis: Merged 5.0.N (5.0.3) to WOLF-6 (WOLF.0.0)       111630: UTF-242: Unix specific test for apply_updates.sh
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/services/full-installer/branches/5.1.1@123919 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- enterprise-update/src/test/java/org/alfresco/update/pkg/test/TgzFormatIntegrationTest.java
+++ enterprise-update/src/test/java/org/alfresco/update/pkg/test/TgzFormatIntegrationTest.java
@@ -7,8 +7,23 @@
  */
 package org.alfresco.update.pkg.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.compress.archivers.ArchiveException;
+import org.apache.commons.compress.archivers.ArchiveStreamFactory;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.compress.compressors.CompressorException;
+import org.apache.commons.compress.compressors.CompressorInputStream;
+import org.apache.commons.compress.compressors.CompressorStreamFactory;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -17,16 +32,106 @@ import org.junit.Test;
  * 
  * @author Matt Ward
  */
-public class TgzFormatIntegrationTest
+public class TgzFormatIntegrationTest extends AbstractIntegrationTest
 {
+    private boolean found;
+    
     @Before
     public void setUp() throws Exception
     {
+        super.setUp();
     }
 
     @Test
-    public void test()
+    public void applyUpdatesScriptHasExecutableBitsSet() throws FileNotFoundException, ArchiveException, IOException, CompressorException
     {
-//        fail("Not yet implemented");
+        if (runningOnWindows())
+        {
+            // This is a Unix only test.
+            return;
+        }
+        
+        File archive = new File(targetDir, "alfresco-enterprise-update-package-"+version+".tgz");
+        assertTrue("File does not exist: "+archive, archive.exists());
+        FileInputStream fis = null;
+        try
+        {
+            fis = new FileInputStream(archive);
+
+            handleArchiveEntries(fis, new ArchiveEntryHandler()
+            {                
+                @Override
+                public boolean handle(TarArchiveEntry entry)
+                {
+                    System.out.println("Handling tar entry: "+entry.getName());
+                    if (entry.getName().equals("bin/apply_updates.sh"))
+                    {
+                        System.out.println("Found the unix shell wrapper script.");
+                        
+                        final int expectedPerms = 0755;
+                        // Other bits may be set, but check 755 octal are set.
+                        System.out.println("File has permissions: "+Integer.toString(entry.getMode(), 8));
+                        assertEquals(expectedPerms, entry.getMode() & expectedPerms);          
+                        found = true;
+                    }
+                    return !found;
+                }
+            });
+            
+            assertTrue("bin/apply_updates.sh is a required file.", found);
+        }
+        finally
+        {
+            if (fis != null)
+            {
+                fis.close();
+            }
+        }
+    }
+
+    private static interface ArchiveEntryHandler
+    {
+        boolean handle(TarArchiveEntry entry);
+    }
+    
+    private void handleArchiveEntries(InputStream raw, ArchiveEntryHandler handler) throws ArchiveException, IOException, CompressorException
+    {
+        CompressorInputStream gzIs = null;
+        BufferedInputStream bis = null;
+        TarArchiveInputStream aris = null;
+        BufferedInputStream bgzIs = null;
+        try
+        {
+            bis = new BufferedInputStream(raw);
+            gzIs = new CompressorStreamFactory().createCompressorInputStream(bis);
+            bgzIs = new BufferedInputStream(gzIs);
+            aris = (TarArchiveInputStream) new ArchiveStreamFactory().createArchiveInputStream(bgzIs);
+            
+            TarArchiveEntry entry = null;
+            boolean carryOn = true;
+            while (carryOn && (entry = aris.getNextTarEntry()) != null)
+            {
+                carryOn = handler.handle(entry);
+            }
+        }
+        finally
+        {
+            if (aris != null)
+            {
+                aris.close();
+            }
+            if (bgzIs != null)
+            {
+                bgzIs.close();
+            }
+            if (gzIs != null)
+            {
+                gzIs.close();
+            }
+            if (bis != null)
+            {
+                bis.close();
+            }
+        }
     }
 }

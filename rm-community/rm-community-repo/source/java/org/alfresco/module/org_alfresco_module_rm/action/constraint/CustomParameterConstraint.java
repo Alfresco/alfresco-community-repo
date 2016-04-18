61c1be42367b09e82b937f28f1bdc0748a99bbce
Tuna Aksoy <tuna.aksoy@alfresco.com>
2016-04-18 16:19:06 +0100

    RM-3150 (Errors when generating Javadoc for RM using Java 8)

--- rm-community/rm-community-repo/source/java/org/alfresco/module/org_alfresco_module_rm/action/constraint/CustomParameterConstraint.java
+++ rm-community/rm-community-repo/source/java/org/alfresco/module/org_alfresco_module_rm/action/constraint/CustomParameterConstraint.java
@@ -35,39 +35,40 @@ import org.alfresco.repo.action.constraint.BaseParameterConstraint;
 
 /**
  * A parameter constraint that reads in a list of allowable values from Spring configuration
- * 
+ *
  * @author Craig Tan
  * @since 2.1
  */
 public class CustomParameterConstraint extends BaseParameterConstraint
 {
-    
     private List<String> parameterValues;
 
     /**
-     * @param parameterValues
+     * Sets the parameter values
+     *
+     * @param parameterValues The parameter values
      */
     public void setParameterValues(List<String> parameterValues)
     {
         this.parameterValues = parameterValues;
     }
-    
+
     /**
      * @see org.alfresco.service.cmr.action.ParameterConstraint#getAllowableValues()
      */
     protected Map<String, String> getAllowableValuesImpl()
-    {                  
+    {
         Map<String, String> allowableValues = new HashMap<String, String>(parameterValues.size());
-        
+
         for (Object parameterValue : parameterValues)
         {
             // Look up the I18N value
             String displayLabel = getI18NLabel(parameterValue.toString());
-            
+
             // Add to the map of allowed values
             allowableValues.put(parameterValue.toString(), displayLabel);
-        } 
-        
+        }
+
         return allowableValues;
     }
 }

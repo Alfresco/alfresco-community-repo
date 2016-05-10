3aaf414678c3fdca6a4e93d783bfb4d5189e4ede
Jamal Kaabi-Mofrad <Jamal.kaabimofrad@alfresco.com>
2016-05-10 11:17:32 +0000

    Merged FILE-FOLDER-API (5.2.0) to HEAD (5.2)    122825 jvonka: (Quick) Shared Links API - add missing file, oops.    RA-830
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@126512 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

---
+++ source/java/org/alfresco/rest/api/quicksharelinks/QuickShareLinkRenditionsRelation.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2005-2016 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.alfresco.rest.api.quicksharelinks;
+
+import org.alfresco.rest.api.QuickShareLinks;
+import org.alfresco.rest.api.Renditions;
+import org.alfresco.rest.api.nodes.NodesEntityResource;
+import org.alfresco.rest.framework.BinaryProperties;
+import org.alfresco.rest.framework.WebApiDescription;
+import org.alfresco.rest.framework.WebApiNoAuth;
+import org.alfresco.rest.framework.resource.RelationshipResource;
+import org.alfresco.rest.framework.resource.actions.interfaces.RelationshipResourceBinaryAction;
+import org.alfresco.rest.framework.resource.content.BinaryResource;
+import org.alfresco.rest.framework.resource.parameters.Parameters;
+import org.alfresco.util.ParameterCheck;
+import org.springframework.beans.factory.InitializingBean;
+
+/**
+ * Enable rendition(s) to be download via Shared Link
+ *
+ * @author janv
+ */
+@RelationshipResource(name = "renditions", entityResource = QuickShareLinkEntityResource.class, title = "Node renditions via shared link")
+public class QuickShareLinkRenditionsRelation implements
+            RelationshipResourceBinaryAction.Read,
+            InitializingBean
+{
+    private QuickShareLinks quickShareLinks;
+
+    public void setQuickShareLinks(QuickShareLinks quickShareLinks)
+    {
+        this.quickShareLinks = quickShareLinks;
+    }
+
+    @Override
+    public void afterPropertiesSet()
+    {
+        ParameterCheck.mandatory("quickShareLinks", this.quickShareLinks);
+    }
+
+    @WebApiDescription(title = "Download shared link rendition", description = "Download rendition for shared link")
+    @WebApiNoAuth
+    @BinaryProperties({"content"})
+    @Override
+    public BinaryResource readProperty(String sharedId, String renditionId, Parameters parameters)
+    {
+        return quickShareLinks.readProperty(sharedId, renditionId, parameters);
+    }
+}

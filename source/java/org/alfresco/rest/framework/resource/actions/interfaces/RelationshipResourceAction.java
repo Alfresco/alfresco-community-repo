26201557fc828649768a1217966d9b2823bdfbc2
Jamal Kaabi-Mofrad <Jamal.kaabimofrad@alfresco.com>
2016-05-10 11:14:02 +0000

    Merged FILE-FOLDER-API (5.2.0) to HEAD (5.2)    122758 gjames: RA-211: Added new interface for WithResponse. Revert>Java7
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@126503 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/java/org/alfresco/rest/framework/resource/actions/interfaces/RelationshipResourceAction.java
+++ source/java/org/alfresco/rest/framework/resource/actions/interfaces/RelationshipResourceAction.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.alfresco.rest.framework.core.exceptions.RelationshipResourceNotFoundException;
 import org.alfresco.rest.framework.resource.parameters.CollectionWithPagingInfo;
 import org.alfresco.rest.framework.resource.parameters.Parameters;
+import org.alfresco.rest.framework.webscripts.WithResponse;
 
 /**
  * Permissible actions for an Relationship Resources
@@ -30,7 +31,23 @@ public interface RelationshipResourceAction
          */
         public CollectionWithPagingInfo<E> readAll (String entityResourceId, Parameters params);
     }
-    
+
+    /**
+     * HTTP GET - Retrieve list of related entities by its related entityResource Id
+     */
+    public static interface ReadWithResponse<E> extends ResourceAction
+    {
+        /**
+         * Reads all the relationship entities from the collection using the related entityResourceId.
+         *
+         * Paging information is provided.
+         * @param entityResourceId Entity resource context for this relationship
+         * @param params - will never be null and will have the PAGING default values
+         * @return CollectionWithPagingInfo<E>
+         */
+        public CollectionWithPagingInfo<E> readAll (String entityResourceId, Parameters params, WithResponse withResponse);
+    }
+
     /**
      * HTTP GET - Retrieve a relation by its unique id & entity context
      */
@@ -38,7 +55,15 @@ public interface RelationshipResourceAction
     {
         public E readById (String entityResourceId, String id,  Parameters parameters) throws RelationshipResourceNotFoundException;
     }
-    
+
+    /**
+     * HTTP GET - Retrieve a relation by its unique id & entity context
+     */
+    public static interface ReadByIdWithResponse<E> extends ResourceAction
+    {
+        public E readById (String entityResourceId, String id,  Parameters parameters, WithResponse withResponse) throws RelationshipResourceNotFoundException;
+    }
+
     /**
      * HTTP PUT - Update entity (by its related entityResource Id) if it exists, error if not
      */
@@ -46,7 +71,15 @@ public interface RelationshipResourceAction
     {
         public E update (String entityResourceId, E entity,  Parameters parameters);
     }
-    
+
+    /**
+     * HTTP PUT - Update entity (by its related entityResource Id) if it exists, error if not
+     */
+    public static interface UpdateWithResponse<E> extends ResourceAction
+    {
+        public E update (String entityResourceId, E entity,  Parameters parameters, WithResponse withResponse);
+    }
+
     /**
      * HTTP POST - Create one or more new entity
      */
@@ -54,7 +87,15 @@ public interface RelationshipResourceAction
     {
         public List<E> create (String entityResourceId, List<E> entity,  Parameters parameters);
     }
-    
+
+    /**
+     * HTTP POST - Create one or more new entity
+     */
+    public static interface CreateWithResponse<E> extends ResourceAction
+    {
+        public List<E> create (String entityResourceId, List<E> entity,  Parameters parameters, WithResponse withResponse);
+    }
+
     /**
      * HTTP DELETE - Deletes a relation by its unique id & entity context
      */
@@ -63,4 +104,11 @@ public interface RelationshipResourceAction
         public void delete (String entityResourceId, String id,  Parameters parameters);
     }
 
+    /**
+     * HTTP DELETE - Deletes a relation by its unique id & entity context
+     */
+    public static interface DeleteWithResponse extends ResourceAction
+    {
+        public void delete (String entityResourceId, String id,  Parameters parameters, WithResponse withResponse);
+    }
 }

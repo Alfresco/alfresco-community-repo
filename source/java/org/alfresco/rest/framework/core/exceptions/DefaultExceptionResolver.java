d0a43345361db5cb88eb1c49ca9506fd17dac91b
Jamal Kaabi-Mofrad <Jamal.kaabimofrad@alfresco.com>
2016-05-10 11:50:39 +0000

    Merged FILE-FOLDER-API (5.2.0) to HEAD (5.2)    125129 gjames: RA-675: Added https to the front of the descriptionURL
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@126612 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/java/org/alfresco/rest/framework/core/exceptions/DefaultExceptionResolver.java
+++ source/java/org/alfresco/rest/framework/core/exceptions/DefaultExceptionResolver.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class DefaultExceptionResolver implements ExceptionResolver<Exception>
 {
-    public static final String ERROR_URL = "api-explorer.alfresco.com";
+    public static final String ERROR_URL = "https://api-explorer.alfresco.com";
     public static final String DEFAULT_MESSAGE_ID = "framework.exception.ApiDefault";
     
     @Override

76f7691c4a1c30e757202a7d8edc598e849a8809
Ancuta Morarasu <ancuta.morarasu@ness.com>
2016-05-11 11:03:19 +0000

    Merged HEAD (5.2) to 5.2.N (5.2.1)    126405 jkaabimofrad: Merged FILE-FOLDER-API (5.2.0) to HEAD (5.2)       121337 gcornwell: RA-758: Ensure all appropriate TCK tests are being run against all bindings
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/BRANCHES/DEV/5.2.N/root@126750 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/test-java/org/alfresco/rest/api/tests/TestPublicApiAtomPub10TCK.java
+++ source/test-java/org/alfresco/rest/api/tests/TestPublicApiAtomPub10TCK.java
@@ -31,6 +31,7 @@ import java.util.Map;
 
 import org.alfresco.opencmis.OpenCMISClientContext;
 import org.alfresco.opencmis.tck.tests.query.QueryForObjectCustom;
+import org.alfresco.opencmis.tck.tests.query.QueryInFolderTestCustom;
 import org.alfresco.opencmis.tck.tests.query.QueryLikeTestCustom;
 import org.alfresco.rest.api.tests.RepoService.TestNetwork;
 import org.apache.chemistry.opencmis.commons.enums.BindingType;
@@ -42,10 +43,8 @@ import org.apache.chemistry.opencmis.tck.tests.control.ControlTestGroup;
 import org.apache.chemistry.opencmis.tck.tests.crud.CRUDTestGroup;
 import org.apache.chemistry.opencmis.tck.tests.filing.FilingTestGroup;
 import org.apache.chemistry.opencmis.tck.tests.query.ContentChangesSmokeTest;
-import org.apache.chemistry.opencmis.tck.tests.versioning.CheckedOutTest;
-import org.apache.chemistry.opencmis.tck.tests.versioning.VersionDeleteTest;
-import org.apache.chemistry.opencmis.tck.tests.versioning.VersioningSmokeTest;
-import org.apache.chemistry.opencmis.tck.tests.versioning.VersioningStateCreateTest;
+import org.apache.chemistry.opencmis.tck.tests.query.QuerySmokeTest;
+import org.apache.chemistry.opencmis.tck.tests.versioning.VersioningTestGroup;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.junit.AfterClass;
@@ -98,7 +97,7 @@ public class TestPublicApiAtomPub10TCK extends AbstractEnterpriseOpenCMIS10TCKTe
     @Test
     public void testCMISTCKVersioning() throws Exception
     {
-        OverrideVersioningTestGroup versioningTestGroup = new OverrideVersioningTestGroup();
+        VersioningTestGroup versioningTestGroup = new VersioningTestGroup();
         JUnitHelper.run(versioningTestGroup);
     }
     
@@ -123,23 +122,23 @@ public class TestPublicApiAtomPub10TCK extends AbstractEnterpriseOpenCMIS10TCKTe
         JUnitHelper.run(queryTestGroup);
     }
     
-    private class OverrideVersioningTestGroup extends AbstractSessionTestGroup
-    {
-        @Override
-        public void init(Map<String, String> parameters) throws Exception
-        {
-            super.init(parameters);
-
-            setName("Versioning Test Group");
-            setDescription("Versioning tests.");
-
-            addTest(new VersioningSmokeTest());
-            addTest(new VersionDeleteTest());
-            addTest(new VersioningStateCreateTest());
-            // relies on Solr being available
-            addTest(new CheckedOutTest());
-        }
-    }
+//    private class OverrideVersioningTestGroup extends AbstractSessionTestGroup
+//    {
+//        @Override
+//        public void init(Map<String, String> parameters) throws Exception
+//        {
+//            super.init(parameters);
+//
+//            setName("Versioning Test Group");
+//            setDescription("Versioning tests.");
+//
+//            addTest(new VersioningSmokeTest());
+//            addTest(new VersionDeleteTest());
+//            addTest(new VersioningStateCreateTest());
+//            // relies on Solr being available
+//            addTest(new CheckedOutTest());
+//        }
+//    }
 
     private class OverrideQueryTestGroup extends AbstractSessionTestGroup
     {
@@ -153,11 +152,12 @@ public class TestPublicApiAtomPub10TCK extends AbstractEnterpriseOpenCMIS10TCKTe
 
             // this is failing because of an MT issue (the thread is a specific tenant but the DB metadata query is searching
             // against the workspace://SpacesStore)
-//            addTest(new QuerySmokeTest());
+            addTest(new QuerySmokeTest());
             // The test fails on Lucene see MNT-11223
 //            addTest(new QueryRootFolderTest());
             addTest(new QueryForObjectCustom());
             addTest(new QueryLikeTestCustom());
+            addTest(new QueryInFolderTestCustom());
             addTest(new ContentChangesSmokeTest());
         }
     }

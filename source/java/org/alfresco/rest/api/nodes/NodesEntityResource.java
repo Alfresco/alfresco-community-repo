78a2510b8d8d08c2913d499d2e4d466719adacbf
Ancuta Morarasu <ancuta.morarasu@ness.com>
2016-05-11 10:47:42 +0000

    Merged HEAD (5.2) to 5.2.N (5.2.1)    126358 jkaabimofrad: Merged FILE-FOLDER-API (5.2.0) to HEAD (5.2)       119623 jvonka: RA-663: Update REST fwk - fix BinaryResource update (currently masks entity PUT operation)       - use "updateProperty" (instead of "update") to be consistent with other binary resource methods       - note: REST fwk relies on unique method names across the resource actions
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/BRANCHES/DEV/5.2.N/root@126704 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/java/org/alfresco/rest/api/nodes/NodesEntityResource.java
+++ source/java/org/alfresco/rest/api/nodes/NodesEntityResource.java
@@ -93,7 +93,7 @@ public class NodesEntityResource implements
     @Override
     @WebApiDescription(title = "Upload content", description = "Upload content")
     @BinaryProperties({"content"})
-    public void update(String fileNodeId, BasicContentInfo contentInfo, InputStream stream, Parameters parameters)
+    public void updateProperty(String fileNodeId, BasicContentInfo contentInfo, InputStream stream, Parameters parameters)
     {
         nodes.updateContent(fileNodeId, contentInfo, stream, parameters);
     }

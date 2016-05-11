e5ca888f6ac914f68bb2c0cbbb9796b38a59086d
Ancuta Morarasu <ancuta.morarasu@ness.com>
2016-05-11 12:10:30 +0000

    Merged HEAD (5.2) to 5.2.N (5.2.1)    126570 jkaabimofrad: Merged FILE-FOLDER-API (5.2.0) to HEAD (5.2)       124580 gjames: RA-884: Adding ActivitiesPostingTest to the APITest suite
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/BRANCHES/DEV/5.2.N/root@126915 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/test-java/org/alfresco/rest/api/tests/ApiTest.java
+++ source/test-java/org/alfresco/rest/api/tests/ApiTest.java
@@ -56,6 +56,7 @@ import org.junit.runners.Suite;
     TestRemovePermissions.class,
     TestPublicApi128.class,
     TestPublicApiCaching.class,
+    ActivitiesPostingTest.class,
     ModulePackagesApiTest.class,
     RenditionsTest.class
 })

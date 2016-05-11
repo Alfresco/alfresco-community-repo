2bcbe263cfe8c1e1d625aa43f7be59d235caa685
Ancuta Morarasu <ancuta.morarasu@ness.com>
2016-05-11 12:13:57 +0000

    Merged HEAD (5.2) to 5.2.N (5.2.1)    126593 jkaabimofrad: Merged FILE-FOLDER-API (5.2.0) to HEAD (5.2)       124826 gjames: Updated the comments
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/BRANCHES/DEV/5.2.N/root@126938 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/java/org/alfresco/rest/framework/Operation.java
+++ source/java/org/alfresco/rest/framework/Operation.java
@@ -8,6 +8,9 @@ import java.lang.annotation.Target;
 /**
  * An Operation as on an entity in the Rest API
  *
+ * Your method signature should have 4 parameters (uniqueId, typePassedin, Parameters, WithResponse),
+ " use Void if you are not interested in the second argument.
+
  * @author Gethin James
  */
 @Target(ElementType.METHOD)

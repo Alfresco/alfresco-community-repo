b326aceb301f5f441378ca6a746774245ccafd1e
Jamal Kaabi-Mofrad <Jamal.kaabimofrad@alfresco.com>
2016-06-02 22:07:15 +0000

    Merged API-STRIKES-BACK (5.2.0) to HEAD (5.2)    126699 jkaabimofrad: Fixed merge issue.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@127586 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/java/org/alfresco/rest/api/PublicApiDeclarativeRegistry.java
+++ source/java/org/alfresco/rest/api/PublicApiDeclarativeRegistry.java
@@ -30,6 +30,7 @@ import org.alfresco.rest.framework.core.ResourceWithMetadata;
 import org.alfresco.rest.framework.core.exceptions.NotFoundException;
 import org.alfresco.rest.framework.resource.actions.interfaces.BinaryResourceAction;
 import org.alfresco.rest.framework.resource.actions.interfaces.EntityResourceAction;
+import org.alfresco.rest.framework.resource.actions.interfaces.RelationshipResourceAction;
 import org.alfresco.rest.framework.resource.actions.interfaces.RelationshipResourceBinaryAction;
 import org.alfresco.rest.framework.resource.actions.interfaces.ResourceAction;
 import org.alfresco.rest.framework.tools.ApiAssistant;
@@ -40,8 +41,6 @@ import org.springframework.extensions.webscripts.Description.RequiredAuthenticat
 import org.springframework.extensions.webscripts.Description.RequiredTransaction;
 import org.springframework.extensions.webscripts.Description.TransactionCapability;
 import org.springframework.http.HttpMethod;
-
-import javax.servlet.http.HttpServletResponse;
 
 /**
  *

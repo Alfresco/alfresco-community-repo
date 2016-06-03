437a01294c37f262d8299382f8c76810233977d0
Alan Davis <alan.davis@alfresco.com>
2016-06-03 14:09:38 +0000

    Merged HEAD (5.2) to 5.2.N (5.2.1)    127569 jkaabimofrad: Merged API-STRIKES-BACK (5.2.0) to HEAD (5.2)       126200 jvonka: Node Associations - follow-on to r126104 to provide initial impl for both peer & child assocs       - experimental -> pending detailed review & discussion (and tests)
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/BRANCHES/DEV/5.2.N/root@127663 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/java/org/alfresco/rest/api/nodes/AbstractNodeRelation.java
+++ source/java/org/alfresco/rest/api/nodes/AbstractNodeRelation.java
@@ -18,35 +18,29 @@
  */
 package org.alfresco.rest.api.nodes;
 
-import org.activiti.engine.history.HistoricActivityInstance;
-import org.alfresco.repo.web.scripts.admin.NodeBrowserPost;
 import org.alfresco.rest.antlr.WhereClauseParser;
 import org.alfresco.rest.api.Nodes;
 import org.alfresco.rest.api.model.Assoc;
-import org.alfresco.rest.api.model.Comment;
+import org.alfresco.rest.api.model.AssocTarget;
 import org.alfresco.rest.api.model.Node;
 import org.alfresco.rest.api.model.UserInfo;
 import org.alfresco.rest.framework.WebApiDescription;
-import org.alfresco.rest.framework.WebApiParam;
-import org.alfresco.rest.framework.WebApiParameters;
+import org.alfresco.rest.framework.core.exceptions.ConstraintViolatedException;
 import org.alfresco.rest.framework.core.exceptions.InvalidArgumentException;
 import org.alfresco.rest.framework.resource.RelationshipResource;
-import org.alfresco.rest.framework.resource.actions.interfaces.MultiPartRelationshipResourceAction;
 import org.alfresco.rest.framework.resource.actions.interfaces.RelationshipResourceAction;
 import org.alfresco.rest.framework.resource.parameters.CollectionWithPagingInfo;
 import org.alfresco.rest.framework.resource.parameters.Paging;
 import org.alfresco.rest.framework.resource.parameters.Parameters;
 import org.alfresco.rest.framework.resource.parameters.where.Query;
 import org.alfresco.rest.framework.resource.parameters.where.QueryHelper;
-import org.alfresco.rest.framework.webscripts.WithResponse;
 import org.alfresco.rest.workflow.api.impl.MapBasedQueryWalker;
-import org.alfresco.rest.workflow.api.model.Activity;
 import org.alfresco.service.ServiceRegistry;
+import org.alfresco.service.cmr.repository.AssociationExistsException;
 import org.alfresco.service.cmr.repository.AssociationRef;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
 import org.alfresco.service.cmr.repository.StoreRef;
-import org.alfresco.service.cmr.search.ResultSetRow;
 import org.alfresco.service.namespace.NamespaceService;
 import org.alfresco.service.namespace.QName;
 import org.alfresco.service.namespace.QNamePattern;
@@ -54,62 +48,77 @@ import org.alfresco.service.namespace.RegexQNamePattern;
 import org.alfresco.util.ParameterCheck;
 import org.alfresco.util.PropertyCheck;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.extensions.webscripts.servlet.FormData;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
- * Node Sources - list node (peer) associations from target to sources
- * 
  * @author janv
  */
-@RelationshipResource(name = "sources",  entityResource = NodesEntityResource.class, title = "Node Sources")
-public class NodeSourcesRelation extends AbstractNodeRelation implements RelationshipResourceAction.Read<Node>
+public class AbstractNodeRelation implements InitializingBean
 {
-    /**
-     * List sources
-     *
-     * @param targetNodeId String id of target node
-     */
-    @Override
-    @WebApiDescription(title = "Return a paged list of sources nodes based on (peer) assocs")
-    public CollectionWithPagingInfo<Node> readAll(String targetNodeId, Parameters parameters)
+    public final static String PARAM_ASSOC_TYPE = "assocType";
+
+    private final static Set<String> WHERE_PARAMS =
+            new HashSet<>(Arrays.asList(new String[] {PARAM_ASSOC_TYPE}));
+
+    protected ServiceRegistry sr;
+    protected NodeService nodeService;
+    protected NamespaceService namespaceService;
+    protected Nodes nodes;
+
+    public void setNodes(Nodes nodes)
     {
-        NodeRef targetNodeRef = nodes.validateOrLookupNode(targetNodeId, null);
+        this.nodes = nodes;
+    }
 
-        QNamePattern assocTypeQNameParam = getAssocTypeFromWhereElseAll(parameters);
+    public void setServiceRegistry(ServiceRegistry sr)
+    {
+        this.sr = sr;
+    }
 
-        List<AssociationRef> assocRefs = nodeService.getSourceAssocs(targetNodeRef, assocTypeQNameParam);
+    @Override
+    public void afterPropertiesSet()
+    {
+        PropertyCheck.mandatory(this, "serviceRegistry", sr);
+        ParameterCheck.mandatory("nodes", this.nodes);
 
-        Map<QName, String> qnameMap = new HashMap<>(3);
+        this.nodeService = sr.getNodeService();
+        this.namespaceService = sr.getNamespaceService();
+    }
 
-        Map<String, UserInfo> mapUserInfo = new HashMap<>(10);
-
-        List<String> includeParam = parameters.getInclude();
-
-        List<Node> collection = new ArrayList<Node>(assocRefs.size());
-        for (AssociationRef assocRef : assocRefs)
+    protected QName getAssocType(String prefixAssocTypeStr, boolean mandatory)
+    {
+        if (mandatory && ((prefixAssocTypeStr == null) || prefixAssocTypeStr.isEmpty()))
         {
-            // minimal info by default (unless "include"d otherwise)
-            Node node = nodes.getFolderOrDocument(assocRef.getSourceRef(), null, null, includeParam, mapUserInfo);
-
-            QName assocTypeQName = assocRef.getTypeQName();
-            String assocType = qnameMap.get(assocTypeQName);
-            if (assocType == null)
-            {
-                assocType = assocTypeQName.toPrefixString(namespaceService);
-                qnameMap.put(assocTypeQName, assocType);
-            }
-            node.setAssociation(new Assoc(assocType));
-
-            collection.add(node);
+            throw new InvalidArgumentException("Missing "+PARAM_ASSOC_TYPE);
         }
 
-        Paging paging = parameters.getPaging();
-        return CollectionWithPagingInfo.asPaged(paging, collection, false, collection.size());
+        return QName.createQName(prefixAssocTypeStr, namespaceService);
+    }
+
+    protected QNamePattern getAssocTypeFromWhereElseAll(Parameters parameters)
+    {
+        QNamePattern assocTypeQNameParam = RegexQNamePattern.MATCH_ALL;
+
+        Query q = parameters.getQuery();
+        if (q != null)
+        {
+            MapBasedQueryWalker propertyWalker = new MapBasedQueryWalker(WHERE_PARAMS, null);
+            QueryHelper.walk(q, propertyWalker);
+
+            String assocTypeQNameStr = propertyWalker.getProperty(PARAM_ASSOC_TYPE, WhereClauseParser.EQUALS, String.class);
+            if (assocTypeQNameStr != null)
+            {
+                assocTypeQNameParam = getAssocType(assocTypeQNameStr, true);
+            }
+        }
+
+        return assocTypeQNameParam;
     }
 }

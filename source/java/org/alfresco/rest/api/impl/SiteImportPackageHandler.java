96a9c61e8b878baf6555fe19a85baa75cbf7066e
Alan Davis <alan.davis@alfresco.com>
2016-06-03 13:59:53 +0000

    Merged HEAD (5.2) to 5.2.N (5.2.1)    127529 jkaabimofrad: Merged API-STRIKES-BACK (5.2.0) to HEAD (5.2)       125453 jvonka: RA-779 / RA-780: Sites API - initial commit for create site [default/functional Share site ;-)] & delete site       -add new files, doh !
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/BRANCHES/DEV/5.2.N/root@127639 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

---
+++ source/java/org/alfresco/rest/api/impl/SiteImportPackageHandler.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2005-2016 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.alfresco.rest.api.impl;
+
+import org.alfresco.service.cmr.view.ImportPackageHandler;
+import org.alfresco.service.cmr.view.ImporterException;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.UnsupportedEncodingException;
+
+// note: based on HomeSiteImportPackageHandler in Cloud/Thor module
+public class SiteImportPackageHandler implements ImportPackageHandler
+{
+    private final static String SITEID_PLACEHOLDER = "${siteId}";
+
+    private SiteSurfConfig config;
+    private String siteId;
+
+    public SiteImportPackageHandler(SiteSurfConfig config, String siteId)
+    {
+        this.config = config;
+        this.siteId = siteId;
+    }
+
+    @Override
+    public void startImport()
+    {
+    }
+
+    @Override
+    public Reader getDataStream()
+    {
+        return new StringReader(config.getImportView());
+    }
+
+    @Override
+    public InputStream importStream(String contentPath)
+    {
+        String content = config.getImportContent(contentPath);
+        if (content == null)
+        {
+            return null;
+        }
+
+        String siteContent = content.replace(SITEID_PLACEHOLDER, siteId);
+        try
+        {
+            return new ByteArrayInputStream(siteContent.getBytes("UTF-8"));
+        }
+        catch(UnsupportedEncodingException e)
+        {
+            throw new ImporterException("Failed to read content " + contentPath, e);
+        }
+    }
+
+    @Override
+    public void endImport()
+    {
+    }
+}

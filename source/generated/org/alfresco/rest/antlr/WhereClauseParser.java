3e1e1edbaa5634844c3506ba3c244e81b2917b1a
Alan Davis <alan.davis@alfresco.com>
2016-06-03 17:08:06 +0000

    Merged 5.2.N (5.2.1) to HEAD (5.2)    125788 rmunteanu: Merged 5.1.N (5.1.2) to 5.2.N (5.2.1)       125606 rmunteanu: Merged 5.1.1 (5.1.1) to 5.1.N (5.1.2)          125515 slanglois: MNT-16155 Update source headers - add new Copyrights for Java and JSP source files + automatic check in the build
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@127810 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/generated/org/alfresco/rest/antlr/WhereClauseParser.java
+++ source/generated/org/alfresco/rest/antlr/WhereClauseParser.java
@@ -1,4 +1,29 @@
-// $ANTLR 3.4 org/alfresco/rest/antlr/WhereClause.g 2015-12-22 09:31:21
+/*
+ * #%L
+ * Alfresco Remote API
+ * %%
+ * Copyright (C) 2005 - 2016 Alfresco Software Limited
+ * %%
+ * This file is part of the Alfresco software. 
+ * If the software was purchased under a paid Alfresco license, the terms of 
+ * the paid license agreement will prevail.  Otherwise, the software is 
+ * provided under the following open source license terms:
+ * 
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+// $ANTLR 3.4 org/alfresco/rest/antlr/WhereClause.g 2013-05-24 09:01:14
 
 package org.alfresco.rest.antlr;
 import org.alfresco.rest.framework.resource.parameters.where.InvalidQueryException;

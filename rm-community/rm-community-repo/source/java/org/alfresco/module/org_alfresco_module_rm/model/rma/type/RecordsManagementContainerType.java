96142085d8731eaf8f979c3593860e1f67194a4f
Roxana Lucanu-Ghetu <P3700616@ness.com>
2016-06-14 12:27:17 +0300

    Throw exception when trying to create records from different types other than cm:content

--- rm-community/rm-community-repo/source/java/org/alfresco/module/org_alfresco_module_rm/model/rma/type/RecordsManagementContainerType.java
+++ rm-community/rm-community-repo/source/java/org/alfresco/module/org_alfresco_module_rm/model/rma/type/RecordsManagementContainerType.java
@@ -27,6 +27,7 @@
 
 package org.alfresco.module.org_alfresco_module_rm.model.rma.type;
 
+import org.alfresco.error.AlfrescoRuntimeException;
 import org.alfresco.model.ContentModel;
 import org.alfresco.module.org_alfresco_module_rm.identifier.IdentifierService;
 import org.alfresco.module.org_alfresco_module_rm.model.BaseBehaviourBean;
@@ -147,6 +148,13 @@ public class RecordsManagementContainerType extends    BaseBehaviourBean
                         }
                         else
                         {
+                            // Throw exception if the type is not cm:content
+                            if (!ContentModel.TYPE_FOLDER.equals(childType) && 
+                                    !dictionaryService.isSubClass(childType, ContentModel.TYPE_CONTENT))
+                            {
+                                throw new AlfrescoRuntimeException("Record can't be created from this type.");
+                            }
+
                             // We need to automatically cast the created folder to RM type if it is a plain folder
                             // This occurs if the RM folder has been created via IMap, WebDav, etc
                             if (!nodeService.hasAspect(child, ASPECT_FILE_PLAN_COMPONENT))

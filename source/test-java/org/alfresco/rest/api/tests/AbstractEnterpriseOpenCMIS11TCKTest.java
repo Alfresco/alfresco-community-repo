8bd1b00193bc7c272c114aec62c37addb6f29d65
Alan Davis <alan.davis@alfresco.com>
2016-06-27 07:05:21 +0000

    Merged 5.1.N (5.1.2) to 5.2.N (5.2.1)    128341 adavis: Merged 5.0.N (5.0.4) to 5.1.N (5.1.2)       128260 adavis: Merged V4.2-BUG-FIX (4.2.7) to 5.0.N (5.0.4)          128213 arebegea: MNT-16437 : CLONE - 4.2.N - 4.2.3 CMIS Bulk Update Properties only updates the properties of one document when more than one document is in the objectIdAndChangeTokens list             Merged V4.2.3 (4.2.3.25) to V4.2-BUG-FIX (4.2.7)                128177 arebegea: MNT-16376 : 4.2.3 CMIS Bulk Update Properties only updates the properties of one document when more than one document is in the objectIdAndChangeTokens list                   - The batch worker threads did not have the thread context to know that they were dealing with CMIS version 1.1
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/BRANCHES/DEV/5.2.N/root@128360 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/test-java/org/alfresco/rest/api/tests/AbstractEnterpriseOpenCMIS11TCKTest.java
+++ source/test-java/org/alfresco/rest/api/tests/AbstractEnterpriseOpenCMIS11TCKTest.java
@@ -34,7 +34,22 @@ import org.apache.chemistry.opencmis.tck.impl.AbstractSessionTestGroup;
 import org.apache.chemistry.opencmis.tck.impl.JUnitHelper;
 import org.apache.chemistry.opencmis.tck.tests.basics.BasicsTestGroup;
 import org.apache.chemistry.opencmis.tck.tests.control.ControlTestGroup;
-import org.apache.chemistry.opencmis.tck.tests.crud.CRUDTestGroup;
+import org.apache.chemistry.opencmis.tck.tests.crud.BulkUpdatePropertiesTest;
+import org.apache.chemistry.opencmis.tck.tests.crud.ChangeTokenTest;
+import org.apache.chemistry.opencmis.tck.tests.crud.ContentRangesTest;
+import org.apache.chemistry.opencmis.tck.tests.crud.CopyTest;
+import org.apache.chemistry.opencmis.tck.tests.crud.CreateAndDeleteDocumentTest;
+import org.apache.chemistry.opencmis.tck.tests.crud.CreateAndDeleteFolderTest;
+import org.apache.chemistry.opencmis.tck.tests.crud.CreateAndDeleteItemTest;
+import org.apache.chemistry.opencmis.tck.tests.crud.CreateAndDeleteRelationshipTest;
+import org.apache.chemistry.opencmis.tck.tests.crud.CreateBigDocument;
+import org.apache.chemistry.opencmis.tck.tests.crud.CreateDocumentWithoutContent;
+import org.apache.chemistry.opencmis.tck.tests.crud.DeleteTreeTest;
+import org.apache.chemistry.opencmis.tck.tests.crud.MoveTest;
+import org.apache.chemistry.opencmis.tck.tests.crud.NameCharsetTest;
+import org.apache.chemistry.opencmis.tck.tests.crud.OperationContextTest;
+import org.apache.chemistry.opencmis.tck.tests.crud.SetAndDeleteContentTest;
+import org.apache.chemistry.opencmis.tck.tests.crud.UpdateSmokeTest;
 import org.apache.chemistry.opencmis.tck.tests.filing.FilingTestGroup;
 import org.apache.chemistry.opencmis.tck.tests.query.ContentChangesSmokeTest;
 import org.apache.chemistry.opencmis.tck.tests.query.QuerySmokeTest;
@@ -105,36 +120,35 @@ public abstract class AbstractEnterpriseOpenCMIS11TCKTest extends AbstractEnterp
         }
     }
        
-//    class OverrideCRUDTestGroup extends AbstractSessionTestGroup
-//    {
-//        @Override
-//        public void init(Map<String, String> parameters) throws Exception
-//        {
-//            super.init(parameters);
-//
-//            setName("CRUD Test Group");
-//            setDescription("Create, Read, Update, and Delete tests.");
-//
-//            addTest(new CreateAndDeleteFolderTest());
-//            addTest(new CreateAndDeleteDocumentTest());
-//            addTest(new CreateBigDocument());
-//            addTest(new CreateDocumentWithoutContent());
-//            addTest(new NameCharsetTest());
-//            addTest(new CreateAndDeleteRelationshipTest());
-//            addTest(new CreateAndDeleteItemTest());
-//            addTest(new UpdateSmokeTest());
-//            // TCK fails because we support only 1 object in bulk update at present
-//            // See ACE-34
-//            //addTest(new BulkUpdatePropertiesTest());
-//            addTest(new SetAndDeleteContentTest());
-//            addTest(new ChangeTokenTest());
-//            addTest(new ContentRangesTest());
-//            addTest(new CopyTest());
-//            addTest(new MoveTest());
-//            addTest(new DeleteTreeTest());
-//            addTest(new OperationContextTest());
-//        }
-//    }
+    class OverrideCRUDTestGroup extends AbstractSessionTestGroup
+    {
+        @Override
+        public void init(Map<String, String> parameters) throws Exception
+        {
+            super.init(parameters);
+
+            setName("CRUD Test Group");
+            setDescription("Create, Read, Update, and Delete tests.");
+
+            addTest(new CreateAndDeleteFolderTest());
+            addTest(new CreateAndDeleteDocumentTest());
+            addTest(new CreateBigDocument());
+            addTest(new CreateDocumentWithoutContent());
+            addTest(new NameCharsetTest());
+            addTest(new CreateAndDeleteRelationshipTest());
+            addTest(new CreateAndDeleteItemTest());
+            addTest(new UpdateSmokeTest());
+            addTest(new BulkUpdatePropertiesTest());
+            addTest(new BulkUpdatePropertiesCustomTest());
+            addTest(new SetAndDeleteContentTest());
+            addTest(new ChangeTokenTest());
+            addTest(new ContentRangesTest());
+            addTest(new CopyTest());
+            addTest(new MoveTest());
+            addTest(new DeleteTreeTest());
+            addTest(new OperationContextTest());
+        }
+    }
 
     public class OverrideQueryTestGroup extends AbstractSessionTestGroup
     {

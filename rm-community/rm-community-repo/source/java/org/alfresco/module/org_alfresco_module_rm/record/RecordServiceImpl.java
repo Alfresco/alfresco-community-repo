7bfd2924806f27bc80a97d2855ad507f10f2e1f2
Roy Wetherall <rwetherall@gmail.com>
2016-07-26 13:07:21 +1000

    RM-3649: missed hard coded error messages

--- rm-community/rm-community-repo/source/java/org/alfresco/module/org_alfresco_module_rm/record/RecordServiceImpl.java
+++ rm-community/rm-community-repo/source/java/org/alfresco/module/org_alfresco_module_rm/record/RecordServiceImpl.java
@@ -1813,7 +1813,7 @@ public class RecordServiceImpl extends BaseBehaviourBean
                 if (recordDispositionSchedule.isRecordLevelDisposition() != recordFolderDispositionSchedule.isRecordLevelDisposition())
                 {
                     // we can't link a record to an incompatible disposition schedule
-                    throw new RecordLinkRuntimeException("Can not link a record to a record folder with an incompatible disposition schedule.  "
+                    throw new RecordLinkRuntimeException("Can not link a record to a record folder with an incompatible retention schedule.  "
                                                      + "They must either both be record level or record folder level dispositions.");
                 }
             }

31a44c72aa4657f37712421e58bc09a5c8182d23
Alan Davis <alan.davis@alfresco.com>
2016-09-06 22:07:56 +0000

    Merged 5.2.N (5.2.1) to HEAD (5.2)    130278 gjames: Merged searchapi (5.2.1) to 5.2.N (5.2.1)       130015 gjames: Merged 5.2.N-AHIND (5.2.1) to searchapi (5.2.1)          129790 jvonka: REPO-164 / REPO-1141: fix error mapping when unable to update content due to locked node (should be 409 not 500)          - also minor cleanup of error handling/mapping (handle within common "writeContent")
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@130352 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/java/org/alfresco/rest/api/impl/NodesImpl.java
+++ source/java/org/alfresco/rest/api/impl/NodesImpl.java
@@ -124,6 +124,7 @@ import org.alfresco.service.cmr.dictionary.DictionaryService;
 import org.alfresco.service.cmr.dictionary.PropertyDefinition;
 import org.alfresco.service.cmr.lock.LockService;
 import org.alfresco.service.cmr.lock.NodeLockedException;
+import org.alfresco.service.cmr.lock.NodeLockedException;
 import org.alfresco.service.cmr.model.FileExistsException;
 import org.alfresco.service.cmr.model.FileFolderService;
 import org.alfresco.service.cmr.model.FileInfo;

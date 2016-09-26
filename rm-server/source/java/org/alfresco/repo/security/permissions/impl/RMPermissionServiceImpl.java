2b4c6616bcf0d846948f32fdfc88d18e7a51f12f
David Webster <david@cognite.net>
2016-09-26 21:55:35 +0100

    MNT-16852: back port code fix for add children permission support

--- rm-server/source/java/org/alfresco/repo/security/permissions/impl/RMPermissionServiceImpl.java
+++ rm-server/source/java/org/alfresco/repo/security/permissions/impl/RMPermissionServiceImpl.java
@@ -93,8 +93,9 @@ public class RMPermissionServiceImpl extends PermissionServiceImpl
         {
             return super.hasPermission(nodeRef, RMPermissionModel.READ_RECORDS);
         }
+        // Added ADD_CHILDREN check in for MNT-16852.
         else if (AccessStatus.DENIED.equals(acs) &&
-                 PermissionService.WRITE.equals(perm) &&
+                (PermissionService.WRITE.equals(perm) || PermissionService.ADD_CHILDREN.equals(perm)) &&
                  nodeService.hasAspect(nodeRef, RecordsManagementModel.ASPECT_FILE_PLAN_COMPONENT))
         {
             return super.hasPermission(nodeRef, RMPermissionModel.FILE_RECORDS);

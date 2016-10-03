5678eb87f0f41c9092758af537d436a6126afbb8
Ana Bozianu <ana.bozianu@ness.com>
2016-10-03 14:35:20 +0300

    RM-4012 - added the ig rest api

---
+++ rm-community/rm-community-repo/source/java/org/alfresco/module/org_alfresco_module_rm/rest/api/RMNodesEntityResource.java
@@ -0,0 +1,56 @@
+/*
+ * #%L
+ * Alfresco Records Management Module
+ * %%
+ * Copyright (C) 2005 - 2016 Alfresco Software Limited
+ * %%
+ * This file is part of the Alfresco software.
+ * -
+ * If the software was purchased under a paid Alfresco license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
+ * provided under the following open source license terms:
+ * -
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * -
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * -
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+package org.alfresco.module.org_alfresco_module_rm.rest.api;
+
+import org.alfresco.module.org_alfresco_module_rm.rest.api.model.RMNode;
+import org.alfresco.rest.framework.core.exceptions.EntityNotFoundException;
+import org.alfresco.rest.framework.resource.EntityResource;
+import org.alfresco.rest.framework.resource.actions.interfaces.EntityResourceAction;
+import org.alfresco.rest.framework.resource.parameters.Parameters;
+
+@EntityResource(name="nodes", title = "Nodes")
+public class RMNodesEntityResource implements 
+        EntityResourceAction.ReadById<String>, 
+        EntityResourceAction.Update<RMNode>
+{
+
+    @Override
+    public String readById(String arg0, Parameters arg1) throws EntityNotFoundException
+    {
+        return null;
+    }
+
+    @Override
+    public RMNode update(String arg0, RMNode arg1, Parameters arg2)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+
+}

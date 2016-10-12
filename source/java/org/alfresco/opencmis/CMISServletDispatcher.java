0c51b17e0f3ecb63273600d3fbc84d90a5a280bc
Alan Davis <alan.davis@alfresco.com>
2016-10-12 11:33:40 +0000

    Merged 5.2.N-SERVLET-3.0 (5.2.1) to 5.2.N (5.2.1)    130708: REPO-843 / REPO-1246: Upgrade to Servlet 3.0 (part I),    130714: REPO-843 / REPO-1246: Upgrade to Servlet 3.0 (part 1b)    - fix web.xml (web-app),    130727: REPO-843 / REPO-1246: Upgrade to Servlet 3.0 (part 1b)    - update other web.xml's (other than web-client project),    130844: REPO-843 / REPO-1251: Upgrade to Servlet API 3.0.x (part 2) - update to Spring WebScripts 6.9 (ACE-5584)    - update to servlet-api 3.0.1,    130847: REPO-843 / REPO-1251: Upgrade to Servlet API 3.0.x (part 2) - update to Spring WebScripts 6.9 (ACE-5584)    - follow-on to commit the correct pom.xml (ie. parent rather than overlay),    130852: REPO-843 / REPO-1251: Upgrade to Servlet API 3.0.x (part 2) - update to Spring WebScripts 6.9 (ACE-5584)    - follow-on
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/BRANCHES/DEV/5.2.N/root@131370 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/java/org/alfresco/opencmis/CMISServletDispatcher.java
+++ source/java/org/alfresco/opencmis/CMISServletDispatcher.java
@@ -31,15 +31,23 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.EventListener;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
+import javax.servlet.Filter;
+import javax.servlet.FilterRegistration;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+import javax.servlet.SessionCookieConfig;
+import javax.servlet.SessionTrackingMode;
+import javax.servlet.descriptor.JspConfigDescriptor;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletResponse;
 
@@ -299,6 +307,12 @@ public abstract class CMISServletDispatcher implements CMISDispatcher
 				}
 
 				@Override
+				public boolean setInitParameter(String name, String value)
+				{
+					return false;
+				}
+
+				@Override
 				public int getMajorVersion()
 				{
 					return 0;
@@ -312,6 +326,18 @@ public abstract class CMISServletDispatcher implements CMISDispatcher
 
 				@Override
 				public int getMinorVersion()
+				{
+					return 0;
+				}
+
+				@Override
+				public int getEffectiveMajorVersion()
+				{
+					return 0;
+				}
+
+				@Override
+				public int getEffectiveMinorVersion()
 				{
 					return 0;
 				}
@@ -368,6 +394,144 @@ public abstract class CMISServletDispatcher implements CMISDispatcher
 				public String getServletContextName()
 				{
 					return null;
+				}
+
+				@Override
+				public ServletRegistration.Dynamic addServlet(String servletName, String className)
+				{
+					return null;
+				}
+
+				@Override
+				public ServletRegistration.Dynamic addServlet(String servletName, Servlet servlet)
+				{
+					return null;
+				}
+
+				@Override
+				public ServletRegistration.Dynamic addServlet(String servletName, Class<? extends Servlet> servletClass)
+				{
+					return null;
+				}
+
+				@Override
+				public <T extends Servlet> T createServlet(Class<T> clazz) throws ServletException
+				{
+					return null;
+				}
+
+				@Override
+				public ServletRegistration getServletRegistration(String servletName)
+				{
+					return null;
+				}
+
+				@Override
+				public Map<String, ? extends ServletRegistration> getServletRegistrations()
+				{
+					return null;
+				}
+
+				@Override
+				public FilterRegistration.Dynamic addFilter(String filterName, String className)
+				{
+					return null;
+				}
+
+				@Override
+				public FilterRegistration.Dynamic addFilter(String filterName, Filter filter)
+				{
+					return null;
+				}
+
+				@Override
+				public FilterRegistration.Dynamic addFilter(String filterName, Class<? extends Filter> filterClass)
+				{
+					return null;
+				}
+
+				@Override
+				public <T extends Filter> T createFilter(Class<T> clazz) throws ServletException
+				{
+					return null;
+				}
+
+				@Override
+				public FilterRegistration getFilterRegistration(String filterName)
+				{
+					return null;
+				}
+
+				@Override
+				public Map<String, ? extends FilterRegistration> getFilterRegistrations()
+				{
+					return null;
+				}
+
+				@Override
+				public SessionCookieConfig getSessionCookieConfig()
+				{
+					return null;
+				}
+
+				@Override
+				public void setSessionTrackingModes(Set<SessionTrackingMode> sessionTrackingModes)
+				{
+
+				}
+
+				@Override
+				public Set<SessionTrackingMode> getDefaultSessionTrackingModes()
+				{
+					return null;
+				}
+
+				@Override
+				public Set<SessionTrackingMode> getEffectiveSessionTrackingModes()
+				{
+					return null;
+				}
+
+				@Override
+				public void addListener(String className)
+				{
+
+				}
+
+				@Override
+				public <T extends EventListener> void addListener(T t)
+				{
+
+				}
+
+				@Override
+				public void addListener(Class<? extends EventListener> listenerClass)
+				{
+
+				}
+
+				@Override
+				public <T extends EventListener> T createListener(Class<T> clazz) throws ServletException
+				{
+					return null;
+				}
+
+				@Override
+				public JspConfigDescriptor getJspConfigDescriptor()
+				{
+					return null;
+				}
+
+				@Override
+				public ClassLoader getClassLoader()
+				{
+					return null;
+				}
+
+				@Override
+				public void declareRoles(String... roleNames)
+				{
+
 				}
 
 				@Override

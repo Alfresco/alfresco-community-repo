ee07a75d67ea20c1b4e0c21931a1ad955f5df95b
Silviu Dinuta <silviu.dinuta@savatech.ro>
2016-10-18 12:21:42 +0300

    RM-4199: create rm site rest api impl

---
+++ rm-community/rm-community-repo/source/java/org/alfresco/rest/api/sites/RMSiteEntityResource.java
@@ -0,0 +1,80 @@
+/*
+ * #%L
+ * Alfresco Records Management Module
+ * %%
+ * Copyright (C) 2005 - 2016 Alfresco Software Limited
+ * %%
+ * This file is part of the Alfresco software.
+ * -
+ * If the software was purchased under a paid Alfresco license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
+ * provided under the following open source license terms:
+ * -
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * -
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * -
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+package org.alfresco.rest.api.sites;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.alfresco.rest.api.Sites;
+import org.alfresco.rest.api.model.Site;
+import org.alfresco.rest.framework.core.exceptions.EntityNotFoundException;
+import org.alfresco.rest.framework.resource.EntityResource;
+import org.alfresco.rest.framework.resource.actions.interfaces.EntityResourceAction;
+import org.alfresco.rest.framework.resource.parameters.CollectionWithPagingInfo;
+import org.alfresco.rest.framework.resource.parameters.Parameters;
+
+/**
+ *
+ * @author Silviu Dinuta
+ * @since 2.6
+ *
+ */
+@EntityResource(name = "ig-sites", title = "IG Sites")
+public class RMSiteEntityResource implements EntityResourceAction.Read<Site>, EntityResourceAction.ReadById<Site>,
+        EntityResourceAction.Delete, EntityResourceAction.Create<Site> {
+
+    private Sites sites;
+
+    public void setSites(Sites sites) {
+        this.sites = sites;
+    }
+
+    @Override
+    public List<Site> create(List<Site> entity, Parameters parameters) {
+        List<Site> result = new ArrayList<>(1);
+        result.add(sites.createSite(entity.get(0), parameters));
+        return result;
+    }
+
+    @Override
+    public void delete(String id, Parameters parameters) {
+        sites.deleteSite(id, parameters);
+
+    }
+
+    @Override
+    public Site readById(String id, Parameters parameters) throws EntityNotFoundException {
+        return sites.getSite(id);
+    }
+
+    @Override
+    public CollectionWithPagingInfo<Site> readAll(Parameters params) {
+        return sites.getSites(params);
+    }
+
+}

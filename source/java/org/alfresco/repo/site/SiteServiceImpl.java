36b0c4d2a7ae940059ffb0541f511d310d248876
Alan Davis <alan.davis@alfresco.com>
2016-12-06 17:17:28 +0000

    Merged 5.2.0 (5.2.0) to HEAD (5.2)    133073 dhulley: Bring imports into line during investigation
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@133387 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/java/org/alfresco/repo/site/SiteServiceImpl.java
+++ source/java/org/alfresco/repo/site/SiteServiceImpl.java
@@ -38,7 +38,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.SortedSet;
-import java.util.StringTokenizer;
 import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;

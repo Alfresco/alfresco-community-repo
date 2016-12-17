6a45a6dab646952e6d16714be6d206c0f22531a3
Ana Bozianu <ana.bozianu@ness.com>
2016-12-17 23:22:12 +0200

    RM-4436 - implemented file record RM-4429 - implemented declare record -renamed the nodes package into fileplancomponents

---
+++ rm-community/rm-community-repo/source/java/org/alfresco/rm/rest/api/files/FilesEntityResource.java
@@ -0,0 +1,69 @@
+/*
+ * #%L
+ * Alfresco Records Management Module
+ * %%
+ * Copyright (C) 2005 - 2016 Alfresco Software Limited
+ * %%
+ * This file is part of the Alfresco software.
+ * -
+ * If the software was purchased under a paid Alfresco license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
+ * provided under the following open source license terms:
+ * -
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * -
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * -
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+package org.alfresco.rm.rest.api.files;
+
+import org.alfresco.rest.api.model.Node;
+import org.alfresco.rest.framework.Operation;
+import org.alfresco.rest.framework.WebApiDescription;
+import org.alfresco.rest.framework.resource.EntityResource;
+import org.alfresco.rest.framework.resource.parameters.Parameters;
+import org.alfresco.rest.framework.webscripts.WithResponse;
+import org.alfresco.rm.rest.api.Records;
+import org.alfresco.rm.rest.api.model.TargetContainer;
+import org.alfresco.util.ParameterCheck;
+import org.springframework.beans.factory.InitializingBean;
+
+/**
+ * An implementation of an Entity Resource for a file
+ *
+ * @author Ana Bozianu
+ * @since 2.6
+ */
+@EntityResource(name="files", title = "Files")
+public class FilesEntityResource implements InitializingBean
+{
+    private Records records;
+
+    public void setRecords(Records records)
+    {
+        this.records = records;
+    }
+
+    @Operation("declare")
+    @WebApiDescription(title = "Declare as record", description="Declare a file as record.")
+    public Node declareAsRecord(String fileId, Void body, Parameters parameters, WithResponse withResponse)
+    {
+        return records.declareFileAsRecord(fileId, parameters);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception
+    {
+        ParameterCheck.mandatory("records", this.records);
+    }
+}

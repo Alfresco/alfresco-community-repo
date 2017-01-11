ad43d5fb66079d5e6c38a40f6a9a2ef6c72259a9
Alan Davis <alan.davis@alfresco.com>
2017-01-11 12:32:55 +0000

    Merged 5.2.0 (5.2.0) to trunk (5.2)    134091 kroast: Merged 5.2.N (5.2.1) to 5.2.0 (5.2.0)       133908 ddraper: ACE-5708: Prevent highlight delimiters on Firefox site search dashlet

--- share-services/src/main/resources/alfresco/templates/webscripts/org/alfresco/slingshot/search/search.get.js
+++ share-services/src/main/resources/alfresco/templates/webscripts/org/alfresco/slingshot/search/search.get.js
@@ -16,23 +16,25 @@ function main()
       startIndex: (args.startIndex !== null) ? parseInt(args.startIndex, 10) : 0,
       facetFields: args.facetFields,
       filters: args.filters,
-      
-      // Data for search term highlighting...
-      highlightFields: (args.highlightFields !== null) ? args.highlightFields : DEFAULT_HIGHLIGHT_FIELDS,
-      highlightPrefix: (args.highlightPrefix !== null) ? args.highlightPrefix : DEFAULT_HIGHLIGHT_POSTFIX,
-      highlightPostfix: (args.highlightPostfix !== null) ? args.highlightPostfix : DEFAULT_HIGHLIGHT_POSTFIX,
-      highlightSnippetCount: (args.highlightSnippetCount !== null) ? parseInt(args.highlightSnippetCount, 10) : DEFAULT_HIGHLIGHT_SNIPPET_COUNT,
-      highlightFragmentSize: (args.highlightFragmentSize !== null) ? parseInt(args.highlightFragmentSize, 10) : DEFAULT_HIGHLIGHT_FRAGMENT_SIZE,
-      highlightUsePhraseHighlighter: (args.highlightUsePhraseHighlighter !== null) ? args.highlightUsePhraseHighlighter.toUpperCase() === "TRUE" : DEFAULT_HIGHLIGHT_USE_PHRASE_HIGHLIGHTER,
-      highlightMergeContiguous: (args.highlightMergeContiguous !== null) ? args.highlightMergeContiguous.toUpperCase() === "TRUE" : DEFAULT_HIGHLIGHT_MERGE_CONTIGUOUS,
-      
       encodedFilters: args.encodedFilters,
       spell: (args.spellcheck !== null) ? (args.spellcheck == "true") : false
    };
 
-   if (args.highlightMaxAnalyzedChars !== null)
+   if (args.highlightFields)
    {
-      params.highlightMaxAnalyzedChars = parseInt(args.highlightMaxAnalyzedChars, 10)
+      // Data for search term highlighting...
+      params.highlightFields = args.highlightFields;
+      params.highlightPrefix = (args.highlightPrefix !== null) ? args.highlightPrefix : DEFAULT_HIGHLIGHT_POSTFIX;
+      params.highlightPostfix = (args.highlightPostfix !== null) ? args.highlightPostfix : DEFAULT_HIGHLIGHT_POSTFIX;
+      params.highlightSnippetCount = (args.highlightSnippetCount !== null) ? parseInt(args.highlightSnippetCount, 10) : DEFAULT_HIGHLIGHT_SNIPPET_COUNT;
+      params.highlightFragmentSize = (args.highlightFragmentSize !== null) ? parseInt(args.highlightFragmentSize, 10) : DEFAULT_HIGHLIGHT_FRAGMENT_SIZE;
+      params.highlightUsePhraseHighlighter = (args.highlightUsePhraseHighlighter !== null) ? args.highlightUsePhraseHighlighter.toUpperCase() === "TRUE" : DEFAULT_HIGHLIGHT_USE_PHRASE_HIGHLIGHTER;
+      params.highlightMergeContiguous = (args.highlightMergeContiguous !== null) ? args.highlightMergeContiguous.toUpperCase() === "TRUE" : DEFAULT_HIGHLIGHT_MERGE_CONTIGUOUS;
+      
+      if (args.highlightMaxAnalyzedChars !== null)
+      {
+         params.highlightMaxAnalyzedChars = parseInt(args.highlightMaxAnalyzedChars, 10)
+      }
    }
    
    model.data = getSearchResults(params);

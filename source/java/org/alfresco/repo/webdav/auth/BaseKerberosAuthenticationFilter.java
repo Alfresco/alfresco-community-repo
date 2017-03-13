9d1dca47a44717a6c8f7b8f7a3e54afceed8f949
Alex Mukha <alex.mukha@alfresco.com>
2017-03-13 16:14:07 +0000

    MNT-16931 Partially revert the fix for MNT-14367
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/BRANCHES/DEV/5.2.N/root@135802 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/java/org/alfresco/repo/webdav/auth/BaseKerberosAuthenticationFilter.java
+++ source/java/org/alfresco/repo/webdav/auth/BaseKerberosAuthenticationFilter.java
@@ -365,7 +365,9 @@ public abstract class BaseKerberosAuthenticationFilter extends BaseSSOAuthentica
                         req.getRemoteAddr() + ":" + req.getRemotePort() + ")");
             
             // Send back a request for SPNEGO authentication
-            logonStartAgain(context, req, resp, true);
+            // if the fallback is ignored - the browser will go into an infinite loop
+            // see MNT-16931
+            logonStartAgain(context, req, resp, false);
             return false;
         }
         else

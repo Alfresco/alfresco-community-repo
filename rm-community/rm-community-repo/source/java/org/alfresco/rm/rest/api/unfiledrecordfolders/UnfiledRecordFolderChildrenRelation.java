cda2da987ecf901fcd68fc75cf9d30c5ef75eb9b
Ramona Popa <Ramona.Popa@ness.com>
2017-05-16 10:21:24 +0300

    RM-5087 - when POST return minimal info

--- rm-community/rm-community-repo/source/java/org/alfresco/rm/rest/api/unfiledrecordfolders/UnfiledRecordFolderChildrenRelation.java
+++ rm-community/rm-community-repo/source/java/org/alfresco/rm/rest/api/unfiledrecordfolders/UnfiledRecordFolderChildrenRelation.java
@@ -208,7 +208,7 @@ public class UnfiledRecordFolderChildrenRelation implements RelationshipResource
         {
             FileInfo info = fileFolderService.getFileInfo(newNodeRef);
             apiUtils.postActivity(info, parentNodeRef, ActivityType.FILE_ADDED);
-            result.add(nodesModelFactory.createUnfiledRecordFolderChild(info, parameters, mapUserInfo, false));
+            result.add(nodesModelFactory.createUnfiledRecordFolderChild(info, parameters, mapUserInfo, true));
         }
 
         return result;
@@ -244,6 +244,6 @@ public class UnfiledRecordFolderChildrenRelation implements RelationshipResource
         FileInfo info = fileFolderService.getFileInfo(newNode);
         apiUtils.postActivity(info, parent, ActivityType.FILE_ADDED);
 
-        return nodesModelFactory.createUnfiledRecordFolderChild(info, parameters, null, false);
+        return nodesModelFactory.createUnfiledRecordFolderChild(info, parameters, null, true);
     }
 }

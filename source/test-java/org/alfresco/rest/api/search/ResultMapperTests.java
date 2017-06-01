d1af5d89b8a0033f7d0c5ae74ec204e44386585b
Gethin James <gethin.james@alfresco.com>
2017-06-01 11:13:06 +0000

    Merged searchrep (5.2.1) to 5.2.N (5.2.1)    136973 msuzuki: Search-426, update mapper to handle facet fields to accept facetFormat
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/BRANCHES/DEV/5.2.N/root@137077 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- source/test-java/org/alfresco/rest/api/search/ResultMapperTests.java
+++ source/test-java/org/alfresco/rest/api/search/ResultMapperTests.java
@@ -656,5 +656,54 @@ public class ResultMapperTests
         ResultSet results = new SolrJSONResultSet(json,sp,nodeService, null, LimitBy.FINAL_SIZE, 10);
         return results;
     }
+    
+    @Test
+    /**
+     * Validates that when facetFormat is specified then all facets are returned
+     * in the generic facet response format AKA V2.
+     */
+    public void facetFormatTest() throws Exception
+    {
+        String jsonQuery = "{\"query\": {\"query\": \"alfresco\"},"
+                + "\"facetQueries\": [" 
+                + "{\"query\": \"content.size:[0 TO 102400]\", \"label\": \"small\"},"
+                + "{\"query\": \"content.size:[102400 TO 1048576]\", \"label\": \"medium\"}," 
+                + "{\"query\": \"content.size:[1048576 TO 16777216]\", \"label\": \"large\"}]"
+                + ",\"facetFormat\":\"V2\""
+                + "}";
+    
+        String expectedResponse = "{\"responseHeader\":{\"status\":0,\"QTime\":9},\"_original_parameters_\":\"org.apache.solr.common.params.DefaultSolrParams:{params(df=TEXT&alternativeDic=DEFAULT_DICTIONARY&fl=DBID,score&start=0&fq={!afts}AUTHORITY_FILTER_FROM_JSON&fq={!afts}TENANT_FILTER_FROM_JSON&rows=1000&locale=en_US&wt=json),defaults(carrot.url=id&spellcheck.collateExtendedResults=true&carrot.produceSummary=true&spellcheck.maxCollations=3&spellcheck.maxCollationTries=5&spellcheck.alternativeTermCount=2&spellcheck.extendedResults=false&defType=afts&spellcheck.maxResultsForSuggest=5&spellcheck=false&carrot.outputSubClusters=false&spellcheck.count=5&carrot.title=mltext@m___t@{http://www.alfresco.org/model/content/1.0}title&carrot.snippet=content@s___t@{http://www.alfresco.org/model/content/1.0}content&spellcheck.collate=true)}\",\"_field_mappings_\":{},\"_date_mappings_\":{},\"_range_mappings_\":{},\"_pivot_mappings_\":{},\"_interval_mappings_\":{},\"_stats_field_mappings_\":{},\"_stats_facet_mappings_\":{},\"_facet_function_mappings_\":{},\"response\":{\"numFound\":6,\"start\":0,\"maxScore\":0.7849362,\"docs\":[{\"DBID\":565,\"score\":0.7849362},{\"DBID\":566,\"score\":0.7849362},{\"DBID\":521,\"score\":0.3540957},{\"DBID\":514,\"score\":0.33025497},{\"DBID\":420,\"score\":0.32440513},{\"DBID\":415,\"score\":0.2780319}]},"
+                    + "\"spellcheck\":{\"searchInsteadFor\":\"alfresco\"},"
+                    + "\"facet_counts\":{\"facet_queries\": {\"small\": 52,\"large\": 0,\"medium\": 0}},"
+                    + "\"processedDenies\":true, \"lastIndexedTx\":34}";
+
+        ResultSet results = mockResultset(expectedResponse);
+        SearchQuery searchQuery = helper.extractFromJson(jsonQuery);
+        SearchRequestContext searchRequest = SearchRequestContext.from(searchQuery);
+        SearchContext searchContext = mapper.toSearchContext((SolrJSONResultSet) results, searchRequest, searchQuery, 0);
+        assertEquals(34l, searchContext.getConsistency().getlastTxId());
+        assertEquals(null, searchContext.getFacetQueries());
+        assertEquals(1, searchContext.getFacets().size());
+        assertEquals(3,searchContext.getFacets().get(0).getBuckets().size());
+        assertEquals("small",searchContext.getFacets().get(0).getBuckets().get(0).getLabel());
+        assertEquals("content.size:[0 TO 102400]",searchContext.getFacets().get(0).getBuckets().get(0).getFilterQuery());
+        assertFalse(searchContext.getFacets().get(0).getBuckets().get(0).getMetrics().isEmpty());
+        Metric[] metrics = searchContext.getFacets().get(0).getBuckets().get(0).getMetrics().toArray(new Metric[searchContext.getFacets().get(0).getBuckets().get(0).getMetrics().size()]);
+        assertEquals(METRIC_TYPE.count, metrics[0].getType());
+        assertEquals("{count=52}", metrics[0].getValue().toString());
+        
+        jsonQuery = jsonQuery.replace("V2", "V1");
+        searchQuery = helper.extractFromJson(jsonQuery);
+        results = mockResultset(expectedResponse);
+        searchRequest = SearchRequestContext.from(searchQuery);
+        searchContext = mapper.toSearchContext((SolrJSONResultSet) results, searchRequest, searchQuery, 0);
+        assertEquals(34l, searchContext.getConsistency().getlastTxId());
+        assertTrue(searchContext.getFacets().isEmpty());
+        assertEquals(3,searchContext.getFacetQueries().size());
+        assertEquals("small",searchContext.getFacetQueries().get(0).getLabel());
+        assertEquals("content.size:[0 TO 102400]",searchContext.getFacetQueries().get(0).getFilterQuery());
+        assertEquals(52, searchContext.getFacetQueries().get(0).getCount());
+        assertEquals("large",searchContext.getFacetQueries().get(1).getLabel());
+    }
 
 }

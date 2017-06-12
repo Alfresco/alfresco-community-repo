b1f84e86c592280bd85103b970a2955311766fc2
Tuna Aksoy <tuna.aksoy@alfresco.com>
2017-06-12 16:17:24 +0100

    RM-4689 (Can delete all RM site content through Repository browser)
    Manually backported following changes:
    
    RM-4293
    135c4e04e0f8401f70222125c2caa54b728a77d2
    
    RM-4326
    fce705ff72b5d5791e749c29a191c2dedfdf9de6
    0836e9682b15422c2a5ae034da7d8a1de7ce4675

--- rm-community/rm-community-repo/unit-test/java/org/alfresco/module/org_alfresco_module_rm/model/rma/type/TransferContainerTypeUnitTest.java
+++ rm-community/rm-community-repo/unit-test/java/org/alfresco/module/org_alfresco_module_rm/model/rma/type/TransferContainerTypeUnitTest.java
@@ -26,11 +26,16 @@
  */
 package org.alfresco.module.org_alfresco_module_rm.model.rma.type;
 
-import org.alfresco.error.AlfrescoRuntimeException;
-import org.alfresco.model.ContentModel;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+import org.alfresco.module.org_alfresco_module_rm.test.util.AlfMock;
 import org.alfresco.module.org_alfresco_module_rm.test.util.BaseUnitTest;
+import org.alfresco.repo.node.integrity.IntegrityException;
 import org.alfresco.service.cmr.repository.ChildAssociationRef;
 import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.namespace.QName;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 
@@ -42,40 +47,27 @@ import org.mockito.InjectMocks;
  */
 public class TransferContainerTypeUnitTest extends BaseUnitTest
 {
-    /** test object */
-    private @InjectMocks TransferContainerType transferContainerType;
+    /**
+     * test object
+     */
+    private @InjectMocks
+    TransferContainerType transferContainerType;
 
     /**
-     * Having the Unfilled Record container and a folder having the aspect ASPECT_HIDDEN When adding a child association
-     * between the folder and the container Then the folder type shouldn't be renamed
+     * Given that we try to add to transfer container,
+     * Then IntegrityException is thrown.
      */
-    @Test(expected = AlfrescoRuntimeException.class)
-    public void testAddContentToTransferContainerTest()
+    @Test(expected = IntegrityException.class)
+    public void testAddToTransferContainerTest()
     {
+        NodeRef transferContainer = generateNodeRef(TYPE_TRANSFER_CONTAINER, true);
 
-        NodeRef transferContainer = createTransferContainer();
+        QName type = AlfMock.generateQName();
+        NodeRef nodeRef = AlfMock.generateNodeRef(mockedNodeService, type);
 
-        /*
-         * When adding a child association between the folder and the container
-         */
-        NodeRef record = generateNodeRef(ContentModel.TYPE_CONTENT);
-        ChildAssociationRef childAssoc = new ChildAssociationRef(ContentModel.TYPE_CONTENT, transferContainer,
-                    ContentModel.TYPE_CONTENT, record);
-
-        transferContainerType.onCreateChildAssociation(childAssoc, true);
-
+        ChildAssociationRef mockedChildAssoc = mock(ChildAssociationRef.class);
+        when(mockedChildAssoc.getChildRef()).thenReturn(nodeRef);
+        when(mockedChildAssoc.getParentRef()).thenReturn(transferContainer);
+        transferContainerType.onCreateChildAssociation(mockedChildAssoc, true);
     }
-
-    /**
-     * Generates a record management container
-     *
-     * @return reference to the generated container
-     */
-    private NodeRef createTransferContainer()
-    {
-        NodeRef holdContainer = generateNodeRef(TYPE_TRANSFER, true);
-
-        return holdContainer;
-    }
-
 }

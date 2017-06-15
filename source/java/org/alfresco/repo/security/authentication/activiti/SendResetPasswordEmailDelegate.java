5e95ad4efd65e5ec65f40cbf70a1ebe00060ce51
Andrei Rebegea <andrei.rebegea@ness.com>
2017-06-15 10:56:28 +0000

    Merged 5.2.N (5.2.2) to HEAD (5.2)    135930 jkaabimofrad: Merged WEBAPP-API (5.2.1) to 5.2.N (5.2.1)       135590 jkaabimofrad: APPSREPO-35: Added password reset V1 API.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@137434 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

---
+++ source/java/org/alfresco/repo/security/authentication/activiti/SendResetPasswordEmailDelegate.java
@@ -0,0 +1,48 @@
+/*
+ * #%L
+ * Alfresco Remote API
+ * %%
+ * Copyright (C) 2005 - 2016 Alfresco Software Limited
+ * %%
+ * This file is part of the Alfresco software. 
+ * If the software was purchased under a paid Alfresco license, the terms of 
+ * the paid license agreement will prevail.  Otherwise, the software is 
+ * provided under the following open source license terms:
+ * 
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+package org.alfresco.repo.security.authentication.activiti;
+
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.JavaDelegate;
+
+/**
+ * This {@link JavaDelegate activiti delegate} is executed when a user request password reset.
+ *
+ * @author Jamal Kaabi-Mofrad
+ * @since 5.2.1
+ */
+public class SendResetPasswordEmailDelegate extends AbstractResetPasswordDelegate
+{
+    private static final String EMAIL_SUBJECT_KEY = "reset-password-request.email.subject";
+    private static final String EMAIL_TEMPLATE_PATH = "alfresco/templates/reset-password-email-templates/reset-password-email-template.ftl";
+
+    @Override
+    public void execute(DelegateExecution execution) throws Exception
+    {
+        resetPasswordService.sendResetPasswordEmail(execution, EMAIL_TEMPLATE_PATH, EMAIL_SUBJECT_KEY);
+    }
+}

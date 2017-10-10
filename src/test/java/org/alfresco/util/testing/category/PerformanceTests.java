1bb2cc97ca616e139eca5c73a0494c193d3ca575
alandavis <alan.davis@alfresco.com>
2017-10-10 16:34:05 +0100

    Fix/repo 2025 cat tests (#3)
    REPO-2025: adding the interface markers to categorise the tests

---
+++ src/test/java/org/alfresco/util/testing/category/PerformanceTests.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2005-2017 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.alfresco.util.testing.category;
+
+/**
+ * Category marker for tests that can't really be run on the build servers because they are meant to compare performance
+ * of some component on the same system. It usually requires manually running them locally for multiple times and
+ * comparing the results.
+ */
+public interface PerformanceTests extends NonBuildTests
+{
+}

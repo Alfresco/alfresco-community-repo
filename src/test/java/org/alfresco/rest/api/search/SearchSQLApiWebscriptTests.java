d1c2d843b2985a885c7e7e072df94fb89a5e83f4
Michael <michael.suzuki@alfresco.com>
2018-03-06 14:57:37 +0000

    Search-577: Moved the sql pass through endpoint from enterprise to public api

---
+++ src/test/java/org/alfresco/rest/api/search/SearchSQLApiWebscriptTests.java
@@ -0,0 +1,84 @@
+/*-
+ * #%L
+ * Alfresco Remote API
+ * %%
+ * Copyright (C) 2005 - 2016 Alfresco Software Limited
+ * %%
+ * This file is part of the Alfresco software. 
+ * If the software was purchased under a paid Alfresco license, the terms of 
+ * the paid license agreement will prevail.  Otherwise, the software is 
+ * provided under the following open source license terms:
+ * 
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+package org.alfresco.rest.api.search;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
+
+import java.util.Collections;
+
+import org.alfresco.rest.api.search.model.SearchSQLQuery;
+import org.alfresco.service.cmr.search.SearchParameters;
+import org.junit.Test;
+
+/**
+ * Tests the SearchSQLApiWebscript class
+ *
+ * @author Michael Suzuki
+ */
+public class SearchSQLApiWebscriptTests
+{
+    static SearchSQLApiWebscript webscript = new SearchSQLApiWebscript();
+
+    @Test
+    public void testSearchQueryParams() throws Exception
+    {
+        String query = "select SITE from alfresco";
+        SearchSQLQuery searchQuery = new SearchSQLQuery(query, "solr", Collections.EMPTY_LIST);
+        SearchParameters sparams = webscript.buildSearchParameters(searchQuery);
+        
+        assertNotNull(sparams);
+        assertEquals(query, sparams.getQuery());
+        assertEquals(Collections.EMPTY_LIST, sparams.getLocales());
+    }
+    @Test
+    public void testSearchQueryNullStmt() throws Exception
+    {
+        SearchSQLQuery searchQuery = new SearchSQLQuery(null, "solr", Collections.EMPTY_LIST);
+        try
+        {
+            webscript.buildSearchParameters(searchQuery);
+        }
+        catch (Exception e) 
+        {
+            assertEquals(true, e.getMessage().contains("Required stmt parameter is missing."));
+        }
+    }
+    @Test
+    public void testSearchSQLQueryAgainstNonInsight() throws Exception
+    {
+        SearchSQLQuery searchQuery = new SearchSQLQuery(null, "solr", Collections.EMPTY_LIST);
+        try
+        {
+            webscript.buildSearchParameters(searchQuery);
+        }
+        catch (Exception e) 
+        {
+            assertEquals(true, e.getMessage().contains("Required stmt parameter is missing."));
+        }
+    }
+
+}

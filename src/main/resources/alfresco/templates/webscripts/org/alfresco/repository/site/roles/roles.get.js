f1a99e88772781a4b4829e5d1a0a873aeb99aed3
Ancuta Morarasu <ancuta.morarasu@gmail.com>
2018-03-30 16:28:27 +0300

    REPO-1323: Upgrade alfresco-repository 6.39 (with Rhino 1.7.9) (#63)
     - Replace the use of concat() on a Java array in js

--- src/main/resources/alfresco/templates/webscripts/org/alfresco/repository/site/roles/roles.get.js
+++ src/main/resources/alfresco/templates/webscripts/org/alfresco/repository/site/roles/roles.get.js
@@ -11,8 +11,15 @@ function main()
    }
    
    // calculate the available "roles" and permissions groups for this site
+   var siteRoles = [];
+   var rolesList = siteService.listSiteRoles();
+   for (var i in rolesList)
+   {
+       siteRoles.push(rolesList[i]);
+   }
    // add the "None" pseudo role 
-   var siteRoles = siteService.listSiteRoles().concat(["None"]);
+   siteRoles.push("None");
+   
    var sitePermissionGroups = site.sitePermissionGroups;
    sitePermissionGroups["everyone"] = "GROUP_EVERYONE";
    

c0b8cef6bf4014133acb821dd95f8af33f29506a
cagache <claudia.agache@ness.com>
2018-04-03 09:21:03 +0300

    RM-2053 UI tests for Manage Record Classification capability

--- rm-automation/rm-automation-community-rest-api/src/main/java/org/alfresco/rest/v0/RMRolesAndActionsAPI.java
+++ rm-automation/rm-automation-community-rest-api/src/main/java/org/alfresco/rest/v0/RMRolesAndActionsAPI.java
@@ -162,6 +162,19 @@ public class RMRolesAndActionsAPI extends BaseAPI
         doPutJsonRequest(adminUser, adminPassword, HttpStatus.SC_OK, requestBody, RM_ROLES_ROLE, roleName);
     }
 
+    /** Delete a created RM role.
+     *
+     * @param adminUser The username of the admin user.
+     * @param adminPassword The password for the admin user.
+     * @param roleName The name of the role to be deleted.
+     */
+    public void deleteRole(String adminUser, String adminPassword, String roleName)
+    {
+        doDeleteRequest(adminUser, adminPassword, MessageFormat.format(RM_ROLES_ROLE, "{0}",roleName));
+        boolean success = !getConfiguredRoles(adminUser, adminPassword).contains(roleName);
+        assertTrue("Failed to delete role " + roleName + " with " + adminUser, success);
+    }
+
     /**
      * create user and assign to records management role
      */

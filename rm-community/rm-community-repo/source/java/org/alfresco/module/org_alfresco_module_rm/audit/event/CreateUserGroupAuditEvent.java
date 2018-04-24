764f636117aed1a4b9854c270809361691e3c74a
Tom Page <tom.page@alfresco.com>
2018-04-24 08:45:56 +0100

    RM-5236 Audit events for creating and deleting a user group.

---
+++ rm-community/rm-community-repo/source/java/org/alfresco/module/org_alfresco_module_rm/audit/event/CreateUserGroupAuditEvent.java
@@ -0,0 +1,50 @@
+/*
+ * #%L
+ * Alfresco Records Management Module
+ * %%
+ * Copyright (C) 2005 - 2018 Alfresco Software Limited
+ * %%
+ * This file is part of the Alfresco software.
+ * -
+ * If the software was purchased under a paid Alfresco license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
+ * provided under the following open source license terms:
+ * -
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * -
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * -
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+package org.alfresco.module.org_alfresco_module_rm.audit.event;
+
+import org.alfresco.repo.node.NodeServicePolicies.OnCreateNodePolicy;
+import org.alfresco.repo.policy.annotation.Behaviour;
+import org.alfresco.repo.policy.annotation.BehaviourBean;
+import org.alfresco.repo.policy.annotation.BehaviourKind;
+import org.alfresco.service.cmr.repository.ChildAssociationRef;
+
+/**
+ * Audits user group creation.
+ *
+ * @author Tom Page
+ * @since 2.7
+ */
+@BehaviourBean
+public class CreateUserGroupAuditEvent extends AuditEvent implements OnCreateNodePolicy
+{
+    @Override
+    @Behaviour(kind = BehaviourKind.CLASS, type = "cm:authorityContainer")
+    public void onCreateNode(ChildAssociationRef childAssocRef)
+    {
+        recordsManagementAuditService.auditEvent(childAssocRef.getChildRef(), getName());
+    }
+}

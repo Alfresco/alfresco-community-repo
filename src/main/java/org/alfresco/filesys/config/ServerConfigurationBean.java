54cfe126c5078dd1d2730914577e2e5210d995ca
Andrei Rebegea <andrei.rebegea@gmail.com>
2018-05-22 15:07:36 +0300

    REPO-3461: add external address support for FTP passive mode (#123)

--- src/main/java/org/alfresco/filesys/config/ServerConfigurationBean.java
+++ src/main/java/org/alfresco/filesys/config/ServerConfigurationBean.java
@@ -4,21 +4,21 @@
  * %%
  * Copyright (C) 2005 - 2016 Alfresco Software Limited
  * %%
- * This file is part of the Alfresco software. 
- * If the software was purchased under a paid Alfresco license, the terms of 
- * the paid license agreement will prevail.  Otherwise, the software is 
+ * This file is part of the Alfresco software.
+ * If the software was purchased under a paid Alfresco license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
  * provided under the following open source license terms:
- * 
+ *
  * Alfresco is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Alfresco is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -1409,8 +1409,15 @@ public class ServerConfigurationBean extends AbstractServerConfigurationBean imp
 	    			logger.info("FTP server data ports restricted to range " + rangeFrom + ":" + rangeTo);
             	}
             }
-            
-    		// FTPS parameter parsing
+
+            // Check for an external address
+            String ExtAddr = ftpConfigBean.getExternalAddress();
+            if (ExtAddr != null && ExtAddr.length() > 0)
+            {
+                ftpConfig.setFTPExternalAddress(ExtAddr);
+            }
+
+            // FTPS parameter parsing
     		//
     		// Check if a key store path has been specified
     		

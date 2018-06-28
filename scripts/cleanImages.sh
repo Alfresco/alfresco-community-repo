b809d899f550bb7fe4342e45febed3b30c35f161
cagache <claudia.agache@ness.com>
2018-06-28 14:44:19 +0300

    updated variable

--- scripts/cleanImages.sh
+++ scripts/cleanImages.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-imagesToBeDeleted=$1
-echo $imagesToBeDeleted
+echo $IMAGES_TO_BE_DELETED
 echo "List all images:"
 docker images -a
 
-docker_images_list=$(docker images | grep $imagesToBeDeleted | awk '{print $3}' | uniq)
+docker_images_list=$(docker images | grep $IMAGES_TO_BE_DELETED | awk '{print $3}' | uniq)
 if [ "$docker_images_list" == "" ]; then
     echo "No docker images on the agent"
 else

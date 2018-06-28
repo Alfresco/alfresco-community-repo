aaf8080d88360006093f1cbdc4dc94d2594d199d
cagache <claudia.agache@ness.com>
2018-06-28 16:24:46 +0300

    try to fix script to clean given docker images

--- cleanImages.sh
+++ cleanImages.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 set -x
 
-echo $imagesToBeDeleted
+imagesToBeDeleted="alfresco-content-repository\|ags-repo-enterprise\|ags-share-enterprise\|alfresco-share"
+echo "List all images:"
+docker images -a
+
+docker_images_list=$(docker images | grep '$imagesToBeDeleted' | awk '{print $3}' | uniq)
+if [ "$docker_images_list" == "" ]; then
+    echo "No docker images on the agent"
+else
+    echo "Clearing images: $docker_images_list"
+    docker rmi -f $docker_images_list
+fi

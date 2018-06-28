6238bdf733a759cfa2dc3eadc48550b4f2a57f6e
cagache <claudia.agache@ness.com>
2018-06-28 17:22:45 +0300

    try to fix script to clean given docker images

--- scripts/cleanImages.sh
+++ scripts/cleanImages.sh
@@ -5,7 +5,7 @@ echo $imagesToBeDeleted
 echo "List all images:"
 docker images -a
 
-docker_images_list=$(docker images | grep '$imagesToBeDeleted' | awk '{print $3}' | uniq)
+docker_images_list=$(docker images | grep $imagesToBeDeleted | awk '{print $3}' | uniq)
 if [ "$docker_images_list" == "" ]; then
     echo "No docker images on the agent"
 else

e381052539c45a4dbe535b71569aef9e68623e65
Alan Davis <alan.davis@alfresco.com>
2019-06-01 10:46:41 +0100

    Revert "Revert "Feature/search 1654 (#463)""
    This reverts commit 418db22fb815cccce5388d9200a3d6cddca73356.
    
    This is putting back a previously committed change that broke alfresco-enterprise-repository

--- src/main/java/org/alfresco/repo/domain/node/Node.java
+++ src/main/java/org/alfresco/repo/domain/node/Node.java
@@ -40,40 +40,42 @@ public interface Node extends NodeIdAndAclId
     /**
      * Helper method to get a key that includes the node and its current version number
      */
-    public NodeVersionKey getNodeVersionKey();
+    NodeVersionKey getNodeVersionKey();
     
     /**
      * Helper method to force the instance to be read-only
      */
-    public void lock();
+    void lock();
     
-    public abstract NodeRef getNodeRef();
+    NodeRef getNodeRef();
 
-    public NodeRef.Status getNodeStatus(QNameDAO qnameDAO);
+    NodeRef.Status getNodeStatus(QNameDAO qnameDAO);
     
-    public abstract Pair<Long, NodeRef> getNodePair();
+    Pair<Long, NodeRef> getNodePair();
     
     /**
      * Checks the {@link #getTypeQNameId() type} of the node to determine if the node is deleted
      * @param qnameDAO          DAO to work out type IDs
      * @return                  <tt>true</tt> if the node is {@link ContentModel#TYPE_DELETED}
      */
-    public abstract boolean getDeleted(QNameDAO qnameDAO);
+    boolean getDeleted(QNameDAO qnameDAO);
 
-    public abstract Long getVersion();
+    Long getVersion();
 
-    public abstract StoreEntity getStore();
+    StoreEntity getStore();
 
-    public abstract String getUuid();
+    String getUuid();
 
-    public abstract Long getTypeQNameId();
+    Long getTypeQNameId();
     
-    public abstract Long getLocaleId();
+    Long getLocaleId();
 
-    public abstract TransactionEntity getTransaction();
+    TransactionEntity getTransaction();
 
-    public abstract AuditablePropertiesEntity getAuditableProperties();
+    AuditablePropertiesEntity getAuditableProperties();
     
-    public abstract String getShardKey();
+    String getShardKey();
+
+    Integer getExplicitShardId();
 
 }

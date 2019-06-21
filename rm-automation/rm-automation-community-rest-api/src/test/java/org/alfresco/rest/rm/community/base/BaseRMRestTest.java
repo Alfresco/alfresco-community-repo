8bac296cff077748ffc32faab73411399ea78d93
cagache <claudia.agache@ness.com>
2019-06-21 16:33:08 +0300

    Revert "removed unused variable"
    This reverts commit c55c5929

--- rm-automation/rm-automation-community-rest-api/src/test/java/org/alfresco/rest/rm/community/base/BaseRMRestTest.java
+++ rm-automation/rm-automation-community-rest-api/src/test/java/org/alfresco/rest/rm/community/base/BaseRMRestTest.java
@@ -76,6 +76,7 @@ import org.alfresco.rest.rm.community.requests.gscore.api.RecordsAPI;
 import org.alfresco.rest.search.RestRequestQueryModel;
 import org.alfresco.rest.search.SearchNodeModel;
 import org.alfresco.rest.search.SearchRequest;
+import org.alfresco.rest.v0.RMRolesAndActionsAPI;
 import org.alfresco.rest.v0.SearchAPI;
 import org.alfresco.utility.data.DataUser;
 import org.alfresco.utility.model.ContentModel;
@@ -109,6 +110,10 @@ public class BaseRMRestTest extends RestTest
     @Autowired
     @Getter(value = PROTECTED)
     private ContentService contentService;
+
+    @Autowired
+    @Getter(value = PROTECTED)
+    private RMRolesAndActionsAPI rmRolesAndActionsAPI;
 
     @Autowired
     @Getter(value = PROTECTED)

697d1aa7998f6820d6408fe568a0c01659ba2264
eliaporciani <e.porciani@sease.io>
2019-07-15 16:21:18 +0200

    [SEARCH-1716] (#116)
    Fixed filter query. The query on user is case sensitive.

--- share-services/src/main/resources/alfresco/templates/webscripts/org/alfresco/slingshot/search/search.lib.js
+++ share-services/src/main/resources/alfresco/templates/webscripts/org/alfresco/slingshot/search/search.lib.js
@@ -1182,7 +1182,7 @@ function getSearchResults(params)
       }
       
       fqs.push('-TYPE:"cm:thumbnail" AND -TYPE:"cm:failedThumbnail" AND -TYPE:"cm:rating" AND -TYPE:"st:site"' +
-               ' AND -ASPECT:"st:siteContainer" AND -ASPECT:"sys:hidden" AND -cm:creator:system AND -QNAME:comment\\-*');
+               ' AND -ASPECT:"st:siteContainer" AND -ASPECT:"sys:hidden" AND -cm:creator:System  AND -QNAME:comment\\-*');
       
       // sort field - expecting field to in one of the following formats:
       //  - short QName form such as: cm:name

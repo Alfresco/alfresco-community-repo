d2c2f142701da8f176bb81132d1cb17bd9623818
rlucanu <roxana.lucanu-ghetu@ness.com>
2019-09-13 13:44:01 +0300

    RM-6943 code review changes

--- rm-community/rm-community-repo/source/java/org/alfresco/module/org_alfresco_module_rm/jscript/app/JSONConversionComponent.java
+++ rm-community/rm-community-repo/source/java/org/alfresco/module/org_alfresco_module_rm/jscript/app/JSONConversionComponent.java
@@ -87,7 +87,6 @@ public class JSONConversionComponent extends    org.alfresco.repo.jscript.app.JS
     private static final String IS_RECORD_CONTRIBUTOR_GROUP_ENABLED = "isRecordContributorGroupEnabled";
     private static final String RECORD_CONTRIBUTOR_GROUP_NAME = "recordContributorGroupName";
     private static final String IS_VISIBLE_FOR_CURRENT_USER = "isVisibleForCurrentUser";
-    private static final String IS_ADD_TO_HOLD_VISIBLE = "isAddToHoldVisible";
 
     /** true if record contributor group is enabled, false otherwise */
     private boolean isRecordContributorsGroupEnabled = false;
@@ -328,11 +327,6 @@ public class JSONConversionComponent extends    org.alfresco.repo.jscript.app.JS
                     boolean hasFilingPermission = !CollectionUtils.isEmpty(roles);
                     rootJSONObject.put(IS_VISIBLE_FOR_CURRENT_USER, hasFilingPermission);
                 }
-            }
-
-            if (AccessStatus.ALLOWED.equals(permissionService.hasPermission(nodeRef, PermissionService.WRITE)))
-            {
-                rootJSONObject.put(IS_ADD_TO_HOLD_VISIBLE, true);
             }
         }
     }

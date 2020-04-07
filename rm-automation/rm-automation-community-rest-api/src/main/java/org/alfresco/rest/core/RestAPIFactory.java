3e4ce77328a769df0762c4927828c91f1837e291
Roxana Lucanu <roxana.lucanu-ghetu@ness.com>
2020-04-07 18:27:58 +0300

    RM-7145 Addressed code review comments

--- rm-automation/rm-automation-community-rest-api/src/main/java/org/alfresco/rest/core/RestAPIFactory.java
+++ rm-automation/rm-automation-community-rest-api/src/main/java/org/alfresco/rest/core/RestAPIFactory.java
@@ -85,12 +85,16 @@ public class RestAPIFactory
         getRmRestWrapper().authenticateUser(userModel != null ? userModel : getDataUser().getAdminUser());
         return getRmRestWrapper().withCoreAPI();
     }
-    
+
     public SearchAPI getSearchAPI(UserModel userModel)
     {
         getRmRestWrapper().authenticateUser(userModel != null ? userModel : getDataUser().getAdminUser());
         return getRmRestWrapper().withSearchAPI();
     }
+
+    /**
+     * When no user is given the default is set to admin
+     */
     public SearchAPI getSearchAPI()
     {
         return getSearchAPI(null);

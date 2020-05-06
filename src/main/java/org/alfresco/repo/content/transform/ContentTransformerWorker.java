956f85bab8c42014c0b22f1f971c3df1813f8b71
Alan Davis <alan.davis@alfresco.com>
2020-05-06 13:53:11 +0100

    APPS-102 Make extending a ContentTransformWorker simpler by providing a default empty setRemoteTransformerClient method.

--- src/main/java/org/alfresco/repo/content/transform/ContentTransformerWorker.java
+++ src/main/java/org/alfresco/repo/content/transform/ContentTransformerWorker.java
@@ -82,5 +82,7 @@ public interface ContentTransformerWorker
         return false;
     }
 
-    public void setRemoteTransformerClient(RemoteTransformerClient remoteTransformerClient);
+    public default void setRemoteTransformerClient(RemoteTransformerClient remoteTransformerClient)
+    {
+    }
 }

a878e6caa20c02df524a6f58a1d7f635f17b53cf
evasques <eva.vasques@gmail.com>
2020-05-19 09:27:40 +0100

    MNT-21572 Upgrade Freemarker to version 2.3.20-alfresco-patched-20200421 (#999)
    * Upgrade freemarker to version 2.3.20-alfresco-patched-20200421 to fix template injection
    
    * REPO-5076 BitlyUrlShortenerTest failing as URL is now longer
    
    * Extended copyright year
    
    Co-authored-by: Alan Davis <alan.davis@alfresco.com>

--- src/test/java/org/alfresco/repo/urlshortening/BitlyUrlShortenerTest.java
+++ src/test/java/org/alfresco/repo/urlshortening/BitlyUrlShortenerTest.java
@@ -2,7 +2,7 @@
  * #%L
  * Alfresco Repository
  * %%
- * Copyright (C) 2005 - 2016 Alfresco Software Limited
+ * Copyright (C) 2005 - 2020 Alfresco Software Limited
  * %%
  * This file is part of the Alfresco software. 
  * If the software was purchased under a paid Alfresco license, the terms of 
@@ -38,7 +38,7 @@ public class BitlyUrlShortenerTest extends TestCase
         assertNotNull(shortUrl);
         assertFalse(shortUrl.isEmpty());
         assertFalse(url.equals(shortUrl));
-        assertTrue(shortUrl.length()<=20);
+        assertTrue(shortUrl.length() <= url.length());
     }
     
     /**

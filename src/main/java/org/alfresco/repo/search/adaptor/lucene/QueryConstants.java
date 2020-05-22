52f14e7e65648d1994d00d5fab781d37039acbc9
Elia Porciani <e.porciani@sease.io>
2020-05-22 11:22:20 +0200

    Revert "Feature/search 2166 (#254)" (#256)
    This reverts commit bf0628be6a46ce34cfac9804df5771245bfc2b67.

--- src/main/java/org/alfresco/repo/search/adaptor/lucene/QueryConstants.java
+++ src/main/java/org/alfresco/repo/search/adaptor/lucene/QueryConstants.java
@@ -181,18 +181,6 @@ public interface QueryConstants
     
     public static final String FIELD_SOLR_NOLOCALE_TOKENISED_SUFFIX = ".__";
 
-    public static final String FIELD_SOLR_UNIT_OF_TIME_SECOND_SUFFIX = "_unit_of_time_second";
-
-    public static final String FIELD_SOLR_UNIT_OF_TIME_MINUTE_SUFFIX = "_unit_of_time_minute";
-
-    public static final String FIELD_SOLR_UNIT_OF_TIME_HOUR_SUFFIX = "_unit_of_time_hour";
-
-    public static final String FIELD_SOLR_UNIT_OF_TIME_DAY_SUFFIX = "_unit_of_time_day";
-
-    public static final String FIELD_SOLR_UNIT_OF_TIME_MONTH_SUFFIX = "_unit_of_time_month";
-
-    public static final String FIELD_SOLR_UNIT_OF_TIME_YEAR_SUFFIX = "_unit_of_time_year";
-
     public static final String FIELD_SITE = "SITE";
     
     public static final String FIELD_GEO = "GEO";

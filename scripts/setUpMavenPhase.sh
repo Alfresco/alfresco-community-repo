df699eb538690646f241d1810b675f74b80c47d1
rodicasutu <rodica.sutu@ness.com>
2020-07-01 16:28:05 +0300

    configure AGS community build

---
+++ scripts/setUpMavenPhase.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+echo "Branch name: ${TRAVIS_BRANCH}"
+
+if [ "${TRAVIS_BRANCH}" == "master" ];
+then
+    export mavenPhase="deploy"
+elif [[ ${TRAVIS_BRANCH} = release* ]];
+then
+    export mavenPhase="deploy"
+else
+    export mavenPhase="verify"
+fi

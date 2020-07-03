912efc0b8a1990e4b667b67dfbdc70df0949c108
Claudia Agache <claudia.agache@ness.com>
2020-07-03 15:16:21 +0300

    another attempt to use installer url variable when calling the script

--- scripts/startAlfresco.sh
+++ scripts/startAlfresco.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 export PROFILE=$1
-export INSTALLER=$2
 
 cd rm-automation
-mvn clean install -Pinstall-alfresco,${PROFILE} -Dinstaller.url=${INSTALLER}
+mvn clean install -Pinstall-alfresco,${PROFILE} -Dinstaller.url=${INSTALLER_URL}

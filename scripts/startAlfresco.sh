6a971e35dd2cbdcf0a452a2ba95d6223aa4c1fa1
Claudia Agache <claudia.agache@ness.com>
2020-07-03 16:48:21 +0300

    remove clean from maven command and increase travis wait time

--- scripts/startAlfresco.sh
+++ scripts/startAlfresco.sh
@@ -3,4 +3,4 @@
 export PROFILE=$1
 
 cd rm-automation
-mvn clean install -Pinstall-alfresco,${PROFILE} -Dinstaller.url=${INSTALLER_URL}
+mvn install -Pinstall-alfresco,${PROFILE} -Dinstaller.url=${INSTALLER_URL}

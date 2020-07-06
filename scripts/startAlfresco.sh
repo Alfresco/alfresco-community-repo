2e00307a85cda75e1483ed0c7e8e556813fa815b
Claudia Agache <claudia.agache@ness.com>
2020-07-06 19:32:32 +0300

    add automation target directories to cache, download installer to target instead of rm-automation root

--- scripts/startAlfresco.sh
+++ scripts/startAlfresco.sh
@@ -2,10 +2,5 @@
 # fail script immediately on any errors in external commands and print the lines
 set -ev
 
-export PROFILE=$1
-
-ls rm-enterprise/rm-enterprise-repo/target
-ls rm-enterprise/rm-enterprise-share/target
-cd rm-automation
-ls
-mvn install -Pinstall-alfresco,${PROFILE} -q
+cd $1
+mvn install -Pinstall-alfresco,$2 -q

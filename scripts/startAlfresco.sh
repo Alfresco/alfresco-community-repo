8ce65808ee6ecacca39da7faf29eee262e75f4b9
Claudia Agache <claudia.agache@ness.com>
2020-07-06 12:43:02 +0300

    added -q argument

--- scripts/startAlfresco.sh
+++ scripts/startAlfresco.sh
@@ -5,4 +5,4 @@ set -ev
 export PROFILE=$1
 
 cd rm-automation
-mvn install -Pinstall-alfresco,${PROFILE} -Dinstaller.url=${INSTALLER_URL}
+mvn install -Pinstall-alfresco,${PROFILE} -Dinstaller.url=${INSTALLER_URL} -q

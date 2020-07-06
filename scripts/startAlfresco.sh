5b824fcf6047c5d4f7dc43bca4a1d08d11c41e1a
Claudia Agache <claudia.agache@ness.com>
2020-07-06 18:54:44 +0300

    list files from target to check if they were cached or not

--- scripts/startAlfresco.sh
+++ scripts/startAlfresco.sh
@@ -4,6 +4,8 @@ set -ev
 
 export PROFILE=$1
 
+ls rm-enterprise/rm-enterprise-repo/target
+ls rm-enterprise/rm-enterprise-share/target
 cd rm-automation
 ls
 mvn install -Pinstall-alfresco,${PROFILE} -q

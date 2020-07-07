7b448be646a5ada6b4c27f449d5fd29e4039e050
Rodica Sutu <rodica.sutu@ness.com>
2020-07-07 17:30:59 +0300

    Merge pull request #1165 from Alfresco/feature-2.7/APPS-230_BuildAGSArtifactsOnTravis
    Feature 2.7/apps 230 build ags artifacts on travis
    
    (cherry picked from commit eeb21e872523aaa2f6bfc3f80ae9a0962159eb75)

---
+++ scripts/setUpMavenPhase.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+echo "Branch name: ${TRAVIS_BRANCH}"
+echo "Pull request: ${TRAVIS_PULL_REQUEST}"
+
+if [[ "${TRAVIS_BRANCH}" == "master" || "${TRAVIS_BRANCH}" = release* ]] && [ "${TRAVIS_PULL_REQUEST}" == "false" ];
+then
+    export MAVEN_PHASE="deploy"
+else
+    export MAVEN_PHASE="verify"
+fi
+

9998add21279ed2efeb77c6ce7af72e9c3612e3a
rodicasutu <rodica.sutu@ness.com>
2020-07-08 10:15:14 +0300

    changes needed for 3.0 onwards

--- scripts/setUpMavenPhase.sh
+++ scripts/setUpMavenPhase.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 echo "Branch name: ${TRAVIS_BRANCH}"
 echo "Pull request: ${TRAVIS_PULL_REQUEST}"
+branchName=${TRAVIS_BRANCH}
+imageTag=${branchName:8}
+echo "Image tag: ${imageTag}"
 
-if [[ "${TRAVIS_BRANCH}" == "master" || "${TRAVIS_BRANCH}" = release* ]] && [ "${TRAVIS_PULL_REQUEST}" == "false" ];
+if [[ "${TRAVIS_BRANCH}" == "master" && "${TRAVIS_PULL_REQUEST}" == "false" ]];
 then
     export MAVEN_PHASE="deploy"
+    export BUILD_PROFILE="master"
+    export IMAGE_TAG="latest"
+elif [[ ${TRAVIS_BRANCH} = release*  && "${TRAVIS_PULL_REQUEST}" == "false" ]];
+then
+    export MAVEN_PHASE="deploy"
+    export BUILD_PROFILE="master"
+    export IMAGE_TAG="${imageTag}-latest"
 else
     export MAVEN_PHASE="verify"
+    export BUILD_PROFILE="buildDockerImage"
+    export IMAGE_TAG="latest"
 fi
 

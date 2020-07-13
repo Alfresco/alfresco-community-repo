b890bddd42828a780de55aa1e28ec8a6040d4166
rodicasutu <rodica.sutu@ness.com>
2020-07-13 09:56:36 +0300

    simplify script (code review)

--- scripts/setUpMavenPhase.sh
+++ scripts/setUpMavenPhase.sh
@@ -2,44 +2,27 @@
 echo "Branch name: ${TRAVIS_BRANCH}"
 echo "Pull request: ${TRAVIS_PULL_REQUEST}"
 echo "Travis job name: ${TRAVIS_JOB_NAME}"
-echo "Travis job id: ${TRAVIS_JOB_ID}"
 branchName=${TRAVIS_BRANCH}
 imageTag=${branchName:8}
 echo "Image tag: ${imageTag}"
 
-case ${TRAVIS_JOB_NAME} in
-"Build AGS Community"|"Build AGS Benchmark" )
-    if [[ "${TRAVIS_BRANCH}" == "master" && "${TRAVIS_PULL_REQUEST}" == "false" ]];
-    then
-        export MAVEN_PHASE="deploy"
-        export BUILD_PROFILE="master"
-        export IMAGE_TAG="latest"
-    elif [[ ${TRAVIS_BRANCH} = release*  && "${TRAVIS_PULL_REQUEST}" == "false" ]];
-    then
-        export MAVEN_PHASE="deploy"
-        export BUILD_PROFILE="master"
-        export IMAGE_TAG="${imageTag}-latest"
-    else
-        export MAVEN_PHASE="verify"
-        export BUILD_PROFILE="buildDockerImage"
-        export IMAGE_TAG="latest"
-    fi
-    ;;
-"Build AGS Enterprise" )
-   if [[ "${TRAVIS_BRANCH}" == "master" && "${TRAVIS_PULL_REQUEST}" == "false" ]];
-    then
-        export MAVEN_PHASE="deploy"
-        export BUILD_PROFILE="internal"
-        export IMAGE_TAG="latest"
-    elif [[ ${TRAVIS_BRANCH} = release*  && "${TRAVIS_PULL_REQUEST}" == "false" ]];
-    then
-        export MAVEN_PHASE="deploy"
-        export BUILD_PROFILE="internal"
-        export IMAGE_TAG="${imageTag}-latest"
-    else
-        export MAVEN_PHASE="verify"
-        export BUILD_PROFILE="buildDockerImage"
-        export IMAGE_TAG="latest"
-    fi
-    ;;
-esac
+  if [[ ${TRAVIS_JOB_NAME} == "Build AGS Enterprise" ]];
+  then
+     export BUILD_PROFILE="internal"
+  else
+     export BUILD_PROFILE="master"
+  fi
+
+  if [[ "${TRAVIS_BRANCH}" == "master" && "${TRAVIS_PULL_REQUEST}" == "false" ]];
+  then
+      export MAVEN_PHASE="deploy"
+      export IMAGE_TAG="latest"
+  elif [[ ${TRAVIS_BRANCH} = release*  && "${TRAVIS_PULL_REQUEST}" == "false" ]];
+  then
+      export MAVEN_PHASE="deploy"
+      export IMAGE_TAG="${imageTag}-latest"
+  else
+      export MAVEN_PHASE="verify"
+      export BUILD_PROFILE="buildDockerImage"
+      export IMAGE_TAG="latest"
+  fi

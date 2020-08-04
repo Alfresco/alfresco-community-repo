3a30c8bbf0043a4f78d41ceafaa5a9456de73584
Cezar.Leahu <cezar.leahu@ness.com>
2020-08-04 16:19:35 +0300

    ACS-457: Build linkage, tagging and release
    - update release scripts

--- scripts/travis/verify_release_tag.sh
+++ scripts/travis/verify_release_tag.sh
@@ -5,14 +5,14 @@ set -ev
 # Check that the version to be released does not already have a docker tag.
 #
 
-if [ -z "${RELEASE_VERSION}" ]; then
-  echo "Please provide a RELEASE_VERSION in the format <acs-version>-<additional-info> (6.3.0-EA or 6.3.0-SNAPSHOT)"
-  exit 1
-fi
+POM_VERSION=$(mvn -B -q help:evaluate -Dexpression=project.version -DforceStdout)
+printf "POM version: %s\n" "${POM_VERSION}"
+
+IMAGE_TAG="${POM_VERSION%-SNAPSHOT}"
 
 # get the image name from the pom file
-ALFRESCO_DOCKER_IMAGE=$(mvn help:evaluate -f ./docker-alfresco/pom.xml -Dexpression=image.name -q -DforceStdout)
-DOCKER_IMAGE_FULL_NAME="${ALFRESCO_DOCKER_IMAGE}:${RELEASE_VERSION}"
+ALFRESCO_DOCKER_IMAGE="$(mvn -B -q help:evaluate -f ./packaging/docker-alfresco/pom.xml -Dexpression=image.name -DforceStdout)"
+DOCKER_IMAGE_FULL_NAME="${ALFRESCO_DOCKER_IMAGE}:${IMAGE_TAG}"
 
 function docker_image_exists() {
   local IMAGE_FULL_NAME="${1}"; shift

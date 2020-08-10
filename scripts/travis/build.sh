5861d54f4b2eb2d9ef176f04d90ef49fe80fcf2b
Cezar.Leahu <cezar.leahu@ness.com>
2020-08-10 17:53:53 +0300

    ACS-457: Build linkage, tagging and release
    - update build scripts
    - remove a few unnecessary TODOs

--- scripts/travis/build.sh
+++ scripts/travis/build.sh
@@ -2,6 +2,8 @@
 set -ev
 pushd "$(dirname "${BASH_SOURCE[0]}")/../../"
 
-mvn -B -V install -DskipTests -Dmaven.javadoc.skip=true -PcommunityDocker
+PROFILES="$([ "${TRAVIS_BUILD_STAGE_NAME,,}" = "release" ] && echo "-Pinternal" || echo "-PcommunityDocker" )"
+
+mvn -B -V install -DskipTests -Dmaven.javadoc.skip=true "${PROFILES}"
 
 

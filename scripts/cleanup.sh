a8cf3c5dffcf0646fdd6026a192d5461131aff78
Bogdan <bogdan.manole@ness.com>
2020-08-13 18:47:59 +0300

    APPS-261 changes to scripts

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -14,10 +14,11 @@ sudo apt-get update
 sudo apt-get install -q -y awscli
 printf "${CREATE_BUCKET_AWS_ACCESS_KEY}\n${CREATE_BUCKET_AWS_SECRET_KEY}\n\n\n" | aws configure
 
-export BUCKET_NAME="travis-ags-worm-${TRAVIS_BUILD_NUMBER}-${TRAVIS_JOB_NUMBER}"
+BUCKET_NAME="travis-ags-${TRAVIS_BUILD_NUMBER}-${TRAVIS_JOB_NUMBER}"
+S3_BUCKET2_NAME="travis-ags-worm-${TRAVIS_BUILD_NUMBER}-${TRAVIS_JOB_NUMBER}-b2"
 
 aws s3 ls | awk '{print $3}' | grep "^${BUCKET_NAME}" | xargs -l -r -I{} aws s3 rb "s3://{}" --force
-
+aws s3 ls | awk '{print $3}' | grep "^${S3_BUCKET2_NAME}" | xargs -l -r -I{} aws s3 rb "s3://{}" --force
 
 popd
 set +vx

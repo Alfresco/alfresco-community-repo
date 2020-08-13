6884e2cada30cca7dcd05456139b1f5f3cfdb228
Bogdan <bogdan.manole@ness.com>
2020-08-13 15:43:27 +0300

    APPS-261 changes to scripts and travis.yml

--- scripts/worm-mb-tests.sh
+++ scripts/worm-mb-tests.sh
@@ -5,6 +5,8 @@ PS4="\[\e[35m\]+ \[\e[m\]"
 set -vex
 pushd "$(dirname "${BASH_SOURCE[0]}")/../"
 
+docker login quay.io -u ${QUAY_USERNAME} -p ${QUAY_PASSWORD}
+
 export AWS_ACCESS_KEY_ID=${CREATE_BUCKET_AWS_ACCESS_KEY}
 export AWS_SECRET_ACCESS_KEY=${CREATE_BUCKET_AWS_SECRET_KEY}
 
@@ -13,8 +15,6 @@ export S3_BUCKET_NAME="travis-ags-${TRAVIS_BUILD_NUMBER}-${TRAVIS_JOB_NUMBER}"
 export S3_BUCKET2_NAME="travis-ags-worm-${TRAVIS_BUILD_NUMBER}-${TRAVIS_JOB_NUMBER}-b2"
 export S3_PROTOCOL=s3v2
 export S3_BUCKET2_PROTOCOL=s3vTest
-
-mvn -B -U clean install -DskipTests -Pbuild-test-image 
 
 bash ./scripts/start-compose.sh ./rm-enterprise/rm-enterprise-share/worm-support-docker-compose.yml
 

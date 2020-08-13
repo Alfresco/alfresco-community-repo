7b6a811af02115ee8de8155b0be09e7cc870db88
Bogdan <bogdan.manole@ness.com>
2020-08-13 18:39:26 +0300

    APPS-261 changes to scripts

--- scripts/worm-mb-tests.sh
+++ scripts/worm-mb-tests.sh
@@ -7,6 +7,10 @@ pushd "$(dirname "${BASH_SOURCE[0]}")/../"
 
 docker login quay.io -u ${QUAY_USERNAME} -p ${QUAY_PASSWORD}
 
+sudo apt-get update
+sudo apt-get install -q -y awscli
+printf "${CREATE_BUCKET_AWS_ACCESS_KEY}\n${CREATE_BUCKET_AWS_SECRET_KEY}\n\n\n" | aws configure
+
 export AWS_ACCESS_KEY_ID=${CREATE_BUCKET_AWS_ACCESS_KEY}
 export AWS_SECRET_ACCESS_KEY=${CREATE_BUCKET_AWS_SECRET_KEY}
 
@@ -15,6 +19,11 @@ export S3_BUCKET_NAME="travis-ags-${TRAVIS_BUILD_NUMBER}-${TRAVIS_JOB_NUMBER}"
 export S3_BUCKET2_NAME="travis-ags-worm-${TRAVIS_BUILD_NUMBER}-${TRAVIS_JOB_NUMBER}-b2"
 export S3_PROTOCOL=s3v2
 export S3_BUCKET2_PROTOCOL=s3vTest
+
+aws s3api create-bucket --bucket "${S3_BUCKET2_NAME}" --region us-east-1 --object-lock-enabled-for-bucket
+aws s3api put-object-lock-configuration \
+    --bucket "${S3_BUCKET2_NAME}" \
+    --object-lock-configuration '{ "ObjectLockEnabled": "Enabled", "Rule": { "DefaultRetention": { "Mode": "COMPLIANCE", "Days": 1 }}}'
 
 bash ./scripts/start-compose.sh rm-enterprise/rm-enterprise-share/worm-support-docker-compose.yml
 

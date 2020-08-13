1661ab8b9669c424b5c92b8699d58d5ea3362f5b
Bogdan <bogdan.manole@ness.com>
2020-08-13 11:01:06 +0300

    APPS-261 first build

--- scripts/worm-mb-tests.sh
+++ scripts/worm-mb-tests.sh
@@ -8,12 +8,9 @@ pushd "$(dirname "${BASH_SOURCE[0]}")/../"
 export AWS_ACCESS_KEY_ID=${CREATE_BUCKET_AWS_ACCESS_KEY}
 export AWS_SECRET_ACCESS_KEY=${CREATE_BUCKET_AWS_SECRET_KEY}
 
-export BUCKET_NAME="travis-ags-worm-${TRAVIS_BUILD_NUMBER}-${TRAVIS_JOB_NUMBER}"
-export BUCKET2_NAME="travis-ags-worm-${TRAVIS_BUILD_NUMBER}-${TRAVIS_JOB_NUMBER}-b2"
-
 export S3_BUCKET_REGION="eu-west-1"
-export S3_BUCKET_NAME="${BUCKET_NAME}"
-export S3_BUCKET2_NAME="${BUCKET2_NAME}"
+export S3_BUCKET_NAME="travis-ags-${TRAVIS_BUILD_NUMBER}-${TRAVIS_JOB_NUMBER}"
+export S3_BUCKET2_NAME="travis-ags-worm-${TRAVIS_BUILD_NUMBER}-${TRAVIS_JOB_NUMBER}-b2"
 export S3_PROTOCOL=s3v2
 export S3_BUCKET2_PROTOCOL=s3vTest
 
@@ -22,13 +19,9 @@ mvn -B -U clean install -DskipTests -Pbuild-test-image
 ./scripts/start-compose.sh ./rm-enterprise/rm-enterprise-share/worm-support-docker-compose.yml
 
 # Run the WORM tests
-# mvn -B -U clean test \
-#   -Prun-tas-tests,run-multiple-buckets-tests \
-#   -Denvironment=default \
-#   -DrunBugs=false \
-#   -Dalfresco.port=8080 \
-#   -Dconnector.s3.bucketName=${BUCKET_NAME2}
-
+mvn -B -U clean test \
+  -DsuiteXmlFile=wormTestSuite.xml
+  -Dconnector.s3.bucketName=${S3_BUCKET2_NAME}
 
 popd
 set +vex

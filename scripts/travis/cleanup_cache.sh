4362c3542c9ac6082a4e2ed77bc9fc85453afddd
Alan Davis <alan.davis@alfresco.com>
2020-08-16 09:13:28 +0100

    REPO-5271 Backport to relaese/6.2.1
    - Basic layout (packaging is the same as master)

---
+++ scripts/travis/cleanup_cache.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -ev
+
+rm -rf "${HOME}/.m2/repository/org/alfresco/acs-community-packaging"
+rm -rf "${HOME}/.m2/repository/org/alfresco/alfresco-community-repo"
+rm -rf "${HOME}/.m2/repository/org/alfresco/alfresco-community-repo-*"
+rm -rf "${HOME}/.m2/repository/org/alfresco/alfresco-core"
+rm -rf "${HOME}/.m2/repository/org/alfresco/alfresco-data-model"
+rm -rf "${HOME}/.m2/repository/org/alfresco/alfresco-enterprise-remote-api"
+rm -rf "${HOME}/.m2/repository/org/alfresco/alfresco-enterprise-repo-*"
+rm -rf "${HOME}/.m2/repository/org/alfresco/alfresco-enterprise-repository"
+rm -rf "${HOME}/.m2/repository/org/alfresco/alfresco-remote-api"
+rm -rf "${HOME}/.m2/repository/org/alfresco/alfresco-repository"
+rm -rf "${HOME}/.m2/repository/org/alfresco/content-services"
+rm -rf "${HOME}/.m2/repository/org/alfresco/content-services*"
+rm -rf "${HOME}/.m2/repository/org/alfresco/content-services-community"
+rm -rf "${HOME}/.m2/repository/org/alfresco/tas/alfresco-community-repo-*-test"
+rm -rf "${HOME}/.m2/repository/org/alfresco/tas/alfresco-enterprise-repo-*-test"
+
---
+++ scripts/travis/verify_release_tag.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -ev
+
+#
+# Check that the version to be released does not already have a docker tag.
+#
+
+POM_VERSION=$(mvn -B -q help:evaluate -Dexpression=project.version -DforceStdout)
+printf "POM version: %s\n" "${POM_VERSION}"
+
+IMAGE_TAG="${POM_VERSION%-SNAPSHOT}"
+
+# get the image name from the pom file
+ALFRESCO_DOCKER_IMAGE="$(mvn -B -q help:evaluate -f ./packaging/docker-alfresco/pom.xml -Dexpression=image.name -DforceStdout)"
+DOCKER_IMAGE_FULL_NAME="${ALFRESCO_DOCKER_IMAGE}:${IMAGE_TAG}"
+
+function docker_image_exists() {
+  local IMAGE_FULL_NAME="${1}"; shift
+  local WAIT_TIME="${1:-5}"
+  local SEARCH_TERM='Pulling|is up to date|not found'
+
+  echo "Looking to see if ${IMAGE_FULL_NAME} already exists..."
+  local RESULT=$( (timeout --preserve-status "${WAIT_TIME}" docker 2>&1 pull "${IMAGE_FULL_NAME}" &) | grep -v 'Pulling repository' | grep -E -o "${SEARCH_TERM}")
+
+  test "${RESULT}" || { echo "Timed out too soon. Try using a wait_time greater than ${WAIT_TIME}..."; return 1 ;}
+  if echo "${RESULT}" | grep -vq 'not found'; then
+      true
+  else
+      false
+  fi
+}
+
+if docker_image_exists "${DOCKER_IMAGE_FULL_NAME}" ; then
+    echo "Tag ${RELEASE_VERSION} already pushed, release process will interrupt."
+    exit 1
+else
+    echo "The ${RELEASE_VERSION} tag was not found"
+fi

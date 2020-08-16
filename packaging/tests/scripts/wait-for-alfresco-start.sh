93eca3354db2c562d8245721dbef08c6cda71486
Alan Davis <alan.davis@alfresco.com>
2020-08-16 10:24:27 +0100

    REPO-5271 Backport to relaese/6.0.1
    - Basic layout (packaging is the same as master)

--- packaging/tests/scripts/wait-for-alfresco-start.sh
+++ packaging/tests/scripts/wait-for-alfresco-start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 export ALFRESCO_URL=$1
+export EXTRA_WAIT_INTERVAL=$2
 
 if [ -z "$ALFRESCO_URL" ]
 then
@@ -24,8 +25,20 @@ if (("$COUNTER" < "$TIMEOUT")) ; then
    t1=$(date +%s)
    delta=$((($t1 - $t0)/60))
    echo "Alfresco Started in $delta minutes"
+
+   if [ -n "$EXTRA_WAIT_INTERVAL" ]
+   then
+      echo "Waiting an extra $EXTRA_WAIT_INTERVAL for all the containers to initialise..."
+      sleep $EXTRA_WAIT_INTERVAL
+      echo "Waited $EXTRA_WAIT_INTERVAL seconds"
+   fi
 else
    echo "Waited $COUNTER seconds"
    echo "Alfresco Could not start in time."
+   echo "All started containers:"	
+   docker ps -a	
+   ALFCONTAINER=`docker ps -a | grep _alfresco | awk '{ print $1 }'`
+   echo "Last 200 lines from alfresco.log on container $ALFCONTAINER:"
+   docker logs --tail=200 $ALFCONTAINER
    exit 1
 fi

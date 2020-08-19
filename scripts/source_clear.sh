5aaf2ef330dfffa8868981060a59083ea56ffb95
Claudia Agache <claudia.agache@ness.com>
2020-08-19 08:34:08 +0300

    exclude rm-automation and rm-benchmark from scan

--- scripts/source_clear.sh
+++ scripts/source_clear.sh
@@ -5,6 +5,7 @@ set -ev
 mvn -B -q clean install \
     -DskipTests \
     -Dmaven.javadoc.skip=true \
+    -pl '!rm-automation,!rm-automation/rm-automation-community-rest-api,!rm-automation/rm-automation-enterprise-rest-api,!rm-automation/rm-automation-ui,!rm-benchmark' \
     com.srcclr:srcclr-maven-plugin:scan \
     -Dcom.srcclr.apiToken=$SRCCLR_API_TOKEN > scan.log
 

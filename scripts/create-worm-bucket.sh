ef852300ca7fd48acdeef3a5333dc888b37c1d11
Claudia Agache <claudia.agache@ness.com>
2020-08-20 18:05:36 +0300

    corrected the bucket region

--- scripts/create-worm-bucket.sh
+++ scripts/create-worm-bucket.sh
@@ -13,10 +13,8 @@ export AWS_SECRET_ACCESS_KEY=${CREATE_BUCKET_AWS_SECRET_KEY}
 
 export S3_BUCKET_REGION="eu-west-1"
 export S3_BUCKET2_NAME="travis-ags-worm-${TRAVIS_JOB_NUMBER}-b2"
-export S3_PROTOCOL=s3v2
-export S3_BUCKET2_PROTOCOL=s3vTest
 
-aws s3api create-bucket --bucket "${S3_BUCKET2_NAME}" --region us-east-1 --object-lock-enabled-for-bucket
+aws s3api create-bucket --bucket "${S3_BUCKET2_NAME}" --region ${S3_BUCKET_REGION} --object-lock-enabled-for-bucket
 aws s3api put-object-lock-configuration \
     --bucket "${S3_BUCKET2_NAME}" \
     --object-lock-configuration 'ObjectLockEnabled=Enabled,Rule={DefaultRetention={Mode=COMPLIANCE,Days=1}}'

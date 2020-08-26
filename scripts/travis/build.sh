0ff416da28a55788fb2c269086a2b699843d3307
Cezar.Leahu <cezar.leahu@ness.com>
2020-08-26 13:29:28 +0300

    ACS-457: Build linkage, tagging and release
    - reorder a few conditions in build.sh

--- scripts/travis/build.sh
+++ scripts/travis/build.sh
@@ -4,9 +4,11 @@ pushd "$(dirname "${BASH_SOURCE[0]}")/../../"
 
 source "$(dirname "${BASH_SOURCE[0]}")/build_functions.sh"
 
-if [ "${TRAVIS_BUILD_STAGE_NAME,,}" = "release" ] && [ "${TRAVIS_BRANCH}" = "master" ] && isBranchBuild ; then
+if isBranchBuild && [ "${TRAVIS_BRANCH}" = "master" ] && [ "${TRAVIS_BUILD_STAGE_NAME,,}" = "release" ] ; then
+  # update ":latest" image tags on remote repositories by using the maven *internal* profile
   PROFILES="-Pinternal"
 else
+  # build the ":latest" image tags locally with the maven *communityDocker* profile
   PROFILES="-PcommunityDocker"
 fi
 

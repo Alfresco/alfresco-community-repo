9838b0fe9ee2c99f287b1496cd8bbf20f8e8f4a7
Cezar.Leahu <cezar.leahu@ness.com>
2020-09-14 12:14:30 +0300

    ACS-550 Test downstream repo update [skip tests] [no-release]

---
+++ scripts/travis/update_downstream.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+echo "=========================== Starting Update Downstream Script ==========================="
+PS4="\[\e[35m\]+ \[\e[m\]"
+set -vex
+pushd "$(dirname "${BASH_SOURCE[0]}")/../../"
+
+source "$(dirname "${BASH_SOURCE[0]}")/build_functions.sh"
+
+DOWNSTREAM_REPO="github.com/Alfresco/alfresco-enterprise-repo.git"
+
+cloneRepo "${DOWNSTREAM_REPO}" "${TRAVIS_BRANCH}"
+
+cd "$(dirname "${BASH_SOURCE[0]}")/../../../$(basename "${DOWNSTREAM_REPO%.git}")"
+
+# Update parent
+mvn versions:update-parent versions:commit
+
+VERSION="$(sed -n '/<parent>/,/<\/parent>/p' pom.xml \
+    | sed -n '/<version>/,/<\/version>/p' \
+    | tr -d '\n' \
+    | grep -oP '(?<=<version>).*(?=</version>)' \
+    | xargs)"
+
+# Update dependency version
+mvn versions:set-property versions:commit \
+  -Dproperty=dependency.alfresco-community-repo.version \
+  "-DnewVersion=${VERSION}"
+
+# Commit changes
+git status
+git --no-pager diff pom.xml
+git add pom.xml
+git commit -m "Update upstream version to ${VERSION}"
+#todo enable this
+#git push
+git status
+
+
+popd
+set +vex
+echo "=========================== Finishing Update Downstream Script =========================="
+

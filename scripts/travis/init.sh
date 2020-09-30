d10538422fb4b73e4ceda20a353fa7a11fa356de
Cezar.Leahu <cezar.leahu@ness.com>
2020-09-30 22:16:11 +0300

    REPO-5111 Update build scripts [skip ci]

--- scripts/travis/init.sh
+++ scripts/travis/init.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-set -ev
+echo "=========================== Starting Init Script ==========================="
+PS4="\[\e[35m\]+ \[\e[m\]"
+set -vex
 pushd "$(dirname "${BASH_SOURCE[0]}")/../../"
 
 # Maven Setup
@@ -7,6 +9,7 @@ mkdir -p "${HOME}/.m2" && cp -f .travis.settings.xml "${HOME}/.m2/settings.xml"
 find "${HOME}/.m2/repository/" -type d -name "*-SNAPSHOT*" | xargs -r -l rm -rf
 
 # Docker Logins
+echo "${DOCKERHUB_PASSWORD}" | docker login -u="${DOCKERHUB_USERNAME}" --password-stdin
 echo "${QUAY_PASSWORD}" | docker login -u="${QUAY_USERNAME}" --password-stdin quay.io
 
 # Enable experimental docker features (for the image squash option)
@@ -15,4 +18,8 @@ sudo service docker restart
 
 # not helpful in this script
 # export HOST_IP=$(hostname  -I | cut -f1 -d' ')
+
+popd
+set +vex
+echo "=========================== Finishing Init Script =========================="
 

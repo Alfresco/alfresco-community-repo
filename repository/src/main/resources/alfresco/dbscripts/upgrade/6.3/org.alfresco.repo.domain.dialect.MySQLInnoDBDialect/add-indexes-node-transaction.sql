4307444e2d0f709fd8fe3b9eda2c90a93e3dca26
Alan Davis <alan.davis@alfresco.com>
2020-10-04 19:50:40 +0100

    Merge commits for 6.2.2.3 changes to release/6.2.2
    * [MNT-21766]: Significant degradation of performance as file and site count increase (#1217)
    - Added the following indexes on alf_transaction:
    idx_alf_txn_ctms_sc, cols (commit_time_ms)
    idx_alf_txn_id_ctms, cols (id, commit_time_ms)
    - Added the following indexes on alf_node:
    idx_alf_node_ver, cols (version)
    idx_alf_node_txn, cols (transaction_id)
    - Created patch patch.db-V6.3-add-indexes-node-transaction
    - Updated version.schema to 14002
    - Added system property system.new-node-transaction-indexes.ignored, set
    as true by default to not apply the patch automatically
    - Created the MySQL update dbscripts to add the new indexes - and also
    drop and recreate the pre-existing index idx_alf_txn_ctms on alf_transaction that was
    inconsistent with the other DBMS: was indexing only commit_time_ms when
    on all other DBMS index idx_alf_txn_ctms was on (commit_time_ms, id)
    - Created the Postgres update dbscripts to add the new indexes
    
    (cherry picked from commit d70746f63a6e6a2154d2e37c577955a17c42256e)
    
    * Change schema upgrade from to 13005..13006 on patch apply
    
    * Fixed merge from cherry pick, missing colon
    
    (cherry picked from commit 73f94fc5ca0d2ea322d12a59cf3ae980ef94bf1c)

---
+++ repository/src/main/resources/alfresco/dbscripts/upgrade/6.3/org.alfresco.repo.domain.dialect.MySQLInnoDBDialect/add-indexes-node-transaction.sql
@@ -0,0 +1,35 @@
+--
+-- Title:      Update alf_node and alf_transaction indexes for more performance
+-- Database:   MySQL
+-- Since:      V6.3
+-- Author:     Eva Vasques
+--
+-- Please contact support@alfresco.com if you need assistance with the upgrade.
+--
+
+DROP INDEX idx_alf_node_ver;  --(optional)
+CREATE INDEX idx_alf_node_ver ON alf_node (version);
+
+DROP INDEX idx_alf_node_txn;  --(optional)
+CREATE INDEX idx_alf_node_txn ON alf_node (transaction_id);
+
+DROP INDEX idx_alf_txn_ctms;  --(optional)
+CREATE INDEX idx_alf_txn_ctms ON alf_transaction (commit_time_ms, id);
+
+DROP INDEX idx_alf_txn_ctms_sc;  --(optional)
+CREATE INDEX idx_alf_txn_ctms_sc ON alf_transaction (commit_time_ms);
+
+DROP INDEX idx_alf_txn_id_ctms;  --(optional)
+CREATE INDEX idx_alf_txn_id_ctms ON alf_transaction (id, commit_time_ms);
+
+--
+-- Record script finish
+--
+DELETE FROM alf_applied_patch WHERE id = 'patch.db-V6.3-add-indexes-node-transaction';
+INSERT INTO alf_applied_patch
+  (id, description, fixes_from_schema, fixes_to_schema, applied_to_schema, target_schema, applied_on_date, applied_to_server, was_executed, succeeded, report)
+  VALUES
+  (
+    'patch.db-V6.3-add-indexes-node-transaction', 'Create aditional indexes on alf_node and alf_transaction',
+    0, 13005, -1, 13006, null, 'UNKNOWN', ${TRUE}, ${TRUE}, 'Script completed'
+  );

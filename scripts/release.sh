5b71f2ec05192c00fbe2b0c75f392b3267c39cb9
ehardon <elena.hardon@ness.com>
2020-10-09 09:54:23 +0300

    APPS-241: - remove release documentation - update regex to get only the first match of the release version - update release script to be used in enterprise release as well

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,15 +7,25 @@ git checkout -B "${TRAVIS_BRANCH}"
 git config user.email "${GIT_COMMITTER_EMAIL}"
 git config user.name "${GIT_COMMITTER_NAME}"
 
+release_type=$1
+
+if [ -z $release_type ]; then
+    echo "Please provide a release type."
+    exit 1
+elif [ $release_type != "community" -a $release_type != "enterprise" ]; then
+    echo "The provided release type is not valid."
+    exit 1
+fi
+
 if [ -z ${RELEASE_VERSION} ] || [ -z ${DEVELOPMENT_VERSION} ];
     then echo "Please provide a Release and Development verison"
-         exit -1
+         exit 1
 else
     mvn --batch-mode
     -Dusername="${GITHUB_USERNAME}" \
     -Dpassword="${GITHUB_PASSWORD}" \
     -DreleaseVersion=${RELEASE_VERSION} \
     -DdevelopmentVersion=${DEVELOPMENT_VERSION} \
-    -DskipTests -Dcommunity -DuseReleaseProfile=false \
-    -Prelease-community release:clean release:prepare release:perform
+    -DskipTests -D${release_type} -DuseReleaseProfile=false \
+    -Prelease-${release_type} release:clean release:prepare release:perform
 fi

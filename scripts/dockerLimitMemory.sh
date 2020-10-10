ec66d90113f443bb94714512e9f01e5d92bb4198
Claudia Agache <claudia.agache@ness.com>
2020-10-10 19:03:53 +0300

    restart alfresco on failure

--- scripts/dockerLimitMemory.sh
+++ scripts/dockerLimitMemory.sh
@@ -5,7 +5,7 @@ set -x
 docker stats --no-stream
 
 #limit memory used by each container
-docker update --memory=2.5Gb --memory-swap -1 $(docker ps -a | grep '_alfresco_' | awk '{print $1}')
+docker update --memory=2.5Gb --memory-swap -1 --restart on-failure $(docker ps -a | grep '_alfresco_' | awk '{print $1}')
 docker update --memory=2Gb --memory-swap -1 $(docker ps -a | grep '_search_' | awk '{print $1}')
 docker update --memory=1Gb --memory-swap -1 $(docker ps -a | grep '_zeppelin_' | awk '{print $1}')
 docker update --memory=256Mb --memory-swap -1 $(docker ps -a | grep '_postgres_' | awk '{print $1}')

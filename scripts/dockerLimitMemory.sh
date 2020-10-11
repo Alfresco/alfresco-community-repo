951bfa3dae2b776e84b4c1da0f580870cc72a854
Claudia Agache <claudia.agache@ness.com>
2020-10-11 11:10:07 +0300

    remove not needed containers

--- scripts/dockerLimitMemory.sh
+++ scripts/dockerLimitMemory.sh
@@ -5,17 +5,15 @@ set -x
 docker stats --no-stream
 
 #limit memory used by each container
-docker update --memory=2Gb --memory-swap -1 $(docker ps -a | grep '_alfresco_' | awk '{print $1}')
-docker update --memory=1.5Gb --memory-swap -1 $(docker ps -a | grep '_search_' | awk '{print $1}')
-docker update --memory=1Gb --memory-swap -1 $(docker ps -a | grep '_zeppelin_' | awk '{print $1}')
+docker update --memory=2Gb --memory-swap -1 --restart on-failure $(docker ps -a | grep '_alfresco_' | awk '{print $1}')
+docker update --memory=1200Mb --memory-swap -1 $(docker ps -a | grep '_search_' | awk '{print $1}')
 docker update --memory=512Mb --memory-swap -1 $(docker ps -a | grep '_postgres_' | awk '{print $1}')
-docker update --memory=512Mb --memory-swap -1 $(docker ps -a | grep '_transform-router_' | awk '{print $1}')
 docker update --memory=512Mb --memory-swap -1 $(docker ps -a | grep '_imagemagick_' | awk '{print $1}')
 docker update --memory=512Mb --memory-swap -1 $(docker ps -a | grep '_alfresco-pdf-renderer_' | awk '{print $1}')
-docker update --memory=512Mb --memory-swap -1 $(docker ps -a | grep '_shared-file-store_' | awk '{print $1}')
-docker update --memory=512Mb --memory-swap -1 $(docker ps -a | grep '_tika_' | awk '{print $1}')
 docker update --memory=512Mb --memory-swap -1 $(docker ps -a | grep '_libreoffice_' | awk '{print $1}')
-docker update --memory=512Mb --memory-swap -1 $(docker ps -a | grep '_activemq_' | awk '{print $1}')
+
+#stop not needed container
+docker stop $(docker ps -a | grep '_zeppelin_' | awk '{print $1}')
 
 # Display containers resources usage after limitation
 docker stats --no-stream

5e5324f7a243d542609672b0df3bbf751644863b
Elena Hardon <elena.hardon@ness.com>
2020-10-22 14:08:05 +0300

    Merge pull request #1246 from Alfresco/merge-2.7/APPS-241_addCoReleaseStage_merge2.7_no
    APPS-241: Create Community and Enterprise Release and Publish Stages

---
+++ scripts/zip-artifacts-staging.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+release_type=$1
+
+mkdir "artifacts_dir"
+cp rm-${release_type}/rm-${release_type}-repo/target/alfresco-rm-${release_type}-repo-*.amp artifacts_dir
+cp rm-${release_type}/rm-${release_type}-share/target/alfresco-rm-${release_type}-share-*.amp artifacts_dir
+cp rm-${release_type}/rm-${release_type}-rest-api-explorer/target/alfresco-rm-${release_type}-rest-api-explorer-*.war artifacts_dir
+
+cd artifacts_dir
+zip alfresco-rm-${release_type}-${RELEASE_VERSION}.zip *
+
+# rm *.amp *.war -f
+ls

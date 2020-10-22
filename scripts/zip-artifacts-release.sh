a929beb737d055b3992a65684cc9d2ade1de8915
ehardon <elena.hardon@ness.com>
2020-10-22 13:25:17 +0300

    - address PR review comments [skip tests]

--- scripts/zip-artifacts-release.sh
+++ scripts/zip-artifacts-release.sh
@@ -4,9 +4,6 @@ set -e
 release_type=$1
 
 mkdir "artifacts_dir"
-cp rm-${release_type}/rm-${release_type}-repo/target/alfresco-rm-*${release_type}*amp artifacts_dir
-cp rm-${release_type}/rm-${release_type}-share/target/alfresco-rm-*${release_type}*amp artifacts_dir
-cp rm-${release_type}/rm-${release_type}-rest-api-explorer/target/alfresco-rm-*${release_type}*war artifacts_dir
 
 mvn -B org.apache.maven.plugins:maven-dependency-plugin:3.1.1:copy \
     -Dartifact=org.alfresco:alfresco-rm-${release_type}-repo:${RELEASE_VERSION}:amp \

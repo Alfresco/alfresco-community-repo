f4817d743fa55fb7e0cfb74f0d00b4d7c4d65214
rodicasutu <rodica.sutu@ness.com>
2020-10-29 14:35:43 +0200

    attempt to fix common path [execute tests on MySQL]

--- scripts/mysql/start.sh
+++ scripts/mysql/start.sh
@@ -2,7 +2,7 @@
 set -e
 DIRECTORY=`dirname $0`
 echo $DIRECTORY
-. $(dirname $0)/common.sh
+ $(dirname "${BASH_SOURCE[0]}")/common.sh
 
 echo "============================================"
 echo "Generate custom config file"

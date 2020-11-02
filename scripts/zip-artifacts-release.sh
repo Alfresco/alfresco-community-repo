f155c4cb47117202d94a61c67128ef866cdf04a6
ehardon <elena.hardon@ness.com>
2020-11-02 09:46:39 +0200

    APPS-238: Update rm artifacts

--- scripts/zip-artifacts-release.sh
+++ scripts/zip-artifacts-release.sh
@@ -6,19 +6,20 @@ release_type=$1
 mkdir "artifacts_dir"
 
 mvn -B org.apache.maven.plugins:maven-dependency-plugin:3.1.1:copy \
-    -Dartifact=org.alfresco:alfresco-rm-${release_type}-repo:${RELEASE_VERSION}:amp \
+    -Dartifact=org.alfresco:alfresco-governance-services-${release_type}-repo:${RELEASE_VERSION}:amp \
+    -DoutputDirectory=artifacts_dir
+    alfresco-governance-services-enterprise-repo
+
+mvn -B org.apache.maven.plugins:maven-dependency-plugin:3.1.1:copy \
+    -Dartifact=org.alfresco:alfresco-governance-services-${release_type}-share:${RELEASE_VERSION}:amp \
     -DoutputDirectory=artifacts_dir
 
 mvn -B org.apache.maven.plugins:maven-dependency-plugin:3.1.1:copy \
-    -Dartifact=org.alfresco:alfresco-rm-${release_type}-share:${RELEASE_VERSION}:amp \
-    -DoutputDirectory=artifacts_dir
-
-mvn -B org.apache.maven.plugins:maven-dependency-plugin:3.1.1:copy \
-    -Dartifact=org.alfresco:alfresco-rm-${release_type}-rest-api-explorer:${RELEASE_VERSION}:war \
+    -Dartifact=org.alfresco:alfresco-governance-services-${release_type}-rest-api-explorer:${RELEASE_VERSION}:war \
     -DoutputDirectory=artifacts_dir
 
 cd artifacts_dir
-zip alfresco-rm-${release_type}-${RELEASE_VERSION}.zip *
+zip alfresco-governance-services-${release_type}-${RELEASE_VERSION}.zip *
 
 # rm *.amp *.war -f
 ls
--- scripts/zip-artifacts-staging.sh
+++ scripts/zip-artifacts-staging.sh
@@ -4,12 +4,12 @@ set -e
 release_type=$1
 
 mkdir "artifacts_dir"
-cp rm-${release_type}/rm-${release_type}-repo/target/alfresco-rm-${release_type}-repo-*.amp artifacts_dir
-cp rm-${release_type}/rm-${release_type}-share/target/alfresco-rm-${release_type}-share-*.amp artifacts_dir
-cp rm-${release_type}/rm-${release_type}-rest-api-explorer/target/alfresco-rm-${release_type}-rest-api-explorer-*.war artifacts_dir
+cp rm-${release_type}/rm-${release_type}-repo/target/alfresco-governance-services-${release_type}-repo-*.amp artifacts_dir
+cp rm-${release_type}/rm-${release_type}-share/target/alfresco-governance-services-${release_type}-share-*.amp artifacts_dir
+cp rm-${release_type}/rm-${release_type}-rest-api-explorer/target/alfresco-governance-services-${release_type}-rest-api-explorer-*.war artifacts_dir
 
 cd artifacts_dir
-zip alfresco-rm-${release_type}-${RELEASE_VERSION}.zip *
+zip alfresco-governance-services-${release_type}-${RELEASE_VERSION}.zip *
 
 # rm *.amp *.war -f
 ls

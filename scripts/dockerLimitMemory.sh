7f6608448dc5ca0ef844e0a253a76ab1224ba256
Claudia Agache <claudia.agache@ness.com>
2020-11-06 13:07:16 +0200

    update transform-core-aio memory limit

--- scripts/dockerLimitMemory.sh
+++ scripts/dockerLimitMemory.sh
@@ -9,7 +9,7 @@ docker update --memory=2.5Gb --memory-swap -1 --restart on-failure $(docker ps -
 docker update --memory=1Gb --memory-swap -1 --restart on-failure $(docker ps -a | grep '_search_' | awk '{print $1}')
 docker update --memory=512Mb --memory-swap -1 --restart on-failure $(docker ps -a | grep '_postgres_' | awk '{print $1}')
 docker update --memory=512Mb --memory-swap -1 --restart on-failure $(docker ps -a | grep '_activemq_' | awk '{print $1}')
-docker update --memory=640Mb --memory-swap -1 --restart on-failure $(docker ps -a | grep '_transform-core-aio_' | awk '{print $1}')
+docker update --memory=1Gb --memory-swap -1 --restart on-failure $(docker ps -a | grep '_transform-core-aio_' | awk '{print $1}')
 
 shareContainerId=$(docker ps -a | grep '_share_' | awk '{print $1}')
 if [ -n "$shareContainerId" ]; then

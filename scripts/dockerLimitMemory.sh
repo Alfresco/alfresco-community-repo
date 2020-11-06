0452df9927a410b5b0549edc3b648bcab9e1ac21
Claudia Agache <claudia.agache@ness.com>
2020-11-06 10:12:38 +0200

    use transform-code-aio

--- scripts/dockerLimitMemory.sh
+++ scripts/dockerLimitMemory.sh
@@ -5,15 +5,11 @@ set -x
 docker stats --no-stream
 
 #limit memory used by each container
-docker update --memory=2Gb --memory-swap -1 --restart on-failure $(docker ps -a | grep '_alfresco_' | awk '{print $1}')
+docker update --memory=2.5Gb --memory-swap -1 --restart on-failure $(docker ps -a | grep '_alfresco_' | awk '{print $1}')
 docker update --memory=1Gb --memory-swap -1 --restart on-failure $(docker ps -a | grep '_search_' | awk '{print $1}')
 docker update --memory=512Mb --memory-swap -1 --restart on-failure $(docker ps -a | grep '_postgres_' | awk '{print $1}')
 docker update --memory=512Mb --memory-swap -1 --restart on-failure $(docker ps -a | grep '_activemq_' | awk '{print $1}')
-docker update --memory=512Mb --memory-swap -1 --restart on-failure $(docker ps -a | grep '_imagemagick_' | awk '{print $1}')
-docker update --memory=350Mb --memory-swap -1 --restart on-failure $(docker ps -a | grep '_alfresco-pdf-renderer_' | awk '{print $1}')
-docker update --memory=512Mb --memory-swap -1 --restart on-failure $(docker ps -a | grep '_tika_' | awk '{print $1}')
-docker update --memory=512Mb --memory-swap -1 --restart on-failure $(docker ps -a | grep '_libreoffice_' | awk '{print $1}')
-docker update --memory=350Mb --memory-swap -1 --restart on-failure $(docker ps -a | grep '_transform-misc_' | awk '{print $1}')
+docker update --memory=640Mb --memory-swap -1 --restart on-failure $(docker ps -a | grep '_transform-core-aio_' | awk '{print $1}')
 
 shareContainerId=$(docker ps -a | grep '_share_' | awk '{print $1}')
 if [ -n "$shareContainerId" ]; then
@@ -22,7 +18,7 @@ if [ -n "$shareContainerId" ]; then
       docker stop $(docker ps -a | grep '_shared-file-store_' | awk '{print $1}')
    else
       docker update --memory=512Mb --memory-swap -1 --restart on-failure $(docker ps -a | grep '_transform-router_' | awk '{print $1}')
-      docker update --memory=300Mb --memory-swap -1 --restart on-failure $(docker ps -a | grep '_shared-file-store_' | awk '{print $1}')
+      docker update --memory=512Mb --memory-swap -1 --restart on-failure $(docker ps -a | grep '_shared-file-store_' | awk '{print $1}')
 fi
 
 #stop not needed containers

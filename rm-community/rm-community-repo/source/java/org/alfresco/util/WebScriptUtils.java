9e331d61033fb345aa1b32229e0d52eac3a7caad
rodicasutu <rodica.sutu@ness.com>
2020-12-03 21:45:49 +0200

    Revert "APPS-581: Fix Classification UnitTests that were failing due to org.json library version upgrade"
    This reverts commit ef9167d6f416d0275aa3dda83ace1d1111c671a1.

--- rm-community/rm-community-repo/source/java/org/alfresco/util/WebScriptUtils.java
+++ rm-community/rm-community-repo/source/java/org/alfresco/util/WebScriptUtils.java
@@ -238,7 +238,7 @@ public final class WebScriptUtils
 
         try
         {
-            value = jsonObject.getString(key);
+            value = jsonObject.get(key).toString();
             if (checkValue && isBlank(value))
             {
                 throw new WebScriptException(Status.STATUS_BAD_REQUEST, "The value is missing for the key '" + key + "'.");
@@ -413,7 +413,7 @@ public final class WebScriptUtils
         mandatory("jsonObject", jsonObject);
         mandatoryString("key", key);
 
-        JSONObject value;
+        JSONObject value = null;
 
         try
         {

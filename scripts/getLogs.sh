4579c66368ea452ce4c98b692fbe5a464445043f
rodicasutu <rodica.sutu@ness.com>
2020-12-07 11:55:57 +0000

    additional logging to figure out why enterprise rest api tests don't run with success

--- scripts/getLogs.sh
+++ scripts/getLogs.sh
@@ -4,8 +4,10 @@ set -x
 alfrescoContainerId=$(docker ps -a | grep '_alfresco_' | awk '{print $1}')
 shareContainerId=$(docker ps -a | grep '_share_' | awk '{print $1}')
 solrContainerId=$(docker ps -a | grep '_search_' | awk '{print $1}')
+activeMQContainerId=$(docker ps -a | grep '_activemq_' | awk '{print $1}')
 
 docker logs $alfrescoContainerId > alfresco.log
+docker logs $activeMQContainerId > activemq.log
 if [ -n "$shareContainerId" ]; then
   docker logs $shareContainerId > share.log
 fi

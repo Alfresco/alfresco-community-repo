c1dc894802b7c1b075ebbc786327d7ae633cc40e
ehardon <elena.hardon@ness.com>
2020-12-07 17:30:57 +0200

    Updated regex for release version

--- scripts/set-release-variables.sh
+++ scripts/set-release-variables.sh
@@ -2,14 +2,14 @@
 set -e
 
 echo "Travis commit message: $TRAVIS_COMMIT_MESSAGE"
-release_message=$(echo "$TRAVIS_COMMIT_MESSAGE" | grep -Po '(\[(community|enterprise)\srelease\s((\d)+\.)+((\d)+|[a-z])(-[A-Z]\d){0,1}\s((\d)+\.)+(\d)+-SNAPSHOT\])')
+release_message=$(echo "$TRAVIS_COMMIT_MESSAGE" | grep -Po '(\[(community|enterprise)\srelease\s((\d)+\.)+((\d)+|[a-z])(-[A-Z](\d)+){0,1}\s((\d)+\.)+(\d)+-SNAPSHOT\])')
 
 if [ ! -n "$release_message" ]; then
   echo "The commit message is in the wrong format or it does not contain all the required properties."
   exit 1
 fi
 
-export RELEASE_VERSION=$(echo $release_message | grep -Po '((\d)+\.)+((\d)+|[a-z])(-[A-Z]\d){0,1}' | head -1)
+export RELEASE_VERSION=$(echo $release_message | grep -Po '((\d)+\.)+((\d)+|[a-z])(-[A-Z](\d)+){0,1}' | head -1)
 export DEVELOPMENT_VERSION=$(echo $release_message | grep -Po '((\d)+\.)+(\d)+-SNAPSHOT')
 
 echo "Release version is set to $RELEASE_VERSION"

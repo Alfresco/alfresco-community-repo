d14d43b742e3578435a8bd0cf935254b1b236fe5
Claudia Agache <claudia.agache@ness.com>
2021-01-07 14:06:54 +0200

    Revert "increase activeMQ memory limit and reduce alfresco's one"
    This reverts commit 1133aae1

--- scripts/dockerLimitMemory.sh
+++ scripts/dockerLimitMemory.sh
@@ -5,10 +5,11 @@ set -x
 docker stats --no-stream
 
 #limit memory used by each container
-docker update --memory=2Gb --memory-swap -1 --restart on-failure $(docker ps -a | grep '_alfresco_' | awk '{print $1}')
+docker update --memory=2.5Gb --memory-swap -1 --restart on-failure $(docker ps -a | grep '_alfresco_' | awk '{print $1}')
 docker update --memory=1200Mb --memory-swap -1 --restart on-failure $(docker ps -a | grep '_search_' | awk '{print $1}')
 docker update --memory=512Mb --memory-swap -1 --restart on-failure $(docker ps -a | grep '_postgres_' | awk '{print $1}')
-docker update --memory=1Gb --memory-swap -1 --restart on-failure $(docker ps -a | grep '_activemq_' | awk '{print $1}')
+docker update --memory=950Mb --memory-swap -1 --restart on-failure $(docker ps -a | grep '_activemq_' | awk '{print $1}')
+docker update --memory=1Gb --memory-swap -1 --restart on-failure $(docker ps -a | grep '_transform-core-aio_' | awk '{print $1}')
 
 shareContainerId=$(docker ps -a | grep '_share_' | awk '{print $1}')
 if [ -n "$shareContainerId" ]; then

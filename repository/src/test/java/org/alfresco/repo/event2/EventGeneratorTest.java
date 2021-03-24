0ba60b1d9e4f12b800e82924c15e02182cc3a729
Bruno Bossola <bruno@meterian.com>
2021-03-24 21:37:39 +0000

    Polished put test: connects to JMS via TCP and validate that the event sent is also received back

--- repository/src/test/java/org/alfresco/repo/event2/EventGeneratorTest.java
+++ repository/src/test/java/org/alfresco/repo/event2/EventGeneratorTest.java
@@ -1,70 +1,179 @@
 package org.alfresco.repo.event2;
 
-import javax.jms.Connection;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
 import javax.jms.Destination;
-import javax.jms.ExceptionListener;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
 import javax.jms.Session;
 
 import org.alfresco.model.ContentModel;
-import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.repo.event.databind.ObjectMapperFactory;
+import org.alfresco.repo.event.v1.model.RepoEvent;
+import org.apache.activemq.ActiveMQConnection;
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.advisory.DestinationSource;
+import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.activemq.command.ActiveMQTextMessage;
+import org.apache.activemq.command.ActiveMQTopic;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-public class EventGeneratorTest extends AbstractContextAwareRepoEvent implements ExceptionListener
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class EventGeneratorTest extends AbstractContextAwareRepoEvent
 {
+    private static final boolean DEBUG = true;
 
-    @Test
-    public void shouldReceiveEvent2Events() throws Exception
-    {
-        NodeRef nodeRef = createNode(ContentModel.TYPE_CONTENT);
-        System.out.println(nodeRef);
-        
-        try
-        {
-            listen();
-        }
-        catch (Exception ex)
-        {
-            ex.printStackTrace();
-        }
-    }
-    
-    public void listen() throws Exception
-    {
-        // Create a ConnectionFactory
-        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost");
+    private static final String EVENT2_TOPIC_NAME = "alfresco.repo.event2";
 
-        // Create a Connection
-        Connection connection = connectionFactory.createConnection();
+    private ActiveMQConnection connection;
+    private ObjectMapper objectMapper;
+    private List<RepoEvent<?>> receivedEvents;
+
+    @Before
+    public void startupTopicListener() throws Exception
+    {
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+        connection = (ActiveMQConnection) connectionFactory.createConnection();
         connection.start();
 
-        connection.setExceptionListener(this);
-
-        // Create a Session
         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-
-        // Create the destination (Topic or Queue)
-        Destination destination = session.createQueue("alfresco.repo.event2");
-
-        // Create a MessageConsumer from the Session to the Topic or Queue
+        Destination destination = session.createTopic(EVENT2_TOPIC_NAME);
         MessageConsumer consumer = session.createConsumer(destination);
 
-        // Wait for a message
-        Message message = consumer.receive(60000);
+        objectMapper = ObjectMapperFactory.createInstance();
+        receivedEvents = Collections.synchronizedList(new ArrayList<>());
+        consumer.setMessageListener(new MessageListener()
+        {
+            @Override
+            public void onMessage(Message message)
+            {
+                String text = getText(message);
+                RepoEvent<?> event = toRepoEvent(text);
 
-        System.out.println("Received: " + message);
+                if (DEBUG)
+                {
+                    System.err.println("Received message " + message + "\n" + text + "\n" + event);
+                }
 
-        consumer.close();
-        session.close();
-        connection.close();
+                receivedEvents.add(event);
+            }
+
+            private RepoEvent<?> toRepoEvent(String json)
+            {
+                try
+                {
+                    return objectMapper.readValue(json, RepoEvent.class);
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+
+            private String getText(Message message)
+            {
+                try
+                {
+                    ActiveMQTextMessage am = (ActiveMQTextMessage) message;
+                    return am.getText();
+                }
+                catch (JMSException e)
+                {
+                    return null;
+                }
+            }
+        });
+
+        if (DEBUG) System.err.println("Now actively listening on topic " + EVENT2_TOPIC_NAME);
     }
 
-    public synchronized void onException(JMSException ex)
+    @After
+    public void shutdownTopicListener() throws Exception
     {
-        System.out.println("JMS Exception occured.  Shutting down client.");
-        ex.printStackTrace();
+        connection.close();
+        connection = null;
+    }
+
+    @Test
+    public void shouldReceiveEvent2EventsOnNodeCreation() throws Exception
+    {
+        createNode(ContentModel.TYPE_CONTENT);
+
+        int i = 100;
+        while (--i > 0)
+        {
+            Thread.sleep(55l);
+            if (receivedEvents.size() == 1)
+                break;
+        }
+
+        assertFalse("No messages were received!", receivedEvents.isEmpty());
+
+        RepoEvent<?> sent = getRepoEvent(1);
+        RepoEvent<?> received = receivedEvents.get(0);
+
+        assertEquals("Events are different!", sent, received);
+    }
+
+    // a simple main to investigate he contents of the local broker
+    public static void main(String[] args) throws Exception
+    {
+        dumpBroker("tcp://localhost:61616");
+        System.exit(0);
+    }
+
+    private static void dumpBroker(String url) throws JMSException
+    {
+        System.out.println("Broker at url: '" + url + "'");
+
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
+        ActiveMQConnection connection = (ActiveMQConnection) connectionFactory.createConnection();
+        try
+        {
+            connection.start();
+
+            DestinationSource ds = connection.getDestinationSource();
+
+            Set<ActiveMQQueue> queues = ds.getQueues();
+            System.out.println("\nFound " + queues.size() + " queues:");
+            for (ActiveMQQueue queue : queues)
+            {
+                try
+                {
+                    System.out.println("- " + queue.getQueueName());
+                }
+                catch (JMSException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+
+            Set<ActiveMQTopic> topics = ds.getTopics();
+            System.out.println("\nFound " + topics.size() + " topics:");
+            for (ActiveMQTopic topic : topics)
+            {
+                try
+                {
+                    System.out.println("- " + topic.getTopicName());
+                }
+                catch (JMSException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+        finally
+        {
+            connection.close();
+        }
     }
 }

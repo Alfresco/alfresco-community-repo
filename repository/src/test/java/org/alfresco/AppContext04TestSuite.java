5ce179f209d4be995940e9248f4961fb7eab639d
Nana Insaidoo <Insaidoo.nana@yahoo.it>
2021-04-27 09:57:47 +0100

    [ACS-1455] Schema validation report expansion with patch specific problems told apart (#386)
    * Update schema validation report
    
    - Introduced changes to tell apart problems due to optional unapplied
      patches within schema validation report
    
    * Update SchemaDifferenceHelper
    
    - Now populating optionalUpgradePatches list during the schema bootstrap
      registration to conform with patch registration mechanising already in use
    - Added tests
    
    * Make minor corrections
    
    Co-authored-by: Nana Insaidoo <nana.insaidoo@meterian.com>

--- repository/src/test/java/org/alfresco/AppContext04TestSuite.java
+++ repository/src/test/java/org/alfresco/AppContext04TestSuite.java
@@ -2,7 +2,7 @@
  * #%L
  * Alfresco Repository
  * %%
- * Copyright (C) 2005 - 2017 Alfresco Software Limited
+ * Copyright (C) 2005 - 2021 Alfresco Software Limited
  * %%
  * This file is part of the Alfresco software. 
  * If the software was purchased under a paid Alfresco license, the terms of 
@@ -85,6 +85,7 @@ import org.junit.runners.Suite;
     org.alfresco.util.schemacomp.DbToXMLTest.class,
     org.alfresco.util.schemacomp.ExportDbTest.class,
     org.alfresco.util.schemacomp.SchemaReferenceFileTest.class,
+    org.alfresco.util.schemacomp.SchemaBootstrapTest.class,
     org.alfresco.repo.module.ModuleComponentHelperTest.class,
     org.alfresco.repo.node.getchildren.GetChildrenCannedQueryTest.class,
 

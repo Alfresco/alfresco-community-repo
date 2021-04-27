4d04559a26057198cf62a2fbc9e5a7e59c65ea1e
Cezar.Leahu <cezar.leahu@ness.com>
2021-04-28 00:17:07 +0300

    Remove redundant Maven profiles with project structure (modules)

--- scripts/travis/maven_release.sh
+++ scripts/travis/maven_release.sh
@@ -12,8 +12,8 @@ git config user.email "${GIT_EMAIL}"
 
 # Run the release plugin - with "[skip ci]" in the release commit message
 mvn -B \
-  -PfullBuild,all-tas-tests \
-  "-Darguments=-Prelease,fullBuild,all-tas-tests -DskipTests -Dbuild-number=${TRAVIS_BUILD_NUMBER}" \
+  -Pall-tas-tests \
+  "-Darguments=-Prelease,all-tas-tests -DskipTests -Dbuild-number=${TRAVIS_BUILD_NUMBER}" \
   release:clean release:prepare release:perform \
   -DscmCommentPrefix="[maven-release-plugin][skip ci] " \
   -Dusername="${GIT_USERNAME}" \

994f0c2a82c3e527ddd0310871168576f782481a
DenisGabriela <Denis.Ungureanu1@ness.com>
2021-06-24 10:49:12 +0300

    ACS-1631 : Storage classes - REST api - POST & GET /nodes/{nodeId}/children

--- repository/src/main/java/org/alfresco/repo/content/ContentServiceImpl.java
+++ repository/src/main/java/org/alfresco/repo/content/ContentServiceImpl.java
@@ -475,6 +475,33 @@ public class ContentServiceImpl implements ContentService, ApplicationContextAwa
         // check for an existing URL - the get of the reader will perform type checking
         ContentReader existingContentReader = getReader(nodeRef, propertyQName, false);
 
+        if (storageClasses != null)
+        {
+            if (existingContentReader != null && 
+                existingContentReader.getContentData() != null && 
+                existingContentReader.getContentData().getContentUrl() != null)
+            {
+                Set<String> currentStorageClasses = findStorageClasses(nodeRef);
+                if (currentStorageClasses != null && 
+                    !currentStorageClasses.equals(storageClasses))
+                {
+                    Set<Set<String>> possibleTransitions = findStorageClassesTransitions(nodeRef)
+                        .get(currentStorageClasses);
+
+                    if (possibleTransitions == null || 
+                        !possibleTransitions.contains(storageClasses))
+                    {
+                        throw new UnsupportedStorageClassException(store, storageClasses,
+                                                                   "Transition from "
+                                                                       + currentStorageClasses
+                                                                       + " storage classes to "
+                                                                       + storageClasses
+                                                                       + " is not supported");
+                    }
+                }
+            }
+        }
+        
         // get the content using the (potentially) existing content - the new content
         // can be wherever the store decides.
         ContentContext ctx = new NodeContentContext(existingContentReader, null, nodeRef,

0445ba9c9390b3bc7d2c8a7812804bcac59809c1
alandavis <alan.davis@alfresco.com>
2021-06-25 18:36:57 +0100

    MNT-21902 minor fixes to logic added in REPO-5549

--- repository/src/main/java/org/alfresco/transform/client/registry/CombinedConfig.java
+++ repository/src/main/java/org/alfresco/transform/client/registry/CombinedConfig.java
@@ -322,7 +322,7 @@ public class CombinedConfig
                 {
                     combinedTransformers.remove(indexToRemove);
                     // this may also require the current index i to be changed so we don't skip one.
-                    if (i <= indexToRemove)
+                    if (i >= indexToRemove)
                     {
                         i--;
                     }

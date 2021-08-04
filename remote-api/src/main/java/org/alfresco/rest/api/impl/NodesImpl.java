1e628c0b04d57ed28744b5e15dc9a1b41c5406c3
Sara Aspery <sara.aspery@alfresco.com>
2021-08-04 09:05:55 +0100

    ACS-1783 Rest Api for Nodes

--- remote-api/src/main/java/org/alfresco/rest/api/impl/NodesImpl.java
+++ remote-api/src/main/java/org/alfresco/rest/api/impl/NodesImpl.java
@@ -2,7 +2,7 @@
  * #%L
  * Alfresco Remote API
  * %%
- * Copyright (C) 2005 - 2019 Alfresco Software Limited
+ * Copyright (C) 2005 - 2021 Alfresco Software Limited
  * %%
  * This file is part of the Alfresco software. 
  * If the software was purchased under a paid Alfresco license, the terms of 
@@ -139,6 +139,7 @@ import org.alfresco.service.cmr.repository.ContentData;
 import org.alfresco.service.cmr.repository.ContentIOException;
 import org.alfresco.service.cmr.repository.ContentService;
 import org.alfresco.service.cmr.repository.ContentWriter;
+import org.alfresco.service.cmr.repository.DirectAccessUrl;
 import org.alfresco.service.cmr.repository.DuplicateChildNodeNameException;
 import org.alfresco.service.cmr.repository.InvalidNodeRefException;
 import org.alfresco.service.cmr.repository.MimetypeService;
@@ -3411,6 +3412,16 @@ public class NodesImpl implements Nodes
 
         lockService.unlock(nodeRef);
         return getFolderOrDocument(nodeId, parameters);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public DirectAccessUrl requestContentDirectUrl(NodeRef nodeRef, boolean attachment, Long validFor)
+    {
+        DirectAccessUrl directAccessUrl = contentService.requestContentDirectUrl(nodeRef, attachment, validFor);
+        return directAccessUrl;
     }
 
     /**

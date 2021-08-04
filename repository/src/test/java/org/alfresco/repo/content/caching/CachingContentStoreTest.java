a3184b76b4b3b03a6031229a5d4f3883803bcd9f
Sara Aspery <sara.aspery@alfresco.com>
2021-08-04 13:31:09 +0100

    ACS-1782 fix test

--- repository/src/test/java/org/alfresco/repo/content/caching/CachingContentStoreTest.java
+++ repository/src/test/java/org/alfresco/repo/content/caching/CachingContentStoreTest.java
@@ -35,6 +35,7 @@ import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
@@ -504,8 +505,8 @@ public class CachingContentStoreTest
     {
         try
         {
-            when(backingStore.requestContentDirectUrl(anyString(), any(), anyString(), any())).thenThrow(new UnsupportedOperationException());
-            cachingStore.requestContentDirectUrl("url", true,null, 30L);
+            when(backingStore.requestContentDirectUrl(anyString(), eq(true), anyString(), anyLong())).thenThrow(new UnsupportedOperationException());
+            cachingStore.requestContentDirectUrl("url", true,"someFile", 30L);
             fail();
         }
         catch (UnsupportedOperationException e)
@@ -517,7 +518,7 @@ public class CachingContentStoreTest
     @Test
     public void getDirectAccessUrl()
     {
-        when(backingStore.requestContentDirectUrl(anyString(), any(), anyString(), any())).thenReturn(new DirectAccessUrl());
-        cachingStore.requestContentDirectUrl("url", true,null, 30L);
+        when(backingStore.requestContentDirectUrl(anyString(), eq(true), anyString(), anyLong())).thenReturn(new DirectAccessUrl());
+        cachingStore.requestContentDirectUrl("url", true,"someFile", 30L);
     }
 }

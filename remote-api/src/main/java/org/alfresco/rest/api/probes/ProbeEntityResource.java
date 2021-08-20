ac0446708d938e88b6867b0cd905da67715fd7af
Nithin Nambiar <nithin.nambiar@alfresco.com>
2021-08-20 20:46:23 +0100

    ACS-849_incorrect_probes added db check

--- remote-api/src/main/java/org/alfresco/rest/api/probes/ProbeEntityResource.java
+++ remote-api/src/main/java/org/alfresco/rest/api/probes/ProbeEntityResource.java
@@ -23,8 +23,10 @@
  * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
  * #L%
  */
+
 package org.alfresco.rest.api.probes;
 
+import org.alfresco.repo.admin.RepoHealthChecker;
 import org.alfresco.rest.api.discovery.DiscoveryApiWebscript;
 import org.alfresco.rest.api.model.Probe;
 import org.alfresco.rest.framework.WebApiDescription;
@@ -41,22 +43,24 @@ import org.apache.commons.logging.LogFactory;
 /**
  * An implementation of an Entity Resource for Probes.
  */
-@EntityResource(name="probes", title = "Probes")
-public class ProbeEntityResource implements EntityResourceAction.ReadById<Probe>
+@EntityResource(name = "probes", title = "Probes") public class ProbeEntityResource
+            implements EntityResourceAction.ReadById<Probe>
 {
     public static final String LIVE = "-live-";
     public static final String READY = "-ready-";
 
     public static final long CHECK_PERIOD = 10 * 1000; // Maximum of only one checkResult every 10 seconds.
 
-    protected static Log logger = LogFactory.getLog(ProbeEntityResource.class);;
-
+    protected static Log logger = LogFactory.getLog(ProbeEntityResource.class);
+    ;
+    private final Object lock = new Object();
+    private final Probe liveProbe = new Probe(getMessage(true, true, "Tested"));
     private long nextCheckTime = 0;
+    private long lastCheckTime = 0;
     private Boolean checkResult;
-    private Boolean checking = false;
-    private boolean readySent;
-
     private DiscoveryApiWebscript discovery;
+
+    private RepoHealthChecker repoHealthChecker;
 
     public DiscoveryApiWebscript setDiscovery(DiscoveryApiWebscript discovery)
     {
@@ -65,16 +69,17 @@ public class ProbeEntityResource implements EntityResourceAction.ReadById<Probe>
         return result;
     }
 
+    public void setRepoHealthChecker(RepoHealthChecker repoHealthChecker)
+    {
+        this.repoHealthChecker = repoHealthChecker;
+    }
+
     /**
      * Returns a status code of 200 for okay. The probe contains little information for security reasons.
-     *
      * Note: does *not* require authenticated access, so limits the amount of work performed to avoid a DDOS.
      */
-    @Override
-    @WebApiDescription(title="Get probe status", description = "Returns 200 if valid")
-    @WebApiParam(name = "probeName", title = "The probe's name")
-    @WebApiNoAuth
-    public Probe readById(String name, Parameters parameters)
+    @Override @WebApiDescription(title = "Get probe status", description = "Returns 200 if valid") @WebApiParam(name = "probeName", title = "The probe's name") @WebApiNoAuth public Probe readById(
+                String name, Parameters parameters)
     {
         boolean isLiveProbe = LIVE.equalsIgnoreCase(name);
         if (!isLiveProbe && !READY.equalsIgnoreCase(name))
@@ -82,82 +87,63 @@ public class ProbeEntityResource implements EntityResourceAction.ReadById<Probe>
             throw new InvalidArgumentException("Bad probe name");
         }
 
-        String message = doCheckOrNothing(isLiveProbe);
+        if (isLiveProbe)
+        {
+            return liveProbe;
+        }
+        String message = doReadyCheck();
         return new Probe(message);
     }
 
     // We don't want to be doing checks all the time or holding monitors for a long time to avoid a DDOS.
-    public String doCheckOrNothing(boolean isLiveProbe)
+    public String doReadyCheck()
     {
         boolean doCheck = false;
         long now = 0;
-        boolean result;
+        boolean result = false;
         String message = "No test";
         boolean logInfo = false;
-        synchronized(checking)
+        synchronized (lock)
         {
-            // Initially ready needs to be false so we don't get requests and live true so the pod is not killed.
-            if (checkResult == null)
+
+            now = System.currentTimeMillis();
+            if (checkResult == null || isAfterCheckPeriod(now))
             {
-                result = isLiveProbe;
+                doCheck = true;
+            }
+
+            if (doCheck)
+            {
+                try
+                {
+                    message = "Tested";
+                    performReadinessCheck();
+                    result = true;
+                }
+                catch (Exception e)
+                {
+                    result = false;
+                }
+                finally
+                {
+
+                    checkResult = result;
+
+                    setLastCheckTime(now);
+
+                }
             }
             else
             {
+                // if no check is performed, use previous check result
+
                 result = checkResult;
-            }
+                logInfo = true;
 
-            if (checking) // Is another thread is checking?
-            {
-                if (!readySent && result && !isLiveProbe)
-                {
-                    readySent = true;
-                    logInfo = true;
-                }
-            }
-            else // This thread will do a check
-            {
-                now = System.currentTimeMillis();
-                if (checkResult == null || nextCheckTime <= now)
-                {
-                    doCheck = true;
-                    checking = true;
-                }
             }
         }
 
-        if (doCheck)
-        {
-            try
-            {
-                message = "Tested";
-                doCheck(isLiveProbe);
-                result = true;
-            }
-            catch (Exception e)
-            {
-                result = false;
-            }
-            finally
-            {
-                synchronized (checking)
-                {
-                    checking = false;
-                    checkResult = result;
-                    setNextCheckTime(now);
-                    if (result && !readySent && !isLiveProbe) // Are we initially ready
-                    {
-                        readySent = true;
-                        logInfo = true;
-                    }
-                    else if (!result && (isLiveProbe || readySent)) // Are we sick
-                    {
-                        logInfo = true;
-                    }
-                }
-            }
-        }
-
-        message = getMessage(isLiveProbe, result, message);
+        message = getMessage(false, result, message);
 
         if (logInfo)
         {
@@ -177,33 +163,30 @@ public class ProbeEntityResource implements EntityResourceAction.ReadById<Probe>
 
     private String getMessage(boolean isLiveProbe, boolean result, String message)
     {
-        return (isLiveProbe ? "liveProbe" : "readyProbe")+": "+
-        (result ? "Success" : "Failure") +
-        " - "+message;
+        return (isLiveProbe ? "liveProbe" : "readyProbe") + ": " + (result ? "Success" : "Failure") + " - " + message;
     }
 
-    private void doCheck(boolean isLiveProbe)
+    private void performReadinessCheck()
     {
+
         discovery.getRepositoryInfo();
+        repoHealthChecker.checkDatabase();
+
     }
 
-    private void setNextCheckTime(long now)
+    private void setLastCheckTime(long time)
     {
-        long oldValue = nextCheckTime;
-        if (nextCheckTime == 0)
-        {
-            nextCheckTime = (now / 60000) * 60000;
-        }
-
-        do
-        {
-            nextCheckTime += CHECK_PERIOD;
-        }
-        while (nextCheckTime <= now);
-
+        this.lastCheckTime = time;
+        long nextCheckTime = lastCheckTime + CHECK_PERIOD;
         if (logger.isTraceEnabled())
         {
-            logger.trace("nextCheckTime: " + nextCheckTime + " (+" + ((nextCheckTime - oldValue) / 1000) + " secs)");
+            logger.trace("nextCheckTime: " + nextCheckTime + " (+" + ((nextCheckTime) / 1000) + " secs)");
         }
     }
+
+    private boolean isAfterCheckPeriod(long currentTime)
+    {
+        return ((currentTime - lastCheckTime) >= CHECK_PERIOD);
+    }
+
 }

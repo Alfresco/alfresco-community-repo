715732839749fb7bf7783e06afff0a92a3cb9ff1
Sara Aspery <sara.aspery@alfresco.com>
2021-09-01 15:39:45 +0100

    ACS-1924 Correct WebApiParams

--- remote-api/src/main/java/org/alfresco/rest/api/nodes/NodeVersionsRelation.java
+++ remote-api/src/main/java/org/alfresco/rest/api/nodes/NodeVersionsRelation.java
@@ -303,7 +303,7 @@ public class NodeVersionsRelation extends AbstractNodeRelation implements
     }
 
     @Operation("request-direct-access-url")
-    @WebApiParam (name = "requestVersionDirectAccessUrl", title = "Request direct access url", description = "Request direct access url", kind = ResourceParameter.KIND.HTTP_BODY_OBJECT)
+    @WebApiParam (name = "directAccessUrlRequest", title = "Request direct access url", description = "Options for direct access url request", kind = ResourceParameter.KIND.HTTP_BODY_OBJECT)
     @WebApiDescription(title = "Request content url",
             description="Generates a direct access URL.",
             successStatus = HttpServletResponse.SC_OK)

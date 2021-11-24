4197c29fce6d4d2135eeb2b37905a01325d9e903
tiagosalvado10 <9038083+tiagosalvado10@users.noreply.github.com>
2021-11-24 13:44:20 +0000

    [MNT-21953] [MNT-22491] Clear rendition content data on content change. Prevent rendition from having contentHashCode without content (#752) (#819)
    * [MNT-21953] [MNT-22491] Clear rendition content data on content change. Prevent rendition from having contentHashCode without content
    
    * [MNT-21953] [MNT-22491] Added tests
    
    * [MNT-21953] [MNT-22491] Removed update content from test
    
    * [MNT-21953] [MNT-22491] Improve log messages
    
    * [MNT-21953] [MNT-22491] Changed Copyright year to 2021. Minor change in test comments.
    
    (cherry picked from commit 4314a30f3a6f34dd3450bc620ba9a702a30e2aa9)

--- repository/src/main/java/org/alfresco/repo/rendition/RenditionServiceImpl.java
+++ repository/src/main/java/org/alfresco/repo/rendition/RenditionServiceImpl.java
@@ -2,7 +2,7 @@
  * #%L
  * Alfresco Repository
  * %%
- * Copyright (C) 2005 - 2016 Alfresco Software Limited
+ * Copyright (C) 2005 - 2021 Alfresco Software Limited
  * %%
  * This file is part of the Alfresco software. 
  * If the software was purchased under a paid Alfresco license, the terms of 
@@ -683,6 +683,7 @@ public class RenditionServiceImpl implements
                     log.debug("OnContentUpdate calling RenditionService2.render(\""+sourceNodeRef+"\", \""+renditionName+"\" so we switch to the new service.");
                 }
                 useRenditionService2 = true;
+                renditionService2.clearRenditionContentData(sourceNodeRef, renditionName);
                 renditionService2.render(sourceNodeRef, renditionName);
             }
         }

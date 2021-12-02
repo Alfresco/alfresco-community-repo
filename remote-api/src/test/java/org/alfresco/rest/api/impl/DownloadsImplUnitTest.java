1c19ec20b20b268fe7ba9b2e2842263c109687e0
David Edwards <david.edwards@hyland.com>
2021-12-02 14:13:49 +0000

    ACS-2321 Update the file name

--- remote-api/src/test/java/org/alfresco/rest/api/impl/DownloadsImplUnitTest.java
+++ remote-api/src/test/java/org/alfresco/rest/api/impl/DownloadsImplUnitTest.java
@@ -59,7 +59,7 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
-public class DownloadsImplTest 
+public class DownloadsImplUnitTest 
 {
     
     @InjectMocks

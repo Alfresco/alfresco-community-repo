361ab1b69be677643fe7208de1104fcf1c883b5f
David Edwards <david.edwards@hyland.com>
2021-12-03 16:07:34 +0000

    Modify test name

--- remote-api/src/test/java/org/alfresco/rest/api/impl/DownloadsImplCheckArchiveStatusUnitTest.java
+++ remote-api/src/test/java/org/alfresco/rest/api/impl/DownloadsImplCheckArchiveStatusUnitTest.java
@@ -59,7 +59,7 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
-public class DownloadsImplUnitCheckArchiveStatusTest 
+public class DownloadsImplCheckArchiveStatusUnitTest 
 {
     @InjectMocks
     private final DownloadsImpl downloads = new DownloadsImpl();

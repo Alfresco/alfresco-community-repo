73648e9aa60ec1c267ae8b7583d94a0f0dbae794
Tom Page <thomas.page@alfresco.com>
2022-01-10 14:37:12 +0000

    Update headers for AGS.

--- amps/ags/rm-community/rm-community-repo/source/java/org/alfresco/util/SortDirection.java
+++ amps/ags/rm-community/rm-community-repo/source/java/org/alfresco/util/SortDirection.java
@@ -6,7 +6,7 @@ import org.alfresco.api.AlfrescoPublicApi;
  * #%L
  * Alfresco Records Management Module
  * %%
- * Copyright (C) 2005 - 2021 Alfresco Software Limited
+ * Copyright (C) 2005 - 2022 Alfresco Software Limited
  * %%
  * This file is part of the Alfresco software.
  * -

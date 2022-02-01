1057b17b15f6df63901165954e7cea04099b5815
pzurek <Piotr.Zurek@hyland.com>
2022-02-01 14:51:35 +0100

    ACS-2488 ACS-2119 ACS-2091 	Restrict AGS/RM API compatibility test to the org_alfresco_module_rm package

--- amps/ags/rm-community/rm-community-repo/unit-test/java/org/alfresco/module/org_alfresco_module_rm/api/PublicAPITestUtil.java
+++ amps/ags/rm-community/rm-community-repo/unit-test/java/org/alfresco/module/org_alfresco_module_rm/api/PublicAPITestUtil.java
@@ -29,6 +29,7 @@ package org.alfresco.module.org_alfresco_module_rm.api;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.reflections.scanners.Scanners.TypesAnnotated;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Executable;
@@ -77,7 +78,7 @@ public class PublicAPITestUtil
      */
     public static void testPublicAPIConsistency(String basePackageName, SetMultimap<Class<?>, Class<?>> knownBadReferences)
     {
-        Reflections reflections = new Reflections(basePackageName);
+        Reflections reflections = new Reflections(basePackageName, TypesAnnotated);
         Set<Class<?>> publicAPIClasses = reflections.getTypesAnnotatedWith(AlfrescoPublicApi.class, true);
 
         SetMultimap<Class<?>, Class<?>> referencedFrom = HashMultimap.create();

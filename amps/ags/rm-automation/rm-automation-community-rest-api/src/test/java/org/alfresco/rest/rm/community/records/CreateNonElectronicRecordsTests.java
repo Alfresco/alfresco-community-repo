0a7189ce103080d4ef13c3709666fbff5c3ef578
shubhamjain10 <shubham.jain@globallogic.com>
2022-02-28 17:32:26 +0530

    [APPS-1242]-Removed unused code

--- amps/ags/rm-automation/rm-automation-community-rest-api/src/test/java/org/alfresco/rest/rm/community/records/CreateNonElectronicRecordsTests.java
+++ amps/ags/rm-automation/rm-automation-community-rest-api/src/test/java/org/alfresco/rest/rm/community/records/CreateNonElectronicRecordsTests.java
@@ -26,43 +26,24 @@
  */
 package org.alfresco.rest.rm.community.records;
 
-import static com.fasterxml.jackson.databind.type.LogicalType.Map;
-
 import static org.alfresco.rest.rm.community.util.CommonTestUtils.generateTestPrefix;
-import static org.alfresco.rest.rm.community.utils.FilePlanComponentsUtil.IMAGE_FILE;
-import static org.alfresco.rest.rm.community.utils.FilePlanComponentsUtil.createElectronicRecordModel;
-import static org.alfresco.rest.rm.community.utils.FilePlanComponentsUtil.createNonElectronicRecordModel;
-import static org.alfresco.rest.rm.community.utils.FilePlanComponentsUtil.getFile;
-import static org.alfresco.rest.rm.community.utils.RMSiteUtil.createDOD5015RMSiteModel;
 import static org.alfresco.utility.data.RandomData.getRandomName;
 import static org.alfresco.utility.report.log.Step.STEP;
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.CREATED;
-import static org.springframework.http.HttpStatus.OK;
-import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
-import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
 
-import java.security.Key;
 import java.util.HashMap;
-import java.util.Map;
 
 import org.alfresco.rest.core.v0.BaseAPI.RMProperty;
 import org.alfresco.rest.rm.community.base.BaseRMRestTest;
-import org.alfresco.rest.rm.community.model.fileplan.FilePlan;
-import org.alfresco.rest.rm.community.model.record.Record;
 import org.alfresco.rest.rm.community.model.recordcategory.RecordCategory;
 import org.alfresco.rest.rm.community.model.recordcategory.RecordCategoryChild;
-import org.alfresco.rest.rm.community.requests.gscore.api.RecordFolderAPI;
-import org.alfresco.rest.v0.ExportAPI;
 import org.alfresco.rest.v0.RMRolesAndActionsAPI;
 import org.alfresco.rest.v0.RecordsAPI;
 import org.alfresco.test.AlfrescoTest;
 import org.apache.http.HttpResponse;
-import org.checkerframework.checker.units.qual.K;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 /**
@@ -83,7 +64,9 @@ public class CreateNonElectronicRecordsTests extends BaseRMRestTest
 
     private RecordCategoryChild recordFolder;
 
-    /** data prep services */
+    /**
+     * data prep services
+     */
     @Autowired
     private RMRolesAndActionsAPI rmRolesAndActionsAPI;
 
@@ -101,7 +84,6 @@ public class CreateNonElectronicRecordsTests extends BaseRMRestTest
     String editedRecord = "edited RM-2777 record";
     String editedRecordTitle = "edited RM-2777 record title";
     String editedRecordDescription = "edited RM-2777 record description";
-
 
 
     @BeforeClass (alwaysRun = true)
@@ -123,29 +105,28 @@ public class CreateNonElectronicRecordsTests extends BaseRMRestTest
     }
 
     @Test
-    @AlfrescoTest(jira = "RM-2777")
+    @AlfrescoTest (jira = "RM-2777")
     public void createNonElectronicRecord()
     {
         // create a non-electronic record by completing some of the fields
-        HashMap<Enum<?>,String> properties = new HashMap<Enum<?>,String>();
-        properties.put(RMProperty.TITLE,recordTitle);
-        properties.put(RMProperty.DESCRIPTION,recordDescription);
-        properties.put(RMProperty.NAME,recordName);
-        properties.put(RMProperty.PHYSICAL_SIZE,"");
-        properties.put(RMProperty.NUMBER_OF_COPIES,"");
-        properties.put(RMProperty.SHELF,"");
-        properties.put(RMProperty.STORAGE_LOCATION,"");
-        properties.put(RMProperty.BOX,"");
-        properties.put(RMProperty.FILE,"");
+        HashMap<Enum<?>, String> properties = new HashMap<Enum<?>, String>();
+        properties.put(RMProperty.TITLE, recordTitle);
+        properties.put(RMProperty.DESCRIPTION, recordDescription);
+        properties.put(RMProperty.NAME, recordName);
+        properties.put(RMProperty.PHYSICAL_SIZE, "");
+        properties.put(RMProperty.NUMBER_OF_COPIES, "");
+        properties.put(RMProperty.SHELF, "");
+        properties.put(RMProperty.STORAGE_LOCATION, "");
+        properties.put(RMProperty.BOX, "");
+        properties.put(RMProperty.FILE, "");
 
-        HttpResponse response= recordsAPI.createNonElectronicRecord(getAdminUser().getUsername(),
-                getAdminUser().getPassword(), properties, rootCategory.getName(),recordFolder.getName());
+        HttpResponse response = recordsAPI.createNonElectronicRecord(getAdminUser().getUsername(),
+                getAdminUser().getPassword(), properties, rootCategory.getName(), recordFolder.getName());
 
         // check the non-electronic record has been created
         assertStatusCode(CREATED);
-        assertNotNull(recordsAPI.getRecord(getAdminUser().getUsername(),getAdminUser().getPassword(),
-                recordFolder.getName(),recordName));
-        recordsAPI.GetRecordActions(getAdminUser().getUsername(),getAdminUser().getPassword(),recordName);
+        assertNotNull(recordsAPI.getRecord(getAdminUser().getUsername(), getAdminUser().getPassword(),
+                recordFolder.getName(), recordName));
 
     }
 

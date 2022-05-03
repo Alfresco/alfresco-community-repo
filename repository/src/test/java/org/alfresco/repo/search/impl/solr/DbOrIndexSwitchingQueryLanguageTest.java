e29e1173ad16113bb2fdecf1e2fc216c50c38037
Jan Vonka <jan.vonka@alfresco.com>
2022-05-03 13:58:14 +0100

    ACS-2925: Elasticsearch - add support for OpenCMIS & refactor DB switching
    - minor:
    - remove spurious space char (thanks GE)
    - also took opportunity to bump header to 2022 for other touched Java files in this PR

--- repository/src/test/java/org/alfresco/repo/search/impl/solr/DbOrIndexSwitchingQueryLanguageTest.java
+++ repository/src/test/java/org/alfresco/repo/search/impl/solr/DbOrIndexSwitchingQueryLanguageTest.java
@@ -2,7 +2,7 @@
  * #%L
  * Alfresco Repository
  * %%
- * Copyright (C) 2005 - 2016 Alfresco Software Limited
+ * Copyright (C) 2005 - 2022 Alfresco Software Limited
  * %%
  * This file is part of the Alfresco software. 
  * If the software was purchased under a paid Alfresco license, the terms of 
@@ -161,7 +161,7 @@ public class DbOrIndexSwitchingQueryLanguageTest
         queryLang.executeQuery(searchParameters);
     }
 
-    @Test(expected= DisabledFeatureException.class)
+    @Test(expected=DisabledFeatureException.class)
     public void canDisableHybridSearch()
     {
         queryLang.setHybridEnabled(false);

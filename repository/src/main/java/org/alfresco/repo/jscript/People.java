215ad952f53aa20165e7a1a2d053a2e351479b36
mikolajbrzezinski <mikolaj.brzezinski@hyland.com>
2022-05-06 14:03:39 +0200

    useCQ = true

--- repository/src/main/java/org/alfresco/repo/jscript/People.java
+++ repository/src/main/java/org/alfresco/repo/jscript/People.java
@@ -582,16 +582,8 @@ public class People extends BaseScopableProcessorExtension implements Initializi
     {
         ParameterCheck.mandatory("pagingRequest", pagingRequest);
         
-        boolean useCQ = false;
-        if (filter != null)
-        {
-            if (filter.endsWith(HINT_CQ_SUFFIX))
-            {
-                useCQ = honorHintUseCQ;
-                filter = filter.substring(0, filter.length()-HINT_CQ_SUFFIX.length());
-            }
-        }
-        else
+        boolean useCQ = honorHintUseCQ;
+        if (filter == null)
         {
             filter = "*";
         }

c0947cef357b80e04910e45e753979d6997c4299
kavitshah-gl <96051513+kavitshah-gl@users.noreply.github.com>
2022-06-02 09:34:21 +0530

    Feature/apps 2755 UI conversion (#1127)
    * RM-2366 - UI to Rest Conversion
    
    * RM-2366 - UI to Rest Conversion Skipping Test Cases
    
    * RM-2366 - Correcting RDAdmin UserName
    
    * RM-2366 - Adding License Header
    
    * Adding Test case for FileUnfiledRecordsTests
    
    * CreateElectronicRecordsTests
    
    * Added TestClass for InplaceRecordSearchTests
    
    * Added TestClass for SearchRecordsTests and Utility Changes for InplaceRecordSearchTests
    
    * Revert "RM-2366 - UI to Rest Conversion Skipping Test Cases"
    
    This reverts commit 37828788deb83fd4e96e8985425ffc9395418c47.
    
    * Refactoring code cleanup
    
    * AddRelationshipToHoldRecord added in comunity repo
    
    * AddRelationshipToHoldRecord added in branch
    
    * Adding SearchRecordsTests for Community AGS Records
    
    * Adding License Header
    
    * Adding TestCases for AddRelationshipTests
    
    * Adding TestCases for AddRelationshipTests
    
    * Analysing Test Failure in Travis
    
    * Analysing Test Failure in Travis
    
    * Analysing Test Failure in Travis
    
    * Revert "Analysing Test Failure in Travis"
    
    This reverts commit b02df3edcd6b4f84cfd052e73bfe4046f197e02a.
    
    * Analysing Test Failure in Travis
    
    * [skip repo][skip db][skip tas]
    
    * [skip repo][skip db][skip tas]
    
    * [skip repo][skip db][skip tas]
    
    * [skip repo][skip db][skip tas]
    
    * [skip repo][skip db][skip tas]
    
    * Fix all review comments
    
    Co-authored-by: sbisht <shishuraj.bisht@globallogic.com>

---
+++ amps/ags/rm-automation/rm-automation-community-rest-api/src/test/java/org/alfresco/rest/rm/community/records/InplaceRecordSearchTests.java
@@ -0,0 +1,179 @@
+/*
+ * #%L
+ * Alfresco Records Management Module
+ * %%
+ * Copyright (C) 2005 - 2022 Alfresco Software Limited
+ * %%
+ * This file is part of the Alfresco software.
+ * -
+ * If the software was purchased under a paid Alfresco license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
+ * provided under the following open source license terms:
+ * -
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * -
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * -
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+package org.alfresco.rest.rm.community.records;
+
+import org.alfresco.dataprep.CMISUtil;
+import org.alfresco.rest.rm.community.base.BaseRMRestTest;
+import org.alfresco.rest.rm.community.model.record.Record;
+import org.alfresco.rest.v0.RecordsAPI;
+import org.alfresco.utility.Utility;
+import org.alfresco.utility.constants.UserRole;
+import org.alfresco.utility.model.FileModel;
+import org.alfresco.utility.model.SiteModel;
+import org.alfresco.utility.model.UserModel;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.alfresco.utility.report.log.Step.STEP;
+import static org.junit.Assert.assertNotNull;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.test.util.AssertionErrors.assertTrue;
+import static org.testng.Assert.fail;
+
+public class InplaceRecordSearchTests extends BaseRMRestTest {
+
+    private UserModel siteCollaborator, siteConsumer, nonSiteMember;
+    private SiteModel privateSite;
+    private Record uploadedDocRecordbyCollabUser;
+    private FileModel uploadedDocbyCollabUser;
+    @Autowired
+    private RecordsAPI recordsAPI;
+
+    @BeforeClass(alwaysRun = true)
+    public void preConditions() {
+
+        STEP("Create RM Site");
+        createRMSiteIfNotExists();
+
+        // And a private collaboration site
+        privateSite = dataSite.usingAdmin().createPrivateRandomSite();
+
+        // And a site collaborator
+        siteCollaborator = getDataUser().createRandomTestUser();
+        getDataUser().addUserToSite(siteCollaborator, privateSite, UserRole.SiteCollaborator);
+
+        // And a site consumer
+        siteConsumer = getDataUser().createRandomTestUser();
+        getDataUser().addUserToSite(siteConsumer, privateSite, UserRole.SiteConsumer);
+
+        nonSiteMember = getDataUser().createRandomTestUser();
+    }
+
+    /**
+     * Given a RM site
+     * And a private collaboration site
+     * And a site collaborator
+     * And a site consumer
+     * And a user who is not a member of the site
+     * And a document that isn't a record
+     * When the collaborator declares it as a record
+     * Then the collaborator can browse to the record in the document library
+     * And can find the record using live search
+     * And can find the record using advanced search
+     * And the consumer can browse to the record in the document library
+     * And can find the record using live search
+     * And can find the record using advanced search
+     * And the user who is not a member of the site can't find the record using live search
+     * And can't find the record using advanced search
+     */
+    @Test
+    public void searchForInplaceRecord() {
+        // And a document that isn't a record
+        uploadedDocbyCollabUser = dataContent.usingSite(privateSite)
+            .usingUser(siteCollaborator)
+            .createContent(CMISUtil.DocumentType.TEXT_PLAIN);
+
+        assertNotNull(uploadedDocbyCollabUser.getNodeRef());
+
+        // declare uploadedDocument as record
+        uploadedDocRecordbyCollabUser = getRestAPIFactory().getFilesAPI(siteCollaborator)
+            .declareAsRecord(uploadedDocbyCollabUser.getNodeRefWithoutVersion());
+        assertStatusCode(CREATED);
+
+        assertNotNull(uploadedDocRecordbyCollabUser.getId());
+
+        STEP("Allow the Document to be index for it to be available");
+
+        try
+        {
+            Utility.sleep(1000, 40000, () ->
+            {
+                JSONObject siteConsumerSearchJson = getSearchApi().liveSearchForDocuments(siteConsumer.getUsername(),
+                    siteConsumer.getPassword(),
+                    uploadedDocbyCollabUser.getName());
+                assertTrue("Site Consumer not able to find the document.",siteConsumerSearchJson.getJSONArray("items").length() != 0);
+            });
+        }
+        catch (InterruptedException e)
+        {
+            fail("InterruptedException received while waiting for results.");
+        }
+
+        try
+        {
+            Utility.sleep(1000, 40000, () ->
+                {
+                    JSONObject siteCollaboratorSearchJson = getSearchApi().liveSearchForDocuments(siteCollaborator.getUsername(),
+                    siteCollaborator.getPassword(),
+                    uploadedDocbyCollabUser.getName());
+                    assertTrue("Site Collaborator not able to find the document.",siteCollaboratorSearchJson.getJSONArray("items").length() != 0);
+                });
+        }
+        catch (InterruptedException e)
+        {
+            fail("InterruptedException received while waiting for results.");
+        }
+
+        JSONObject nonSiteMemberSearchJson = getSearchApi().liveSearchForDocuments(nonSiteMember.getUsername(),
+            nonSiteMember.getPassword(),
+            uploadedDocbyCollabUser.getName());
+
+        assertTrue("Non Site Member is able to access restricted document.",nonSiteMemberSearchJson.getJSONArray("items").isEmpty());
+    }
+
+    /**
+     * Given @see {@link #searchForInplaceRecord()}
+     * When the collaboration user hides the record in the collaboration site
+     * Then the collaborator can not browse to the record in the document library
+     * And can't find the record using live search
+     * And can't find the record using advanced search
+     */
+    @Test(dependsOnMethods = {"searchForInplaceRecord"})
+    public void usersCantFindRecordAfterHide() {
+        recordsAPI.hideRecord(siteCollaborator.getUsername(),siteCollaborator.getPassword(),uploadedDocRecordbyCollabUser.getId());
+
+        JSONObject siteCollaboratorSearchJson = getSearchApi().liveSearchForDocuments(siteCollaborator.getUsername(),
+            siteCollaborator.getPassword(),
+            uploadedDocbyCollabUser.getName());
+        assertTrue("Site Collaborator able to find the document after it is hidden.",siteCollaboratorSearchJson.getJSONArray("items").isEmpty());
+    }
+
+    @AfterClass
+    public void tearDown() {
+        // clean-up collab site
+        dataSite.usingAdmin().deleteSite(privateSite);
+
+        // clean-up users siteCollaborator, siteConsumer, nonSiteMember
+        dataUser.deleteUser(siteCollaborator);
+        dataUser.deleteUser(siteConsumer);
+        dataUser.deleteUser(nonSiteMember);
+    }
+}

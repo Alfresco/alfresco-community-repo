0fc4e22c4e98f9e2588d2b9762e310c6d7beec99
mstrankowski <marcin.strankowski@hyland.com>
2022-06-27 08:23:19 +0200

    [skip tests] Updating copyrights of modified files

--- repository/src/main/java/org/alfresco/repo/action/RuntimeActionService.java
+++ repository/src/main/java/org/alfresco/repo/action/RuntimeActionService.java
@@ -2,7 +2,7 @@
  * #%L
  * Alfresco Repository
  * %%
- * Copyright (C) 2005 - 2016 Alfresco Software Limited
+ * Copyright (C) 2005 - 2022 Alfresco Software Limited
  * %%
  * This file is part of the Alfresco software. 
  * If the software was purchased under a paid Alfresco license, the terms of 

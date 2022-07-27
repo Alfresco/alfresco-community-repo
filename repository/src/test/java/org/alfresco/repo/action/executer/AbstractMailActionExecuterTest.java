542e62fa59bb2e9d4236b5d9459e6f0c781b969d
Domenico Sibilio <domenicosibilio@gmail.com>
2022-07-27 14:43:51 +0200

    ACS-3347 Spurious dot at the end of month was removed with JDK-8208487

--- repository/src/test/java/org/alfresco/repo/action/executer/AbstractMailActionExecuterTest.java
+++ repository/src/test/java/org/alfresco/repo/action/executer/AbstractMailActionExecuterTest.java
@@ -2,7 +2,7 @@
  * #%L
  * Alfresco Repository
  * %%
- * Copyright (C) 2005 - 2016 Alfresco Software Limited
+ * Copyright (C) 2005 - 2022 Alfresco Software Limited
  * %%
  * This file is part of the Alfresco software. 
  * If the software was purchased under a paid Alfresco license, the terms of 
@@ -363,7 +363,7 @@ public abstract class AbstractMailActionExecuterTest
         MimeMessage message = sendMessage(from, to, subject, template);
 
         Assert.assertNotNull(message);
-        Assert.assertEquals("G'Day 1 Jan. 1970", (String) message.getContent());
+        Assert.assertEquals("G'Day 1 Jan 1970", (String) message.getContent());
     }
 
     @Test

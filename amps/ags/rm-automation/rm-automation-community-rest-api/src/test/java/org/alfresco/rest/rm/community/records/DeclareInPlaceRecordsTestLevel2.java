20b6e2065b5e0e7ae85738b5b9a013506665a4fa
kshah <kavit.shah@globallogic.com>
2022-08-08 01:34:07 +0530

    [ags]

---
+++ amps/ags/rm-automation/rm-automation-community-rest-api/src/test/java/org/alfresco/rest/rm/community/records/DeclareInPlaceRecordsTestLevel2.java
@@ -0,0 +1,221 @@
+/*
+ * #%L
+ * Alfresco Records Management Module
+ * %%
+ * Copyright (C) 2005 - 2022 Alfresco Software Limited
+ * %%
+ * This file is part of the Alfresco software.
+ * -
+ * If the software was purchased under a paid Alfresco license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
+ * provided under the following open source license terms:
+ * -
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * -
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * -
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+package org.alfresco.rest.rm.community.records;
+
+import org.alfresco.dataprep.CMISUtil;
+import org.alfresco.rest.rm.community.base.BaseRMRestTest;
+import org.alfresco.rest.rm.community.model.record.Record;
+import org.alfresco.rest.rm.community.model.recordcategory.RecordCategory;
+import org.alfresco.rest.rm.community.model.recordcategory.RecordCategoryChild;
+import org.alfresco.rest.rm.community.model.rules.ActionsOnRule;
+import org.alfresco.rest.rm.community.model.rules.RuleDefinition;
+import org.alfresco.rest.v0.RMRolesAndActionsAPI;
+import org.alfresco.rest.v0.RecordsAPI;
+import org.alfresco.rest.v0.RulesAPI;
+import org.alfresco.rest.v0.service.DispositionScheduleService;
+import org.alfresco.test.AlfrescoTest;
+import org.alfresco.utility.constants.UserRole;
+import org.alfresco.utility.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.UUID;
+
+import static java.util.Arrays.asList;
+import static org.alfresco.rest.rm.community.base.TestData.DEFAULT_PASSWORD;
+import static org.alfresco.rest.rm.community.model.recordcategory.RetentionPeriodProperty.CREATED_DATE;
+import static org.alfresco.rest.rm.community.util.CommonTestUtils.generateTestPrefix;
+import static org.alfresco.utility.report.log.Step.STEP;
+import static org.junit.Assert.assertTrue;
+import static org.springframework.http.HttpStatus.CREATED;
+
+public class DeclareInPlaceRecordsTestLevel2 extends BaseRMRestTest {
+
+    private final String TEST_PREFIX = generateTestPrefix(DeclareInPlaceRecordTests.class);
+    private final String RM_ADMIN = TEST_PREFIX + "rm_admin";
+    private final String RECORDS_CATEGORY = TEST_PREFIX + "category";
+    public static final String RECORD_FOLDER_ONE = "record-folder-one";
+    private final String RULE_NAME = TEST_PREFIX + "rule unfiled";
+    private String unfiledRecordsNodeRef;
+    private UserModel testUser;
+    private UserModel RmAdminUser;
+    private SiteModel testSite;
+    private FolderModel testFolder;
+    private RecordCategory Category;
+    private SiteModel privateSite;
+    @Autowired
+    private DispositionScheduleService dispositionScheduleService;
+    @Autowired
+    private RulesAPI rulesAPI;
+
+    /**
+     * data prep services
+     */
+    @Autowired
+    private RMRolesAndActionsAPI rmRolesAndActionsAPI;
+    @Autowired
+    private RecordsAPI recordsAPI;
+
+    @BeforeClass(alwaysRun = true)
+    public void preConditions() {
+        STEP("Create RM Site");
+        createRMSiteIfNotExists();
+
+        STEP("Create RM Admin user");
+        rmRolesAndActionsAPI.createUserAndAssignToRole(getAdminUser().getUsername(), getAdminUser().getPassword(), RM_ADMIN,
+            getAdminUser().getPassword(),
+            "Administrator");
+
+        RmAdminUser = new UserModel(RM_ADMIN,getAdminUser().getPassword());
+
+        STEP("Create collab_user user");
+        testUser = getDataUser().createRandomTestUser();
+        testSite = dataSite.usingAdmin().createPublicRandomSite();
+
+        // invite collab_user to Collaboration site with Contributor role
+        getDataUser().addUserToSite(testUser, testSite, UserRole.SiteContributor);
+
+        testFolder = dataContent.usingSite(testSite).usingUser(testUser).createFolder();
+    }
+
+
+    @Test
+    @AlfrescoTest(jira="RM-2584")
+    public void DeclareRecordOwnerDeleted() throws Exception {
+        // Upload document in a folder in a collaboration site
+        FileModel uploadedDoc = dataContent.usingSite(testSite)
+            .usingUser(testUser)
+            .usingResource(testFolder)
+            .createContent(CMISUtil.DocumentType.TEXT_PLAIN);
+
+        // delete the test user
+        dataUser.deleteUser(testUser);
+
+        // declare uploadedDocument as record
+        Record uploadedRecord = getRestAPIFactory().getFilesAPI(getDataUser().getAdminUser()).declareAsRecord(uploadedDoc.getNodeRefWithoutVersion());
+        assertStatusCode(CREATED);
+
+        // assert that the document is now a record
+        assertTrue(hasRecordAspect(uploadedDoc));
+    }
+
+    @Test
+    @AlfrescoTest(jira="MNT-18558")
+    public void inPlaceRecordVisibilityAfterFilingToCategoryWithCutOffStep() throws Exception {
+
+        // create test precondition
+        createTestPrecondition(RECORDS_CATEGORY);
+
+        //create a disposition schedule on Records level with a cut off step
+        dispositionScheduleService.createCategoryRetentionSchedule(RECORDS_CATEGORY, true);
+        dispositionScheduleService.addCutOffAfterPeriodStep(RECORDS_CATEGORY, "day|2", CREATED_DATE);
+
+        //create a folder in category
+        RecordCategoryChild FOLDER = createFolder(getAdminUser(),Category.getId(),RECORD_FOLDER_ONE);
+
+        // create a File to record folder rule applied on Unfiled Records container
+        fileToRuleAppliedOnUnfiledRecords();
+
+        //create a new test user
+        UserModel testUser = createSiteManager();
+
+        // upload a new document as the user and declare the document as record
+        String fileName = generateText();
+        FileModel uploadedDoc = dataContent.usingSite(privateSite)
+            .usingUser(testUser)
+            .usingResource(testFolder)
+            .createContent(CMISUtil.DocumentType.TEXT_PLAIN);
+
+        Record uploadedRecord = getRestAPIFactory().getFilesAPI(getDataUser().getAdminUser()).declareAsRecord(uploadedDoc.getNodeRefWithoutVersion());
+        assertStatusCode(CREATED);
+
+        //Complete the record as admin to be sure that pending action is now Cut off
+        String inPlaceRecordName = recordsAPI.getRecordFullName(getDataUser().usingAdmin().getAdminUser().getUsername(),
+            getDataUser().usingAdmin().getAdminUser().getPassword(), RECORD_FOLDER_ONE,
+            fileName);
+        recordsAPI.completeRecord(getDataUser().usingAdmin().getAdminUser().getUsername(),
+            getDataUser().usingAdmin().getAdminUser().getPassword(), inPlaceRecordName);
+
+        // As test user navigate to collaboration site documents library and check that the record is still visible
+        ContentModel documentLibrary = getDocumentLibrary(testUser,privateSite);
+
+
+    }
+
+    private void createTestPrecondition(String categoryName) {
+        createRMSiteIfNotExists();
+
+        // create "rm admin" user if it does not exist and assign it to RM Administrator role
+        rmRolesAndActionsAPI.createUserAndAssignToRole(
+            getDataUser().usingAdmin().getAdminUser().getUsername(),
+            getDataUser().usingAdmin().getAdminUser().getPassword(),
+            RM_ADMIN, DEFAULT_PASSWORD, "Administrator");
+
+        // create category
+        STEP("Create category");
+        Category = createRootCategory(categoryName,"Title");
+
+        privateSite = dataSite.usingAdmin().createPrivateRandomSite();
+    }
+
+    private void fileToRuleAppliedOnUnfiledRecords()
+    {
+        unfiledRecordsRuleTeardown();
+
+        // create a rule
+        RuleDefinition ruleDefinition = RuleDefinition.createNewRule().title(RULE_NAME)
+            .description(RULE_NAME)
+            .createRecordPath(false)
+            .path("/" + RECORDS_CATEGORY + "/" + RECORD_FOLDER_ONE)
+            .runInBackground(true)
+            .actions(asList(ActionsOnRule.FILE_TO.getActionValue()));
+
+        // create a rule on unfiledRecords
+        rulesAPI.createRule(getDataUser().usingAdmin().getAdminUser().getUsername(),
+            getDataUser().usingAdmin().getAdminUser().getPassword(), unfiledRecordsNodeRef, ruleDefinition);
+    }
+
+    private void unfiledRecordsRuleTeardown()
+    {
+        rulesAPI.deleteAllRulesOnContainer(getDataUser().usingAdmin().getAdminUser().getUsername(),
+            getDataUser().usingAdmin().getAdminUser().getPassword(), unfiledRecordsNodeRef);
+    }
+
+
+    public UserModel createSiteManager() {
+        UserModel siteManager = getDataUser().createRandomTestUser();
+        getDataUser().addUserToSite(siteManager, privateSite, UserRole.SiteManager);
+        return testUser;
+    }
+
+    protected String generateText()
+    {
+        return UUID.randomUUID().toString();
+    }
+}

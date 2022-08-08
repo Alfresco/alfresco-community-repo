639122d5ce3e872dd066153f425eb1d007e002b8
mikolajbrzezinski <mikolaj.brzezinski@hyland.com>
2022-08-08 14:26:33 +0200

    ACS-3316 Fix HTML sanitisation bypass (#1266)
    * ContentGet context file update
    
    * Get mimetype from ContentReader and check if should be forced to downlaod
    
    * Changes according to comments
    
    * Code duplication removal
    
    * Missing spaces, touch-ups to Javadoc and logic
    
    * tests for slingshot
    
    * Tests for contentGet and formatting changes
    
    (cherry picked from commit 885f21ff86e2a2fdae0d6d9f83bfc6df2e6fc648)

--- amps/share-services/src/test/java/org/alfresco/slingshot/web/scripts/SlingshotContentGetTest.java
+++ amps/share-services/src/test/java/org/alfresco/slingshot/web/scripts/SlingshotContentGetTest.java
@@ -1,65 +1,72 @@
 /*
- * #%L
- * Alfresco Share Services AMP
- * %%
- * Copyright (C) 2005 - 2016 Alfresco Software Limited
- * %%
- * This file is part of the Alfresco software. 
- * If the software was purchased under a paid Alfresco license, the terms of 
- * the paid license agreement will prevail.  Otherwise, the software is 
- * provided under the following open source license terms:
- * 
+ * Copyright 2005 - 2020 Alfresco Software Limited.
+ *
+ * This file is part of the Alfresco software.
+ * If the software was purchased under a paid Alfresco license, the terms of the paid license agreement will prevail.
+ * Otherwise, the software is provided under the following open source license terms:
+ *
  * Alfresco is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Alfresco is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
- * #L%
  */
-package org.alfresco.repo.web.scripts.content;
+package org.alfresco.slingshot.web.scripts;
 
 import org.alfresco.model.ContentModel;
 import org.alfresco.repo.content.MimetypeMap;
 import org.alfresco.repo.model.Repository;
+import org.alfresco.repo.security.authentication.AuthenticationComponent;
 import org.alfresco.repo.security.authentication.AuthenticationUtil;
 import org.alfresco.repo.web.scripts.BaseWebScriptTest;
 import org.alfresco.service.cmr.repository.*;
 import org.alfresco.service.cmr.security.MutableAuthenticationService;
+import org.alfresco.service.cmr.security.PermissionService;
 import org.alfresco.service.cmr.security.PersonService;
+import org.alfresco.service.cmr.site.SiteService;
+import org.alfresco.service.cmr.site.SiteVisibility;
 import org.alfresco.service.namespace.NamespaceService;
 import org.alfresco.service.namespace.QName;
+import org.alfresco.util.GUID;
 import org.alfresco.util.PropertyMap;
+import org.json.JSONObject;
 import org.junit.Assert;
+import org.springframework.extensions.webscripts.TestWebScriptServer;
 import org.springframework.extensions.webscripts.TestWebScriptServer.GetRequest;
 import org.springframework.extensions.webscripts.TestWebScriptServer.Response;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
- * Test for ContentGet web script
- *
- * @author martin.muller
+ * Test for SlingshotContentGet web script
+ * @author alex.mukha
+ * @since 5.0.0
  */
-public class ContentGetTest extends BaseWebScriptTest
+public class SlingshotContentGetTest extends BaseWebScriptTest
 {
     private MutableAuthenticationService authenticationService;
+    private AuthenticationComponent authenticationComponent;
     private PersonService personService;
+    private SiteService siteService;
     private NodeService nodeService;
     private ContentService contentService;
+    private PermissionService permissionService;
 
-    NodeRef rootFolder;
-
-    private static final String USER_ONE = "ContentGetTestOne";
-    private static final String URL_CONTENT_DOWNLOAD = "/api/node/content/workspace/SpacesStore/";
+    private static final String USER_ONE = "SlingshotContentGetTestOne";
+    private static final String URL_SITES = "/api/sites";
+    private static final String URL_CONTENT_DOWNLOAD = "/slingshot/node/content/workspace/SpacesStore/";
+    private List<String> createdSites = new ArrayList<String>(1);
 
     @Override
     protected void setUp() throws Exception
@@ -67,16 +74,14 @@ public class ContentGetTest extends BaseWebScriptTest
         super.setUp();
 
         this.authenticationService = (MutableAuthenticationService)getServer().getApplicationContext().getBean("AuthenticationService");
-        this.personService = (PersonService) getServer().getApplicationContext().getBean("PersonService");
-        this.nodeService = (NodeService) getServer().getApplicationContext().getBean("NodeService");
-        this.contentService = (ContentService) getServer().getApplicationContext().getBean("ContentService");
+        this.authenticationComponent = (AuthenticationComponent)getServer().getApplicationContext().getBean("authenticationComponent");
+        this.personService = (PersonService)getServer().getApplicationContext().getBean("PersonService");
+        this.siteService = (SiteService)getServer().getApplicationContext().getBean("SiteService");
+        this.nodeService = (NodeService)getServer().getApplicationContext().getBean("NodeService");
+        this.permissionService = (PermissionService)getServer().getApplicationContext().getBean("PermissionService");
+        this.contentService = (ContentService)getServer().getApplicationContext().getBean("ContentService");
         AuthenticationUtil.setAdminUserAsFullyAuthenticatedUser();
         createUser(USER_ONE);
-
-        Repository repositoryHelper = (Repository) getServer().getApplicationContext().getBean("repositoryHelper");
-        NodeRef companyHome = repositoryHelper.getCompanyHome();
-
-        rootFolder = createNode(companyHome, "rootFolder", ContentModel.TYPE_FOLDER);
     }
 
     private void createUser(String userName)
@@ -95,25 +100,89 @@ public class ContentGetTest extends BaseWebScriptTest
             this.personService.createPerson(ppOne);
         }
     }
-
     private void deleteUser(String username)
     {
         this.personService.deletePerson(username);
-        if (this.authenticationService.authenticationExists(username))
+        if(this.authenticationService.authenticationExists(username))
         {
             this.authenticationService.deleteAuthentication(username);
         }
+    }
+
+
+    private JSONObject createSite(String sitePreset, String shortName, String title, String description, SiteVisibility visibility, int expectedStatus)
+            throws Exception
+    {
+        JSONObject site = new JSONObject();
+        site.put("sitePreset", sitePreset);
+        site.put("shortName", shortName);
+        site.put("title", title);
+        site.put("description", description);
+        site.put("visibility", visibility.toString());
+        TestWebScriptServer.Response response = sendRequest(new TestWebScriptServer.PostRequest(URL_SITES, site.toString(), "application/json"), expectedStatus);
+        this.createdSites.add(shortName);
+        return new JSONObject(response.getContentAsString());
     }
 
     @Override
     protected void tearDown() throws Exception
     {
         super.tearDown();
-
-        nodeService.deleteNode(rootFolder);
         AuthenticationUtil.setAdminUserAsFullyAuthenticatedUser();
         // Clear the user
         deleteUser(USER_ONE);
+        // Tidy-up any site's create during the execution of the test
+        for (String shortName : this.createdSites)
+        {
+            sendRequest(new TestWebScriptServer.DeleteRequest(URL_SITES + "/" + shortName), 0);
+        }
+        // Clear the list
+        this.createdSites.clear();
+        this.authenticationComponent.clearCurrentSecurityContext();
+    }
+
+    public void testDownloadBySiteMemberFromPrivateSite() throws Exception
+    {
+        String shortName  = GUID.generate();
+        // Create a new site
+        createSite("myPreset", shortName, "myTitle", "myDescription", SiteVisibility.PRIVATE, 200);
+
+        // Ensure we have th document library
+        NodeRef docLib = siteService.createContainer(shortName, SiteService.DOCUMENT_LIBRARY, ContentModel.TYPE_FOLDER, null);
+
+        NodeRef doc = nodeService.createNode(docLib, ContentModel.ASSOC_CONTAINS, ContentModel.ASSOC_CONTAINS, ContentModel.TYPE_CONTENT).getChildRef();
+        nodeService.setProperty(doc, ContentModel.PROP_CONTENT, new ContentData(null, MimetypeMap.MIMETYPE_TEXT_PLAIN, 0L, null));
+        nodeService.setProperty(doc, ContentModel.PROP_TITLE, "title");
+        ContentWriter writer = contentService.getWriter(doc, ContentModel.PROP_CONTENT, true);
+        writer.setMimetype(MimetypeMap.MIMETYPE_TEXT_PLAIN);
+        writer.setEncoding("UTF-8");
+        writer.putContent("test");
+
+        String uri = URL_CONTENT_DOWNLOAD + doc.getId() + "?a=true";
+        sendRequest(new GetRequest(uri), 200);
+    }
+
+    public void testDownloadByNonSiteMemberFromPrivateSite() throws Exception
+    {
+        String shortName  = GUID.generate();
+        // Create a new site
+        createSite("myPreset", shortName, "myTitle", "myDescription", SiteVisibility.PRIVATE, 200);
+
+        NodeRef docLib = siteService.createContainer(shortName, SiteService.DOCUMENT_LIBRARY, ContentModel.TYPE_FOLDER, null);
+        NodeRef doc = nodeService.createNode(docLib, ContentModel.ASSOC_CONTAINS, ContentModel.ASSOC_CONTAINS, ContentModel.TYPE_CONTENT).getChildRef();
+        nodeService.setProperty(doc, ContentModel.PROP_CONTENT, new ContentData(null, MimetypeMap.MIMETYPE_TEXT_PLAIN, 0L, null));
+        nodeService.setProperty(doc, ContentModel.PROP_TITLE, "title");
+        ContentWriter writer = contentService.getWriter(doc, ContentModel.PROP_CONTENT, true);
+        writer.setMimetype(MimetypeMap.MIMETYPE_TEXT_PLAIN);
+        writer.setEncoding("UTF-8");
+        writer.putContent("test");
+
+        permissionService.setPermission(doc, USER_ONE, PermissionService.CONSUMER, true);
+
+        AuthenticationUtil.setFullyAuthenticatedUser(USER_ONE);
+
+        String uri = URL_CONTENT_DOWNLOAD + doc.getId() + "?a=true";
+        sendRequest(new GetRequest(uri), 200);
     }
 
     /**
@@ -121,6 +190,11 @@ public class ContentGetTest extends BaseWebScriptTest
      */
     public void testRelativePath() throws Exception
     {
+        Repository repositoryHelper = (Repository) getServer().getApplicationContext().getBean("repositoryHelper");
+        NodeRef companyHome = repositoryHelper.getCompanyHome();
+
+        NodeRef rootFolder = createNode(companyHome, "rootFolder", ContentModel.TYPE_FOLDER);
+
         NodeRef doc1 = createNodeWithTextContent(rootFolder, "doc1", ContentModel.TYPE_CONTENT, "doc1 file content", MimetypeMap.MIMETYPE_TEXT_PLAIN);
 
         NodeRef folderX = createNode(rootFolder, "X", ContentModel.TYPE_FOLDER);
@@ -131,16 +205,20 @@ public class ContentGetTest extends BaseWebScriptTest
 
         // uri with relative path at the end
         String uri = URL_CONTENT_DOWNLOAD + doc1.getId() + "/X/Y/Z/doc2";
-        Response resp = sendRequest(new GetRequest(uri), 200);
+        TestWebScriptServer.Response resp = sendRequest(new GetRequest(uri), 200);
 
         // check if we really have doc2 as target
         Assert.assertEquals("doc2 file content", resp.getContentAsString());
+
+        nodeService.deleteNode(rootFolder);
     }
-
-
 
     public void testForcedAttachment() throws Exception
     {
+        Repository repositoryHelper = (Repository) getServer().getApplicationContext().getBean("repositoryHelper");
+        NodeRef companyHome = repositoryHelper.getCompanyHome();
+
+        NodeRef rootFolder = createNode(companyHome, "rootFolder", ContentModel.TYPE_FOLDER);
         NodeRef testhtml = createNodeWithTextContent(rootFolder, "testhtml", ContentModel.TYPE_CONTENT, "testhtml content", MimetypeMap.MIMETYPE_HTML);
         NodeRef testpdf = createNodeWithTextContent(rootFolder, "testpdf", ContentModel.TYPE_CONTENT, "testpdf content", MimetypeMap.MIMETYPE_PDF);
 
@@ -174,6 +252,8 @@ public class ContentGetTest extends BaseWebScriptTest
         res = sendRequest(new GetRequest(uri), 200);
         assertEquals("attachment", res.getHeader("Content-Disposition"));
         assertEquals(MimetypeMap.MIMETYPE_PDF + ";charset=UTF-8", res.getContentType());
+
+        nodeService.deleteNode(rootFolder);
     }
 
     public NodeRef createNodeWithTextContent(NodeRef parentNode, String nodeCmName, QName nodeType, String content, String mimetype)

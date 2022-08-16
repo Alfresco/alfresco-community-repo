b12549a4886aa4cbc1af95a8695f1560ff6fd9d9
kshah <kavit.shah@globallogic.com>
2022-08-16 12:05:10 +0530

    [ags]

--- amps/ags/rm-automation/rm-automation-community-rest-api/src/test/java/org/alfresco/rest/rm/community/records/DeclareInPlaceRecordsTestLevel2.java
+++ amps/ags/rm-automation/rm-automation-community-rest-api/src/test/java/org/alfresco/rest/rm/community/records/DeclareInPlaceRecordsTestLevel2.java
@@ -42,8 +42,12 @@ import org.alfresco.rest.v0.RulesAPI;
 import org.alfresco.rest.v0.service.DispositionScheduleService;
 import org.alfresco.test.AlfrescoTest;
 import org.alfresco.utility.constants.UserRole;
-import org.alfresco.utility.model.*;
+import org.alfresco.utility.model.FileModel;
+import org.alfresco.utility.model.FolderModel;
+import org.alfresco.utility.model.SiteModel;
+import org.alfresco.utility.model.UserModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -53,26 +57,29 @@ import static org.alfresco.rest.core.v0.BaseAPI.RM_SITE_ID;
 import static org.alfresco.rest.rm.community.base.TestData.DEFAULT_PASSWORD;
 import static org.alfresco.rest.rm.community.model.fileplancomponents.FilePlanComponentAlias.UNFILED_RECORDS_CONTAINER_ALIAS;
 import static org.alfresco.rest.rm.community.model.recordcategory.RetentionPeriodProperty.CREATED_DATE;
+import static org.alfresco.rest.rm.community.model.user.UserPermissions.PERMISSION_READ_RECORDS;
+import static org.alfresco.rest.rm.community.requests.gscore.api.FilesAPI.PARENT_ID_PARAM;
 import static org.alfresco.rest.rm.community.util.CommonTestUtils.generateTestPrefix;
 import static org.alfresco.utility.report.log.Step.STEP;
 import static org.junit.Assert.assertTrue;
 import static org.springframework.http.HttpStatus.CREATED;
 
 public class DeclareInPlaceRecordsTestLevel2 extends BaseRMRestTest {
-
-    private final String TEST_PREFIX = generateTestPrefix(DeclareInPlaceRecordTests.class);
+    private final String TEST_PREFIX = generateTestPrefix(DeclareInPlaceRecordsTestLevel2.class);
     private final String RM_ADMIN = TEST_PREFIX + "rm_admin";
-    private final String RECORDS_CATEGORY = TEST_PREFIX + "category";
+    private final String RECORDS_CATEGORY_ONE = TEST_PREFIX + "category";
     public static final String RECORD_FOLDER_ONE = "record-folder-one";
+    public static final String RECORD_CATEGORY_TWO = "record-category-two" + System.currentTimeMillis();
+    public static final String RECORD_FOLDER_TWO = "record-folder-two";
     private final String RULE_NAME = TEST_PREFIX + "rule unfiled";
     private String unfiledRecordsNodeRef;
-    private UserModel testUser;
-    private SiteModel testSite;
-    private FolderModel testFolder;
-    private RecordCategory Category;
+    private RecordCategory RecordCategoryOne, RecordCategoryTwo;
     private RecordCategoryChild recordFolder;
     private UnfiledContainer unfiledContainer;
+    private FolderModel testFolder;
+    private SiteModel testSite;
     private SiteModel privateSite;
+    private UserModel testUser;
     @Autowired
     private DispositionScheduleService dispositionScheduleService;
     @Autowired
@@ -89,6 +96,7 @@ public class DeclareInPlaceRecordsTestLevel2 extends BaseRMRestTest {
     public void preConditions() {
         STEP("Create RM Site");
         createRMSiteIfNotExists();
+        privateSite = dataSite.usingAdmin().createPrivateRandomSite();
     }
 
     /**
@@ -131,14 +139,14 @@ public class DeclareInPlaceRecordsTestLevel2 extends BaseRMRestTest {
     public void inPlaceRecordVisibilityAfterFilingToCategoryWithCutOffStep() throws Exception {
 
         // create test precondition
-        createTestPrecondition(RECORDS_CATEGORY);
+        createTestPrecondition(RECORDS_CATEGORY_ONE);
 
         //create a disposition schedule on Records level with a cut off step
-        dispositionScheduleService.createCategoryRetentionSchedule(RECORDS_CATEGORY, true);
-        dispositionScheduleService.addCutOffAfterPeriodStep(RECORDS_CATEGORY, "day|2", CREATED_DATE);
+        dispositionScheduleService.createCategoryRetentionSchedule(RECORDS_CATEGORY_ONE, true);
+        dispositionScheduleService.addCutOffAfterPeriodStep(RECORDS_CATEGORY_ONE, "day|2", CREATED_DATE);
 
         //create a folder in category
-        recordFolder = createFolder(getAdminUser(),Category.getId(),RECORD_FOLDER_ONE);
+        recordFolder = createFolder(getAdminUser(),RecordCategoryOne.getId(),RECORD_FOLDER_ONE);
 
         // create a File to record folder rule applied on Unfiled Records container
         fileToRuleAppliedOnUnfiledRecords();
@@ -161,23 +169,77 @@ public class DeclareInPlaceRecordsTestLevel2 extends BaseRMRestTest {
         // As test user navigate to collaboration site documents library and check that the record is still visible
         dataContent.usingAdmin().usingSite(privateSite).assertContentExist();
     }
+    /**
+     * Create a user called test
+     * Create a collaboration site
+     * Add the user to the collaboration site as consumer
+     * Create an RM site
+     * In RM, create a new categories under file plan with a cut off step set after an event happens
+     * Under the previously created category create a folder
+     * Set READ-ONLY permission for test user for the folder previously created (the user does not have Read
+     * permissions to the category containing the folder)
+     * In the collaboration site create two files
+     * File as record the first file
+     * Log in with test user and check if he can still see the two files in the collaboration site
+     */
+    @Test
+    @AlfrescoTest (jira = "MNT-22138")
+    public void filesVisibilityAfterFilingToCategoryWithCutOffAfterEventStep() throws Exception {
+
+        //create a category
+        RecordCategoryTwo = createRootCategory(RECORD_CATEGORY_TWO);
+
+        //create a disposition schedule on Records level with a cut off step
+        dispositionScheduleService.createCategoryRetentionSchedule(RECORD_CATEGORY_TWO, true);
+        dispositionScheduleService.addCutOffAfterPeriodStep(RECORD_CATEGORY_TWO, "day|2", CREATED_DATE);
+
+        //create a folder in category
+        recordFolder = createFolder(getAdminUser(),RecordCategoryTwo.getId(),RECORD_FOLDER_TWO);
+
+        //create a new test user
+        UserModel siteConsumer = getDataUser().createRandomTestUser();
+        getDataUser().addUserToSite(siteConsumer,privateSite,UserRole.SiteConsumer);
+
+        // give read permissions to test user
+        getRestAPIFactory().getRMUserAPI().addUserPermission(recordFolder.getId(), siteConsumer, PERMISSION_READ_RECORDS);
+
+        // create two documents
+        FileModel testFile = dataContent.usingSite(new SiteModel(privateSite.getTitle()))
+            .usingAdmin()
+            .createContent(CMISUtil.DocumentType.TEXT_PLAIN);
+
+        FileModel testFileNotFiled = dataContent.usingSite(new SiteModel(privateSite.getTitle()))
+            .usingAdmin()
+            .createContent(CMISUtil.DocumentType.TEXT_PLAIN);
+
+        // file one of the documents as record
+        getRestAPIFactory().getFilesAPI()
+            .usingParams(String.format("%s=%s", PARENT_ID_PARAM, recordFolder.getId()))
+            .declareAsRecord(testFile.getNodeRefWithoutVersion());
+        getRestAPIFactory().getRmRestWrapper().assertStatusCodeIs(CREATED);
+
+        // As test user navigate to collaboration site documents library and check that both of the documents are
+        // visible
+        STEP("Verify the document in collaboration site is now a record");
+        Assert.assertTrue(hasRecordAspect(testFile), "File should have record aspect");
+        Assert.assertFalse(hasRecordAspect(testFileNotFiled), "File should not have record aspect");
+    }
+
     private void createTestPrecondition(String categoryName) {
 
-        // create "rm admin" user if it does not exist and assign it to RM Administrator role
-        rmRolesAndActionsAPI.createUserAndAssignToRole(
-            getDataUser().usingAdmin().getAdminUser().getUsername(),
-            getDataUser().usingAdmin().getAdminUser().getPassword(),
-            RM_ADMIN, DEFAULT_PASSWORD, "Administrator");
+    // create "rm admin" user if it does not exist and assign it to RM Administrator role
+    rmRolesAndActionsAPI.createUserAndAssignToRole(
+        getDataUser().usingAdmin().getAdminUser().getUsername(),
+        getDataUser().usingAdmin().getAdminUser().getPassword(),
+        RM_ADMIN, DEFAULT_PASSWORD, "Administrator");
 
-        // create category
-        STEP("Create category");
-        Category = createRootCategory(categoryName,"Title");
+    // create category
+    STEP("Create category");
+    RecordCategoryOne = createRootCategory(categoryName,"Title");
 
-        privateSite = dataSite.usingAdmin().createPrivateRandomSite();
+    unfiledContainer = getRestAPIFactory().getUnfiledContainersAPI().getUnfiledContainer(UNFILED_RECORDS_CONTAINER_ALIAS);
 
-        unfiledContainer = getRestAPIFactory().getUnfiledContainersAPI().getUnfiledContainer(UNFILED_RECORDS_CONTAINER_ALIAS);
-
-        unfiledRecordsNodeRef = NODE_PREFIX + unfiledContainer.getId();
+    unfiledRecordsNodeRef = NODE_PREFIX + unfiledContainer.getId();
     }
     private void createTestPrecondition() {
         STEP("Create collab_user user");
@@ -196,7 +258,7 @@ public class DeclareInPlaceRecordsTestLevel2 extends BaseRMRestTest {
         RuleDefinition ruleDefinition = RuleDefinition.createNewRule().title(RULE_NAME)
             .description(RULE_NAME)
             .createRecordPath(false)
-            .path("/" + RECORDS_CATEGORY + "/" + RECORD_FOLDER_ONE)
+            .path("/" + RECORDS_CATEGORY_ONE + "/" + RECORD_FOLDER_ONE)
             .runInBackground(true)
             .actions(asList(ActionsOnRule.FILE_TO.getActionValue()));
 
@@ -219,9 +281,15 @@ public class DeclareInPlaceRecordsTestLevel2 extends BaseRMRestTest {
         rmRolesAndActionsAPI.deleteAllItemsInContainer(getDataUser().usingAdmin().getAdminUser().getUsername(),
             getDataUser().usingAdmin().getAdminUser().getPassword(), RM_SITE_ID, RECORD_FOLDER_ONE);
         rmRolesAndActionsAPI.deleteAllItemsInContainer(getDataUser().usingAdmin().getAdminUser().getUsername(),
-            getDataUser().usingAdmin().getAdminUser().getPassword(), RM_SITE_ID, Category.getName());
-        deleteRecordFolder(recordFolder.getId());
-        deleteRecordCategory(Category.getId());
+            getDataUser().usingAdmin().getAdminUser().getPassword(), RM_SITE_ID, RecordCategoryOne.getName());
+        deleteRecordCategory(RecordCategoryOne.getId());
+
+        rmRolesAndActionsAPI.deleteAllItemsInContainer(getDataUser().usingAdmin().getAdminUser().getUsername(),
+            getDataUser().usingAdmin().getAdminUser().getPassword(), RM_SITE_ID, RECORD_FOLDER_TWO);
+        rmRolesAndActionsAPI.deleteAllItemsInContainer(getDataUser().usingAdmin().getAdminUser().getUsername(),
+            getDataUser().usingAdmin().getAdminUser().getPassword(), RM_SITE_ID, RecordCategoryTwo.getName());
+        deleteRecordCategory(RecordCategoryTwo.getId());
+
         dataSite.usingAdmin().deleteSite(privateSite);
         dataSite.usingAdmin().deleteSite(testSite);
 

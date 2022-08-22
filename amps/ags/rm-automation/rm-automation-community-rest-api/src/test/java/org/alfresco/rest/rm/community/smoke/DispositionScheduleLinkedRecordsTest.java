cc5d1899eb49efe6525ad5baa0f217cbd4deba35
kshah <kavit.shah@globallogic.com>
2022-08-22 18:52:11 +0530

    [ags]

--- amps/ags/rm-automation/rm-automation-community-rest-api/src/test/java/org/alfresco/rest/rm/community/smoke/DispositionScheduleLinkedRecordsTest.java
+++ amps/ags/rm-automation/rm-automation-community-rest-api/src/test/java/org/alfresco/rest/rm/community/smoke/DispositionScheduleLinkedRecordsTest.java
@@ -24,8 +24,6 @@
  * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
  * #L%
  */
-
-
 package org.alfresco.rest.rm.community.smoke;
 
 import org.alfresco.rest.model.RestNodeBodyMoveCopyModel;
@@ -34,122 +32,156 @@ import org.alfresco.rest.requests.Node;
 import org.alfresco.rest.rm.community.base.BaseRMRestTest;
 import org.alfresco.rest.rm.community.model.fileplan.FilePlan;
 import org.alfresco.rest.rm.community.model.record.Record;
-import org.alfresco.rest.rm.community.model.record.RecordBodyFile;
 import org.alfresco.rest.rm.community.model.recordcategory.RecordCategory;
 import org.alfresco.rest.rm.community.model.recordcategory.RecordCategoryChild;
+import org.alfresco.rest.rm.community.model.user.UserRoles;
+import org.alfresco.rest.v0.LinksAPI;
 import org.alfresco.rest.v0.RMRolesAndActionsAPI;
+import org.alfresco.rest.v0.RecordFoldersAPI;
+import org.alfresco.rest.v0.RecordsAPI;
 import org.alfresco.rest.v0.service.DispositionScheduleService;
 import org.alfresco.test.AlfrescoTest;
+import org.alfresco.utility.model.RepoTestModel;
+import org.alfresco.utility.model.UserModel;
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import static org.alfresco.rest.rm.community.base.TestData.DEFAULT_PASSWORD;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.alfresco.rest.core.v0.BaseAPI.NODE_REF_WORKSPACE_SPACES_STORE;
 import static org.alfresco.rest.rm.community.model.fileplancomponents.FilePlanComponentAlias.FILE_PLAN_ALIAS;
 import static org.alfresco.rest.rm.community.model.recordcategory.RetentionPeriodProperty.CREATED_DATE;
 import static org.alfresco.rest.rm.community.util.CommonTestUtils.generateTestPrefix;
-import static org.alfresco.rest.rm.community.utils.CoreUtil.createBodyForMoveCopy;
-import static org.alfresco.rest.rm.community.utils.CoreUtil.toContentModel;
-import static org.alfresco.utility.data.RandomData.getRandomName;
 import static org.alfresco.utility.report.log.Step.STEP;
-import static org.springframework.http.HttpStatus.CREATED;
-import static org.testng.Assert.assertEquals;
 
 public class DispositionScheduleLinkedRecordsTest extends BaseRMRestTest {
-
-
     @Autowired
     private RMRolesAndActionsAPI rmRolesAndActionsAPI;
     @Autowired
     private DispositionScheduleService dispositionScheduleService;
-
-    private final String TEST_PREFIX = generateTestPrefix(DispositionScheduleLinkedRecordsTest.class);
-    private final String RM_ADMIN = TEST_PREFIX + "rm_admin";
-    private final String RM_MANAGER = TEST_PREFIX + "rm_admin";
+    @Autowired
+    private LinksAPI linksAPI;
+    @Autowired
+    private RecordsAPI recordsAPI;
+    @Autowired
+    private RecordFoldersAPI recordFoldersAPI;
+    private final static  String TEST_PREFIX = generateTestPrefix(DispositionScheduleLinkedRecordsTest.class);
     private RecordCategory Category1;
-    private RestNodeModel CopyCategory1;
-    private final String folderDisposition = TEST_PREFIX + "RM-2801 folder";
     private RecordCategoryChild CopyCatFolder,CatFolder;
-
-
-
-
+    private static final String categoryRM3077 = TEST_PREFIX + "RM-3077_manager_sees_me";
+    private static final String copyCategoryRM3077 = "Copy_of_" + categoryRM3077;
+    private static final String folderRM3077 = "RM-3077_folder_"+ categoryRM3077;
+    private static final String copyFolderRM3077 = "Copy_of_" + folderRM3077;
+    private FilePlan filePlanModel;
+    private UserModel rmAdmin, rmManager;
     @BeforeClass(alwaysRun = true)
     public void setupDispositionScheduleLinkedRecordsTest() {
-
         createRMSiteIfNotExists();
+        //get file plan
+        filePlanModel = getRestAPIFactory().getFilePlansAPI().getFilePlan(FILE_PLAN_ALIAS);
 
         // create "rm admin" user if it does not exist and assign it to RM Administrator role
-        rmRolesAndActionsAPI.createUserAndAssignToRole(
-            getDataUser().usingAdmin().getAdminUser().getUsername(),
-            getDataUser().usingAdmin().getAdminUser().getPassword(),
-            RM_ADMIN, DEFAULT_PASSWORD, "Administrator");
+        rmAdmin = getDataUser().createRandomTestUser();
+        rmRolesAndActionsAPI.assignRoleToUser(getDataUser().usingAdmin().getAdminUser().getUsername(),
+            getDataUser().usingAdmin().getAdminUser().getPassword(),rmAdmin.getUsername(),
+            UserRoles.ROLE_RM_ADMIN.roleId);
 
         // create "rm Manager" user if it does not exist and assign it to RM Administrator role
-        rmRolesAndActionsAPI.createUserAndAssignToRole(
-            getDataUser().usingAdmin().getAdminUser().getUsername(),
-            getDataUser().usingAdmin().getAdminUser().getPassword(),
-            RM_MANAGER, DEFAULT_PASSWORD, "Administrator");
-
-        // add transfer step
-        //dispositionScheduleService.addTransferAfterEventStep(Category1.getName(),"transferred records","all_allowances_granted_are_terminated");
-
+        rmManager = getDataUser().createRandomTestUser();
+        rmRolesAndActionsAPI.assignRoleToUser(getDataUser().usingAdmin().getAdminUser().getUsername(),
+            getDataUser().usingAdmin().getAdminUser().getPassword(),rmManager.getUsername(),
+            UserRoles.ROLE_RM_MANAGER.roleId);
     }
-
-
     @Test
     @AlfrescoTest(jira = "RM-1622")
-    public void dispositionScheduleLinkedRecords() throws Exception {
+    public void dispositionScheduleLinkedRecords() throws UnsupportedEncodingException {
         STEP("Create record category");
-        Category1 = createRootCategory(getRandomName("Title"));
+        Category1 = createRootCategory(categoryRM3077);
 
         //create retention schedule
         dispositionScheduleService.createCategoryRetentionSchedule(Category1.getName(), false);
+
         // add cut off step
         dispositionScheduleService.addCutOffAfterPeriodStep(Category1.getName(), "day|2", CREATED_DATE);
 
         //create a copy of the category recordsCategory
-        FilePlan filePlan = getRestAPIFactory().getFilePlansAPI().getFilePlan(FILE_PLAN_ALIAS);
-       CopyCategory1= getRestAPIFactory().getNodeAPI(toContentModel(Category1.getId())).copy(createBodyForMoveCopy(filePlan.getId()));
-
+        String CopyCategoryId = copyCategory(getAdminUser(),Category1.getId(), copyCategoryRM3077);
 
         // create folders in both categories
-        CatFolder = createRecordFolder(Category1.getId(), getRandomName("recFolder"));
-       // CopyCatFolder = createRecordFolder(CopyCategory1.getId(), getRandomName("recFolder"));
+        CatFolder = createRecordFolder(Category1.getId(), folderRM3077);
+        CopyCatFolder = createRecordFolder(CopyCategoryId, copyFolderRM3077);
 
         // create record  files
         String electronicRecord = "RM-2801 electronic record";
         Record elRecord = createElectronicRecord(CatFolder.getId(), electronicRecord);
+        String elRecordFullName = recordsAPI.getRecordFullName(getDataUser().usingAdmin().getAdminUser().getUsername(),
+            getDataUser().usingAdmin().getAdminUser().getPassword(), CatFolder.getName(), electronicRecord);
+
         String nonElectronicRecord = "RM-2801 non-electronic record";
         Record nonElRecord = createNonElectronicRecord(CatFolder.getId(), nonElectronicRecord);
-
+        String nonElRecordFullName = recordsAPI.getRecordFullName(getDataUser().usingAdmin().getAdminUser().getUsername(),
+            getDataUser().usingAdmin().getAdminUser().getPassword(), CatFolder.getName(), nonElectronicRecord);
 
         // link the records to copy folder, then complete them
-       /* Record recordLink = fileRecordToFolder(unfiledRecordId, folderToLink);
-        assertStatusCode(CREATED);
-        assertEquals(recordLink.getParentId(), targetFolderId);
+        List<String> recordLists = new ArrayList<>();
+        recordLists.add(NODE_REF_WORKSPACE_SPACES_STORE + elRecord.getId());
+        recordLists.add(NODE_REF_WORKSPACE_SPACES_STORE + nonElRecord.getId());
 
-        private Record fileRecordToFolder(String recordId, String targetFolderId)
+        linksAPI.linkRecord(getDataUser().getAdminUser().getUsername(),
+            getDataUser().getAdminUser().getPassword(), HttpStatus.SC_OK,copyCategoryRM3077 + "/" +
+                copyFolderRM3077, recordLists);
+        recordsAPI.completeRecord(rmAdmin.getUsername(), rmAdmin.getPassword(), elRecordFullName);
+        recordsAPI.completeRecord(rmAdmin.getUsername(), rmAdmin.getPassword(), nonElRecordFullName);
+
+        // edit disposition date
+        recordFoldersAPI.postFolderAction(getAdminUser().getUsername(),
+            getAdminUser().getPassword(),editDispositionDateJson(),CatFolder.getName());
+
+        // cut off the Folder
+        recordFoldersAPI.postFolderAction(getAdminUser().getUsername(),
+            getAdminUser().getPassword(),new JSONObject().put("name","cutoff"),CatFolder.getName());
+
+        // Verify the Content
+//        Node electronicNode = getNode(elRecord.getId());
+//        assertTrue("The content of " + electronicRecord + " is not available",
+//            StringUtils.isEmpty(electronicNode.getNodeContent().toString()));
+
+        // verify the Properties
+
+        // delete precondition
+        deleteRecordCategory(Category1.getId());
+        deleteRecordCategory(CopyCategoryId);
+    }
+    private String copyCategory(UserModel user, String categoryId, String copyName) {
+        RepoTestModel repoTestModel = new RepoTestModel() {};
+        repoTestModel.setNodeRef(categoryId);
+        RestNodeModel restNodeModel;
+
+        RestNodeBodyMoveCopyModel copyDestinationInfo = new RestNodeBodyMoveCopyModel();
+        copyDestinationInfo.setTargetParentId(filePlanModel.getId());
+        copyDestinationInfo.setName(copyName);
+
+        try
         {
-            RecordBodyFile recordBodyFile = RecordBodyFile.builder().targetParentId(targetFolderId).build();
-            return getRestAPIFactory().getRecordsAPI().fileRecord(recordBodyFile, recordId);
-        }*/
+            restNodeModel = getRestAPIFactory().getNodeAPI(user, repoTestModel).copy(copyDestinationInfo);
 
-
-
-
-
-
-
-
-
-
-
-
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException("Problem copying category.", e);
+        }
+        return restNodeModel.getId();
     }
 
-
-
-
+    private Node getNode(String recordId)
+    {
+        RepoTestModel repoTestModel = new RepoTestModel() {};
+        repoTestModel.setNodeRef(recordId);
+        return getRestAPIFactory().getNodeAPI(repoTestModel);
     }
-
+}

6b2980511679deb85bfcb0dd8150f8f27abfd447
Antonio Felix <antonio.felix@texter.ai>
2022-08-23 18:30:22 +0100

    MNT-23087 - Rollback white spaces

--- repository/src/main/java/org/alfresco/repo/exporter/ExporterComponent.java
+++ repository/src/main/java/org/alfresco/repo/exporter/ExporterComponent.java
@@ -4,21 +4,21 @@
  * %%
  * Copyright (C) 2005 - 2016 Alfresco Software Limited
  * %%
- * This file is part of the Alfresco software. 
- * If the software was purchased under a paid Alfresco license, the terms of 
- * the paid license agreement will prevail.  Otherwise, the software is 
+ * This file is part of the Alfresco software.
+ * If the software was purchased under a paid Alfresco license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
  * provided under the following open source license terms:
- * 
+ *
  * Alfresco is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Alfresco is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -1017,7 +1017,6 @@ public class ExporterComponent
         private NodeRef[] exportList;
         private Map<Integer,NodeRef[]> exportListMap;
         private NodeRef[] parentList;
-
         private Map<Integer,NodeRef[]> parentListMap;
         private String exportedBy;
         private Date exportedDate;

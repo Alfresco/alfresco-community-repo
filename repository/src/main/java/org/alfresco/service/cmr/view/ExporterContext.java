49664620229a940667f8c8d027cd713bb58ecf1b
Antonio Felix <antonio.felix@texter.ai>
2022-08-25 15:36:50 +0100

    Fix/mnt 23087 export of records failing (#1333)
    * MNT-23087 - Split the export list into several smaller list for better performance
    
    (cherry picked from commit 9c98f7b0fbc58fcceab34ae9186a9606984c5a12)

--- repository/src/main/java/org/alfresco/service/cmr/view/ExporterContext.java
+++ repository/src/main/java/org/alfresco/service/cmr/view/ExporterContext.java
@@ -2,23 +2,23 @@
  * #%L
  * Alfresco Repository
  * %%
- * Copyright (C) 2005 - 2016 Alfresco Software Limited
+ * Copyright (C) 2005 - 2022 Alfresco Software Limited
  * %%
- * This file is part of the Alfresco software. 
- * If the software was purchased under a paid Alfresco license, the terms of 
- * the paid license agreement will prevail.  Otherwise, the software is 
+ * This file is part of the Alfresco software.
+ * If the software was purchased under a paid Alfresco license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
  * provided under the following open source license terms:
- * 
+ *
  * Alfresco is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Alfresco is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -26,6 +26,7 @@
 package org.alfresco.service.cmr.view;
 
 import java.util.Date;
+import java.util.Map;
 
 import org.alfresco.service.cmr.repository.NodeRef;
 
@@ -73,6 +74,8 @@ public interface ExporterContext
      * @return NodeRef[]
      */
     public NodeRef[] getExportList();
+
+    public Map<Integer, NodeRef[]> getExportMap();
     
     /**
      * Gets list of parents for exporting nodes

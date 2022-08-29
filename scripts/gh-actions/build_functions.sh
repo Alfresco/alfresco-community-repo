ff18fba533f87ea51b6419855b898136d5a955dd
Damian.Ujma@hyland.com <Damian.Ujma@hyland.com>
2022-08-29 13:23:28 +0200

    ACS-3465 Add AGS-Community-Rest-API-Tests, Push-to-Nexus, Update-downstream jobs
    ACS-3465 Test AGS-Community-Rest-API-Tests
    
    ACS-3465 Test AGS-Community-Rest-API-Tests
    
    ACS-3465 Test script execution
    
    ACS-3465 Increase SearchRecordsTests timeout
    
    ACS-3465 Make a script executable
    
    ACS-3465 Test script execution
    
    ACS-3465 Fix build step in AGS-Community-Rest-API-Tests
    
    ACS-3465 Test AGS-Community-Rest-API-Tests
    
    ACS-3465 Test AGS-Community-Rest-API-Tests
    
    ACS-3465 Test AGS-Community-Rest-API-Tests

---
+++ scripts/gh-actions/build_functions.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set +vx
+
+function cloneRepo() {
+  local REPO="${1}"
+  local TAG_OR_BRANCH="${2}"
+
+  printf "Clonning \"%s\" on %s\n" "${TAG_OR_BRANCH}" "${REPO}"
+
+  # clone the repository branch/tag
+  pushd "$(dirname "${BASH_SOURCE[0]}")/../../../" >/dev/null
+
+  rm -rf "$(basename "${REPO%.git}")"
+
+  git clone -b "${TAG_OR_BRANCH}" --depth=1 "https://${GIT_USERNAME}:${GIT_PASSWORD}@${REPO}"
+
+  popd >/dev/null
+}
+
+set -vx

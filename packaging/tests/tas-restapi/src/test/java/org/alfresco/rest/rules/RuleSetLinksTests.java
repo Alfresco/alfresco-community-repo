1efe6e26462c1ff6fafbfe3a82d24661976404a8
mpichura <maciej.pichura@hyland.com>
2022-08-29 17:02:56 +0200

    ACS-3431: Initial TAS REST tests for link to rule set.

---
+++ packaging/tests/tas-restapi/src/test/java/org/alfresco/rest/rules/RuleSetLinksTests.java
@@ -0,0 +1,131 @@
+/*
+ * #%L
+ * Alfresco Remote API
+ * %%
+ * Copyright (C) 2005 - 2022 Alfresco Software Limited
+ * %%
+ * This file is part of the Alfresco software.
+ * If the software was purchased under a paid Alfresco license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
+ * provided under the following open source license terms:
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+package org.alfresco.rest.rules;
+
+import static org.alfresco.rest.rules.RulesTestsUtils.createRuleModel;
+import static org.alfresco.utility.report.log.Step.STEP;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+import org.alfresco.rest.RestTest;
+import org.alfresco.rest.model.RestRuleModel;
+import org.alfresco.rest.model.RestRuleModelsCollection;
+import org.alfresco.rest.model.RestRuleSetLinkModel;
+import org.alfresco.rest.model.RestRuleSetModelsCollection;
+import org.alfresco.utility.model.FolderModel;
+import org.alfresco.utility.model.SiteModel;
+import org.alfresco.utility.model.TestGroup;
+import org.alfresco.utility.model.UserModel;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * Tests for /nodes/{nodeId}/rule-sets and /nodes/{nodeId}/rule-set-links.
+ */
+@Test (groups = { TestGroup.RULES })
+public class RuleSetLinksTests extends RestTest
+{
+    private UserModel user;
+    private SiteModel site;
+    private FolderModel ruleFolder;
+    private RestRuleModel rule;
+    private String ruleSetId;
+
+    @BeforeClass (alwaysRun = true)
+    public void dataPreparation()
+    {
+        STEP("Create a user, site and folder.");
+        user = dataUser.createRandomTestUser();
+        site = dataSite.usingUser(user).createPublicRandomSite();
+        ruleFolder = dataContent.usingUser(user).usingSite(site).createFolder();
+
+        STEP("Create a rule in the rule folder.");
+        RestRuleModel ruleModel = createRuleModel("ruleName");
+        rule = restClient.authenticateUser(user).withCoreAPI().usingNode(ruleFolder).usingDefaultRuleSet()
+                                       .createSingleRule(ruleModel);
+
+        STEP("Get the rule sets for the folder and find the rule set id");
+        RestRuleSetModelsCollection ruleSets = restClient.authenticateUser(user).withCoreAPI().usingNode(ruleFolder)
+                                                         .getListOfRuleSets();
+        ruleSets.assertThat().entriesListCountIs(1);
+        ruleSetId = ruleSets.getEntries().get(0).onModel().getId();
+    }
+
+    /** Check we can link to folder containing a rule set. */
+    @Test (groups = { TestGroup.REST_API, TestGroup.RULES })
+    public void linkToFolderContainingRules()
+    {
+        STEP("Create another folder in existing site");
+        final FolderModel folder = dataContent.usingUser(user).usingSite(site).createFolder();
+
+        STEP("Link to folder");
+        final RestRuleSetLinkModel request = new RestRuleSetLinkModel();
+        request.setId(ruleFolder.getNodeRef());
+        final RestRuleSetLinkModel ruleLink = restClient.authenticateUser(user).withCoreAPI().usingNode(folder).createRuleLink(request);
+
+        restClient.assertStatusCodeIs(CREATED);
+        ruleLink.assertThat().field("id").contains(ruleSetId);
+
+        final RestRuleModelsCollection rules = restClient.authenticateUser(user).withCoreAPI().usingNode(folder).usingDefaultRuleSet().getListOfRules();
+        rules.assertThat().entriesListCountIs(1);
+    }
+
+    /** Check we can link to a rule set. */
+    @Test (groups = { TestGroup.REST_API, TestGroup.RULES })
+    public void linkToRuleSet()
+    {
+        STEP("Create another folder in existing site");
+        final FolderModel folder = dataContent.usingUser(user).usingSite(site).createFolder();
+
+        STEP("Link to rule set");
+        final RestRuleSetLinkModel request = new RestRuleSetLinkModel();
+        request.setId(ruleSetId);
+        final RestRuleSetLinkModel ruleLink = restClient.authenticateUser(user).withCoreAPI().usingNode(folder).createRuleLink(request);
+
+        restClient.assertStatusCodeIs(CREATED);
+        ruleLink.assertThat().field("id").contains(ruleSetId);
+
+        final RestRuleModelsCollection rules = restClient.authenticateUser(user).withCoreAPI().usingNode(folder).usingDefaultRuleSet().getListOfRules();
+        rules.assertThat().entriesListCountIs(1);
+    }
+
+
+    /** Check we get 404 when linking to a non-existing rule set/folder. */
+    @Test (groups = { TestGroup.REST_API, TestGroup.RULES })
+    public void linkToNonExistingRuleSet()
+    {
+        STEP("Create another folder in existing site");
+        final FolderModel folder = dataContent.usingUser(user).usingSite(site).createFolder();
+
+        STEP("Link to non-existing rule set");
+        final RestRuleSetLinkModel request = new RestRuleSetLinkModel();
+        request.setId("dummy-rule-set-id");
+        restClient.authenticateUser(user).withCoreAPI().usingNode(folder).createRuleLink(request);
+
+        restClient.assertStatusCodeIs(NOT_FOUND);
+    }
+
+}

e0709e88c20d9d3770ff84f9404f97be72b0406c
Sara Aspery <sara.aspery@alfresco.com>
2022-10-24 10:19:13 +0100

    ACS-3732 Remove mocking and use class for template loading

--- repository/src/test/java/org/alfresco/repo/template/UnsafeMethodsTest.java
+++ repository/src/test/java/org/alfresco/repo/template/UnsafeMethodsTest.java
@@ -25,14 +25,8 @@
  */
 package org.alfresco.repo.template;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.StringWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import freemarker.template.Configuration;
@@ -40,84 +34,45 @@ import freemarker.template.Template;
 import freemarker.template.TemplateException;
 import freemarker.template.TemplateExceptionHandler;
 import junit.framework.TestCase;
-import org.mockito.Mockito;
 
 public class UnsafeMethodsTest extends TestCase
 {
-    private static final String TEST_TEMPLATES_DIR = "/org/alfresco/repo/template/templates/";
+    private static final String TEST_TEMPLATES_PACKAGE = "/org/alfresco/repo/template/templates/";
     private static final String ALLOWED_TEXT = ": ALLOWED";
     private static final String BLOCKED_TEXT = ": BLOCKED";
+    private static final String EXPECTED_RESULT = "Freemarker Unsafe Methods Testing\n" +
+            "=================================\n" +
+            "java.lang.Thread.getId(): ALLOWED\n" +
+            "java.lang.Thread.interrupt(): BLOCKED\n" +
+            "java.lang.Thread.currentThread(): BLOCKED\n";
 
     private final Configuration configuration = new Configuration(Configuration.VERSION_2_3_31);
 
     public void testUnsafeMethods() throws Exception
     {
-        final File jarFile = new File(getClass().getProtectionDomain().getCodeSource().getLocation().toURI());
-        File templatesDir = new File(jarFile.getPath() + TEST_TEMPLATES_DIR);
-        configuration.setDirectoryForTemplateLoading(templatesDir);
+        configuration.setClassForTemplateLoading(getClass(), TEST_TEMPLATES_PACKAGE);
         configuration.setDefaultEncoding("UTF-8");
         configuration.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
         Template template = configuration.getTemplate("unsafemethods.ftl");
 
-        Map<String, Object> model = new HashMap<String, Object>();
-        model.put("allowedText", ALLOWED_TEXT);
-        model.put( "blockedText", BLOCKED_TEXT);
+        Thread currentThread = Thread.currentThread();
+        Map<String, Object> model = Map.of(
+                "allowedText", ALLOWED_TEXT,
+                "blockedText", BLOCKED_TEXT,
+                "thread", currentThread);
 
-        final List<String> allowedMethods = createListOfAllowedMethods();
-        final List<String> blockedMethods = createListOfBlockedMethods();
+        String result = applyTemplate(template, model);
 
-        // Prepare Spies for methods testing
-        Thread thread = Mockito.spy(new Thread());
-        model.put("thread", thread);
-
-        // Apply the freemarker template
-        List<String> results = applyTemplate(template, model);
-
-        // Verify methods were allowed or blocked
-        verifyFreemarkerOutput(results, allowedMethods, blockedMethods);
-        verifyMethodInvocations(thread);
+        assertFalse(currentThread.isInterrupted());
+        assertEquals(EXPECTED_RESULT, result);
     }
 
-    private List<String> applyTemplate(Template template, Map<String, Object> inputModel ) throws TemplateException, IOException
+    private String applyTemplate(Template template, Map<String, Object> inputModel ) throws TemplateException, IOException
     {
-        StringWriter stringWriter = new StringWriter();
-        template.process(inputModel, stringWriter);
-        String[] lines = stringWriter.toString().split(System.getProperty("line.separator"));
-        return new ArrayList<>(Arrays.asList(lines));
-    }
-
-    private void verifyFreemarkerOutput(Collection<String> results, List<String> allowedMethods, List<String> blockedMethods)
-    {
-        allowedMethods.forEach(method -> assertTrue("Expected method '" + method + "' to be allowed", results.contains(method + ALLOWED_TEXT)));
-
-        blockedMethods.forEach(method -> assertTrue("Expected method '" + method + "' to be blocked", results.contains(method + BLOCKED_TEXT)));
-    }
-
-    private void verifyMethodInvocations(Thread thread) throws InterruptedException
-    {
-        // verify an unlisted method is not blocked
-        Mockito.verify(thread, Mockito.times(1)).getId();
-
-        // Verify an originally blocked method is still blocked
-        Mockito.verify(thread, Mockito.never()).interrupt();
-
-        // Verify a newly blocked method in the patched version is blocked
-        Mockito.verify(thread, Mockito.never()).currentThread();
-
-    }
-
-    private static List<String> createListOfAllowedMethods()
-    {
-        final List<String> allowedMethods = new ArrayList<>(1);
-        allowedMethods.add("java.lang.Thread.getId()");
-        return allowedMethods;
-    }
-
-    private static List<String> createListOfBlockedMethods()
-    {
-        List<String> blockedMethods = new ArrayList<>(2);
-        blockedMethods.add("java.lang.Thread.interrupt()");
-        blockedMethods.add("java.lang.Thread.currentThread()");
-        return blockedMethods;
+        try (StringWriter stringWriter = new StringWriter())
+        {
+            template.process(inputModel, stringWriter);
+            return stringWriter.toString();
+        }
     }
 }

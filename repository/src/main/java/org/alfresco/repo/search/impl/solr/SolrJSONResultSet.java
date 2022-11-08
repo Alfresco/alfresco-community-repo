7bb6dc2cf94a8363e6e15d265d9e0030e5c2ee56
Antonio Felix <antonio.felix@texter.ai>
2022-11-08 13:02:57 +0000

    MNT-23276 - The null facet name should be considered

--- repository/src/main/java/org/alfresco/repo/search/impl/solr/SolrJSONResultSet.java
+++ repository/src/main/java/org/alfresco/repo/search/impl/solr/SolrJSONResultSet.java
@@ -267,13 +267,14 @@ public class SolrJSONResultSet implements SearchEngineResultSet {
                         ArrayList<Pair<String, Integer>> facetValues = new ArrayList<Pair<String, Integer>>(facetArraySize/2);
                         for(int i = 0; i < facetArraySize; i+=2)
                         {
+                            String facetEntryName = "Null";
                             if(!facets.isNull(i))
                             {
-                                String facetEntryName = facets.getString(i);
-                                Integer facetEntryCount = Integer.valueOf(facets.getInt(i + 1));
-                                Pair<String, Integer> pair = new Pair<String, Integer>(facetEntryName, facetEntryCount);
-                                facetValues.add(pair);
+                                facetEntryName = facets.getString(i);
                             }
+                            Integer facetEntryCount = Integer.valueOf(facets.getInt(i + 1));
+                            Pair<String, Integer> pair = new Pair<String, Integer>(facetEntryName, facetEntryCount);
+                            facetValues.add(pair);
                         }
                         fieldFacets.put(fieldName, facetValues);
                     }

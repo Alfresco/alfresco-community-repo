5749a1a67adf026df890effa83d3de61312c9520
Damian.Ujma@hyland.com <Damian.Ujma@hyland.com>
2022-12-12 10:37:09 +0100

    ACS-3841 Improve maven_release.sh

--- scripts/ci/maven_release.sh
+++ scripts/ci/maven_release.sh
@@ -4,6 +4,12 @@ PS4="\[\e[35m\]+ \[\e[m\]"
 set -vex
 pushd "$(dirname "${BASH_SOURCE[0]}")/../../"
 
+# Use full history for release
+git checkout -B "${BRANCH_NAME}"
+# Define git identity for commits
+git config user.email "${GIT_EMAIL}"
+git config user.name "${GIT_USERNAME}"
+
 # Run the release plugin - with "[skip ci]" in the release commit message
 mvn -B \
   -Pall-tas-tests \

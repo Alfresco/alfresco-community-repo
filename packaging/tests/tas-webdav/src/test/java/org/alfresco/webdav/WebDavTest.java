c3b01aa89e9592a7b5bb6db2b4a88b5387f5c307
Damian.Ujma@hyland.com <Damian.Ujma@hyland.com>
2022-12-16 12:31:53 +0100

    Revert "ACS-3841 Test WebDAV TAS test [tas]"

--- packaging/tests/tas-webdav/src/test/java/org/alfresco/webdav/WebDavTest.java
+++ packaging/tests/tas-webdav/src/test/java/org/alfresco/webdav/WebDavTest.java
@@ -1,25 +1,17 @@
 package org.alfresco.webdav;
 
-import java.lang.reflect.Method;
-
 import org.alfresco.utility.data.DataContent;
 import org.alfresco.utility.data.DataSite;
 import org.alfresco.utility.data.DataUser;
-import org.alfresco.utility.LogFactory;
 import org.alfresco.utility.network.ServerHealth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
-import org.slf4j.Logger;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
 @ContextConfiguration("classpath:alfresco-webdav-context.xml")
 public abstract class WebDavTest extends AbstractTestNGSpringContextTests
 {
-    private static Logger LOG = LogFactory.getLogger();
-
     @Autowired
     protected DataSite dataSite;
 
@@ -43,17 +35,5 @@ public abstract class WebDavTest extends AbstractTestNGSpringContextTests
         // Since alfresco 6.0 JMX connection is deprecated
         // The webdav protocol is enabled by default.
         //webDavProtocol.assertThat().protocolIsEnabled();
-    }
-
-    @BeforeMethod(alwaysRun=true)
-    public void showStartTestInfo(Method method)
-    {
-        LOG.info(String.format("*** STARTING Test: [%s] ***",method.getName()));
-    }
-
-    @AfterMethod(alwaysRun=true)
-    public void showEndTestInfo(Method method)
-    {
-        LOG.info(String.format("*** ENDING Test: [%s] ***", method.getName()));
     }
 }

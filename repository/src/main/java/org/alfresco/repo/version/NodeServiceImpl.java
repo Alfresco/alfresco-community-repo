a38545115b0273e0f674a1fbe78703695f2a8798
Antonio Felix <antonio.felix@texter.ai>
2022-12-22 13:03:24 +0000

    Fix/mnt 23290 slow group membership (#1637) (#1644)
    * MNT-23290 - Change query to get the root groups
    
    (cherry picked from commit d785b100df430408d441e3a6c8d5e200f292219e)

--- repository/src/main/java/org/alfresco/repo/version/NodeServiceImpl.java
+++ repository/src/main/java/org/alfresco/repo/version/NodeServiceImpl.java
@@ -2,7 +2,7 @@
  * #%L
  * Alfresco Repository
  * %%
- * Copyright (C) 2005 - 2016 Alfresco Software Limited
+ * Copyright (C) 2005 - 2022 Alfresco Software Limited
  * %%
  * This file is part of the Alfresco software. 
  * If the software was purchased under a paid Alfresco license, the terms of 
@@ -762,7 +762,15 @@ public class NodeServiceImpl implements NodeService, VersionModel
     public Collection<ChildAssociationRef> getChildAssocsWithoutParentAssocsOfType(NodeRef parent, QName assocTypeQName)
     {
         throw new UnsupportedOperationException(MSG_UNSUPPORTED);
-    }        
+    }
+
+    /**
+     * @throws UnsupportedOperationException always
+     */
+    public List<String> findAssocsNotLinkedByTwoOtherAssocs(NodeRef parent)
+    {
+        throw new UnsupportedOperationException(MSG_UNSUPPORTED);
+    }
 
     /**
      * Gets, converts and adds the intrinsic properties to the current node's properties

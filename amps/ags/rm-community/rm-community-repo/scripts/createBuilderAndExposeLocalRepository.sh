71422898e6de738c4853d915bc7bbac6f227702b
Damian.Ujma@hyland.com <Damian.Ujma@hyland.com>
2023-01-19 17:01:35 +0100

    ACS-4137 Test wih GHA [ags][tas]

--- amps/ags/rm-community/rm-community-repo/scripts/createBuilderAndExposeLocalRepository.sh
+++ amps/ags/rm-community/rm-community-repo/scripts/createBuilderAndExposeLocalRepository.sh
@@ -1,5 +1,5 @@
 docker buildx create --use --name insecure-builder --driver-opt network=host --buildkitd-flags '--allow-insecure-entitlement security.insecure --allow-insecure-entitlement network.host'
 
-docker run -d --name local-registry --network=host registry:2
+docker run -d -p 5000:5000 --restart=always --name registry registry:2
 docker tag alfresco/alfresco-community-repo-base:latest localhost:5000/alfresco/alfresco-community-repo-base:latest
 docker push localhost:5000/alfresco/alfresco-community-repo-base:latest

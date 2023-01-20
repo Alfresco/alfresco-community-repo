6fcbf14c5033cd26cf7e7b28a0e3e85fb814d6e5
Damian.Ujma@hyland.com <Damian.Ujma@hyland.com>
2023-01-20 13:15:14 +0100

    ACS-4137 Fix creating builder [ags][tas]

--- amps/ags/rm-community/rm-community-repo/scripts/createBuilderAndExposeLocalRepository.sh
+++ amps/ags/rm-community/rm-community-repo/scripts/createBuilderAndExposeLocalRepository.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-docker buildx create --use --name insecure-builder --driver-opt network=host --buildkitd-flags '--allow-insecure-entitlement security.insecure --allow-insecure-entitlement network.host'
-
 docker run -d --restart=always --network=host --name registry registry:2
 docker tag alfresco/alfresco-community-repo-base:latest localhost:5000/alfresco/alfresco-community-repo-base:latest
 docker push localhost:5000/alfresco/alfresco-community-repo-base:latest
+docker --config target/docker/alfresco/alfresco-governance-repository-community-base/latest/docker \
+buildx create --use --name insecure-builder --driver-opt network=host \
+--buildkitd-flags '--allow-insecure-entitlement security.insecure --allow-insecure-entitlement network.host'

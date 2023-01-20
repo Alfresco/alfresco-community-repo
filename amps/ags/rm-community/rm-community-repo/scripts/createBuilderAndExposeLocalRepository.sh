cb8d59986526e99bd1af1a21d84cf44b99f683e7
Damian.Ujma@hyland.com <Damian.Ujma@hyland.com>
2023-01-20 09:58:29 +0100

    ACS-4137 Fix setting network [ags][tas]

--- amps/ags/rm-community/rm-community-repo/scripts/createBuilderAndExposeLocalRepository.sh
+++ amps/ags/rm-community/rm-community-repo/scripts/createBuilderAndExposeLocalRepository.sh
@@ -1,5 +1,6 @@
+#!/usr/bin/env bash
 docker buildx create --use --name insecure-builder --driver-opt network=host --buildkitd-flags '--allow-insecure-entitlement security.insecure --allow-insecure-entitlement network.host'
 
-docker run -d -p 5000:5000 --restart=always --name registry registry:2
+docker run -d --restart=always --network=host --name registry registry:2
 docker tag alfresco/alfresco-community-repo-base:latest localhost:5000/alfresco/alfresco-community-repo-base:latest
 docker push localhost:5000/alfresco/alfresco-community-repo-base:latest

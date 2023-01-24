ca73041734ce266ae03631ac437d76ad8f7a5bcf
Damian Ujma <92095156+damianujma@users.noreply.github.com>
2023-01-24 14:55:20 +0100

    ACS-4137 Fix building a multiarch image (#1697)
    * ACS-4137 Test wih GHA [ags][tas]
    
    * ACS-4137 Test wih GHA [ags][tas]
    
    * ACS-4137 Fix setting network [ags][tas]
    
    * ACS-4137 Fix creating builder [ags][tas]
    
    * ACS-4137 Change image tag [ags][tas]
    
    * ACS-4137 Fix starting the registry [ags][tas]
    
    * ACS-4137 Refactor code [ags][tas]
    
    * ACS-4137 Uncomment all jobs [tas][ags]
    
    * ACS-4137 Improve prepare_buildx.sh [tas][ags]
    
    * ACS-4137 Implement timeout + remove hardcoded base image tag [tas][ags]

---
+++ amps/ags/rm-community/rm-community-repo/scripts/prepare_buildx.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+LOCAL_REGISTRY_HOST="${1}"
+LOCAL_REGISTRY_PORT="${2}"
+BASE_IMAGE="${3}"
+BASE_IMAGE_TAG="${4}"
+BUILDER_NAME="${5}"
+LOCAL_REGISTRY="${LOCAL_REGISTRY_HOST}":"${LOCAL_REGISTRY_PORT}"
+SLEEP_SECONDS=0
+
+#Run a local registry server
+docker run -d -p "${LOCAL_REGISTRY_PORT}":"${LOCAL_REGISTRY_PORT}" --restart=always --name registry registry:2
+
+#Push base image to the local repository
+docker tag "${BASE_IMAGE}":"${BASE_IMAGE_TAG}" "${LOCAL_REGISTRY}"/"${BASE_IMAGE}":"${BASE_IMAGE_TAG}"
+while [ "$( docker container inspect -f '{{.State.Running}}' registry )" != "true" ] && [ $SLEEP_SECONDS -lt 120 ]
+do
+  ((SLEEP_SECONDS++))
+  sleep 1
+done
+docker push "${LOCAL_REGISTRY}"/"${BASE_IMAGE}":"${BASE_IMAGE_TAG}"
+
+#Create a `docker-container` builder with host networking and required flags
+docker --config target/docker/alfresco/alfresco-governance-repository-community-base/latest/docker \
+buildx create --use --name "${BUILDER_NAME}" --driver-opt network=host \
+--buildkitd-flags '--allow-insecure-entitlement security.insecure --allow-insecure-entitlement network.host'

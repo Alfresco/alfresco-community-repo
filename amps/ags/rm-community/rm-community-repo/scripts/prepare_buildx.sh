64e91aefd65915b88f47cbd8663c633722511098
Damian.Ujma@hyland.com <Damian.Ujma@hyland.com>
2023-01-27 16:54:54 +0100

    ACS-4137 Generalize prepare_buildx.sh + increase registry timeout

--- amps/ags/rm-community/rm-community-repo/scripts/prepare_buildx.sh
+++ amps/ags/rm-community/rm-community-repo/scripts/prepare_buildx.sh
@@ -5,6 +5,9 @@ LOCAL_REGISTRY_PORT="${2}"
 BASE_IMAGE="${3}"
 BASE_IMAGE_TAG="${4}"
 BUILDER_NAME="${5}"
+TARGET_REGISTRY="${6}"
+TARGET_IMAGE="${7}"
+TARGET_TAG="${8}"
 LOCAL_REGISTRY="${LOCAL_REGISTRY_HOST}":"${LOCAL_REGISTRY_PORT}"
 SLEEP_SECONDS=0
 
@@ -13,14 +16,19 @@ docker run -d -p "${LOCAL_REGISTRY_PORT}":"${LOCAL_REGISTRY_PORT}" --restart=alw
 
 #Push base image to the local repository
 docker tag "${BASE_IMAGE}":"${BASE_IMAGE_TAG}" "${LOCAL_REGISTRY}"/"${BASE_IMAGE}":"${BASE_IMAGE_TAG}"
-while [ "$( docker container inspect -f '{{.State.Running}}' registry )" != "true" ] && [ $SLEEP_SECONDS -lt 120 ]
+while [ "$( docker container inspect -f '{{.State.Running}}' registry )" != "true" ] && [ $SLEEP_SECONDS -lt 600 ]
 do
   ((SLEEP_SECONDS++))
   sleep 1
 done
 docker push "${LOCAL_REGISTRY}"/"${BASE_IMAGE}":"${BASE_IMAGE_TAG}"
 
-#Create a `docker-container` builder with host networking and required flags
-docker --config target/docker/alfresco/alfresco-governance-repository-community-base/latest/docker \
+#Create a `docker-container` builder with host networking and required flags (quay.io)
+docker --config target/docker/"${TARGET_REGISTRY}"/"${TARGET_IMAGE}"/"${TARGET_TAG}"/docker \
+buildx create --use --name "${BUILDER_NAME}" --driver-opt network=host \
+--buildkitd-flags '--allow-insecure-entitlement security.insecure --allow-insecure-entitlement network.host'
+
+#Create a `docker-container` builder with host networking and required flags (docker.io)
+docker --config target/docker/"${TARGET_IMAGE}"/"${TARGET_TAG}"/docker \
 buildx create --use --name "${BUILDER_NAME}" --driver-opt network=host \
 --buildkitd-flags '--allow-insecure-entitlement security.insecure --allow-insecure-entitlement network.host'

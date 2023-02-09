acf5cfe3b8190e9ae12408fb60cde245c0b7d28b
Wojtek.Swieton <wojtek.swieton@hyland.com>
2023-02-09 19:16:09 +0100

    ACS-4137 added combine.self="override" attribute to configuration in build-docker-images profile to not use buildx. Delete redundant base.image.tag

--- amps/ags/rm-community/rm-community-repo/scripts/prepare_buildx.sh
+++ amps/ags/rm-community/rm-community-repo/scripts/prepare_buildx.sh
@@ -2,11 +2,10 @@
 
 LOCAL_REGISTRY="${1}"
 BASE_IMAGE="${2}"
-BASE_IMAGE_TAG="${3}"
-BUILDER_NAME="${4}"
-TARGET_REGISTRY="${5}"
-TARGET_IMAGE="${6}"
-TARGET_TAG="${7}"
+BUILDER_NAME="${3}"
+TARGET_REGISTRY="${4}"
+TARGET_IMAGE="${5}"
+IMAGE_TAG="${6}"
 SLEEP_SECONDS=0
 
 if [ "${LOCAL_REGISTRY}" != "127.0.0.1:5000" ]; then
@@ -16,21 +15,21 @@ else
   docker run -d -p 5000:5000 --restart=always --name registry registry:2
 
   #Push base image to the local repository
-  docker tag "${BASE_IMAGE}":"${BASE_IMAGE_TAG}" "${LOCAL_REGISTRY}"/"${BASE_IMAGE}":"${BASE_IMAGE_TAG}"
+  docker tag "${BASE_IMAGE}":"${IMAGE_TAG}" "${LOCAL_REGISTRY}"/"${BASE_IMAGE}":"${IMAGE_TAG}"
   while [ "$( docker container inspect -f '{{.State.Running}}' registry )" != "true" ] && [ $SLEEP_SECONDS -lt 600 ]
   do
     ((SLEEP_SECONDS++))
     sleep 1
   done
-  docker push "${LOCAL_REGISTRY}"/"${BASE_IMAGE}":"${BASE_IMAGE_TAG}"
+  docker push "${LOCAL_REGISTRY}"/"${BASE_IMAGE}":"${IMAGE_TAG}"
 
   #Create a `docker-container` builder with host networking and required flags (quay.io)
-  docker --config target/docker/"${TARGET_REGISTRY}"/"${TARGET_IMAGE}"/"${TARGET_TAG}"/docker \
+  docker --config target/docker/"${TARGET_REGISTRY}"/"${TARGET_IMAGE}"/"${IMAGE_TAG}"/docker \
   buildx create --use --name "${BUILDER_NAME}" --driver-opt network=host \
   --buildkitd-flags '--allow-insecure-entitlement security.insecure --allow-insecure-entitlement network.host'
 
   #Create a `docker-container` builder with host networking and required flags (docker.io)
-  docker --config target/docker/"${TARGET_IMAGE}"/"${TARGET_TAG}"/docker \
+  docker --config target/docker/"${TARGET_IMAGE}"/"${IMAGE_TAG}"/docker \
   buildx create --use --name "${BUILDER_NAME}" --driver-opt network=host \
   --buildkitd-flags '--allow-insecure-entitlement security.insecure --allow-insecure-entitlement network.host'
 fi

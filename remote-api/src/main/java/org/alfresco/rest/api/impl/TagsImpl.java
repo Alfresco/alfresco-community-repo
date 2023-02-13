8473b0044081914b0e2deb0c55f465fe96a0a4e4
suneet.gupta <suneet.gupta@hyland.com>
2023-02-13 12:01:36 +0530

    Added tagById validation if node is a tag

--- remote-api/src/main/java/org/alfresco/rest/api/impl/TagsImpl.java
+++ remote-api/src/main/java/org/alfresco/rest/api/impl/TagsImpl.java
@@ -185,11 +185,11 @@ public class TagsImpl implements Tags
     public NodeRef validateTag(String tagId)
     {
     	NodeRef tagNodeRef = nodes.validateNode(tagId);
-		String tag = taggingService.getTagName(tagNodeRef);
-		if(!taggingService.isTag(storeRef,tag))
-		{
-			throw new EntityNotFoundException(tagId);
-		}
+    	String tag = taggingService.getTagName(tagNodeRef);
+    	if(!taggingService.isTag(tagNodeRef.getStoreRef(),tag))
+    	{
+    		throw new EntityNotFoundException(tagId);
+    	}
     	return tagNodeRef;
     }
     

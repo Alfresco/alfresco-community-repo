5ea42f353ae47619b77159a217f5cdc3b6f9617b
Eva Vasques <eva.vasques@gmail.com>
2023-02-20 15:06:16 +0000

    Added github action configuration for 7.2.1

---
+++ scripts/ci/init.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+echo "=========================== Starting Init Script ==========================="
+PS4="\[\e[35m\]+ \[\e[m\]"
+set -vex
+pushd "$(dirname "${BASH_SOURCE[0]}")/../../"
+
+# Maven Setup
+find "${HOME}/.m2/repository/" -type d -name "*-SNAPSHOT*" | xargs -r -l rm -rf
+
+# Docker Logins
+echo "${DOCKERHUB_PASSWORD}" | docker login -u="${DOCKERHUB_USERNAME}" --password-stdin
+echo "${QUAY_PASSWORD}" | docker login -u="${QUAY_USERNAME}" --password-stdin quay.io
+
+popd
+set +vex
+echo "=========================== Finishing Init Script =========================="
+

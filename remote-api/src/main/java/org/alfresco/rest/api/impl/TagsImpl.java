2166eec98bba4f3b27d933102234ae2dcd320dd2
pjoshi31 <piyush.joshi2@globallogic.com>
2023-02-20 19:28:06 +0530

    Issue Fixed and added unit test cases.

--- remote-api/src/main/java/org/alfresco/rest/api/impl/TagsImpl.java
+++ remote-api/src/main/java/org/alfresco/rest/api/impl/TagsImpl.java
@@ -179,7 +179,7 @@ public class TagsImpl implements Tags
         for (Pair<NodeRef, String> pair : page)
         {
             Tag selectedTag = new Tag(pair.getFirst(), pair.getSecond());
-            selectedTag.setCount(Optional.ofNullable(tagsByCountMap.get(selectedTag.getTag()).orElse(0));
+            selectedTag.setCount(Optional.ofNullable(tagsByCountMap.get(selectedTag.getTag())).orElse(0));
             tags.add(selectedTag);
         }
 

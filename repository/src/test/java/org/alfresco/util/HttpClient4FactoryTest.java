1860ea520eac50a0266f36c1f97ae279249607ce
kmagdziarz <Kacper.Magdziarz@hyland.com>
2023-03-07 10:28:40 +0100

    ACS-4459 add missing author header for HttpClient4FactoryTest

--- repository/src/test/java/org/alfresco/util/HttpClient4FactoryTest.java
+++ repository/src/test/java/org/alfresco/util/HttpClient4FactoryTest.java
@@ -1,25 +1,24 @@
-/*
+/*-
  * #%L
- * Alfresco Records Management Module
+ * Alfresco Repository
  * %%
  * Copyright (C) 2023 Alfresco Software Limited
  * %%
  * This file is part of the Alfresco software.
- * -
  * If the software was purchased under a paid Alfresco license, the terms of
  * the paid license agreement will prevail.  Otherwise, the software is
  * provided under the following open source license terms:
- * -
+ *
  * Alfresco is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * -
+ *
  * Alfresco is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * -
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -46,6 +45,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * Tests for {@link HttpClient4Factory}
+ *
+ * @author Kacper Magdziarz
+ */
 public class HttpClient4FactoryTest
 {
     @Mock

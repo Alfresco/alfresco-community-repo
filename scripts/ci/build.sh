c57228528facfdb1e655ce429a1ec0e1251757ea
Tiago Salvado <tiagosalvado10@gmail.com>
2023-03-07 19:05:33 +0000

    Added job rules as master branch. Cleanup

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -6,11 +6,6 @@ pushd "$(dirname "${BASH_SOURCE[0]}")/../../"
 
 source "$(dirname "${BASH_SOURCE[0]}")/build_functions.sh"
 
-# TODO
-#if isBranchBuild && [ "${BRANCH_NAME}" = "master" ] && [ "${BUILD_STAGE_NAME,,}" = "release" ] ; then
-  # update ":latest" image tags on remote repositories by using the maven *internal* profile
-#  PROFILES="-Pinternal"
-
 if [[ -n ${BUILD_PROFILES} ]]; then
   PROFILES="${BUILD_PROFILES}"
 elif [[ "${REQUIRES_LOCAL_IMAGES}" == "true" ]]; then

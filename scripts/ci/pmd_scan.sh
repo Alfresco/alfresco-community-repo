440de03eb9c274ba94778a46e3d380b5cf1fc063
Tom Page <thomas.page@alfresco.com>
2023-06-21 10:00:24 +0100

    PRODENG-222 Use standard settings for script.
    Make a copy of the PMD ruleset so that we

--- scripts/ci/pmd_scan.sh
+++ scripts/ci/pmd_scan.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+echo "=========================== Starting PMD Script ==========================="
+PS4="\[\e[35m\]+ \[\e[m\]"
+set -vex
 
 target_ref=$1
 head_ref=$2
@@ -6,15 +9,18 @@ head_ref=$2
 # Requires pmd/pmd-github-action to have been executed already, as this will download PMD and set up the changed file list.
 runPMD="/opt/hostedtoolcache/pmd/${PMD_VERSION}/x64/pmd-bin-${PMD_VERSION}/bin/run.sh"
 
+# Make a copy of the ruleset so that we ignore any changes between commits.
+cp pmd-ruleset.xml /tmp/pmd-ruleset.xml
+
 # Run PMD against the baseline commit.
 baseline_ref=$(git merge-base "${target_ref}" "${head_ref}")
 git checkout ${baseline_ref}
-${runPMD} pmd --cache pmd.cache --file-list pmd.filelist -R pmd-ruleset.xml -r old_report.txt
+${runPMD} pmd --cache pmd.cache --file-list pmd.filelist -R /tmp/pmd-ruleset.xml -r old_report.txt
 old_issue_count=$(cat old_report.txt | wc -l)
 
 # Rerun PMD against the PR head commit.
 git checkout ${head_ref}
-${runPMD} pmd --cache pmd.cache --file-list pmd.filelist -R pmd-ruleset.xml -r new_report.txt
+${runPMD} pmd --cache pmd.cache --file-list pmd.filelist -R /tmp/pmd-ruleset.xml -r new_report.txt
 new_issue_count=$(cat new_report.txt | wc -l)
 
 # Display the differences between the two files.
@@ -28,3 +34,6 @@ then
 else
     echo "Number of PMD issues in edited files went from ${old_issue_count} to ${new_issue_count}"
 fi
+
+set +vex
+echo "=========================== Finishing PMD Script =========================="

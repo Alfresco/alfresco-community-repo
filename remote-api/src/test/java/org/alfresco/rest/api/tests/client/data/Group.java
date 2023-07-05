91967e6f34b24cb5be1183f331dbc95b1d08aa51
MichalKinas <michal.kinas@hyland.com>
2023-07-05 19:34:46 +0200

    ACS-5506 Proper Group serialization

--- remote-api/src/test/java/org/alfresco/rest/api/tests/client/data/Group.java
+++ remote-api/src/test/java/org/alfresco/rest/api/tests/client/data/Group.java
@@ -58,7 +58,9 @@ public class Group extends org.alfresco.rest.api.model.Group implements Serializ
 
         AssertUtil.assertEquals("id", getId(), other.getId());
         AssertUtil.assertEquals("displayName", getDisplayName(), other.getDisplayName());
+        AssertUtil.assertEquals("description", getDescription(), other.getDescription());
         AssertUtil.assertEquals("isRoot", getIsRoot(), other.getIsRoot());
+        AssertUtil.assertEquals("hasSubgroups", getHasSubgroups(), other.getHasSubgroups());
         AssertUtil.assertEquals("parentIds", getParentIds(), other.getParentIds());
         AssertUtil.assertEquals("zones", getZones(), other.getZones());
     }
@@ -73,9 +75,19 @@ public class Group extends org.alfresco.rest.api.model.Group implements Serializ
 
         groupJson.put("displayName", getDisplayName());
 
+        if (getDescription() != null)
+        {
+            groupJson.put("description", getDescription());
+        }
+
         if (getIsRoot() != null)
         {
             groupJson.put("isRoot", getIsRoot());
+        }
+
+        if (getHasSubgroups() != null)
+        {
+            groupJson.put("hasSubgroups", getHasSubgroups());
         }
 
         if (getParentIds() != null)
@@ -97,6 +109,7 @@ public class Group extends org.alfresco.rest.api.model.Group implements Serializ
         String displayName = (String) jsonObject.get("displayName");
         String description = (String) jsonObject.get("description");
         Boolean isRoot = (Boolean) jsonObject.get("isRoot");
+        Boolean hasSubgroups = (Boolean) jsonObject.get("hasSubgroups");
         List<String> parentIds = (List<String>) jsonObject.get("parentIds");
         List<String> zones = (List<String>) jsonObject.get("zones");
 
@@ -105,6 +118,7 @@ public class Group extends org.alfresco.rest.api.model.Group implements Serializ
         group.setDisplayName(displayName);
         group.setDescription(description);
         group.setIsRoot(isRoot);
+        group.setHasSubgroups(hasSubgroups);
         group.setParentIds(parentIds != null ? new HashSet<>(parentIds) : null);
         group.setZones(zones != null ? new HashSet<>(zones) : null);
 

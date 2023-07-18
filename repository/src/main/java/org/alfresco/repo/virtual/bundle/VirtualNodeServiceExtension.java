69de2c48515844814323eb30eb28cf31b5d883e4
Oskar Rajzner <oskar.rajzner@hyland.com>
2023-07-18 08:35:22 +0200

    ACS-5449 Update VirtualNodeServiceExtension

--- repository/src/main/java/org/alfresco/repo/virtual/bundle/VirtualNodeServiceExtension.java
+++ repository/src/main/java/org/alfresco/repo/virtual/bundle/VirtualNodeServiceExtension.java
@@ -2,7 +2,7 @@
  * #%L
  * Alfresco Repository
  * %%
- * Copyright (C) 2005 - 2022 Alfresco Software Limited
+ * Copyright (C) 2005 - 2023 Alfresco Software Limited
  * %%
  * This file is part of the Alfresco software. 
  * If the software was purchased under a paid Alfresco license, the terms of 
@@ -26,7 +26,6 @@
 
 package org.alfresco.repo.virtual.bundle;
 
-import java.io.IOException;
 import java.io.Serializable;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;

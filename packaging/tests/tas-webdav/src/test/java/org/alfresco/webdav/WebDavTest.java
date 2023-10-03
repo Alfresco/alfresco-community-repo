ed0f79c1b956386c879f6dac77e829480ec5e907
Eva Vasques <eva.vasques@gmail.com>
2023-10-03 15:36:13 +0100

    Correct WebDavTest

--- packaging/tests/tas-webdav/src/test/java/org/alfresco/webdav/WebDavTest.java
+++ packaging/tests/tas-webdav/src/test/java/org/alfresco/webdav/WebDavTest.java
@@ -1,12 +1,18 @@
 package org.alfresco.webdav;
 
+import java.lang.reflect.Method;
+
+import org.alfresco.utility.LogFactory;
 import org.alfresco.utility.data.DataContent;
 import org.alfresco.utility.data.DataSite;
 import org.alfresco.utility.data.DataUser;
 import org.alfresco.utility.network.ServerHealth;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
 @ContextConfiguration("classpath:alfresco-webdav-context.xml")

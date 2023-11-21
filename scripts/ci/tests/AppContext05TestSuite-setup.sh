f41d8b37ac0c1d31cee49baeca9f2885509db20a
Damian.Ujma@hyland.com <Damian.Ujma@hyland.com>
2023-11-21 16:31:29 +0100

    ACS-6303 Revert local change

--- scripts/ci/tests/AppContext05TestSuite-setup.sh
+++ scripts/ci/tests/AppContext05TestSuite-setup.sh
@@ -7,7 +7,7 @@ pushd "$(dirname "${BASH_SOURCE[0]}")/../../../"
 
 mkdir -p "${HOME}/tmp"
 cp repository/src/test/resources/realms/alfresco-realm.json "${HOME}/tmp"
-HOST_IP=192.168.0.178
+echo "HOST_IP=$(hostname  -I | cut -f1 -d' ')" >> $GITHUB_ENV
 docker run -d -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin -e DB_VENDOR=h2 -p 8999:8080 -e KEYCLOAK_IMPORT=/tmp/alfresco-realm.json -v $HOME/tmp/alfresco-realm.json:/tmp/alfresco-realm.json alfresco/alfresco-identity-service:1.2
 
 popd

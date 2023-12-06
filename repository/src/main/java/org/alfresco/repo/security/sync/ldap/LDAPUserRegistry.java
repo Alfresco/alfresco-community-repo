db09eb9993eaf41051bf556d387addccc2673c8a
pksingh <PraveenKumar.Singh@hyland.com>
2023-12-06 16:13:06 +0530

    IDS - fixing sync LDAP accounts issues

--- repository/src/main/java/org/alfresco/repo/security/sync/ldap/LDAPUserRegistry.java
+++ repository/src/main/java/org/alfresco/repo/security/sync/ldap/LDAPUserRegistry.java
@@ -1709,15 +1709,15 @@ public class LDAPUserRegistry implements UserRegistry, LDAPNameResolver, Initial
                     readyForNextPage = this.searchResults == null;
                     while (!readyForNextPage && this.searchResults.hasMore())
                     {
-                        SearchResult result = this.searchResults.nextElement();
+                        SearchResult result = this.searchResults.next();
                         Attributes attributes = result.getAttributes();
                         Attribute uidAttribute = attributes.get(LDAPUserRegistry.this.userIdAttributeName);
-                        Attribute uidAttributes = result.getAttributes().get(userIdAttributeName);
-                        if(uidAttribute== null || uidAttributes!= null)
+                        Attribute uidAttributes = result.getAttributes().get(LDAPUserRegistry.this.userIdAttributeName);
+                        if(uidAttribute == null || uidAttributes != null)
                         {
                             if (LDAPUserRegistry.this.errorOnMissingUID)
                             {
-                                Object[] params = {result.getName(), LDAPUserRegistry.this.userIdAttributeName};
+                                Object[] params = {result.getNameInNamespace(), LDAPUserRegistry.this.userIdAttributeName};
                                 throw new AlfrescoRuntimeException("synchronization.err.ldap.get.user.id.missing", params);
                             }
                             else

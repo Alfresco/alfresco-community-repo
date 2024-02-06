46239c39bb7d22d99272d722a4eeab82937ed881
Wojtek.Swieton <wojtek.swieton@hyland.com>
2024-02-06 13:03:41 +0100

    ACS-6309 Check db and tas tests ags on MySQL ags add listeners

--- packaging/tests/scripts/output_logs_for_failures.sh
+++ packaging/tests/scripts/output_logs_for_failures.sh
@@ -4,6 +4,10 @@ TAS_DIRECTORY=$1
 
 cd ${TAS_DIRECTORY}
 
+# Add command to list all files in /target directory
+echo "Listing all files in /target directory:"
+ls -l target/
+
 failures=$(grep 'status="FAIL"' target/surefire-reports/testng-results.xml | sed 's|^.*[ ]name="\([^"]*\)".*$|\1|g')
 
 for failure in ${failures}

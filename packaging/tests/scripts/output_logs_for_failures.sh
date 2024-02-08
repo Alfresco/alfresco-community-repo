aa4f353f28fd54b6281d374cead1b4af98c81273
Wojtek.Swieton <wojtek.swieton@hyland.com>
2024-02-08 21:29:42 +0100

    ACS-6309 Check db and tas tests ags on MySQL ags add listeners

--- packaging/tests/scripts/output_logs_for_failures.sh
+++ packaging/tests/scripts/output_logs_for_failures.sh
@@ -4,45 +4,6 @@ TAS_DIRECTORY=$1
 
 cd ${TAS_DIRECTORY}
 
-# Add command to list all files in /target directory
-echo "Listing all files in /target directory:"
-ls -l target/
-
-echo "" # This adds an empty line for clarity
-
-# Add command to list all files in /target/surefire-reports directory
-echo "Listing all files in /target/surefire-reports directory:"
-ls -l target/surefire-reports/
-
-echo "" # This adds an empty line for clarity
-
-
-echo "" # This adds an empty line for clarity
-
-# Add command to list all files in /target/surefire-reports/testng-junit-results directory
-echo "Listing all files in /target/surefire-reports/testng-junit-results directory:"
-ls -l target/surefire-reports/testng-junit-results
-
-echo "" # This adds an empty line for clarity
-
-# Add command to list all files in /target/surefire-reports/testng-junit-results/Surefire suite directory
-echo "Listing all files in /target/surefire-reports/testng-junit-results/Surefire suite directory:"
-ls -l "target/surefire-reports/testng-junit-results/Surefire suite"
-
-echo "" # This adds an empty line for clarity
-
-echo "Print : Surefire test.xml"
-cat "target/surefire-reports/testng-junit-results/Surefire suite/Surefire test.xml"
-
-echo "" # This adds an empty line for clarity
-
-# Add command to list all files in /target/surefire-reports/testng-native-results directory
-echo "Listing all files in /target/surefire-reports/testng-native-results directory:"
-ls -l target/surefire-reports/testng-native-results
-
-echo "" # This adds an empty line for clarity
-
-
 failures=$(grep 'status="FAIL"' target/surefire-reports/testng-results.xml | sed 's|^.*[ ]name="\([^"]*\)".*$|\1|g')
 
 for failure in ${failures}

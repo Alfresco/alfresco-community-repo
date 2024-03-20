6a14f0ed9d81bcc9bb222c5564c78852f90c117c
Wojtek.Swieton <wojtek.swieton@hyland.com>
2024-03-20 16:19:29 +0100

    ACS-6309 Try to run using a TestSuite

--- data-model/src/test/java/org/alfresco/AllDataModelTestSuite.java
+++ data-model/src/test/java/org/alfresco/AllDataModelTestSuite.java
@@ -7,9 +7,9 @@ import org.junit.runners.Suite;
 @RunWith(Categories.class)
 @Suite.SuiteClasses({
         org.alfresco.opencmis.dictionary.CMISAbstractDictionaryServiceTest.class,
-        org.alfresco.repo.content.encoding.CharsetFinderTest.class,
-        //org.alfresco.repo.content.DataModelContentTestSuite.class,
-        org.alfresco.repo.content.MimetypeMapTest.class,
+        //org.alfresco.repo.content.encoding.CharsetFinderTest.class,
+        org.alfresco.repo.content.DataModelContentTestSuite.class,
+        //org.alfresco.repo.content.MimetypeMapTest.class,
         org.alfresco.repo.dictionary.AbstractModelTest.class,
         org.alfresco.repo.dictionary.DictionaryComponentTest.class,
         org.alfresco.repo.dictionary.DictionaryDAOTest.class,
@@ -19,7 +19,6 @@ import org.junit.runners.Suite;
         org.alfresco.repo.search.impl.parsers.CMIS_FTSTest.class,
         org.alfresco.repo.search.impl.parsers.CMISTest.class,
         org.alfresco.repo.search.impl.parsers.FTSTest.class,
-        //org.alfresco.repo.search.impl.parsers.gUnitExecutor.class,
         org.alfresco.repo.security.authentication.InMemoryTicketComponentTest.class,
         org.alfresco.service.cmr.repository.datatype.DefaultTypeConverterTest.class,
         org.alfresco.service.cmr.repository.MLTextTest.class,

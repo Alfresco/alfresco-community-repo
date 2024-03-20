0b96ca780634734e72eca3038f67138cabae7886
Wojtek.Swieton <wojtek.swieton@hyland.com>
2024-03-20 14:26:15 +0100

    ACS-6309 Try to run using a TestSuite

--- data-model/src/test/java/org/alfresco/AllDataModelTestSuite.java
+++ data-model/src/test/java/org/alfresco/AllDataModelTestSuite.java
@@ -8,7 +8,34 @@ import org.junit.runners.Suite;
 @Suite.SuiteClasses({
         org.alfresco.opencmis.dictionary.CMISAbstractDictionaryServiceTest.class,
         org.alfresco.repo.content.encoding.CharsetFinderTest.class,
-        org.alfresco.repo.content.DataModelContentTestSuite.class
+        org.alfresco.repo.content.DataModelContentTestSuite.class,
+        org.alfresco.repo.content.MimetypeMapTest.class,
+        org.alfresco.repo.dictionary.AbstractModelTest.class,
+        org.alfresco.repo.dictionary.DictionaryComponentTest.class,
+        org.alfresco.repo.dictionary.DictionaryDAOTest.class,
+        org.alfresco.repo.dictionary.DiffModelTest.class,
+        org.alfresco.repo.dictionary.constraint.ConstraintsTest.class,
+        org.alfresco.repo.index.ShardMethodEnumTest.class,
+        org.alfresco.repo.search.impl.parsers.CMIS_FTSTest.class,
+        org.alfresco.repo.search.impl.parsers.CMISTest.class,
+        org.alfresco.repo.search.impl.parsers.FTSTest.class,
+        org.alfresco.repo.search.impl.parsers.gUnitExecutor.class,
+        org.alfresco.repo.security.authentication.InMemoryTicketComponentTest.class,
+        org.alfresco.service.cmr.repository.datatype.DefaultTypeConverterTest.class,
+        org.alfresco.service.cmr.repository.MLTextTest.class,
+        org.alfresco.service.cmr.repository.NodeRefTest.class,
+        org.alfresco.service.cmr.repository.PathTest.class,
+        org.alfresco.service.cmr.repository.PeriodTest.class,
+        org.alfresco.service.cmr.search.StatsProcessorTest.class,
+        org.alfresco.service.namespace.DynamicNameSpaceResolverTest.class,
+        org.alfresco.service.namespace.QNamePatternTest.class,
+        org.alfresco.service.namespace.QNameTest.class,
+        org.alfresco.util.ConfigFileFinderTest.class,
+        org.alfresco.util.ConfigSchedulerTest.class,
+        org.alfresco.util.ISO9075Test.class,
+        org.alfresco.util.NumericEncodingTest.class,
+        org.alfresco.util.SearchDateConversionTest.class,
+        org.alfresco.util.SearchLanguageConversionTest.class
 })
 public class AllDataModelTestSuite {
 }

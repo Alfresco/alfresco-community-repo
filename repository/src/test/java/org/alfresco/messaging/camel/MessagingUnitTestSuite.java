9ddf70ba743518e2a06c6d1b3f36122bd9244e3b
Wojtek.Swieton <wojtek.swieton@hyland.com>
2024-03-25 11:26:35 +0100

    ACS-6309 Try to run using a TestSuite - messaging unit tests

--- repository/src/test/java/org/alfresco/messaging/camel/MessagingUnitTestSuite.java
+++ repository/src/test/java/org/alfresco/messaging/camel/MessagingUnitTestSuite.java
@@ -1,7 +1,13 @@
 /*
- * Copyright (C) 2005-2016 Alfresco Software Limited.
- *
- * This file is part of Alfresco
+ * #%L
+ * Alfresco Repository
+ * %%
+ * Copyright (C) 2005 - 2018 Alfresco Software Limited
+ * %%
+ * This file is part of the Alfresco software.
+ * If the software was purchased under a paid Alfresco license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
+ * provided under the following open source license terms:
  *
  * Alfresco is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -15,6 +21,7 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
  */
 package org.alfresco;
 

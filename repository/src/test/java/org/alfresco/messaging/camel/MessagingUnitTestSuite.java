6051acba98e71f205225fa3596ede54e2bdb595d
Wojtek.Swieton <wojtek.swieton@hyland.com>
2024-03-25 10:22:31 +0100

    ACS-6309 Try to run using a TestSuite - messaging unit tests

---
+++ repository/src/test/java/org/alfresco/messaging/camel/MessagingUnitTestSuite.java
@@ -0,0 +1,13 @@
+package org.alfresco.messaging.camel;
+
+import org.junit.experimental.categories.Categories;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Categories.class)
+@Suite.SuiteClasses({
+        org.alfresco.messaging.camel.CamelRoutesTest.class,
+        org.alfresco.messaging.camel.CamelComponentsTest.class
+})
+public class MessagingUnitTestSuite {
+}

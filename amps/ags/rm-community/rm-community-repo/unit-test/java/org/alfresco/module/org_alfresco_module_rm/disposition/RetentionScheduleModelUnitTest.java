8db5782e83118e5d0ae48322b8303dfcc7eda9ba
Sathish Kumar <ST28@ford.com>
2024-06-26 13:30:55 +0530

    [APPS-2836][APPS-2837] POST and GET API for Retention Schedule

--- amps/ags/rm-community/rm-community-repo/unit-test/java/org/alfresco/module/org_alfresco_module_rm/disposition/RetentionScheduleModelUnitTest.java
+++ amps/ags/rm-community/rm-community-repo/unit-test/java/org/alfresco/module/org_alfresco_module_rm/disposition/RetentionScheduleModelUnitTest.java
@@ -33,14 +33,10 @@ import org.alfresco.rm.rest.api.model.RetentionSchedule;
 import org.alfresco.rm.rest.api.model.RetentionScheduleActionDefinition;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.Period;
-import org.alfresco.service.namespace.QName;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 
-import java.util.List;
-
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -61,12 +57,13 @@ public class RetentionScheduleModelUnitTest extends BaseUnitTest
     {
         // Mock data
         DispositionSchedule dispositionSchedule = mock(DispositionSchedule.class);
+        RetentionSchedule retentionSchedule = mock(RetentionSchedule.class);
         NodeRef nodeRef = generateNodeRef(RecordsManagementModel.TYPE_DISPOSITION_SCHEDULE, true);
         when(dispositionSchedule.getDispositionAuthority()).thenReturn(AUTHORITY);
         when(dispositionSchedule.getDispositionInstructions()).thenReturn(INSTRUCTIONS);
         when(dispositionSchedule.getNodeRef()).thenReturn(nodeRef);
         when(dispositionSchedule.isRecordLevelDisposition()).thenReturn(false);
-        when(apiNodesModelFactory.mapRetentionScheduleData(dispositionSchedule)).thenReturn(mock(RetentionSchedule.class));
+        when(apiNodesModelFactory.mapRetentionScheduleData(dispositionSchedule)).thenReturn(retentionSchedule);
         // Call the method
         RetentionSchedule result = apiNodesModelFactory.mapRetentionScheduleData(dispositionSchedule);
 
@@ -83,14 +80,12 @@ public class RetentionScheduleModelUnitTest extends BaseUnitTest
         DispositionActionDefinition dispositionActionDefinition = mock(DispositionActionDefinition.class);
         NodeRef nodeRef = generateNodeRef(RecordsManagementModel.TYPE_DISPOSITION_SCHEDULE, true);
         String period = "month|10";
-        QName mockQName = mock(QName.createQName("qname"));
         when(dispositionActionDefinition.getNodeRef()).thenReturn(nodeRef);
         when(dispositionActionDefinition.getName()).thenReturn(RETAIN_STEP);
         when(dispositionActionDefinition.getDescription()).thenReturn("Description");
         when(dispositionActionDefinition.getIndex()).thenReturn(1);
         when(dispositionActionDefinition.getGhostOnDestroy()).thenReturn("ghost");
         when(dispositionActionDefinition.getPeriod()).thenReturn(new Period(period));
-        when(dispositionActionDefinition.getPeriodProperty()).thenReturn(mockQName);
         when(dispositionActionDefinition.getLocation()).thenReturn("location");
         // Call the method
         RetentionScheduleActionDefinition result = apiNodesModelFactory.mapRetentionScheduleActionDefData(dispositionActionDefinition);
@@ -103,19 +98,5 @@ public class RetentionScheduleModelUnitTest extends BaseUnitTest
         assertEquals(dispositionActionDefinition.getLocation(),result.getLocation());
         assertEquals(dispositionActionDefinition.getPeriodProperty().getLocalName(),result.getPeriodProperty());
         assertEquals(dispositionActionDefinition.getPeriod(),new Period(resultPeriod));
-    }
-
-    @Test
-    public void mapRetentionScheduleOptionalInfoTest()
-    {
-        // Mock data
-        RetentionSchedule retentionSchedule = mock(RetentionSchedule.class);
-        DispositionSchedule schedule = mock(DispositionSchedule.class);
-        List<String> includeParam = List.of("actions");
-        when(schedule.getDispositionActionDefinitions()).thenReturn(List.of(mock(DispositionActionDefinition.class)));
-        // Call the method
-        apiNodesModelFactory.mapRetentionScheduleOptionalInfo(retentionSchedule, schedule, includeParam);
-        // Assertions
-        assertNotNull(retentionSchedule);
     }
 }

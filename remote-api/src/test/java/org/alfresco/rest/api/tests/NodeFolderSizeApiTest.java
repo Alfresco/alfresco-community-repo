2deb4f428fc8a008738753cdebcd594e725dc6b7
mohit-singh4 <mohit.singh@hyland.com>
2024-06-27 10:41:58 +0530

    [MNT-24127] Added Endpoint to Calculate Size of the folder with Integration Test Cases [ags][tas]

--- remote-api/src/test/java/org/alfresco/rest/api/tests/NodeFolderSizeApiTest.java
+++ remote-api/src/test/java/org/alfresco/rest/api/tests/NodeFolderSizeApiTest.java
@@ -39,10 +39,7 @@ import org.junit.Test;
 import org.junit.runners.MethodSorters;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 

f2870ee8168a3f84fb14382f1bca1bae6d6561c7
Kacper Magdziarz <kacper.magdziarz@hyland.com>
2024-07-25 10:58:05 +0200

    [ACS-8474] Add new bean for intercepting current audit entries.

--- repository/src/main/java/org/alfresco/repo/audit/AuditComponentImpl.java
+++ repository/src/main/java/org/alfresco/repo/audit/AuditComponentImpl.java
@@ -78,6 +78,7 @@ public class AuditComponentImpl implements AuditComponent
     private AuditModelRegistryImpl auditModelRegistry;
     private PropertyValueDAO propertyValueDAO;
     private AuditDAO auditDAO;
+    private AuditOutboxSystem auditOutboxSystem;
     private TransactionService transactionService;
     private AuditFilter auditFilter;
     private UserAuditFilter userAuditFilter;
@@ -114,6 +115,11 @@ public class AuditComponentImpl implements AuditComponent
     public void setAuditDAO(AuditDAO auditDAO)
     {
         this.auditDAO = auditDAO;
+    }
+
+    public void setAuditOutboxSystem(AuditOutboxSystem auditOutboxSystem)
+    {
+        this.auditOutboxSystem = auditOutboxSystem;
     }
 
     /**
@@ -750,6 +756,7 @@ public class AuditComponentImpl implements AuditComponent
             if (!justGatherPreCallData)
             {
                 entryId = auditDAO.createAuditEntry(applicationId, time, username, auditData);
+                auditOutboxSystem.createAuditRepoEvent(applicationId, time, username, auditData);
             }
             // Done
             if (logger.isDebugEnabled())

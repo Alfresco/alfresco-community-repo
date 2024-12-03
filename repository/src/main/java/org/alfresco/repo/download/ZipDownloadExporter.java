32c3b426f60843666fbf98e0006a2e9fb285d4cc
mohit-singh4 <mohit.singh@contractors.hyland.com>
2024-12-03 12:03:56 +0530

    [fix/MNT-24209-logging-for-download-restapi] Adding loggers in downloads RestAPI

--- repository/src/main/java/org/alfresco/repo/download/ZipDownloadExporter.java
+++ repository/src/main/java/org/alfresco/repo/download/ZipDownloadExporter.java
@@ -4,21 +4,21 @@
  * %%
  * Copyright (C) 2005 - 2024 Alfresco Software Limited
  * %%
- * This file is part of the Alfresco software. 
- * If the software was purchased under a paid Alfresco license, the terms of 
- * the paid license agreement will prevail.  Otherwise, the software is 
+ * This file is part of the Alfresco software.
+ * If the software was purchased under a paid Alfresco license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
  * provided under the following open source license terms:
- * 
+ *
  * Alfresco is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Alfresco is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -114,7 +114,10 @@ public class ZipDownloadExporter extends BaseExporter
      * @param totalFileCount
      *            long
      */
-    public ZipDownloadExporter(File zipFile, CheckOutCheckInService checkOutCheckInService, NodeService nodeService, RetryingTransactionHelper transactionHelper, DownloadStatusUpdateService updateService, DownloadStorage downloadStorage, DictionaryService dictionaryService, NodeRef downloadNodeRef, long total, long totalFileCount)
+    public ZipDownloadExporter(File zipFile, CheckOutCheckInService checkOutCheckInService, NodeService nodeService,
+            RetryingTransactionHelper transactionHelper, DownloadStatusUpdateService updateService,
+            DownloadStorage downloadStorage, DictionaryService dictionaryService,
+            NodeRef downloadNodeRef, long total, long totalFileCount)
     {
         super(checkOutCheckInService, nodeService);
         try
@@ -188,7 +191,8 @@ public class ZipDownloadExporter extends BaseExporter
         {
             if (log.isDebugEnabled())
             {
-                log.debug("Archiving content for nodeRef: " + nodeRef + " with contentURL: " + contentData.getContentUrl());
+                log.debug("Archiving content for nodeRef: " + nodeRef + " with contentURL: "
+                        + contentData.getContentUrl());
             }
             // ALF-2016
             ZipArchiveEntry zipEntry = new ZipArchiveEntry(getPath());
@@ -261,8 +265,7 @@ public class ZipDownloadExporter extends BaseExporter
      *            input stream
      * @throws IOException
      */
-    private void copyStream(OutputStream output, InputStream in)
-            throws IOException
+    private void copyStream(OutputStream output, InputStream in) throws IOException
     {
         byte[] buffer = new byte[2048 * 10];
         int read = in.read(buffer, 0, 2048 * 10);

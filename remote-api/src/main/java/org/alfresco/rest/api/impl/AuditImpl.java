d0de18cb4c5e3a0ee7abe59e9644dedf310c95fa
Eva Vasques <eva.vasques@gmail.com>
2024-12-17 15:10:26 +0000

    Revert "[MNT-24137] Audit Issue Internal Server Error fix (#2786)"
    This reverts commit cabc38b386cbb400a901047e7a101b619107d31b.

--- remote-api/src/main/java/org/alfresco/rest/api/impl/AuditImpl.java
+++ remote-api/src/main/java/org/alfresco/rest/api/impl/AuditImpl.java
@@ -2,7 +2,7 @@
  * #%L
  * Alfresco Remote API
  * %%
- * Copyright (C) 2005 - 2024 Alfresco Software Limited
+ * Copyright (C) 2005 - 2016 Alfresco Software Limited
  * %%
  * This file is part of the Alfresco software. 
  * If the software was purchased under a paid Alfresco license, the terms of 
@@ -909,14 +909,16 @@ public class AuditImpl implements Audit
 
     public int getAuditEntriesCountByAppAndProperties(AuditService.AuditApplication auditApplication, AuditEntryQueryWalker propertyWalker)
     {
+        final String applicationName = auditApplication.getKey().substring(1);
+
         AuditQueryParameters parameters = new AuditQueryParameters();
-        parameters.setApplicationName(auditApplication.getName());
+        parameters.setApplicationName(applicationName);
         parameters.setFromTime(propertyWalker.getFromTime());
         parameters.setToTime(propertyWalker.getToTime());
         parameters.setFromId(propertyWalker.getFromId());
         parameters.setToId(propertyWalker.getToId());
         parameters.setUser(propertyWalker.getCreatedByUser());
 
-        return auditService.getAuditEntriesCountByAppAndProperties(parameters);
+        return auditService.getAuditEntriesCountByAppAndProperties(applicationName, parameters);
     }
 }

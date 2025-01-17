305342b5dca462a9487f6ab8cca27ca05f7827ec
cezary-witkowski <cezary.witkowski@hyland.com>
2025-01-17 11:08:33 +0100

    [MNT-24807] Updated year in licence for some files I missed
    Signed-off-by: cezary-witkowski <cezary.witkowski@hyland.com>

--- repository/src/main/java/org/alfresco/repo/event2/filter/AbstractNodeEventFilter.java
+++ repository/src/main/java/org/alfresco/repo/event2/filter/AbstractNodeEventFilter.java
@@ -2,7 +2,7 @@
  * #%L
  * Alfresco Repository
  * %%
- * Copyright (C) 2005 - 2022 Alfresco Software Limited
+ * Copyright (C) 2005 - 2025 Alfresco Software Limited
  * %%
  * This file is part of the Alfresco software.
  * If the software was purchased under a paid Alfresco license, the terms of
@@ -35,8 +35,7 @@ import org.alfresco.repo.event2.shared.TypeDefExpander;
 import org.alfresco.service.namespace.QName;
 
 /**
- * Abstract {@link EventFilter} implementation, containing common event filtering
- * functionality for the {@link QName} type.
+ * Abstract {@link EventFilter} implementation, containing common event filtering functionality for the {@link QName} type.
  *
  * @author Jamal Kaabi-Mofrad
  */

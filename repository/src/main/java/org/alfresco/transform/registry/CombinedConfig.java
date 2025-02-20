fb4040793cbe50bc85e11ae71fdd1d700185361e
jaikant79 <Jaikant.Singh@hyland.com>
2025-02-20 14:25:55 +0530

    logger corrections

--- repository/src/main/java/org/alfresco/transform/registry/CombinedConfig.java
+++ repository/src/main/java/org/alfresco/transform/registry/CombinedConfig.java
@@ -169,6 +169,7 @@ public class CombinedConfig extends CombinedTransformConfig
                 throw new AlfrescoRuntimeException("This error was generated because the engine requires Transform AIO, but it is not being used " +remoteType+
                         " on " + url, e);
             }
+
         }
         catch (AlfrescoRuntimeException e)
         {

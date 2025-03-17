ad3981299925c5b3146c322fe587fdb5d3ab1b85
Tiago Salvado <tiagosalvado10@gmail.com>
2025-03-17 16:38:50 +0000

    Trigger CI

--- amps/ags/rm-community/rm-community-repo/unit-test/java/org/alfresco/rm/rest/api/sites/RMSiteEntityResourceUnitTest.java
+++ amps/ags/rm-community/rm-community-repo/unit-test/java/org/alfresco/rm/rest/api/sites/RMSiteEntityResourceUnitTest.java
@@ -23,7 +23,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
  * #L%
- */
+ */  
 
 package org.alfresco.rm.rest.api.sites;
 

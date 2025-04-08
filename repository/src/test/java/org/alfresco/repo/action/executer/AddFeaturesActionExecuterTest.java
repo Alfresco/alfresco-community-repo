0cd77397e772e1979553ce52749377d5dd487113
Belal Ansari <belal.ansari@hyland.com>
2025-04-08 18:23:16 +0530

    fix/MNT-24891-BackPort - Skip actionContext to classify as an adhoc property in add-features action logic

--- repository/src/test/java/org/alfresco/repo/action/executer/AddFeaturesActionExecuterTest.java
+++ repository/src/test/java/org/alfresco/repo/action/executer/AddFeaturesActionExecuterTest.java
@@ -4,21 +4,21 @@
  * %%
  * Copyright (C) 2005 - 2025 Alfresco Software Limited
  * %%
- * This file is part of the Alfresco software. 
- * If the software was purchased under a paid Alfresco license, the terms of 
- * the paid license agreement will prevail.  Otherwise, the software is 
+ * This file is part of the Alfresco software.
+ * If the software was purchased under a paid Alfresco license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
  * provided under the following open source license terms:
- * 
+ *
  * Alfresco is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Alfresco is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -27,6 +27,11 @@ package org.alfresco.repo.action.executer;
 
 import java.util.List;
 import java.util.Locale;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.extensions.surf.util.I18NUtil;
+import org.springframework.transaction.annotation.Transactional;
 
 import org.alfresco.model.ContentModel;
 import org.alfresco.repo.action.ActionImpl;
@@ -40,14 +45,10 @@ import org.alfresco.service.cmr.repository.StoreRef;
 import org.alfresco.service.namespace.QName;
 import org.alfresco.util.BaseSpringTest;
 import org.alfresco.util.GUID;
-import org.junit.Before;
-import org.junit.Test;
-import org.springframework.extensions.surf.util.I18NUtil;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Add features action execution test
- * 
+ *
  * @author Roy Wetherall
  */
 @Transactional
@@ -57,43 +58,43 @@ public class AddFeaturesActionExecuterTest extends BaseSpringTest
      * The node service
      */
     private NodeService nodeService;
-    
+
     /**
      * The store reference
      */
     private StoreRef testStoreRef;
-    
+
     /**
      * The root node reference
      */
     private NodeRef rootNodeRef;
-    
+
     /**
      * The test node reference
      */
     private NodeRef nodeRef;
-    
+
     /**
      * The add features action executer
      */
     private AddFeaturesActionExecuter executer;
-    
+
     /**
      * Id used to identify the test action created
      */
     private final static String ID = GUID.generate();
-    
+
     /**
      * Called at the begining of all tests
      */
     @Before
     public void before() throws Exception
     {
-        this.nodeService = (NodeService)this.applicationContext.getBean("nodeService");
-        
-        AuthenticationComponent authenticationComponent = (AuthenticationComponent)applicationContext.getBean("authenticationComponent");
+        this.nodeService = (NodeService) this.applicationContext.getBean("nodeService");
+
+        AuthenticationComponent authenticationComponent = (AuthenticationComponent) applicationContext.getBean("authenticationComponent");
         authenticationComponent.setCurrentUser(authenticationComponent.getSystemUserName());
-        
+
         // Create the store and get the root node
         this.testStoreRef = this.nodeService.createStore(
                 StoreRef.PROTOCOL_WORKSPACE, "Test_"
@@ -106,11 +107,11 @@ public class AddFeaturesActionExecuterTest extends BaseSpringTest
                 ContentModel.ASSOC_CHILDREN,
                 QName.createQName("{test}testnode"),
                 ContentModel.TYPE_CONTENT).getChildRef();
-        
-        // Get the executer instance 
-        this.executer = (AddFeaturesActionExecuter)this.applicationContext.getBean(AddFeaturesActionExecuter.NAME);
+
+        // Get the executer instance
+        this.executer = (AddFeaturesActionExecuter) this.applicationContext.getBean(AddFeaturesActionExecuter.NAME);
     }
-    
+
     /**
      * Test execution
      */
@@ -119,16 +120,16 @@ public class AddFeaturesActionExecuterTest extends BaseSpringTest
     {
         // Check that the node does not have the classifiable aspect
         assertFalse(this.nodeService.hasAspect(this.nodeRef, ContentModel.ASPECT_CLASSIFIABLE));
-        
+
         // Execute the action
         ActionImpl action = new ActionImpl(null, ID, AddFeaturesActionExecuter.NAME, null);
         action.setParameterValue(AddFeaturesActionExecuter.PARAM_ASPECT_NAME, ContentModel.ASPECT_CLASSIFIABLE);
         this.executer.execute(action, this.nodeRef);
-        
+
         // Check that the node now has the classifiable aspect applied
         assertTrue(this.nodeService.hasAspect(this.nodeRef, ContentModel.ASPECT_CLASSIFIABLE));
     }
-    
+
     /**
      * MNT-15802
      */

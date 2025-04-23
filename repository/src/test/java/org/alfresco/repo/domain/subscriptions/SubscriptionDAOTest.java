81bcc1a02357f15b3902118f69d9cb9ec125d1b7
Kushal Banik <kushal.banik@hyland.com>
2025-04-23 17:29:12 +0530

    [MNT-24490] precommits fixed

--- repository/src/test/java/org/alfresco/repo/domain/subscriptions/SubscriptionDAOTest.java
+++ repository/src/test/java/org/alfresco/repo/domain/subscriptions/SubscriptionDAOTest.java
@@ -2,7 +2,7 @@
  * #%L
  * Alfresco Repository
  * %%
- * Copyright (C) 2005 - 2016 Alfresco Software Limited
+ * Copyright (C) 2005 - 2025 Alfresco Software Limited
  * %%
  * This file is part of the Alfresco software. 
  * If the software was purchased under a paid Alfresco license, the terms of 
@@ -26,7 +26,6 @@
 package org.alfresco.repo.domain.subscriptions;
 
 import junit.framework.TestCase;
-import org.alfresco.util.testing.category.DBTests;
 import org.junit.experimental.categories.Category;
 import org.springframework.context.ApplicationContext;
 
@@ -44,8 +43,7 @@ import org.alfresco.service.cmr.subscriptions.SubscriptionItemTypeEnum;
 import org.alfresco.service.transaction.TransactionService;
 import org.alfresco.test_category.OwnJVMTestsCategory;
 import org.alfresco.util.ApplicationContextHelper;
-import org.alfresco.util.testing.category.NeverRunsTests;
-
+import org.alfresco.util.testing.category.DBTests;
 @Category({OwnJVMTestsCategory.class, DBTests.class})
 public class SubscriptionDAOTest extends TestCase
 {

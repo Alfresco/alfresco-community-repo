c048be0e45ad86766948e1439cc168ab4c440497
SatyamSah5 <Satyam.Sah@hyland.com>
2025-05-19 12:45:51 +0530

    pre commit fix windows os

--- repository/src/main/java/org/alfresco/service/cmr/model/FileExistsException.java
+++ repository/src/main/java/org/alfresco/service/cmr/model/FileExistsException.java
@@ -40,14 +40,14 @@ public class FileExistsException extends AlfrescoRuntimeException
     private static final String MESSAGE_ID = "file_folder_service.file_exists_message";
 
     private static final long serialVersionUID = -4133713912784624118L;
-    
+
     private NodeRef parentNodeRef;
     private String name;
     private String type;
 
     public FileExistsException(NodeRef parentNodeRef, String name)
     {
-        super(MESSAGE_ID, new Object[] { name });
+        super(MESSAGE_ID, new Object[]{name});
         this.parentNodeRef = parentNodeRef;
         this.name = name;
     }

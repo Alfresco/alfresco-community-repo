35577ac41639119fc93178b5706918031dff9319
SatyamSah5 <Satyam.Sah@hyland.com>
2025-05-30 12:08:09 +0530

    precommit fix

--- remote-api/src/main/java/org/alfresco/rest/api/impl/validator/actions/ActionNodeParameterValidator.java
+++ remote-api/src/main/java/org/alfresco/rest/api/impl/validator/actions/ActionNodeParameterValidator.java
@@ -2,7 +2,7 @@
  * #%L
  * Alfresco Remote API
  * %%
- * Copyright (C) 2005 - 2022 Alfresco Software Limited
+ * Copyright (C) 2005 - 2025 Alfresco Software Limited
  * %%
  * This file is part of the Alfresco software.
  * If the software was purchased under a paid Alfresco license, the terms of
@@ -181,7 +181,7 @@ public class ActionNodeParameterValidator implements ActionValidator
 
     private String formatParamName(String paramName)
     {
-        if (paramName == null || paramName.isEmpty()) 
+        if (paramName == null || paramName.isEmpty())
         {
             return "";
         }

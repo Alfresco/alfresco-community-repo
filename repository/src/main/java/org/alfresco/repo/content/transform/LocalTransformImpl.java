52ac743b4af6a659094d6b56e3efdd373ccdac03
bsayan2 <sayan.bhattacharya@hyland.com>
2025-06-05 20:16:15 +0530

    24883 transform options test sourceModecheck

--- repository/src/main/java/org/alfresco/repo/content/transform/LocalTransformImpl.java
+++ repository/src/main/java/org/alfresco/repo/content/transform/LocalTransformImpl.java
@@ -153,7 +153,10 @@ public class LocalTransformImpl extends AbstractLocalTransform
         {
             transformOptions.put(SOURCE_NODE_REF, sourceNodeRef.toString());
         }
-        // transformOptions.put(SOURCE_FILENAME, transformerDebug.getFilename(sourceNodeRef, true));
+        if (sourceNodeRef != null)
+        {
+            transformOptions.put(SOURCE_FILENAME, transformerDebug.getFilename(sourceNodeRef, true));
+        }
 
         // Build an array of option names and values and extract the timeout.
         long timeoutMs = 0;

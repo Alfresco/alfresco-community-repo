11e43d3b206ce6205e54cea234ff89be92c47142
tathagta15 <tathagata.nandi@hyland.com>
2025-06-16 14:16:28 +0530

    fixed imports

--- repository/src/test/java/org/alfresco/repo/content/caching/CachingContentStoreTest.java
+++ repository/src/test/java/org/alfresco/repo/content/caching/CachingContentStoreTest.java
@@ -35,7 +35,14 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 import java.io.Serializable;

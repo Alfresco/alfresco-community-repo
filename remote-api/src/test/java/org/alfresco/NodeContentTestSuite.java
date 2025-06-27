ccb5ee8abce76ab064b66a9a3e943c112bb2b840
bsayan2 <sayan.bhattacharya@hyland.com>
2025-06-27 16:26:32 +0530

    ACS-9788 [skip tests] test run

--- remote-api/src/test/java/org/alfresco/NodeContentTestSuite.java
+++ remote-api/src/test/java/org/alfresco/NodeContentTestSuite.java
@@ -1,14 +1,18 @@
 package org.alfresco;
 
+import org.junit.experimental.categories.Categories;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 import org.alfresco.repo.web.scripts.solr.NodeContentGetTest;
+import org.alfresco.util.testing.category.DBTests;
+import org.alfresco.util.testing.category.NonBuildTests;
 
+@RunWith(Categories.class)
+@Categories.ExcludeCategory({DBTests.class, NonBuildTests.class})
 @Suite.SuiteClasses({
         // Add your test classes here
         NodeContentGetTest.class
 })
-@RunWith(Suite.class)
 public class NodeContentTestSuite
 {}

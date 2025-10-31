97fb8ca8a85f54d78e13352d4bba64a952e93dbe
Somnath-Deshmukh <somnath.deshmukh@hyland.com>
2025-10-31 11:04:16 +0530

    MNT-25422 Bulleted list, numbered list, and underline are not working properly when adding comment in the file. Backporting to 23.N

--- remote-api/src/main/java/org/alfresco/repo/web/scripts/comments/CommentsPost.java
+++ remote-api/src/main/java/org/alfresco/repo/web/scripts/comments/CommentsPost.java
@@ -33,8 +33,8 @@ import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
 import org.json.simple.JSONObject;
+import org.owasp.html.HtmlPolicyBuilder;
 import org.owasp.html.PolicyFactory;
-import org.owasp.html.Sanitizers;
 import org.springframework.extensions.webscripts.Cache;
 import org.springframework.extensions.webscripts.Status;
 import org.springframework.extensions.webscripts.WebScriptRequest;
@@ -78,7 +78,17 @@ public class CommentsPost extends AbstractCommentsWebScript
         }
         else
         {
-            PolicyFactory policy = Sanitizers.FORMATTING.and(Sanitizers.LINKS);
+            // Allowed HTML elements and attributes in comment content e.g. Text formatting ,Lists and Structure & Styling
+            String[] allowedElements = {"b", "i", "u", "strong", "em", "ul", "ol", "li", "p", "br", "span", "div"};
+
+            PolicyFactory policy = new HtmlPolicyBuilder()
+                    .allowElements(allowedElements)
+                    .allowAttributes("style")
+                    .onElements("span", "div", "p", "ul")
+                    .allowStyling()
+                    .allowStandardUrlProtocols()
+                    .toFactory();
+
             String safeContent = policy.sanitize(commentContent);
             json.replace("content", safeContent);
         }

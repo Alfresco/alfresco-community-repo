6d7d41a348b6c12edea1a22adae1715031cff7c2
Kacper Magdziarz <kacper.magdziarz@hyland.com>
2025-11-21 14:43:22 +0100

    [ACS-10739] Spotless

--- repository/src/main/java/org/alfresco/repo/audit/AuditRecordUtils.java
+++ repository/src/main/java/org/alfresco/repo/audit/AuditRecordUtils.java
@@ -63,7 +63,7 @@ public final class AuditRecordUtils
      */
     public static AuditRecord.Builder generateAuditRecordBuilder(Map<?, Serializable> data, int keyRootLength)
     {
-        LOGGER.debug("generating Audit Record from: \n {}", data );
+        LOGGER.debug("generating Audit Record from: \n {}", data);
 
         var auditRecordBuilder = AuditRecord.builder();
 

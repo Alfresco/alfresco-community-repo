31bd503de468b74224ad392bf6d861aece1220d3
Kacper Magdziarz <kacper.magdziarz@hyland.com>
2025-12-30 09:25:08 +0100

    [ACS-10892] Spotless

--- repository/src/test/java/org/alfresco/repo/security/authentication/identityservice/SpringBasedIdentityServiceFacadeUnitTest.java
+++ repository/src/test/java/org/alfresco/repo/security/authentication/identityservice/SpringBasedIdentityServiceFacadeUnitTest.java
@@ -27,7 +27,6 @@ package org.alfresco.repo.security.authentication.identityservice;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 

fc2980364b11931065cba330ada6a1c4125ee1a3
cezary-witkowski <cezary.witkowski@hyland.com>
2026-01-28 14:27:20 +0100

    [ACS-11042] Performance improvement in getPropertiesForNodeRefs()

--- repository/src/main/java/org/alfresco/repo/node/db/DbNodeServiceImpl.java
+++ repository/src/main/java/org/alfresco/repo/node/db/DbNodeServiceImpl.java
@@ -60,6 +60,7 @@ import org.alfresco.repo.node.db.traitextender.NodeServiceExtension;
 import org.alfresco.repo.node.db.traitextender.NodeServiceTrait;
 import org.alfresco.repo.policy.BehaviourFilter;
 import org.alfresco.repo.security.authentication.AuthenticationUtil;
+import org.alfresco.repo.security.permissions.PermissionServiceSPI;
 import org.alfresco.repo.transaction.AlfrescoTransactionSupport;
 import org.alfresco.repo.transaction.AlfrescoTransactionSupport.TxnReadState;
 import org.alfresco.repo.transaction.RetryingTransactionHelper;
@@ -118,7 +119,7 @@ public class DbNodeServiceImpl extends AbstractNodeServiceImpl implements Extens
 
     private QNameDAO qnameDAO;
     private NodeDAO nodeDAO;
-    private PermissionService permissionService;
+    private PermissionServiceSPI permissionService;
     private StoreArchiveMap storeArchiveMap;
     private BehaviourFilter policyBehaviourFilter;
     private boolean enableTimestampPropagation;
@@ -140,7 +141,7 @@ public class DbNodeServiceImpl extends AbstractNodeServiceImpl implements Extens
         this.nodeDAO = nodeDAO;
     }
 
-    public void setPermissionService(PermissionService permissionService)
+    public void setPermissionService(PermissionServiceSPI permissionService)
     {
         this.permissionService = permissionService;
     }
@@ -1697,8 +1698,10 @@ public class DbNodeServiceImpl extends AbstractNodeServiceImpl implements Extens
             logger.debug("getPropertiesForNodeRefs: Checking permissions for " + nodeRefs.size() + " nodeRefs");
         }
 
+        // locking only once instead of per nodeRef
+        var permissionReference = permissionService.getPermissionReference(PermissionService.READ_PROPERTIES);
         // check permissions per node (since we can't do this in config). Remove nodes that the user has no permission to see
-        nodeRefs.removeIf(nodeRef -> permissionService.hasPermission(nodeRef, PermissionService.READ_PROPERTIES) != AccessStatus.ALLOWED);
+        nodeRefs.removeIf(nodeRef -> permissionService.hasPermission(nodeRef, permissionReference) != AccessStatus.ALLOWED);
 
         // if no nodes left, return empty map
         if (nodeRefs.isEmpty())
@@ -1719,7 +1722,7 @@ public class DbNodeServiceImpl extends AbstractNodeServiceImpl implements Extens
             logger.debug("getPropertiesForNodeRefs: Retrieved " + nodePairs.size() + " node pairs after permission check");
         }
 
-        Map<Long, Map<QName, Serializable>> propertiesMappedById = getPropertiesImpl(nodePairs);
+        Map<Long, Map<QName, Serializable>> propertiesMappedById = getPropertiesImpl(nodePairs); // 30% time
 
         if (logger.isDebugEnabled())
         {

1a0e04d575d625acf9a94ff311c106353c5b1fd2
Shreasi Khan <shreasi.khan@hyland.com>
2026-03-11 17:41:45 +0530

    [MNT-25474] Refactored the code

--- repository/src/main/java/org/alfresco/repo/security/authority/AuthorityDAOImpl.java
+++ repository/src/main/java/org/alfresco/repo/security/authority/AuthorityDAOImpl.java
@@ -1436,15 +1436,17 @@ public class AuthorityDAOImpl implements AuthorityDAO, NodeServicePolicies.Befor
         Serializable value = nodeService.getProperty(ref, ContentModel.PROP_AUTHORITY_DISPLAY_NAME);
         if (value == null)
         {
-            String firstName = DefaultTypeConverter.INSTANCE.convert(String.class, nodeService.getProperty(ref, ContentModel.PROP_FIRSTNAME));
-            String lastName = DefaultTypeConverter.INSTANCE.convert(String.class, nodeService.getProperty(ref, ContentModel.PROP_LASTNAME));
-            if (firstName == null && lastName == null)
-            {
-                return null;
-            }
-            return ((firstName != null ? firstName + " " : "") + (lastName != null ? lastName : "")).trim();
+            return getDisplayName(ref);
         }
         return DefaultTypeConverter.INSTANCE.convert(String.class, value);
+    }
+
+    private String getDisplayName(NodeRef ref)
+    {
+        String firstName = DefaultTypeConverter.INSTANCE.convert(String.class, nodeService.getProperty(ref, ContentModel.PROP_FIRSTNAME));
+        String lastName = DefaultTypeConverter.INSTANCE.convert(String.class, nodeService.getProperty(ref, ContentModel.PROP_LASTNAME));
+        String displayName = ((firstName != null ? firstName + " " : "") + (lastName != null ? lastName : "")).trim();
+        return displayName.isEmpty() ? null : displayName;
     }
 
     public void setAuthorityDisplayName(String authorityName, String authorityDisplayName)

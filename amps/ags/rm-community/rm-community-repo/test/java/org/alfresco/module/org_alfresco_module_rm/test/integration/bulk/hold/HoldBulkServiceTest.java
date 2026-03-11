f15af1f68710f07319ddee30d9e72efc404864c7
Tiago Salvado <tiagosalvado10gmail.com>
2026-03-11 17:03:09 +0000

    Update license headers

--- amps/ags/rm-community/rm-community-repo/test/java/org/alfresco/module/org_alfresco_module_rm/test/integration/bulk/hold/HoldBulkServiceTest.java
+++ amps/ags/rm-community/rm-community-repo/test/java/org/alfresco/module/org_alfresco_module_rm/test/integration/bulk/hold/HoldBulkServiceTest.java
@@ -2,7 +2,7 @@
  * #%L
  * Alfresco Records Management Module
  * %%
- * Copyright (C) 2005 - 2025 Alfresco Software Limited
+ * Copyright (C) 2005 - 2026 Alfresco Software Limited
  * %%
  * This file is part of the Alfresco software.
  * -

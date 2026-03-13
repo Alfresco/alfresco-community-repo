f4be0d887eac5394d6af7a873242f140db90c8d7
bansari <belal.ansari@hyland.com>
2026-03-13 17:46:04 +0530

    fixing pre-commit failures

--- data-model/src/test/java/org/alfresco/repo/content/encoding/CharsetFinderTest.java
+++ data-model/src/test/java/org/alfresco/repo/content/encoding/CharsetFinderTest.java
@@ -32,11 +32,11 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 import junit.framework.TestCase;
+import org.springframework.context.ApplicationContext;
 
 import org.alfresco.encoding.CharactersetFinder;
 import org.alfresco.repo.content.MimetypeMap;
 import org.alfresco.util.DataModelTestApplicationContextHelper;
-import org.springframework.context.ApplicationContext;
 
 /**
  * @see CharsetFinderTest
@@ -47,28 +47,28 @@ import org.springframework.context.ApplicationContext;
 public class CharsetFinderTest extends TestCase
 {
     private static ApplicationContext ctx = DataModelTestApplicationContextHelper.getApplicationContext();
-    
+
     private ContentCharsetFinder charsetFinder;
-    
+
     @Override
     public void setUp() throws Exception
     {
         charsetFinder = (ContentCharsetFinder) ctx.getBean("charset.finder");
     }
-    
+
     public void testPlainText() throws Exception
     {
         String test = "The quick brown fox jumps over the lazy dog" +
-                      "\n\nLe renard brun rapide saute par-dessus le chien paresseux" +
-                      "\n\nDer schnelle braune Fuchs springt über den faulen Hund\n\n" +
-                      "براون وكس السريع يقفز فوق الكلب كسالي";
-        
+                "\n\nLe renard brun rapide saute par-dessus le chien paresseux" +
+                "\n\nDer schnelle braune Fuchs springt über den faulen Hund\n\n" +
+                "براون وكس السريع يقفز فوق الكلب كسالي";
+
         // As UTF-8
         InputStream is = new BufferedInputStream(new ByteArrayInputStream(test.getBytes(StandardCharsets.UTF_8)));
         Charset charset = charsetFinder.getCharset(is, MimetypeMap.MIMETYPE_TEXT_PLAIN);
         assertNotNull(charset);
         assertEquals("UTF-8", charset.displayName());
-        
+
         // Now try with longer encodings
 
         // UTF-16 with byte order mark
@@ -76,27 +76,26 @@ public class CharsetFinderTest extends TestCase
         charset = charsetFinder.getCharset(is, MimetypeMap.MIMETYPE_TEXT_PLAIN);
         assertNotNull(charset);
         assertEquals("UTF-16BE", charset.displayName());
-        
+
         is = new BufferedInputStream(new ByteArrayInputStream(test.getBytes("UnicodeBig")));
         charset = charsetFinder.getCharset(is, MimetypeMap.MIMETYPE_TEXT_PLAIN);
         assertNotNull(charset);
         assertEquals("UTF-16BE", charset.displayName());
-        
+
         is = new BufferedInputStream(new ByteArrayInputStream(test.getBytes("UnicodeLittle")));
         charset = charsetFinder.getCharset(is, MimetypeMap.MIMETYPE_TEXT_PLAIN);
         assertNotNull(charset);
         assertEquals("UTF-16LE", charset.displayName());
-        
+
         // UTF-32 with byte order mark
         is = new BufferedInputStream(new ByteArrayInputStream(test.getBytes("UTF-32")));
         charset = charsetFinder.getCharset(is, MimetypeMap.MIMETYPE_TEXT_PLAIN);
         assertNotNull(charset);
         assertEquals("UTF-32BE", charset.displayName());
     }
-    
+
     /**
-     * Tries with various 8 bit (non-unicode) encodings, such as
-     *  ISO-8859-1
+     * Tries with various 8 bit (non-unicode) encodings, such as ISO-8859-1
      */
     public void test8BitText() throws Exception
     {
@@ -119,7 +118,7 @@ public class CharsetFinderTest extends TestCase
         assertNotNull(charset);
         assertEquals("windows-1251", charset.displayName()); // AKA CP1251
     }
-    
+
     /**
      * Ensure that a Japanese HTML file encoded with Shift_JS is detected correctly
      */

2717b3d5c24171a0017d6079263a5001540ac6e0
Damian Ujma <damian.ujma@hyland.com>
2026-03-20 11:49:25 +0100

    ACS-10653 Fix Loading datalists page when a repository-based message bundle cannot be found

--- repository/src/main/java/org/alfresco/repo/i18n/MessageServiceImpl.java
+++ repository/src/main/java/org/alfresco/repo/i18n/MessageServiceImpl.java
@@ -2,7 +2,7 @@
  * #%L
  * Alfresco Repository
  * %%
- * Copyright (C) 2005 - 2016 Alfresco Software Limited
+ * Copyright (C) 2005 - 2026 Alfresco Software Limited
  * %%
  * This file is part of the Alfresco software.
  * If the software was purchased under a paid Alfresco license, the terms of
@@ -516,10 +516,9 @@ public class MessageServiceImpl implements MessageService
                                 String key = enumKeys.nextElement();
                                 props.put(key, resourcebundle.getString(key));
                             }
-
-                            loadedBundles.add(resBundlePath);
                             count++;
                         }
+                        loadedBundles.add(resBundlePath);
                     }
                 }
 

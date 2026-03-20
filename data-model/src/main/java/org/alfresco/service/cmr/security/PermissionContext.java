cdd896efe21dbd92a2fc0605b63bf6841a956333
Cezary Witkowski <cezary.witkowski@hyland.com>
2026-03-20 11:33:31 +0100

    [ACS-11122] Improve permissionService performance backport 23.N (#3930)
    Signed-off-by: cezary-witkowski <cezary.witkowski@hyland.com>

--- data-model/src/main/java/org/alfresco/service/cmr/security/PermissionContext.java
+++ data-model/src/main/java/org/alfresco/service/cmr/security/PermissionContext.java
@@ -31,24 +31,24 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-import org.alfresco.api.AlfrescoPublicApi;  
+import org.alfresco.api.AlfrescoPublicApi;
 import org.alfresco.service.namespace.QName;
 
 @AlfrescoPublicApi
-public class PermissionContext 
+public class PermissionContext
 {
-    private QName type;
-    
-    private HashSet<QName> aspects = new HashSet<QName>();
-    
-    private Map<QName, Serializable> properties = new HashMap<QName, Serializable>();
-    
-    private Map<String, Set<String>> dynamicAuthorityAssignment = new HashMap<String, Set<String>>();
-    
-    private Map<String, Object> additionalContext = new HashMap<String, Object>();
-    
+    private final QName type;
+
+    private final HashSet<QName> aspects = new HashSet<>();
+
+    private final Map<QName, Serializable> properties = new HashMap<>();
+
+    private final Map<String, Set<String>> dynamicAuthorityAssignment = new HashMap<>();
+
+    private final Map<String, Object> additionalContext = new HashMap<>();
+
     private Long storeAcl = null;
-    
+
     public PermissionContext(QName type)
     {
         this.type = type;
@@ -66,15 +66,10 @@ public class PermissionContext
 
     public void addDynamicAuthorityAssignment(String user, String dynamicAuthority)
     {
-        Set<String> dynamicAuthorities = dynamicAuthorityAssignment.get(user);
-        if(dynamicAuthorities == null)
-        {
-            dynamicAuthorities = new HashSet<String>();
-            dynamicAuthorityAssignment.put(user, dynamicAuthorities);
-        }
+        Set<String> dynamicAuthorities = dynamicAuthorityAssignment.computeIfAbsent(user, k -> new HashSet<>());
         dynamicAuthorities.add(dynamicAuthority);
     }
-    
+
     public Map<String, Object> getAdditionalContext()
     {
         return additionalContext;
@@ -99,7 +94,5 @@ public class PermissionContext
     {
         this.storeAcl = storeAcl;
     }
-    
-    
-    
+
 }

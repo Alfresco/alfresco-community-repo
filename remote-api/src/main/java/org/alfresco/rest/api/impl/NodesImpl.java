b1c8b87a1a59155f792c412e99b1fa92274758a4
Shreasi Khan <Shreasi.Khan@hyland.com>
2026-04-09 19:51:11 +0530

    [MNT-25185] Add authority display name to permissions (#3992)

--- remote-api/src/main/java/org/alfresco/rest/api/impl/NodesImpl.java
+++ remote-api/src/main/java/org/alfresco/rest/api/impl/NodesImpl.java
@@ -1024,7 +1024,9 @@ public class NodesImpl implements Nodes
             {
                 for (AccessPermission accessPerm : permissionService.getAllSetPermissions(nodeRef))
                 {
-                    NodePermissions.NodePermission nodePerm = new NodePermissions.NodePermission(accessPerm.getAuthority(), accessPerm.getPermission(), accessPerm.getAccessStatus().toString());
+                    String authority = accessPerm.getAuthority();
+                    String displayName = resolveAuthorityDisplayName(authority);
+                    NodePermissions.NodePermission nodePerm = new NodePermissions.NodePermission(authority, accessPerm.getPermission(), accessPerm.getAccessStatus().toString(), displayName);
                     if (accessPerm.isSetDirectly())
                     {
                         setDirectlyPerms.add(nodePerm);
@@ -1337,7 +1339,9 @@ public class NodesImpl implements Nodes
                 {
                     for (AccessPermission accessPerm : permissionService.getAllSetPermissions(nodeRef))
                     {
-                        NodePermissions.NodePermission nodePerm = new NodePermissions.NodePermission(accessPerm.getAuthority(), accessPerm.getPermission(), accessPerm.getAccessStatus().toString());
+                        String authority = accessPerm.getAuthority();
+                        String displayName = resolveAuthorityDisplayName(authority);
+                        NodePermissions.NodePermission nodePerm = new NodePermissions.NodePermission(authority, accessPerm.getPermission(), accessPerm.getAccessStatus().toString(), displayName);
                         if (accessPerm.isSetDirectly())
                         {
                             setDirectlyPerms.add(nodePerm);
@@ -3851,6 +3855,16 @@ public class NodesImpl implements Nodes
             throw new DisabledServiceException("Direct access url isn't available.");
         }
         return directAccessUrl;
+    }
+
+    private String resolveAuthorityDisplayName(String authority)
+    {
+        String displayName = authorityService.getAuthorityDisplayName(authority);
+        if (displayName == null || displayName.isEmpty())
+        {
+            displayName = authority;
+        }
+        return displayName;
     }
 
     /**

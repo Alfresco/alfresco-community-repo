2d3f6cffb40aaa9dcb0e75f869aea2e8ec67d356
Eva Vasques <eva.vasques@gmail.com>
2026-04-23 14:24:19 +0100

    ACS-11311 Authentication setup for Microsoft Entra (#3989) (#4045)
    Co-authored-by: Belal Ansari <belal.ansari@hyland.com>

--- repository/src/main/java/org/alfresco/repo/security/authentication/identityservice/IdentityServiceFacadeFactoryBean.java
+++ repository/src/main/java/org/alfresco/repo/security/authentication/identityservice/IdentityServiceFacadeFactoryBean.java
@@ -2,7 +2,7 @@
  * #%L
  * Alfresco Repository
  * %%
- * Copyright (C) 2005 - 2025 Alfresco Software Limited
+ * Copyright (C) 2005 - 2026 Alfresco Software Limited
  * %%
  * This file is part of the Alfresco software.
  * If the software was purchased under a paid Alfresco license, the terms of
@@ -506,6 +506,12 @@ public class IdentityServiceFacadeFactoryBean implements FactoryBean<IdentitySer
 
         private Set<String> getSupportedScopes(Scope scopes)
         {
+            if (config.isScopeValidationDisabled())
+            {
+                // Bypass scope filtering against the IDP's scopes_supported discovery metadata.
+                // Required for IDPs like MS Entra that do not advertise custom API scopes
+                return config.getPasswordGrantScopes();
+            }
             return scopes.stream()
                     .filter(this::hasPasswordGrantScope)
                     .map(Identifier::getValue)

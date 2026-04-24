52087b748323500310fb03894d8ae01e5124f3e3
Arindam Roy <arindam.roy@hyland.com>
2026-04-24 14:35:39 +0530

    Formatting

--- repository/src/main/java/org/alfresco/repo/security/authentication/identityservice/SpringBasedIdentityServiceFacade.java
+++ repository/src/main/java/org/alfresco/repo/security/authentication/identityservice/SpringBasedIdentityServiceFacade.java
@@ -78,7 +78,7 @@ class SpringBasedIdentityServiceFacade implements IdentityServiceFacade
     private final JwtDecoder jwtDecoder;
 
     SpringBasedIdentityServiceFacade(RestTemplate restTemplate, ClientRegistration clientRegistration,
-                                     JwtDecoder jwtDecoder)
+            JwtDecoder jwtDecoder)
     {
         requireNonNull(restTemplate);
         this.clientRegistration = requireNonNull(clientRegistration);

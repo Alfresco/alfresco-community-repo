29a2246e78a291f208054ce7d9236a1bf5f8d144
Damian Ujma <damian.ujma@hyland.com>
2026-07-21 11:40:11 +0200

    ACS-12079 Test update downstream

--- scripts/ci/update_downstream.sh
+++ scripts/ci/update_downstream.sh
@@ -7,8 +7,8 @@ pushd "$(dirname "${BASH_SOURCE[0]}")/../../"
 source "$(dirname "${BASH_SOURCE[0]}")/build_functions.sh"
 
 #Fetch the latest changes, as GHA will only checkout the PR commit
-git fetch origin "${BRANCH_NAME}"
-git checkout "${BRANCH_NAME}"
+git fetch origin "feature/ACS-12079_use_github_app_to_release_test"
+git checkout "feature/ACS-12079_use_github_app_to_release_test"
 git pull
 
 # Retrieve the current Community version - latest tag on the current branch
@@ -16,7 +16,7 @@ VERSION="$(git describe --abbrev=0 --tags)"
 
 DOWNSTREAM_REPO="github.com/Alfresco/alfresco-enterprise-repo.git"
 
-cloneRepo "${DOWNSTREAM_REPO}" "${BRANCH_NAME}"
+cloneRepo "${DOWNSTREAM_REPO}" "feature/ACS-12079_use_github_app_to_release_test"
 
 cd "$(dirname "${BASH_SOURCE[0]}")/../../../$(basename "${DOWNSTREAM_REPO%.git}")"
 
@@ -28,25 +28,6 @@ mvn -B versions:set-property versions:commit \
   -Dproperty=dependency.alfresco-community-repo.version \
   "-DnewVersion=${VERSION}"
 
-# Commit changes
-git status
-git --no-pager diff pom.xml
-git add pom.xml
-
-if [[ "${COMMIT_MESSAGE}" =~ \[force[^\]]*\] ]]; then
-  FORCE_TOKEN=$(echo "${COMMIT_MESSAGE}" | sed "s|^.*\(\[force[^]]*\]\).*$|\1|g")
-  git commit --allow-empty -m "${FORCE_TOKEN} Update community-repo version to ${VERSION}"
-  git push
-elif git status --untracked-files=no --porcelain | grep -q '^' ; then
-  git commit -m "Update community-repo version to ${VERSION}"
-  git push
-else
-  echo "Dependencies are already up to date."
-  git status
-fi
-
-
 popd
 set +vex
 echo "=========================== Finishing Update Downstream Script =========================="
-

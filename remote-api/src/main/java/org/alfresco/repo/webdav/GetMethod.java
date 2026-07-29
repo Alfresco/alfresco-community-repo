c625caa5113ad539c99a3661d6b56fa5d55e13d4
KushalBanik <kushal.banik@hyland.com>
2026-07-29 16:27:38 +0530

    [ACS-12294] Pre-commit fixed

--- remote-api/src/main/java/org/alfresco/repo/webdav/GetMethod.java
+++ remote-api/src/main/java/org/alfresco/repo/webdav/GetMethod.java
@@ -43,7 +43,6 @@ import org.springframework.extensions.surf.util.URLEncoder;
 import org.alfresco.model.ContentModel;
 import org.alfresco.repo.content.filestore.FileContentReader;
 import org.alfresco.repo.web.util.HttpRangeProcessor;
-import org.alfresco.service.cmr.model.FileFolderService;
 import org.alfresco.service.cmr.model.FileInfo;
 import org.alfresco.service.cmr.model.FileNotFoundException;
 import org.alfresco.service.cmr.repository.ContentData;
